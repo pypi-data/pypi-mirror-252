f72ffffb0cc9fe02d64a25a1991acc4d1ec19bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:04:31 +0000

    Add package sigtech/0.4.2/sigtech-0.4.2.tar.gz

---
+++ packages/sigtech/sigtech-0.4.2.tar.gz/sigtech-0.4.2/sigtech/api/__init__.py
@@ -0,0 +1,29 @@
+from sigtech.api import data
+from sigtech.api.client.client import Client
+from sigtech.api.framework import config
+from sigtech.api.framework.basket_strategy import BasketStrategy
+from sigtech.api.framework.environment import env, init, obj
+from sigtech.api.framework.indices.tradable_index import TradableTSIndex
+from sigtech.api.framework.instruments.ois_swap import OISSwap
+from sigtech.api.framework.reinvestment_strategy import (
+    ReinvestmentStrategy,
+    get_single_stock_strategy,
+)
+from sigtech.api.framework.rolling_future_strategy import RollingFutureStrategy
+from sigtech.api.framework.signal_strategy import SignalStrategy
+
+__all__ = [
+    "Client",
+    "SignalStrategy",
+    "BasketStrategy",
+    "ReinvestmentStrategy",
+    "RollingFutureStrategy",
+    "TradableTSIndex",
+    "OISSwap",
+    "env",
+    "get_single_stock_strategy",
+    "init",
+    "obj",
+    "data",
+    "config",
+]
