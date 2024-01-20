14acf66ddf03fa54c24ce5165464029413101090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:03:22 +0000

    Add package quantum-xyz/0.1.0/quantum_xyz-0.1.0-py3-none-any.whl

---
+++ packages/quantum-xyz/quantum_xyz-0.1.0-py3-none-any.whl/xyz/algorithms/synthesis/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+"""
+Author: Hanyu Wang
+Created time: 2023-06-21 14:02:28
+Last Modified by: Hanyu Wang
+Last Modified time: 2023-06-25 12:59:39
+"""
+
+from .synthesize import *
+from .hybrid_synthesis import *
+from ._exact_cnot_synthesis_legacy import exact_cnot_synthesis_legacy
+from ._heuristic_cnot_synthesis import heuristic_cnot_synthesis
+from .library_cnot_synthesis import library_cnot_synthesis
