93d42f5c1bd8a62b08be9f8fff4c6c466f5db15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:16 +0000

    Add package PyPlumIO/0.5.13/PyPlumIO-0.5.13.tar.gz

---
+++ packages/PyPlumIO/PyPlumIO-0.5.13.tar.gz/PyPlumIO-0.5.13/pyplumio/helpers/timeout.py
@@ -0,0 +1,35 @@
+"""Contains a timeout decorator."""
+from __future__ import annotations
+
+import asyncio
+from functools import wraps
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def timeout(seconds: int, raise_exception: bool = True):
+    """Decorate a timeout for the awaitable.
+
+    Return None on exception if raise_exception parameter is set to false.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await asyncio.wait_for(func(*args, **kwargs), timeout=seconds)
+            except asyncio.TimeoutError:
+                if raise_exception:
+                    raise
+
+                _LOGGER.warning(
+                    "Function '%s' timed out after %i seconds",
+                    func.__name__,
+                    seconds,
+                )
+                return None
+
+        return wrapper
+
+    return decorator
