e90df6e73d6b22c348d7ee9970c108e2a96ca955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:43 +0000

    Add package ubo-gui/0.7.8a0/ubo_gui-0.7.8a0.tar.gz

---
+++ packages/ubo-gui/ubo_gui-0.7.8a0.tar.gz/ubo_gui-0.7.8a0/ubo_gui/menu/item_widget.py
@@ -0,0 +1,66 @@
+"""Implement `ItemWidget`."""
+from __future__ import annotations
+
+import pathlib
+from typing import TYPE_CHECKING
+
+from kivy.app import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import (
+    BooleanProperty,
+    ColorProperty,
+    ObjectProperty,
+    StringProperty,
+)
+
+from ubo_gui.constants import PRIMARY_COLOR
+from ubo_gui.menu.types import string_attribute
+
+if TYPE_CHECKING:
+    from . import Item
+
+
+class ItemWidget(BoxLayout):
+    """Renders an `Item`.
+
+    Attributes
+    ----------
+    color: `tuple` of `int`
+        Color of the item, used for its text and icon.
+
+    background_color: `tuple` of `int`
+        Background color of the item.
+
+    label:
+        The text rendered in the item.
+
+    icon: `str`
+        Name of a Material Symbols icon.
+    """
+
+    is_set = BooleanProperty(defaultvalue=False)
+    label = StringProperty()
+    color = ColorProperty((1, 1, 1, 1))
+    background_color = ColorProperty(PRIMARY_COLOR)
+    icon = StringProperty(defaultvalue='')
+    is_short = BooleanProperty(defaultvalue=False)
+    item = ObjectProperty()
+
+    def on_item(self: ItemWidget, instance: ItemWidget, value: Item | None) -> None:
+        """Update the widget properties when the item changes."""
+        if value is not None:
+            instance.is_set = True
+            instance.label = string_attribute(value, 'label') or ''
+            instance.is_short = False if value.is_short is None else value.is_short
+            instance.color = value.color or ItemWidget.color.defaultvalue
+            instance.background_color = (
+                value.background_color or ItemWidget.background_color.defaultvalue
+            )
+            instance.icon = value.icon or ''
+        else:
+            instance.is_set = False
+
+
+Builder.load_file(
+    pathlib.Path(__file__).parent.joinpath('item_widget.kv').resolve().as_posix(),
+)
---
+++ packages/ubo-gui/ubo_gui-0.7.8a0.tar.gz/ubo_gui-0.7.8a0/ubo_gui/menu/types.py
@@ -0,0 +1,151 @@
+"""Class definition of main datatypes use in menus."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Callable, Sequence
+
+from immutable import Immutable
+
+from ubo_gui.constants import PRIMARY_COLOR
+
+if TYPE_CHECKING:
+    from page import PageWidget
+
+
+class BaseMenu(Immutable):
+    """A class used to represent a menu.
+
+    Attributes
+    ----------
+    title: `str`
+        Rendered on top of the menu in all pages. Optionally it can be a callable
+    returning the title.
+
+    items: `list` of `Item`
+        List of the items of the menu. Optionally it can be a callable returning the
+    list of items
+    """
+
+    title: str | Callable[[], str]
+    items: Sequence[Item] | Callable[[], Sequence[Item]]
+    _id: str | None = None
+
+
+class HeadedMenu(BaseMenu):
+    """A class used to represent a headed menu.
+
+    Attributes
+    ----------
+    heading: `str`
+        Rendered in the first page of the menu, stating the purpose of the menu and its
+    items.
+
+    sub_heading: `str`
+        Rendered beneath the heading in the first page of the menu with a smaller font.
+    """
+
+    heading: str
+    sub_heading: str
+
+
+class HeadlessMenu(BaseMenu):
+    """A class used to represent a headless menu."""
+
+
+Menu = HeadedMenu | HeadlessMenu
+
+
+def menu_items(menu: Menu | None) -> Sequence[Item]:
+    """Return items of the menu.
+
+    in case it's a function, the return value of the function is called.
+    """
+    if not menu:
+        return []
+    return menu.items() if callable(menu.items) else menu.items
+
+
+def string_attribute(instance: Menu | Item, attribute: str) -> str:
+    """Return the string attribute of the menu.
+
+    in case it's a function, the return value of the function is called.
+    """
+    value = getattr(instance, attribute)
+    return value() if callable(value) else value
+
+
+class BaseItem(Immutable):
+    """A class used to represent a menu item.
+
+    Attributes
+    ----------
+    label: `str`
+        The label of the item.
+
+    color: `str` or `tuple` of `float`
+        The color by its name or in rgba format as a list of floats, the list should
+    contain 4 elements: red, green, blue and alpha, each being a number in the range
+    [0..1].
+        For example (0.5, 0, 0.5, 0.8) represents a semi transparent purple, or the
+    string 'yellow' represents color yellow.
+
+    background_color: `str` or `tuple` of `float`
+        The background color by color name or in rgba format as a list of floats, the
+    list should contain 4 elements red, green, blue and alpha, each being a number in
+    the range [0..1].
+        For example (0.5, 0, 0.5, 0.8) represents a semi transparent purple, or the
+    string 'yellow' represents color yellow.
+
+    icon: `str`
+        Name of a Material Symbols icon, check here for the complete list:
+    https://fonts.google.com/icons
+
+    is_short: `bool`
+        Whether the item should be rendered in short form or not. In short form only the
+    icon of the item is rendered and its label is hidden.
+    """
+
+    label: str
+    color: str | tuple[float, float, float, float] = (1, 1, 1, 1)
+    background_color: str | tuple[float, float, float, float] = PRIMARY_COLOR
+    icon: str | None = None
+    is_short: bool = False
+
+
+class ActionItem(BaseItem):
+    """A class used to represent an action menu item.
+
+    Attributes
+    ----------
+    action: `Function`
+        If provided, activating this item will call this function.
+    """
+
+    action: Callable[[], Menu | type[PageWidget] | None]
+
+
+class ApplicationItem(BaseItem):
+    """A class used to represent an application menu item.
+
+    Attributes
+    ----------
+    application: `PageWidget`
+        If provided, activating this item will show this widget
+    """
+
+    application: type[PageWidget]
+
+
+class SubMenuItem(BaseItem):
+    """A class used to represent a sub-menu menu item.
+
+    Attributes
+    ----------
+    sub_menu: `Menu`
+        If provided, activating this item will open another menu, the description
+        described in this field.
+    """
+
+    sub_menu: Menu
+
+
+Item = ActionItem | SubMenuItem | ApplicationItem
