75aeee283942c7c2013ab4bd71b8cfed26c68c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 18:03:10 +0000

    Add package fuzzy-secret-stdout/0.3.1b37/fuzzy_secret_stdout-0.3.1b37-py3-none-any.whl

---
+++ packages/fuzzy-secret-stdout/fuzzy_secret_stdout-0.3.1b37-py3-none-any.whl/fuzzy_secret_stdout/__main__.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import argparse
+import logging
+
+from pyfzf.pyfzf import FzfPrompt
+
+from fuzzy_secret_stdout.models import SecretStoreItem
+from fuzzy_secret_stdout.integrations.factory import create_integration, Integration
+
+from rich.live import Live
+from rich.spinner import Spinner
+from rich.text import Text
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--integration', choices=Integration.list_options(), default=Integration.AWS_SSM, type=Integration)
+    parser.add_argument('-b', '--max-batch-size', type=int, default=os.environ.get('FSS_MAX_BATCH_RESULTS', 50), dest='max_batch_results')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    search = FzfPrompt()
+    integration_client = create_integration(args.integration)
+
+    with Live(Spinner('dots', text=Text('Loading')), transient=True):
+        result: list[SecretStoreItem] = integration_client.fetch_all(max_batch_results=args.max_batch_results)
+
+    keys: list[str] = [x.key for x in result]
+
+    selected: list[str] = search.prompt(keys)
+
+    if not selected:
+        logger.debug('nothing was selected, exiting early.')
+        return
+
+    result: list[SecretStoreItem] = integration_client.fetch_secrets(selected)
+
+    for current_result in result:
+        sys.stdout.write(current_result.value)
+
+
+if __name__ == '__main__':  # pragma: nocover
+    main()
