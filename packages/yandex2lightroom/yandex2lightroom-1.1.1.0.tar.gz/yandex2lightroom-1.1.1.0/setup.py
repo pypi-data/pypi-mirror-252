42ff15e8ce7f074c5297ffebd6b73b73b321c384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 18:03:12 +0000

    Add package yandex2lightroom/1.1.1.0/yandex2lightroom-1.1.1.0.tar.gz

---
+++ packages/yandex2lightroom/yandex2lightroom-1.1.1.0.tar.gz/yandex2lightroom-1.1.1.0/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '1.1.1.0'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+install_requires = [
+    "beautifulsoup4>=4.10.0",
+    "certifi>=2023.7.22",
+    "chardet>=4.0.0",
+    "dataclasses>=0.6",
+    "dataclasses-json>=0.5.14",
+    "idna>=3.4",
+    "lxml>=4.9.3",
+    "marshmallow>=3.20.1",
+    "marshmallow-enum>=1.5.1",
+    "mypy-extensions>=0.4.3",
+    "pyopenssl>=23.0.0",
+    "requests>=2.31.0",
+    "selenium>=4.13.0",
+    "selenium-wire>=5.1.0",
+    "soupsieve>=2.5",
+    "stringcase>=1.2.0",
+    "typing>=3.7.4",
+    "typing-extensions>=3.10.0.2",
+    "typing-inspect>=0.9.0",
+    "urllib3>=2.0.5",
+    "webdriver_auto_update>=1.1.0"
+]
+
+setup(
+    name='yandex2lightroom',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    version=__version__,
+    license='MIT',
+    description="Python Script to download images from Yandex.Images for the use in Adobe Lightroom.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dieter Stockhausen',
+    author_email='dieter@schwingenhausen.at',
+    url='https://github.com/sto3014/yandex2lightroom-git',
+    download_url=f'https://github.com/sto3014/yandex2lightroom-git/archive/main.zip',
+    keywords='yandex images download save terminal command-line scrapper lightroom',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+    entry_points={
+        "console_scripts": [
+            'yandex2lightroom = yandex2lightroom:run_main'
+        ]
+    }
+)
---
+++ packages/yandex2lightroom/yandex2lightroom-1.1.1.0.tar.gz/yandex2lightroom-1.1.1.0/src/yandex2lightroom/image_download.py
@@ -0,0 +1,116 @@
+import os
+import time
+import logging
+import pathlib
+import sys
+
+from multiprocessing import Pool
+from .downloader import YandexImagesDownloader, get_driver, download_single_image, save_json
+from .parse import parse_args
+
+
+def scrap(args):
+    keywords = []
+
+    if args.keywords:
+        keywords.extend([
+            str(item).strip() for item in args.keywords.split(",") if len(item)
+        ])
+
+    if args.keywords_from_file:
+        with open(args.keywords_from_file, "r") as f:
+            keywords.extend([line.strip() for line in f])
+
+    driver = get_driver(args.browser, args.driver_path, args.show_browser)
+
+    try:
+        pool = Pool(args.num_workers) if args.num_workers else None
+
+        downloader = YandexImagesDownloader(driver, args.output_directory,
+                                            args.limit, args.isize,
+                                            args.exact_isize, args.iorient,
+                                            args.extension, args.color,
+                                            args.itype, args.commercial,
+                                            args.recent, pool, args.skip_existing, args.show_browser,
+                                            args.delay_for_refresh, args.delay_for_captcha_handling,
+                                            args.wait_for_captcha_handling)
+
+        start_time = time.time()
+        total_errors = 0
+        total_skipped = 0
+
+        if keywords:
+            downloader_result = downloader.download_images(keywords, args.isize)
+            total_errors += sum(
+                keyword_result.errors_count
+                for keyword_result in downloader_result.keyword_results)
+            total_skipped += sum(
+                keyword_result.skipped_count
+                for keyword_result in downloader_result.keyword_results)
+    finally:
+        driver.quit()
+        if args.num_workers:
+            pool.close()
+            pool.join()
+
+    if args.single_image:
+        img_url_result = download_single_image(
+            args.single_image, pathlib.Path(args.output_directory))
+        total_errors += 1 if img_url_result.status == "fail" else 0
+
+    total_time = time.time() - start_time
+
+    logging.info("Download completed.")
+    logging.info(f"Total errors: {total_errors}")
+    logging.info(f"Total skipped: {total_skipped}")
+    logging.info(
+        f"Total files downloaded: {len(keywords) * args.limit - total_errors - total_skipped}")
+    logging.info(f"Total time taken: {total_time} seconds.")
+    if keywords and args.json:
+        save_json(args, downloader_result)
+
+
+def setup_logging(quiet_mode, output_directory):
+    log_file = os.path.join(output_directory, 'yandex2lightroom.log')
+    logging.basicConfig(filename=log_file, level=logging.WARNING if quiet_mode else logging.INFO,
+                        format="%(asctime)s %(levelname)-8s %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    selenium_logger = logging.getLogger('seleniumwire')
+    selenium_logger.setLevel(logging.WARNING)
+
+
+def main(arguments=None):
+    try:
+        args = parse_args(arguments)
+        if args.output_directory[0] != "/" and args.output_directory[1] != ":":
+            # output_directory is relative path
+            home_directory = os.path.expanduser('~')
+            args.output_directory = os.path.join(home_directory, args.output_directory)
+        setup_logging(args.quiet_mode, args.output_directory)
+        logging.info(f"arg: show_browser={args.show_browser}")
+        scrap(args)
+        errlog = args.error_log
+        if errlog:
+            with open(errlog, "w") as f:
+                f.write("ok")
+        sys.exit(0)
+    except KeyboardInterrupt as e:
+        errlog = args.error_log
+        if errlog:
+            with open(errlog, "w") as f:
+                f.write(e.msg)
+        logging.error("KeyboardInterrupt")
+        sys.exit(1)
+
+    except Exception as e:
+        errlog = args.error_log
+        if errlog:
+            with open(errlog, "w") as f:
+                f.write(str(e))
+
+        logging.error(e, exc_info=True)
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    main()
