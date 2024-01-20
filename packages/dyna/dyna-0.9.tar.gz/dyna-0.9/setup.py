8efbb1543a14c577e6bfef338c02098501ce06d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:03:17 +0000

    Add package dyna/0.9/dyna-0.9.tar.gz

---
+++ packages/dyna/dyna-0.9.tar.gz/dyna-0.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='dyna',
+      version='0.9',
+      description='',
+      author='Tim Vieira',
+#      scripts=['bin/dyna'],
+      install_requires=[
+          'arsenal',     # http://github.com/timvieira/arsenal
+          'semirings',   # http://github.com/timvieira/semirings
+          'orderedset',
+          'frozendict',
+          'lark-parser',
+          'ansi2html',
+          'networkx',
+          'z3-solver',
+          'sympy',
+          'graphviz',
+          'rich',
+          'IPython',
+          # -------------------------------------------------------------------
+          # development
+          'coveragepy',
+          'pytest',
+          'pytest-cov',
+          'pytest-timeout',
+      ],
+      packages = find_packages(),
+)
