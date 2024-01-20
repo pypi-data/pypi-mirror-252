41df5dcbd766ea190cef9bef5d8607d17a42dba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:40 +0000

    Add package nodestream-plugin-akamai/0.11.0/nodestream_plugin_akamai-0.11.0.tar.gz

---
+++ packages/nodestream-plugin-akamai/nodestream_plugin_akamai-0.11.0.tar.gz/nodestream_plugin_akamai-0.11.0/nodestream_akamai/__init__.py
@@ -0,0 +1,38 @@
+from .appsec_coverage import AkamaiAppSecCoverageExtractor
+from .cloudlets import AkamaiCloudletExtractor
+from .cpcodes import AkamaiCPCodesExtractor
+from .cps import AkamaiCPSExtractor
+from .edgeworkers import AkamaiEdgeWorkersExtractor
+from .ehn import AkamaiEHNExtractor
+from .gtm import AkamaiGTMExtractor
+from .iam_clients import AkamaiIAMClientExtractor
+from .iam_users import AkamaiIAMUserExtractor
+from .ivm import AkamaiIVMExtractor
+from .netstorage_account import AkamaiNetstorageAccountExtractor
+from .netstorage_group import AkamaiNetstorageGroupExtractor
+from .plugin import AkamaiPlugin
+from .property import AkamaiPropertyExtractor
+from .redirect import AkamaiRedirectExtractor
+from .siteshield import AkamaiSiteShieldExtractor
+from .waf import AkamaiWAFExtractor
+
+__all__ = (
+    "AkamaiPlugin",
+    "AkamaiAppSecCoverageExtractor",
+    "AkamaiCloudletExtractor",
+    "AkamaiCPCodesExtractor",
+    "AkamaiCPSExtractor",
+    "AkamaiEdgeWorkersExtractor",
+    "AkamaiEHNExtractor",
+    "AkamaiGTMExtractor",
+    "AkamaiIVMExtractor",
+    "AkamaiIAMClientExtractor",
+    "AkamaiIAMUserExtractor",
+    "AkamaiNetstorageAccountExtractor",
+    "AkamaiNetstorageGroupExtractor",
+    "AkamaiNetstorageAccountExtractor",
+    "AkamaiPropertyExtractor",
+    "AkamaiRedirectExtractor",
+    "AkamaiSiteShieldExtractor",
+    "AkamaiWAFExtractor",
+)
