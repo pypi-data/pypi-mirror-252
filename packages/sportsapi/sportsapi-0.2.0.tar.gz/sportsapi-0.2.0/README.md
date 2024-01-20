ea45b6d8b61ab5d218f98591c332b162c6f5a24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:36 +0000

    Add package sportsapi/0.2.0/sportsapi-0.2.0.tar.gz

---
+++ packages/sportsapi/sportsapi-0.2.0.tar.gz/sportsapi-0.2.0/README.md
@@ -0,0 +1,15 @@
+# Description
+
+This package aims to facilitate API integrations of providers present in professional sport
+
+# Install
+![Python](https://img.shields.io/badge/python-v3.10.9-blue.svg)
+
+```
+pip install sportsapi
+```
+
+# Contacts
+contact@sportdatalab.fr
+##### [sportdatalab.fr](https://www.sportdatalab.fr/)
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555)](https://www.linkedin.com/in/bastien-angeloz-204360a4/)
---
+++ packages/sportsapi/sportsapi-0.2.0.tar.gz/sportsapi-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+SHORT_DESCRIPTION = 'This package aims to facilitate API integrations of providers present in professional sport'
+
+setup(
+    name='sportsapi',
+    version='0.2.0',
+    author='Bastien Angeloz',
+    author_email='bastien.angeloz@sportdatalab.fr',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sportdatalab/sportsapi',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
