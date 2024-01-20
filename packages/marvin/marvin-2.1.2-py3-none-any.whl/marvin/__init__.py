2a2dcd797328b80d3ce69a820d484fff1eceb203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:46 +0000

    Add package marvin/2.1.2/marvin-2.1.2-py3-none-any.whl

---
+++ packages/marvin/marvin-2.1.2-py3-none-any.whl/marvin/__init__.py
@@ -0,0 +1,38 @@
+from .settings import settings
+
+from .ai.text import fn, cast, extract, classify, classifier, generate, model, Model
+from .ai.images import paint, image
+from .ai.audio import speak, speech
+from . import beta
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+__all__ = [
+    # --- text ---
+    "Model",
+    "cast",
+    "classify",
+    "classifier",
+    "extract",
+    "fn",
+    "generate",
+    "model",
+    # --- images ---
+    "image",
+    "paint",
+    # --- audio ---
+    "speak",
+    "speech",
+    # --- beta ---
+    "beta",
+]
+
+
+# compatibility with Marvin v1
+ai_fn = fn
+ai_model = model
+ai_classifier = classifier
