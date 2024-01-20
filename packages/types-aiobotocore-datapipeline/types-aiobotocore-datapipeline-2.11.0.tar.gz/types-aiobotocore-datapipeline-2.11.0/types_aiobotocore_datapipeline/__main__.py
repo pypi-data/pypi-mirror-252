6900a049c693084d89ded8a23d6e839db90905e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:33 +0000

    Add package types-aiobotocore-datapipeline/2.11.0/types-aiobotocore-datapipeline-2.11.0.tar.gz

---
+++ packages/types-aiobotocore-datapipeline/types-aiobotocore-datapipeline-2.11.0.tar.gz/types-aiobotocore-datapipeline-2.11.0/types_aiobotocore_datapipeline/__main__.py
@@ -0,0 +1,40 @@
+"""
+Main CLI entrypoint.
+"""
+
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for aiobotocore.DataPipeline 2.11.0\nVersion:         2.11.0\nBuilder"
+        " version: 7.23.1\nDocs:           "
+        " https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline//\nBoto3"
+        " docs:     "
+        " https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline\nOther"
+        " services:  https://pypi.org/project/boto3-stubs/\nChangelog:      "
+        " https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("2.11.0")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
