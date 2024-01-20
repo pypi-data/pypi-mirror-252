e503ec385e04c56fda9aa260e4ef11eb60877fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:41 +0000

    Add package pyUSID/0.0.12/pyUSID-0.0.12-py2.py3-none-any.whl

---
+++ packages/pyUSID/pyUSID-0.0.12-py2.py3-none-any.whl/pyUSID/processing/__init__.py
@@ -0,0 +1,15 @@
+"""
+Formalizing data processing on USID datasets using parallel computing tools
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+"""
+
+from .process import Process
+from .comp_utils import *
+
+__all__ = ['Process', 'comp_utils']
