030f7479685c52f7b1731ceb739934b4301ef1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:52 +0000

    Add package skimfunctions/0.0.1/skimfunctions-0.0.1.tar.gz

---
+++ packages/skimfunctions/skimfunctions-0.0.1.tar.gz/skimfunctions-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'A Python package containing functions that I made and found useful'
+
+setup(
+    name='skimfunctions',
+    version=VERSION,
+    author="Scott Kim",
+    author_email="<scott.kim00@hotmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+        # any dependencies your package may have
+
+)
