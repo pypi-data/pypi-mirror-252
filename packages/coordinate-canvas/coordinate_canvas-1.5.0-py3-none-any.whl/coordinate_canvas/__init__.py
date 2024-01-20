eaa684e1d59d7e0c89bf5d6961fceb9e0b2f9dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 16:03:11 +0000

    Add package coordinate-canvas/1.5.0/coordinate_canvas-1.5.0-py3-none-any.whl

---
+++ packages/coordinate-canvas/coordinate_canvas-1.5.0-py3-none-any.whl/coordinate_canvas/__init__.py
@@ -0,0 +1,8 @@
+"""Coordinate canvas package.
+
+This package contains the coordinate canvas CLI utility, used for drawing
+lines on a matplotlib plot for further coordinate extraction.
+
+Author:
+    Paulo Sanchez (@erlete)
+"""
