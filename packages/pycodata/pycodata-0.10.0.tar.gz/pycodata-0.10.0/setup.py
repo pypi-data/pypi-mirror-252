bce04d1bef67e2050363d3852c356b3ae08ee982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 18:03:06 +0000

    Add package pycodata/0.10.0/pycodata-0.10.0.tar.gz

---
+++ packages/pycodata/pycodata-0.10.0.tar.gz/pycodata-0.10.0/setup.py
@@ -0,0 +1,36 @@
+r"""setup"""
+import platform
+from setuptools import setup, Extension
+
+libraries = None
+library_dirs = None
+runtime_library_dirs = None
+extra_objects = None
+
+if platform.system() == "Linux":
+    libraries = ["codata"]
+    library_dirs = ["./pycodata"]
+    runtime_library_dirs = ["$ORIGIN"]
+if platform.system() == "Windows":
+    extra_objects = ["./pycodata/libcodata.dll.a"]
+if platform.system() == "Darwin":
+    libraries = ["codata"]
+    library_dirs = ["./pycodata"]
+    runtime_library_dirs = ["@loader_path"]
+
+if __name__ == "__main__":
+
+    mod_constants = Extension(name="pycodata.constants",
+                        sources=["./pycodata/cpy_codata_constants.c"],
+                        libraries=libraries,
+                        library_dirs=library_dirs,
+                        runtime_library_dirs=runtime_library_dirs,
+                        extra_objects=extra_objects)
+    mod_version = Extension(name="pycodata.version",
+                        sources=["./pycodata/cpy_codata_version.c"],
+                        libraries=libraries,
+                        library_dirs=library_dirs,
+                        runtime_library_dirs=runtime_library_dirs,
+                        extra_objects=extra_objects)
+    setup(ext_modules=[mod_constants, mod_version])
+
