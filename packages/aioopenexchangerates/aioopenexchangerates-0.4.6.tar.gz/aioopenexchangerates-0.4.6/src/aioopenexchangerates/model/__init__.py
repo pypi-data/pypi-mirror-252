822726ad102733420bd66bb8ea53cef18e7670a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:17 +0000

    Add package aioopenexchangerates/0.4.6/aioopenexchangerates-0.4.6.tar.gz

---
+++ packages/aioopenexchangerates/aioopenexchangerates-0.4.6.tar.gz/aioopenexchangerates-0.4.6/src/aioopenexchangerates/model/__init__.py
@@ -0,0 +1,4 @@
+"""Provide a model for the Open Exchange Rates API."""
+from .latest import Latest
+
+__all__ = ["Latest"]
---
+++ packages/aioopenexchangerates/aioopenexchangerates-0.4.6.tar.gz/aioopenexchangerates-0.4.6/src/aioopenexchangerates/model/latest.py
@@ -0,0 +1,6 @@
+"""Provide a model for endpoint latest.json."""
+from .response import BaseRatesResponse
+
+
+class Latest(BaseRatesResponse):
+    """Represent the model for endpoint latest.json."""
