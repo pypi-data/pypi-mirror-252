1932ff9bb56ba2fc73bf4f3618c38a8e5b6da059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:42 +0000

    Add package mngs/0.5.0/mngs-0.5.0-py3-none-any.whl

---
+++ packages/mngs/mngs-0.5.0-py3-none-any.whl/mngs/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Time-stamp: "2024-01-20 10:40:58 (ywatanabe)"
+
+from . import dsp, general, io, ml, plt, resource, stats, linalg, nn
+from . import general as gen
+from .general.debug import *
+# import numpy as np
+# import matplotlib
+# matplotlib.use("TkAgg")
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import scipy
+# import torch
+# import pandas as pd
+
+
+# from .auto_ipdb import auto_ipdb
+
+__copyright__ = "Copyright (C) 2021 Yusuke Watanabe"
+__version__ = "0.5.0"
+__license__ = "GPL3.0"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/mngs"
