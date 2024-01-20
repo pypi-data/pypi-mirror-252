7172f665601acd144818f0322f840d00a439ca7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:03:19 +0000

    Add package tibiapi/0.3.0/tibiapi-0.3.0-py3-none-any.whl

---
+++ packages/tibiapi/tibiapi-0.3.0-py3-none-any.whl/tibiapi/_common/container/__init__.py
@@ -0,0 +1,2 @@
+from .container import *
+from .topBar import *
---
+++ packages/tibiapi/tibiapi-0.3.0-py3-none-any.whl/tibiapi/utils/image.py
@@ -0,0 +1,28 @@
+import cv2
+from farmhash import FarmHash64
+import numpy as np
+from PIL import Image as PilImage
+from typing import Optional
+from tibiapi._common.typings import Image
+
+
+def hashit(arr: np.ndarray) -> int:
+    return FarmHash64(arr)
+
+
+def load(path: str) -> Image:
+    bgraImage = cv2.imread(path)
+    return np.ascontiguousarray(bgraImage, dtype=np.uint8)
+
+
+def locate(compareImage: np.ndarray, img: np.ndarray, confidence: float = 0.85) -> Optional[tuple[int, int, int, int]]:
+    match = cv2.matchTemplate(compareImage, img, cv2.TM_CCOEFF_NORMED)
+    res = cv2.minMaxLoc(match)
+    if res[1] <= confidence:
+        return None
+    return res[3][0], res[3][1], len(img[0]), len(img)
+
+
+def save(arr: np.ndarray, name: str):
+    im = PilImage.fromarray(arr)
+    im.save(name)
