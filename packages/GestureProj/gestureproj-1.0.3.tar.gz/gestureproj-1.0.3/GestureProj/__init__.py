f1b992e204dc4cb3e0a6fa6a8b5386d5e775cb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 18:03:07 +0000

    Add package GestureProj/1.0.3/gestureproj-1.0.3.tar.gz

---
+++ packages/GestureProj/gestureproj-1.0.3.tar.gz/gestureproj-1.0.3/GestureProj/__init__.py
@@ -0,0 +1,9 @@
+#load a python script as a module
+import os
+import sys
+
+module_path = os.path.abspath(__file__)
+module_root = os.path.dirname(module_path)
+sys.path.append(module_root)
+sys.path.append(module_root+'/'+'rootfold')
+os.chdir(module_root)
