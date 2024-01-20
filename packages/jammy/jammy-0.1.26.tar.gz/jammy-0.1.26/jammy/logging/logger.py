41a801441cadc7838adb106293b91a108b959e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 14:03:45 +0000

    Add package jammy/0.1.26/jammy-0.1.26.tar.gz

---
+++ packages/jammy/jammy-0.1.26.tar.gz/jammy-0.1.26/jammy/logging/logger.py
@@ -0,0 +1,43 @@
+import sys
+
+from loguru import logger
+
+__all__ = [
+    "get_logger",
+    "LOG_FORMAT",
+]
+
+logger.remove()
+
+logger_sink = {}
+LOG_FORMAT = (
+    "[<green>{time:MM-DD HH:mm:ss}</green>|"
+    "<red>{process.name}</red>|"
+    "<level>{level: <8}</level>|"
+    "<cyan>{file.path}</cyan>:<cyan>{line}</cyan>:<cyan>{function}</cyan>]"
+    "<level>{message}</level>"
+)
+
+
+def get_logger(file_name=None, clear=False, **kwargs):
+    global logger_sink  # pylint: disable=global-statement,global-variable-not-assigned
+    if clear:
+        logger.remove()
+        logger_sink.clear()
+    if file_name is None:
+        file_name = sys.stderr
+        if "level" not in kwargs:
+            kwargs["level"] = "INFO"
+    if "format" not in kwargs:
+        kwargs["format"] = LOG_FORMAT
+    if file_name in logger_sink.values():
+        # FIXME:
+        # logger.debug("already registered")
+        logger.debug(f"{str(file_name)} already registered")
+    else:
+        # if "level" not in kwargs:
+        # kwargs["level"] = "DEBUG" if jam_is_debug() else "INFO"
+        sink_id = logger.add(file_name, **kwargs)
+        logger_sink[sink_id] = file_name
+        logger.debug(f"sink_id: {sink_id:02d} ---> {str(file_name)}")
+    return logger
---
+++ packages/jammy/jammy-0.1.26.tar.gz/jammy-0.1.26/jammy/utils/__init__.py
@@ -0,0 +1,7 @@
+from .argument import *
+from .cache import *
+from .cfg import *
+from .cnt import *
+from .hash import *
+from .meta import Singleton
+from .profiler import IS_PROFILING, profile, profile_now
