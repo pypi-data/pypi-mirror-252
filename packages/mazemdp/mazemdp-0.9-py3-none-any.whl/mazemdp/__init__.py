a21bedcf6f6397a4965561fa071cfe7c980d9a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 17:02:58 +0000

    Add package mazemdp/0.9/mazemdp-0.9-py3-none-any.whl

---
+++ packages/mazemdp/mazemdp-0.9-py3-none-any.whl/mazemdp/__init__.py
@@ -0,0 +1,9 @@
+from tabularmazemdp.maze import build_maze, create_random_maze
+from tabularmazemdp.toolbox import (
+    egreedy,
+    egreedy_loc,
+    random_policy,
+    softmax,
+    sample_categorical,
+)
+from ._version import __version__, __version_tuple__
