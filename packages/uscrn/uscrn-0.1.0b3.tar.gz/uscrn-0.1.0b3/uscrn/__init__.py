cefb7758444aa985834c475962411074a1b0fecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:04:11 +0000

    Add package uscrn/0.1.0b3/uscrn-0.1.0b3.tar.gz

---
+++ packages/uscrn/uscrn-0.1.0b3.tar.gz/uscrn-0.1.0b3/uscrn/__init__.py
@@ -0,0 +1,37 @@
+"""
+Load U.S. CRN data.
+"""
+
+__version__ = "0.1.0b3"
+
+from .attrs import load_attrs
+from .data import (
+    get_data,
+    load_meta,
+    read,
+    read_daily,
+    read_hourly,
+    read_monthly,
+    read_subhourly,
+    to_xarray,
+)
+
+ATTRS = load_attrs()
+"""Dataset and variable attributes, mainly taken from the respective readmes.
+The relevant top-level keys are ``hourly``, ``daily``, etc.
+"""
+
+del load_attrs
+
+__all__ = [
+    "ATTRS",
+    "get_data",
+    "load_meta",
+    "read",
+    "read_daily",
+    "read_hourly",
+    "read_monthly",
+    "read_subhourly",
+    "to_xarray",
+    "__version__",
+]
