501a16f357c19e944103b37076cf7a66f5c0b9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 17:03:05 +0000

    Add package forefront/0.3.8/forefront-0.3.8.tar.gz

---
+++ packages/forefront/forefront-0.3.8.tar.gz/forefront-0.3.8/forefront/forefront_client.py
@@ -0,0 +1,24 @@
+
+from .chat import Chat
+from .datasets import Datasets
+from .models import Models
+from .fine_tunes import FineTunes
+from .datasets import Datasets
+from .pipelines import Pipelines
+
+
+class ForefrontClient:
+    def __init__(self, api_key, default_model=None, base_url=None):
+        self.api_key = api_key
+        self.api_url = base_url if base_url else 'https://api-worker.forefront.workers.dev'  # Replace with your actual API URL
+        self.headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        self.default_model = default_model
+
+        self.chat = Chat(self)  # Instantiate Chat class
+        self.datasets = Datasets(self)
+        self.models = Models(self)
+        self.fine_tunes = FineTunes(self)
+        self.pipelines = Pipelines(self)
