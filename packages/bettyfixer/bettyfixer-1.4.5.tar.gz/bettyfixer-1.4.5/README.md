2d8f3205a1bd12c8f95f52fc3b1df6f3bb361908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:04:52 +0000

    Add package bettyfixer/1.4.5/bettyfixer-1.4.5.tar.gz

---
+++ packages/bettyfixer/bettyfixer-1.4.5.tar.gz/bettyfixer-1.4.5/README.md
@@ -0,0 +1,57 @@
+# Betty Fixer
+
+Betty Fixer is a tool designed to automatically fix coding style issues in C files based on the Betty coding style guidelines. It performs corrections to ensure that the code complies with the Betty style, making it more readable and consistent.
+
+## Features
+
+- **Betty Style Fixes**: Automatically corrects coding style issues following the Betty style guidelines.
+- **Vi Script Execution**: Runs a Vi script for each fixed file, making it convenient for developers to review and further modify the code.
+- **Create header.h**: You can now create header file by specifing header file name after flag -H .
+
+## Prerequisites
+
+Before using Betty Fixer, ensure you have the following installed:
+
+- [Betty](https://github.com/holbertonschool/Betty) - The Betty linter for C code.
+- [Vi Editor](https://www.vim.org/) - The Vi editor for script execution.
+
+## Getting Started
+
+1. Clone the repository:
+
+    ```bash
+    pip install bettyfixer
+    ```
+
+2. Run Betty Fixer on your C files:
+
+    ```bash
+    bettyfixer file1.c file2.c ...
+    ```
+
+3. To create header file run:
+
+    ```bash
+    bettyfixer -H <header_name>.h ...
+    ```
+
+
+## Contributing
+
+If you'd like to contribute to Betty Fixer, please follow these steps:
+
+1. Go to the github repository.
+2. Fork the repository.
+3. Create a new branch for your feature or bug fix.
+4. Make your changes and ensure the code style follows the project conventions.
+5. Test your changes thoroughly.
+6. Create a pull request with a clear description of your changes.
+
+
+Happy coding!
+
+
+
+### Creaters: - 
+[@Moealsir](https://github.com/Moealsir) <br>
+[@malazmuzamil98](https://github.com/malazmuzamil98)
---
+++ packages/bettyfixer/bettyfixer-1.4.5.tar.gz/bettyfixer-1.4.5/setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from pathlib import Path
+import subprocess
+import shutil
+
+from setuptools.command.install import install
+import shutil
+import subprocess
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        # Check if Betty is installed
+        betty_executable = shutil.which('betty')
+        if betty_executable is None:
+            print("Betty not found. Installing...")
+            self.install_betty()
+
+        # Check if black is installed, if not install it
+        black_executable = shutil.which('black')
+        if black_executable is None:
+            print("Black not found. Installing...")
+            self.install_black()
+
+        # Check if exuberant-ctags is installed, if not install it
+        ctags_executable = shutil.which('ctags')
+        if ctags_executable is None:
+            print("Exuberant-ctags not found. Installing...")
+            self.install_ctags()
+
+    def install_betty(self):
+        subprocess.run(["sh", "bettyfixer/install_dependency/.Betty/install.sh"])
+        subprocess.run(["sudo", "cp", "bettyfixer/install_dependency/.betty", "/bin/betty"])
+
+    def install_black(self):
+        subprocess.run(["sudo", "pip", "install", "black"])
+
+    def install_ctags(self):
+        subprocess.run(["sudo", "apt-get", "install", "exuberant-ctags"])
+
+setup(
+    name='bettyfixer',
+    version='1.4.5',
+    packages=find_packages(),
+    cmdclass={'install': CustomInstallCommand},  # Use the custom install command
+    entry_points={
+        'console_scripts': [
+            'bettyfixer = bettyfixer.betty_fixer:main',
+            'show-changelog = show_changelog:main',  # Include the show-changelog script
+        ],
+    },
+    install_requires=[
+        'colorama',
+        # other dependencies...
+    ],
+    author='Moealsir',
+    author_email='mohamedwdalsir@gmail.com',
+    description='Betty Fixer is a tool designed to automatically fix coding style issues in C files based on the Betty coding style guidelines. It performs corrections to ensure that the code complies with the Betty style, making it more readable and consistent.',
+    url='https://github.com/Moealsir/betty_fixer',  # Replace with your GitHub repository URL
+    license='MIT',  # Replace with your desired license
+
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown"
+)
