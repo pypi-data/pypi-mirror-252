e8a91d5a9af2b7a6e315b0608619ae991a2e58bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:03:22 +0000

    Add package httpmdhtml/0.1.4/httpmdhtml-0.1.4.tar.gz

---
+++ packages/httpmdhtml/httpmdhtml-0.1.4.tar.gz/httpmdhtml-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="httpmdhtml",
+    version="0.1.4",
+    license="gpl-3.0",
+    author="John Hupperts",
+    author_email="jrock4503@hotmail.com",
+    description="HTTP server that converts markdown to HTML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/treatmesubj/python-md-to-html-server",
+    download_url="https://github.com/treatmesubj/python-md-to-html-server/archive/refs/tags/v0.1.4.tar.gz",
+    packages=["httpmdhtml"],
+    package_dir={"python-md-to-html-server": "httpmdhtml"},
+    project_urls={
+        "Source": "https://github.com/treatmesubj/python-md-to-html-server",
+    },
+    install_requires=[
+        "markdown-it-py",
+        "beautifulsoup4",
+        "html5lib"
+    ],
+)
