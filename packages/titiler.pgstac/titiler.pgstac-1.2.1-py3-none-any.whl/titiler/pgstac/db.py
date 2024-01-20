d8ea48372b4dac5b1e013946bd5427ee07cc5c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:04:37 +0000

    Add package titiler.pgstac/1.2.1/titiler.pgstac-1.2.1-py3-none-any.whl

---
+++ packages/titiler.pgstac/titiler.pgstac-1.2.1-py3-none-any.whl/titiler/pgstac/db.py
@@ -0,0 +1,35 @@
+"""Database connection handling."""
+
+from typing import Optional
+
+from fastapi import FastAPI
+from psycopg_pool import ConnectionPool
+
+from titiler.pgstac.settings import PostgresSettings
+
+
+async def connect_to_db(
+    app: FastAPI, settings: Optional[PostgresSettings] = None
+) -> None:
+    """Connect to Database."""
+    if not settings:
+        settings = PostgresSettings()
+
+    app.state.dbpool = ConnectionPool(
+        conninfo=str(settings.database_url),
+        min_size=settings.db_min_conn_size,
+        max_size=settings.db_max_conn_size,
+        max_waiting=settings.db_max_queries,
+        max_idle=settings.db_max_idle,
+        num_workers=settings.db_num_workers,
+        kwargs={"options": "-c search_path=pgstac,public -c application_name=pgstac"},
+    )
+
+    # Make sure the pool is ready
+    # ref: https://www.psycopg.org/psycopg3/docs/advanced/pool.html#pool-startup-check
+    app.state.dbpool.wait()
+
+
+async def close_db_connection(app: FastAPI) -> None:
+    """Close Pool."""
+    app.state.dbpool.close()
