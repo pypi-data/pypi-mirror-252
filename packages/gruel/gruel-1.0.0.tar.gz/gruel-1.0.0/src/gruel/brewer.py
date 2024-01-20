5af23189e363e3ec9d83c0b21de248b12a21e8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:05:09 +0000

    Add package gruel/1.0.0/gruel-1.0.0.tar.gz

---
+++ packages/gruel/gruel-1.0.0.tar.gz/gruel-1.0.0/src/gruel/brewer.py
@@ -0,0 +1,263 @@
+import argparse
+import importlib
+import importlib.machinery
+import importlib.util
+import inspect
+from typing import Any
+
+import loggi
+import quickpool
+from pathier import Pathier, Pathish
+from younotyou import younotyou
+
+from gruel import Gruel
+
+
+class Brewer:
+    def __init__(
+        self,
+        subgruel_classes: list[str],
+        file_exclude_patterns: list[str] = [],
+        scan_path: Pathish = Pathier.cwd(),
+        file_include_patterns: list[str] = ["*.py"],
+        recursive: bool = True,
+        log_dir: Pathish | None = None,
+    ):
+        """Run `Gruel` scrapers.
+
+        #### :params:
+
+        `subgruel_classes`: A list of class names for scrapers that should be loaded.
+        In order to be loaded, a scraper class must have a name in this list and have `Gruel` somewhere in its inheritance hierarchy.
+
+        `file_exclude_patterns`: Files that match these patterns will not be scanned.
+
+        `scan_path`: The path to scan for scraper classes.
+
+        `file_include_patterns`: Files that match these patterns will be scanned.
+
+        `recursive`: Whether the scan should be recursive or not.
+
+        `log_dir`: The directory this instance's log should be saved to.
+        If `None`, it will be saved to the current working directory.
+
+        >>> brewer = Brewer(["VenueScraper"], ["*template*", "*giggruel*"], "scrapers")
+        >>> brewer.brew()"""
+        self._init_logger(log_dir)
+        self.subgruel_classes = subgruel_classes
+        self.file_exclude_patterns = file_exclude_patterns
+        self.file_include_patterns = file_include_patterns
+        self.scan_path = Pathier(scan_path)
+        self.recursive = recursive
+
+    def _init_logger(self, log_dir: Pathish | None = None):
+        # When Brewer is subclassed, use that file's stem instead of `brewer`
+        log_dir = Pathier(log_dir) if log_dir else Pathier.cwd()
+        source_file = inspect.getsourcefile(type(self))
+        if source_file:
+            log_name = Pathier(source_file).stem
+        else:
+            log_name = Pathier(__file__).stem
+        self.logger = loggi.getLogger(log_name, log_dir)
+
+    def load_scrapers(self) -> list[Gruel]:
+        """Load scraper classes that inherit from `Gruel`.
+
+        NOTE: Classes are loaded, but scraper objects are not instantiated until the `scrape()` method is called.
+
+        #### :params:
+
+        `directory`: The path to scan for scraper classes.
+
+        `class_names`: A list of class names for scrapers that should be loaded.
+        In order to be loaded, a scraper class must have a name in this list and have `Gruel` somewhere in its inheritance hierarchy.
+
+        `include_patterns`: Files that match these patterns will be scanned.
+
+        `exclude_patterns`: Files that match these patterns will not be scanned.
+
+        `recursive`: Whether the search should be recursive or not.
+
+        >>> load_scrapers("getToTheGig/scrapers", ["VenueScraper"], ["*.py"], ["*template*", "*giggruel*"])
+        """
+        globber = self.scan_path.glob
+        if self.recursive:
+            globber = self.scan_path.rglob
+        files = [
+            str(file)
+            for pattern in self.file_include_patterns
+            for file in globber(pattern)
+        ]
+        files = younotyou(files, exclude_patterns=self.file_exclude_patterns)
+        self.modules = {}
+        self._module_names = []
+        for file in files:
+            module_name = Pathier(file).stem
+            try:
+                module = importlib.machinery.SourceFileLoader(
+                    module_name, file
+                ).load_module()
+            except Exception as e:
+                self.logger.exception(
+                    f"Failed to load module '{module_name}' from '{file}'."
+                )
+            else:
+                self._module_names.append(module_name)
+                self.modules[module] = module
+        gruels = [
+            getattr(module, class_)
+            for module in self.modules.values()
+            for class_ in self.subgruel_classes
+            if class_ in dir(module) and self.is_subgruel(getattr(module, class_))
+        ]
+        self.logger.info(
+            "\n".join(
+                [f"Imported {len(gruels)} scrapers: "]
+                + [str(gruel) for gruel in gruels]
+            )
+        )
+        return gruels
+
+    def pop_modules(self):
+        """Unload modules."""
+        for module in self.modules:
+            del module
+        self._module_names = []
+
+    def get_bases(self, object: Any) -> list[Any]:
+        """Returns a recursive list of all the classes `object` inherits from."""
+        parents = []
+        bases = object.__bases__
+        if not bases:
+            return parents
+        for base in bases:
+            parents.append(base)
+            parents.extend(self.get_bases(base))
+        return parents
+
+    def is_subgruel(self, object: Any) -> bool:
+        """Returns whether `object` inherits from `Gruel` somewhere in its ancestory."""
+        if not inspect.isclass(object) or Gruel not in self.get_bases(object):
+            return False
+        return True
+
+    def prescrape_chores(self):
+        """Override to add any tasks to be done before running the scrapers."""
+        ...
+
+    def postscrape_chores(self):
+        """Override to add any tasks to be done after running the scrapers."""
+        self.pop_modules()
+
+    def scrape(self, scrapers: list[Gruel]):
+        """Run the `scrape()` method for each scraper in `scrapers`.
+
+        Execution is multithreaded."""
+        execute = lambda scraper: scraper().scrape()
+        pool = quickpool.ThreadPool(
+            [execute] * len(scrapers), [(scraper,) for scraper in scrapers]
+        )
+        pool.execute()
+
+    def logprint(self, message: str):
+        """Log and print `message`."""
+        self.logger.info(message)
+        print(message)
+
+    def brew(self):
+        """Execute pipeline.
+
+        1. self.prescrape_chores()
+        2. self.load_scrapers()
+        3. self.scrape()
+        4. self.postscrape_chores()"""
+
+        try:
+            self.logprint("Beginning brew")
+            # 1--------------------------------------------
+            self.logprint("Executing prescrape chores")
+            self.prescrape_chores()
+            # 2--------------------------------------------
+            self.logprint("Loading scrapers")
+            scrapers = self.load_scrapers()
+            print(f"Loaded {len(scrapers)} scrapers")
+            # 3--------------------------------------------
+            self.logprint("Starting scrape")
+            self.scrape(scrapers)
+            self.logprint("Scrape complete")
+            # 4--------------------------------------------
+            self.logprint("Executing postscrape chores")
+            self.postscrape_chores()
+            self.logprint("Brew complete")
+        except Exception as e:
+            print(e)
+            self.logger.exception("Exception occured during brew():")
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "subgruel_classes",
+        type=str,
+        nargs="*",
+        help=""" A list of Gruel scraper class names to find and import. """,
+    )
+    parser.add_argument(
+        "-e",
+        "--excludes",
+        type=str,
+        nargs="*",
+        default=[],
+        help=""" A list of glob style file patterns to exclude from the scan. """,
+    )
+    parser.add_argument(
+        "-i",
+        "--includes",
+        type=str,
+        nargs="*",
+        default=["*.py"],
+        help=""" A list of glob style file patterns to include in the scan. Defaults to "*.py". """,
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        default=Pathier.cwd(),
+        help=""" The directory path to scan. Defaults to the current working directory. """,
+    )
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help=""" Whether -p/--path should be scanned recursively or not. """,
+    )
+    parser.add_argument(
+        "-l",
+        "--log_dir",
+        type=str,
+        default=None,
+        help=""" The directory to save the brew log to.""",
+    )
+    args = parser.parse_args()
+    args.path = Pathier(args.path)
+
+    return args
+
+
+def main(args: argparse.Namespace | None = None):
+    if not args:
+        args = get_args()
+    brewer = Brewer(
+        args.subgruel_classes,
+        args.excludes,
+        args.path,
+        args.includes,
+        args.recursive,
+        args.log_dir,
+    )
+    brewer.brew()
+
+
+if __name__ == "__main__":
+    main(get_args())
---
+++ packages/gruel/gruel-1.0.0.tar.gz/gruel-1.0.0/src/gruel/gruel.py
@@ -0,0 +1,188 @@
+import inspect
+import time
+from typing import Any
+
+import loggi
+import requests
+import whosyouragent
+from bs4 import BeautifulSoup, Tag
+from noiftimer import Timer
+from pathier import Pathier, Pathish
+from printbuddies import ProgBar
+
+ParsableItem = dict | str | Tag
+
+
+class Gruel:
+    """Scraper base class.
+
+    Classes subclassing `Gruel` need to implement the following methods:
+
+    * `get_parsable_items(self) -> list[Any]`
+    * `parse_item(self, item: Any)->Any`
+    * `store_item(self, item: Any)`
+
+    Calling the `scrape()` method will execute:
+    1. `self.prescrape_chores()` (does nothing unless overridden)
+    2. `self.get_parsable_items()`
+    3. `self.parse_item()` for each item returned by `self.get_parsable_items()`
+    4. `self.store_item()` for each successfully parsed item
+    5. `self.postscrape_chores()` (only closes this instance's log file unless overridden)
+
+    When overriding `self.postscrape_chores`, it's recommended to either
+    call `super().postscrape_chores()` or make sure to call `self.log.close()`.
+    Otherwise running a large number of scrapers can cause file handle limit issues."""
+
+    def __init__(self, name: str | None = None, log_dir: Pathish | None = None):
+        """
+        :params:
+        * `name`: The name of this scraper. If `None`, the name will be the stem of the file this class/subclass was defined in.
+        i.e. A `Gruel` subclass located in a file called `myscraper.py` will have the name `"myscraper"`.
+        * `log_dir`: The directory this scraper's logs should be saved to.
+        If `None`, the logs will be written to a folder called `"gruel_logs"` within the current working directory.
+        """
+        self._name = name
+        self._init_logger(log_dir)
+        self.timer = Timer()
+        self.success_count = 0
+        self.fail_count = 0
+
+    @property
+    def name(self) -> str:
+        """Returns the name given to __init__ or the stem of the file this instance was defined in if one wasn't given."""
+        return self._name or Pathier(inspect.getsourcefile(type(self))).stem  # type: ignore
+
+    def _init_logger(self, log_dir: Pathish | None):
+        log_dir = Pathier.cwd() / "gruel_logs" if not log_dir else Pathier(log_dir)
+        self.logger = loggi.getLogger(self.name, log_dir)
+
+    @staticmethod
+    def request(
+        url: str,
+        method: str = "get",
+        headers: dict[str, str] = {},
+        params: dict | None = None,
+        data: dict | None = None,
+        timeout: int | None = None,
+        retry_on_fail: bool = True,
+    ) -> requests.Response:
+        """Send a request to `url` and return the `requests.Response` object.
+
+        By default, the only header sent is a randomized user agent string.
+
+        This can be overridden by supplying a user agent in the `headers` param.
+
+        If `retry_on_fail` is `True`, the request will be repeated after 1 second if the originally request causes an exception to be thrown.
+        Otherwise, the exception will be raised."""
+        args = [method, url]
+        headers = whosyouragent.get_header() | headers
+        kwargs = {
+            "headers": headers,
+            "timeout": timeout,
+            "params": params,
+            "data": data,
+        }
+        try:
+            response = requests.request(*args, **kwargs)
+            return response
+        except Exception as e:
+            if retry_on_fail:
+                time.sleep(1)
+                return requests.request(*args, **kwargs)
+            else:
+                raise e
+
+    @staticmethod
+    def as_soup(response: requests.Response) -> BeautifulSoup:
+        """Returns the text content of `response` as a `BeautifulSoup` object."""
+        return BeautifulSoup(response.text, "html.parser")
+
+    def get_soup(
+        self, url: str, method: str = "get", headers: dict[str, str] = {}
+    ) -> BeautifulSoup:
+        """Request `url` with `headers` and return `BeautifulSoup` object."""
+        return self.as_soup(self.request(url, method, headers))
+
+    def clean_string(self, text: str) -> str:
+        """Strip `\\n\\r\\t` and whitespace from `text`."""
+        return text.strip(" \n\t\r")
+
+    # |==============================================================================|
+    # Overridables
+    # |==============================================================================|
+    def prescrape_chores(self):
+        """Chores to do before scraping."""
+        ...
+
+    def postscrape_chores(self):
+        """Chores to do after scraping."""
+        loggi.close(self.logger)
+
+    def get_parsable_items(self) -> list[ParsableItem]:
+        """Get relevant webpages and extract raw data that needs to be parsed.
+
+        e.g. first 10 results for an endpoint that returns json content
+        >>> return self.get_page(some_url).json()[:10]"""
+        raise NotImplementedError
+
+    def parse_item(self, item: ParsableItem) -> Any:
+        """Parse `item` and return parsed data.
+
+        e.g.
+        >>> try:
+        >>>     parsed = {}
+        >>>     parsed["thing1"] = item["element"].split()[0]
+        >>>     self.successes += 1
+        >>>     return parsed
+        >>> except Exception:
+        >>>     self.logger.exception("message")
+        >>>     self.failures += 1
+        >>>     return None"""
+        raise NotImplementedError
+
+    def store_item(self, item: Any):
+        """Store `item`."""
+        raise NotImplementedError
+
+    def _parse_items_no_prog_bar(self, parsable_items: list[ParsableItem]):
+        for item in parsable_items:
+            parsed_item = self.parse_item(item)
+            if parsed_item:
+                self.store_item(parsed_item)
+
+    def _parse_items_prog_bar(self, parsable_items: list[ParsableItem]):
+        with ProgBar(len(parsable_items)) as bar:
+            for item in parsable_items:
+                parsed_item = self.parse_item(item)
+                if parsed_item:
+                    self.store_item(parsed_item)
+                    bar.display(f"{bar.runtime}")
+
+    def scrape(self, parse_items_prog_bar_display: bool = False):
+        """Run the scraper:
+        1. prescrape chores
+        2. get parsable items
+        3. parse and store items
+        5. postscrape chores"""
+        try:
+            self.timer.start()
+            self.logger.info("Scrape started.")
+            self.prescrape_chores()
+            try:
+                parsable_items = self.get_parsable_items()
+                self.logger.info(
+                    f"{self.name}:get_parsable_items() returned {(len(parsable_items))} items"
+                )
+            except Exception:
+                self.logger.exception(f"Error in {self.name}:get_parsable_items().")
+            else:
+                if parse_items_prog_bar_display:
+                    self._parse_items_prog_bar(parsable_items)
+                else:
+                    self._parse_items_no_prog_bar(parsable_items)
+                self.logger.info(
+                    f"Scrape completed in {self.timer.elapsed_str} with {self.success_count} successes and {self.fail_count} failures."
+                )
+        except Exception:
+            self.logger.exception(f"Unexpected failure in {self.name}:scrape()")
+        self.postscrape_chores()
