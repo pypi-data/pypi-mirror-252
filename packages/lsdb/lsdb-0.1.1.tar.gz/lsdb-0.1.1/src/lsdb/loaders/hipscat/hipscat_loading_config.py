e5f22e78779a473c4c566516646a8f4a753db90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:04:16 +0000

    Add package lsdb/0.1.1/lsdb-0.1.1.tar.gz

---
+++ packages/lsdb/lsdb-0.1.1.tar.gz/lsdb-0.1.1/src/lsdb/loaders/hipscat/hipscat_loading_config.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class HipscatLoadingConfig:
+    """Configuration for loading a HiPSCat catalog in lsdb.
+
+    Contains all parameters needed for a user to specify how to correctly read a hipscat catalog.
+    """
+
+    columns: List[str] | None = None
+    """Columns to load from the catalog - if not specified, all columns are loaded"""
+
+    kwargs: dict | None = None
+    """Extra kwargs"""
+
+    def get_kwargs_dict(self) -> dict:
+        """Returns a dictionary with the extra kwargs"""
+        return self.kwargs if self.kwargs is not None else {}
