c3f060b77be45a6b653290fd4501030189a24d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 18:03:06 +0000

    Add package monitor-promdiscovery/0.2.1/monitor_promdiscovery-0.2.1-py3-none-any.whl

---
+++ packages/monitor-promdiscovery/monitor_promdiscovery-0.2.1-py3-none-any.whl/monitor_promdiscovery/main.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2021  Opsdis AB
+
+    This file is part of monitor-exporter.
+
+    monitor-promdiscovery is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    monitor-promdiscovery is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with monitor-exporter.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import argparse
+
+import yaml
+
+import monitor_promdiscovery.log as log
+import monitor_promdiscovery.prom as Prom
+from monitor_promdiscovery.hosts_by_hostgroup import HostByGroup
+from monitor_promdiscovery.system_factory import select_system
+
+
+def main():
+    parser = argparse.ArgumentParser(description='monitor-promdiscovery')
+
+    parser.add_argument('-f', '--configfile',
+                        dest="configfile", help="Configuration file")
+
+    parser.add_argument('-F', '--force', action="store_true",
+                        dest="force", help="Force write of service discovery file")
+
+    parser.add_argument('-s', '--system',
+                        dest="system",
+                        help="System to discover. Will override configuration. Supported are op5monitor and icinga2")
+
+    args = parser.parse_args()
+
+    if args.configfile:
+        config_file = args.configfile
+    else:
+        config_file = 'config.yml'
+
+    config = read_config(config_file)
+
+    # Override system property in config
+    if args.system:
+        config['system'] = args.system
+
+    log.configure_logger(config)
+    log.info("Start synchronizing")
+
+    monitor_system: HostByGroup
+    if 'system' in config and select_system(config['system']):
+        monitor_system = select_system(config['system'])(config)
+    else:
+        log.error("Not a valid system {}".format(config['system']))
+        exit(1)
+
+    promdis = Prom.PromDis(config, monitor_system)
+
+    if not promdis.match() or args.force:
+        promdis.update_targets()
+
+
+def read_config(config_file: str) -> dict:
+    """
+    Read configuration file
+    :param config_file:
+    :return:
+    """
+    try:
+        with open(config_file, 'r') as ymlfile:
+            config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+    except (FileNotFoundError, IOError):
+        print("Config file {} not found".format(config_file))
+        exit(1)
+    except (yaml.YAMLError, yaml.MarkedYAMLError) as err:
+        print("Error will reading config file - {}".format(err))
+        exit(1)
+
+    return config
---
+++ packages/monitor-promdiscovery/monitor_promdiscovery-0.2.1-py3-none-any.whl/monitor_promdiscovery/prom.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2021  Opsdis AB
+
+    This file is part of monitor-exporter.
+
+    monitor-promdiscovery is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    monitor-promdiscovery is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with monitor-exporter.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import os
+
+import yaml
+
+import monitor_promdiscovery.log as log
+from monitor_promdiscovery.hosts_by_hostgroup import HostByGroup
+
+
+class PromDis(object):
+
+    def __init__(self, config, monitor_system: HostByGroup):
+
+        monitor_hosts = []
+        monitor_hosts.extend(monitor_system.get_hosts_by_hostgroup())
+        monitor_hosts.extend(monitor_system.get_hosts_by_servicegroup())
+
+        if 'system' in config:
+            system = config['system']
+            self.file_name = config[system]['prometheus']['sd_file']
+            if 'labels' in config[system]['prometheus']:
+                self.labels = config[system]['prometheus']['labels']
+            else:
+                self.labels = {}
+
+        self.monitor_hosts = monitor_hosts
+        self.prom_targets = []
+        self.set_of_targets = set()
+        self.set_of_monitor_hosts = set()
+        self.existing_labels = {}
+
+        if os.path.exists(self.file_name):
+            yml = yaml.load(open(self.file_name), Loader=yaml.SafeLoader)
+            if yml and 'targets' in yml[0]:
+                self.prom_targets = yml[0]['targets']
+                self.set_of_targets = set(self.prom_targets)
+                if 'labels' in yml[0]:
+                    self.existing_labels = yml[0]['labels']
+
+        if monitor_hosts:
+            self.set_of_monitor_hosts = set(self.monitor_hosts)
+
+    def match(self) -> bool:
+        """
+        Match if the current target hosts are the same as the one you got from Monitor
+        :return:
+        """
+        if self.set_of_targets == self.set_of_monitor_hosts and \
+                self.existing_labels == self.labels:
+            log.info(
+                "Monitor and service discovery file match - {} target entries".format(len(self.set_of_monitor_hosts)))
+            return True
+        log.info("Monitor and service discovery file not match - {} target entries in Monitor and {} entries in service discovery".format(
+                len(self.set_of_monitor_hosts), len(self.set_of_targets)))
+        return False
+
+    def update_targets(self):
+        """
+        Write a new sd_file
+        :return:
+        """
+        try:
+            with open(self.file_name, 'w') as filetowrite:
+                targets = {'labels': self.labels, 'targets': sorted(self.monitor_hosts)}
+                targets_list = [targets]
+                yaml.dump(targets_list, filetowrite, default_flow_style=False)
+                log.info("Service discovery file created {}".format(self.file_name))
+        except Exception as err:
+            log.error("{}".format(str(err)))
+            raise err
