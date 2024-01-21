6eb63e50af42b93a6a7d84af659f589296ff6a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:10 +0000

    Add package gai-lib-gen/0.52/gai-lib-gen-0.52.tar.gz

---
+++ packages/gai-lib-gen/gai-lib-gen-0.52.tar.gz/gai-lib-gen-0.52/gai/gen/itt/Vision_ITT.py
@@ -0,0 +1,71 @@
+import os,openai
+from openai import OpenAI
+from gai.common import generators_utils, logging
+logger = logging.getLogger(__name__)
+
+
+class Vision_ITT:
+    def __init__(self, gai_config):
+        self.gai_config = gai_config
+        self.client = None
+        pass
+
+    def load(self):
+        from dotenv import load_dotenv        
+        load_dotenv()
+        class MissingOpenAIApiKeyException(Exception):
+            pass
+        if 'OPENAI_API_KEY' not in os.environ:
+            msg = "OPENAI_API_KEY not found in environment variables. GPT-4 will not be available."
+            raise MissingOpenAIApiKeyException(msg)
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+        self.client = OpenAI()
+        return self
+
+    def unload(self):
+        self.client = None
+        return self
+
+    def gen(self, text, image_file, **model_params):
+        import base64
+        encoded_string = ""
+        with open("./tests/buses.jpeg", "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        image_url = f"data:image/jpeg;base64,{encoded_string}"
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": text},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                        "url": image_url,
+                        },
+                    },
+                ],
+            }
+        ]
+        return self.create(messages, **model_params)
+
+    def create(self, messages, **model_params):
+        if not self.client:
+            self.load()
+        model_params.pop("model",None)
+        model_params = {**self.gai_config["hyperparameters"],**model_params}
+        stream = model_params.pop("stream",False)
+
+        if stream:
+            response = (chunk for chunk in self.client.chat.completions.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                stream=True,
+                **model_params
+                ))
+        else:
+            response = self.client.chat.completions.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                **model_params
+                )
+        return response
---
+++ packages/gai-lib-gen/gai-lib-gen-0.52.tar.gz/gai-lib-gen-0.52/setup.py
@@ -0,0 +1,52 @@
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+version_file = os.path.join(base_dir, 'gai/gen/api/VERSION')
+with open(version_file, 'r') as f:
+    VERSION = f.read().strip()
+
+from setuptools import setup, find_packages
+from os.path import abspath
+import subprocess, os, sys
+from setuptools.command.install import install
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    README = readme_file.read()
+
+def parse_requirements(filename):
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)) as f:
+        required = f.read().splitlines()
+    return required
+
+setup(
+    name='gai-lib-gen',
+    version=VERSION,
+    author="kakkoii1337",
+    author_email="kakkoii1337@gmail.com",
+    packages=find_packages(exclude=["tests*","gai.gen.api"]),
+    description = """Gai/Gen is the Universal Multi-Modal Wrapper Library for LLM. The library is designed to provide a simplified and unified interface for seamless switching between multi-modal open source language models on a local machine and OpenAI APIs.""",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        "Development Status :: 3 - Alpha",        
+        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",        
+        'Operating System :: OS Independent',
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",        
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",        
+    ],
+    python_requires='>=3.10',        
+    install_requires=[
+    ],
+    extras_require={
+        "TTT": parse_requirements("requirements_ttt.txt"),
+        "ITT": parse_requirements("requirements_itt.txt"),
+        "ITT2": parse_requirements("requirements_itt2.txt"),
+        'STT': parse_requirements("requirements_stt.txt"),
+        'TTS': parse_requirements("requirements_tts.txt"),          
+        'RAG': parse_requirements("requirements_rag.txt")          
+    },
+)
