ed2458cbbf274f0e2bbb4a15898a45e8f860cc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:58 +0000

    Add package pvcast/0.1.1/pvcast-0.1.1.tar.gz

---
+++ packages/pvcast/pvcast-0.1.1.tar.gz/pvcast-0.1.1/README.md
@@ -0,0 +1,6 @@
+## PVCast - Local Solar Forecasting
+
+<!-- PROJECT SHIELDS -->
+[![codecov](https://codecov.io/gh/Open-HEMS/pvcast/graph/badge.svg?token=AT7GIZC76Z)](https://codecov.io/gh/Open-HEMS/pvcast)
+
+For documentation please refer to the [wiki](https://github.com/Open-HEMS/pvcast/wiki).
---
+++ packages/pvcast/pvcast-0.1.1.tar.gz/pvcast-0.1.1/pvcast/__main__.py
@@ -0,0 +1,44 @@
+"""Module that contains the command line application."""
+
+from __future__ import annotations
+
+import logging
+
+import uvicorn
+
+from .webserver.const import PORT, WEBSERVER_URL
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def init_logger() -> None:
+    """Initialize python logger."""
+    logging.basicConfig(level=logging.DEBUG)
+    fmt = "%(asctime)s %(levelname)s (%(threadName)s) " + "[%(name)s] %(message)s"
+    datefmt = "%Y-%m-%d %H:%M:%S"
+
+    # stdout handler
+    logging.getLogger().handlers[0].setFormatter(
+        logging.Formatter(fmt, datefmt=datefmt)
+    )
+
+
+def main() -> None:
+    """Entry point for the application script."""
+    init_logger()
+    _LOGGER.info("Starting pvcast webserver")
+
+    # start uvicorn server
+    uvicorn.run(
+        "pvcast.webserver.app:app",
+        host=WEBSERVER_URL,
+        port=PORT,
+        reload=True,
+        workers=3,
+        reload_includes=["*.yaml", "*.yml"],
+        reload_excludes=["*.pyc", "*.pyo", "__pycache__"],
+    )
+
+
+if __name__ == "__main__":
+    main()
