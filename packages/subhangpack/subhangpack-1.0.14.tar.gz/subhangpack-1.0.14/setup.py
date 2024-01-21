7f52a0e7ef5ed7a44bb0490189b546a9dd20e772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:12 +0000

    Add package subhangpack/1.0.14/subhangpack-1.0.14.tar.gz

---
+++ packages/subhangpack/subhangpack-1.0.14.tar.gz/subhangpack-1.0.14/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '1.0.14'
+DESCRIPTION = 'Youre too early'
+
+# Setting up
+setup(
+    name="subhangpack",
+    version=VERSION,
+    author="SubhangMokkarala",
+    author_email="subhangmokkarala@gmail.com",
+    description=DESCRIPTION,
+    long_description= open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['numpy'],
+    entry_points={
+        'console_scripts': [
+            'subhangpack-hello = subhangpack:hello',
+            'subhangpack-devicename = subhangpack:namegen',
+            'subhangpack-info = subhangpack:sysinfo'
+        ]
+    },
+    keywords=['python', 'name','beginner', 'generator', 'device', 'random', 'fun', 'subhangpack', 'subhang', 'mokkarala', 'subhangmokkarala'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
