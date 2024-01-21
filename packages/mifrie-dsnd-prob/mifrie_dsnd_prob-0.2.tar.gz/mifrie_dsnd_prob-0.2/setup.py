8164269a04721cf89d152c0a767909fc9db4655b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:48 +0000

    Add package mifrie-dsnd-prob/0.2/mifrie_dsnd_prob-0.2.tar.gz

---
+++ packages/mifrie-dsnd-prob/mifrie_dsnd_prob-0.2.tar.gz/mifrie_dsnd_prob-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mifrie_dsnd_prob',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['mifrie_dsnd_prob'],
+      zip_safe=False)
