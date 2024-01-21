fbfae8da70875b39f0eeb251ea928759770bf100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:14 +0000

    Add package hive-c/0.1/hive-c-0.1.tar.gz

---
+++ packages/hive-c/hive-c-0.1.tar.gz/hive-c-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+pkgs = ["hive", "hive.src"]
+
+install_requires = ["ctpbee", "redis", "click", "pandas"]
+setup(
+    name="hive-c",
+    version="0.1",
+    description="Auto data service with ctpbee for linux. Do not support windows in here",
+    author="somewheve",
+    author_email="somewheve@gmail.com",
+    url="https://github.com/ctpbee/hive",
+    license="Apache2",
+    packages=pkgs,
+    install_requires=install_requires,
+    platforms=["Linux", "Windows"],
+    package_dir={"hive": "hive"},
+    zip_safe=False,
+    include_package_data=True,
+    ext_modules=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    entry_points={"console_scripts": ["hive = hive:run_command"]},
+)
