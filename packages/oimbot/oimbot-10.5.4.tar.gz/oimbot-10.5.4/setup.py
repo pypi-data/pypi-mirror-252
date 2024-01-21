998fd27dffc64cc8988b44f12e4fd3b91af061cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:37 +0000

    Add package oimbot/10.5.4/oimbot-10.5.4.tar.gz

---
+++ packages/oimbot/oimbot-10.5.4.tar.gz/oimbot-10.5.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+    
+setuptools.setup(
+    name="oimbot",
+    version="10.5.4",
+    author="Aeroz",
+    long_description="For Create Fortnite LobbyBot",
+    description="Lobby_Bot_Ftn",
+    url="https://bot.aerozoff.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'crayons',
+        'BenBotAsync',
+        'fortnitepy==3.6.7',
+        'FortniteAPIAsync',
+        'sanic==21.6.2',
+        'colorama',
+        'aiohttp'
+    ],
+    dependency_links = [
+        "git+git://github.com/PirxcyFinal/fortnitepy.git"
+    ],
+    include_package_data=True,
+)
