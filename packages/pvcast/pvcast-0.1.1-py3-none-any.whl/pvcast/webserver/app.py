6c0ef06cabd30ebcac3045bf07aa23ce725bff5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:58 +0000

    Add package pvcast/0.1.1/pvcast-0.1.1-py3-none-any.whl

---
+++ packages/pvcast/pvcast-0.1.1-py3-none-any.whl/pvcast/webserver/app.py
@@ -0,0 +1,36 @@
+"""Main module for the webserver."""
+from __future__ import annotations
+
+import logging
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+from .const import API_VERSION
+from .routers.clearsky import router as clearsky_router
+from .routers.historical import router as historical_router
+from .routers.live import router as live_router
+from .routers.utils import router as utils_router
+
+_LOGGER = logging.getLogger("uvicorn")
+FAV_ICON_PATH = "pvcast/webserver/favicon.png"
+
+app = FastAPI(
+    title="PV Cast",
+    description="A webserver for the PV Cast project.",
+    version=API_VERSION,
+    docs_url="/",
+    redoc_url=None,
+)
+
+app.include_router(clearsky_router, prefix="/clearsky", tags=["clearsky"])
+app.include_router(historical_router, prefix="/historical", tags=["historical"])
+app.include_router(live_router, prefix="/live", tags=["live"])
+app.include_router(utils_router, prefix="/utils", tags=["utilities"])
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon() -> FileResponse:
+    """Get the favicon. Favicon attribution: gungyoga04."""
+    _LOGGER.info("Getting favicon")
+    return FileResponse(FAV_ICON_PATH)
---
+++ packages/pvcast/pvcast-0.1.1-py3-none-any.whl/pvcast/webserver/const.py
@@ -0,0 +1,16 @@
+"""Constants for the webserver module."""
+from __future__ import annotations
+
+import datetime as dt
+
+PORT = 4557
+WEBSERVER_URL = "127.0.0.1"  # Bind to localhost interface only
+API_VERSION = "0.1.0"
+
+START_DT_DEFAULT = dt.datetime.now(dt.timezone.utc).replace(
+    hour=0, minute=0, second=0, microsecond=0
+)
+
+END_DT_DEFAULT = dt.datetime.now(dt.timezone.utc).replace(
+    hour=23, minute=59, second=59, microsecond=0
+)
