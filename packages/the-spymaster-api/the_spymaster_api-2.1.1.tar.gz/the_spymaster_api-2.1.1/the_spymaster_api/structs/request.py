cceb808d2f694ffd4e579a8fa54a7ae45113dca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:41 +0000

    Add package the-spymaster-api/2.1.1/the_spymaster_api-2.1.1.tar.gz

---
+++ packages/the-spymaster-api/the_spymaster_api-2.1.1.tar.gz/the_spymaster_api-2.1.1/the_spymaster_api/structs/request.py
@@ -0,0 +1,44 @@
+from typing import List, Optional
+
+from codenames.game.color import TeamColor
+from pydantic import BaseModel, Extra
+from the_spymaster_solvers_api.structs.base import ModelIdentifier, Solver
+
+
+class BaseRequest(BaseModel):
+    class Config:
+        extra = Extra.allow
+        fields = {"drf_request": {"exclude": True}}
+        arbitrary_types_allowed = True
+
+    @property
+    def preforming_user(self):
+        drf_request = getattr(self, "drf_request", None)
+        return drf_request.user if drf_request else None
+
+
+class StartGameRequest(BaseRequest):
+    language: str = "english"
+    first_team: Optional[TeamColor] = None
+
+
+class HintRequest(BaseRequest):
+    game_id: str
+    word: str
+    card_amount: int
+    for_words: Optional[List[str]] = None
+
+
+class GuessRequest(BaseRequest):
+    game_id: str
+    card_index: int
+
+
+class GetGameStateRequest(BaseRequest):
+    game_id: str
+
+
+class NextMoveRequest(BaseRequest):
+    game_id: str
+    solver: Solver = Solver.NAIVE
+    model_identifier: Optional[ModelIdentifier] = None
