7027f2fc5602549afc1efcc8407706bf298992b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:38 +0000

    Add package easymyai/5.0/easymyai-5.0.tar.gz

---
+++ packages/easymyai/easymyai-5.0.tar.gz/easymyai-5.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# Это для библиотеки easymyai
+
+setup(
+    name="easymyai",
+    version="5.0",
+    description="Easy creation of your own simple neural network",
+    long_description=open("./easymyai/README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    packages=["easymyai"],
+    install_requires=["numpy>=1.26.2"],
+    url="https://github.com/ZenSam7/My_AI",
+    author="ZenSam7",
+    author_email="samkirich@yandex.ru",
+    zip_safe=False,
+)
