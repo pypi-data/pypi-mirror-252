55b33290bb615ec8705bd5c1ec2af59afa8d9059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:08 +0000

    Add package pyflowlauncher/0.6.2.dev0/pyflowlauncher-0.6.2.dev0-py3-none-any.whl

---
+++ packages/pyflowlauncher/pyflowlauncher-0.6.2.dev0-py3-none-any.whl/pyflowlauncher/event.py
@@ -0,0 +1,29 @@
+
+
+from typing import Any, Callable
+
+
+class EventHandler:
+
+    def __init__(self):
+        self._methods = {}
+        self._handlers = {}
+
+    def add_method(self, method, *, name=None):
+        self._methods[name or method.__name__] = method
+
+    def add_methods(self, methods):
+        for method in methods:
+            self.add_method(method)
+
+    def add_exception_handler(self, exception: Exception, handler: Callable[..., Any]):
+        self._handlers[exception.__class__.__name__] = handler
+
+    def __call__(self, method, *args, **kwargs):
+        try:
+            return self._methods[method](*args, **kwargs)
+        except Exception as e:
+            handler = self._handlers.get(e.__class__.__name__, None)
+            if handler:
+                return handler(e)
+            raise e
