a388983165396a3a189151e1bb56de071b73ce58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:39 +0000

    Add package py_git_auto_version/1.1.1/py_git_auto_version-1.1.1-py3-none-any.whl

---
+++ packages/py_git_auto_version/py_git_auto_version-1.1.1-py3-none-any.whl/py_git_auto_version/get_version.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import Optional
+
+from py_git_auto_version.version_generators import generate_version_string_for_scenario
+from py_git_auto_version.settings.auto_version import PyGitAutoVersionSettings
+from py_git_auto_version.settings.github import GitHubActionEnvVars
+
+
+def for_toml():
+    github_ = GitHubActionEnvVars.generate_failover_env_then_dotenv()
+    auto_version_ = PyGitAutoVersionSettings.generate_failover_try_env_then_dotenv_then_defaults()
+    return generate_version_string_for_scenario(
+            auto_version_settings=auto_version_,
+            github_action_env_vars=github_
+    )
