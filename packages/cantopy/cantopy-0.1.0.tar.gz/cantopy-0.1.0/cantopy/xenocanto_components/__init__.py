15f4060e6a2d71d10fcd008ca933a5e319b90f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:16 +0000

    Add package cantopy/0.1.0/cantopy-0.1.0.tar.gz

---
+++ packages/cantopy/cantopy-0.1.0.tar.gz/cantopy-0.1.0/cantopy/xenocanto_components/__init__.py
@@ -0,0 +1,4 @@
+from cantopy.xenocanto_components.query import Query # type: ignore
+from cantopy.xenocanto_components.query_result import QueryResult # type: ignore
+from cantopy.xenocanto_components.recording import Recording # type: ignore
+from cantopy.xenocanto_components.result_page import ResultPage # type: ignore
