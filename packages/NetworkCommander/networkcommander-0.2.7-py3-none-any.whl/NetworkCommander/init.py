4629becac622989f9e62cddd2b384a864b13cdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:29 +0000

    Add package NetworkCommander/0.2.7/networkcommander-0.2.7-py3-none-any.whl

---
+++ packages/NetworkCommander/networkcommander-0.2.7-py3-none-any.whl/NetworkCommander/init.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+from NetworkCommander.keepass import KeepassDB, DEVICE_GROUP_NAME
+
+
+def delete_project_files(directory):
+    if not os.path.isdir(directory):
+        raise Exception(f"directory {directory} doesn't exist")
+    shutil.rmtree(directory)
+
+
+def init_program(directory, keepass_db_path):
+    if is_initialized(directory, keepass_db_path):
+        return
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    if not os.path.isfile(keepass_db_path):
+        create_new_keepass_db(keepass_db_path)
+
+
+def create_new_keepass_db(keepass_db_path, keepass_password=None):
+    with KeepassDB(keepass_db_path, keepass_password) as kp:
+        kp.add_group(kp.root_group, DEVICE_GROUP_NAME)
+
+
+def is_initialized(directory, keepass_db_path):
+    if os.path.isdir(directory):
+        return os.path.isfile(keepass_db_path)
+    return False
