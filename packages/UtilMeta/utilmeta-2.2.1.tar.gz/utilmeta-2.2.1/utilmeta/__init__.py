10d12471b1defe5333d7f2539ab0ccdb2705f62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:07 +0000

    Add package UtilMeta/2.2.1/utilmeta-2.2.1.tar.gz

---
+++ packages/UtilMeta/utilmeta-2.2.1.tar.gz/utilmeta-2.2.1/utilmeta/__init__.py
@@ -0,0 +1,8 @@
+from .core.server.service import UtilMeta
+
+service: 'UtilMeta'     # current service in this process
+
+__website__ = 'https://utilmeta.com'
+__homepage__ = 'https://utilmeta.com/py'
+__author__ = 'Xulin Zhou (voidZXL)'
+__version__ = '2.2.1'
