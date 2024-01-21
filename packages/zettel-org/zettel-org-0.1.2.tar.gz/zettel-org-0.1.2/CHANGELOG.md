8476f2b806c70440652ff8790ce6f2b26c50a821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:04:26 +0000

    Add package zettel-org/0.1.2/zettel-org-0.1.2.tar.gz

---
+++ packages/zettel-org/zettel-org-0.1.2.tar.gz/zettel-org-0.1.2/CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog for `zorg`
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog], and this project adheres to
+[Semantic Versioning].
+
+[Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
+[Semantic Versioning]: https://semver.org/
+
+
+## [Unreleased](https://github.com/bbugyi200/zorg/compare/0.1.2...HEAD)
+
+No notable changes have been made.
+
+## [0.1.2](https://github.com/bbugyi200/zorg/compare/0.1.1...0.1.2) - 2024-01-21
+
+* Add support for initial template # comments. These comments MUST be followed
+  by a blank line and are NOT included in generated files (which is the point).
+  Comments that we want to be included in generated files should use ## instead
+  of #.
+
+## [0.1.1](https://github.com/bbugyi200/zorg/compare/0.1.0...0.1.1) - 2024-01-20
+
+* Add 'day' sub-command that generates my daily logs (day, habit, and done) and
+  opens my day log in vim.
+
+## [0.1.0](https://github.com/bbugyi200/zorg/releases/tag/0.1.0) - 2024-01-15
+
+* First release.
---
+++ packages/zettel-org/zettel-org-0.1.2.tar.gz/zettel-org-0.1.2/src/zorg/runners.py
@@ -0,0 +1,115 @@
+"""Contains this project's clack runners."""
+
+from __future__ import annotations
+
+import datetime as dt
+from functools import partial
+from pathlib import Path
+import tempfile
+from typing import Iterable, Iterator, List
+
+from clack.types import ClackRunner
+import jinja2
+from logrus import Logger
+import metaman
+import vimala
+
+from .config import DayConfig
+
+
+RUNNERS: List[ClackRunner] = []
+runner = metaman.register_function_factory(RUNNERS)
+
+logger = Logger(__name__)
+
+
+@runner
+def run_day(cfg: DayConfig) -> int:
+    """Runner for the 'day' command."""
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_dir_path = Path(temp_dir.name)
+    template_loader = jinja2.FileSystemLoader(searchpath=temp_dir_path)
+    template_env = jinja2.Environment(loader=template_loader)
+    _build_template_in_dir(
+        temp_dir_path, cfg.zettel_dir / cfg.day_log_template
+    )
+    _build_template_in_dir(
+        temp_dir_path, cfg.zettel_dir / cfg.habit_log_template
+    )
+    _build_template_in_dir(
+        temp_dir_path, cfg.zettel_dir / cfg.done_log_template
+    )
+    day_log_template = template_env.get_template(cfg.day_log_template)
+    habit_log_template = template_env.get_template(cfg.habit_log_template)
+    done_log_template = template_env.get_template(cfg.done_log_template)
+
+    today = dt.datetime.now()
+    two_days_ago = today - dt.timedelta(days=2)
+    yesterday = today - dt.timedelta(days=1)
+    tomorrow = today + dt.timedelta(days=1)
+
+    day_log_vars = {
+        "two_days_ago": two_days_ago,
+        "yesterday": yesterday,
+        "today": today,
+        "tomorrow": tomorrow,
+    }
+    day_log_contents = day_log_template.render(day_log_vars)
+    done_log_contents = done_log_template.render(day_log_vars)
+    habit_log_contents = habit_log_template.render(day_log_vars)
+
+    ensureDayLogFileExists = partial(_ensureDayLogFileExists, cfg.zettel_dir)
+    day_log_path = ensureDayLogFileExists(day_log_contents, today)
+    ensureDayLogFileExists(habit_log_contents, yesterday, suffix="habit")
+    ensureDayLogFileExists(done_log_contents, today, suffix="done")
+
+    if cfg.edit_day_log:
+        vimala.vim(
+            day_log_path,
+            commands=_process_vim_commands(cfg.zettel_dir, cfg.vim_commands),
+        )
+    return 0
+
+
+def _build_template_in_dir(temp_dir: Path, template_path: Path) -> None:
+    temp_template_path = temp_dir / template_path.name
+    new_lines = []
+    blank_line_found = False
+    for line in template_path.open("r"):
+        if not blank_line_found and not line.strip():
+            blank_line_found = True
+            continue
+        if blank_line_found:
+            new_lines.append(
+                line[1:]
+                if line.startswith("## ") or line.strip() == "##"
+                else line
+            )
+    temp_template_path.write_text("".join(new_lines))
+
+
+def _ensureDayLogFileExists(
+    zettel_dir: Path, contents: str, date: dt.date, *, suffix: str = None
+) -> Path:
+    path = _get_day_path(zettel_dir, date, suffix=suffix)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    if not path.exists():
+        path.write_text(contents)
+    return path
+
+
+def _get_day_path(
+    zettel_dir: Path, date: dt.date, *, suffix: str = None
+) -> Path:
+    suffix = f"_{suffix}" if suffix else ""
+    return zettel_dir / str(date.year) / date.strftime(f"%Y%m%d{suffix}.zo")
+
+
+def _process_vim_commands(
+    zettel_dir: Path, vim_commands: Iterable[str]
+) -> Iterator[str]:
+    for vim_cmd in vim_commands:
+        if "{zdir}" in vim_cmd:
+            yield vim_cmd.format(zdir=zettel_dir)
+        else:
+            yield vim_cmd
