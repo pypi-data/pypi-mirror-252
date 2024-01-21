f4893ba4922cb326f660b824cc8d0b9d254ebfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:34 +0000

    Add package numalogic/0.6.1a9/numalogic-0.6.1a9.tar.gz

---
+++ packages/numalogic/numalogic-0.6.1a9.tar.gz/numalogic-0.6.1a9/numalogic/udfs/_config.py
@@ -0,0 +1,63 @@
+import logging
+from dataclasses import dataclass, field
+from typing import Optional
+
+from omegaconf import OmegaConf
+
+from numalogic.config import NumalogicConf, RegistryInfo
+
+from numalogic.connectors import (
+    ConnectorType,
+    RedisConf,
+    PrometheusConf,
+    DruidConf,
+)
+from numalogic.tools.exceptions import ConfigNotFoundError
+
+_logger = logging.getLogger(__name__)
+
+
+@dataclass
+class MLPipelineConf:
+    pipeline_id: str = "default"
+    metrics: list[str] = field(default_factory=list)
+    numalogic_conf: NumalogicConf = field(default_factory=lambda: NumalogicConf())
+
+
+@dataclass
+class StreamConf:
+    config_id: str = "default"
+    source: ConnectorType = ConnectorType.druid  # TODO: do not allow redis connector here
+    window_size: int = 12
+    composite_keys: list[str] = field(default_factory=list)
+    ml_pipelines: dict[str, MLPipelineConf] = field(default_factory=dict)
+
+
+@dataclass
+class PipelineConf:
+    stream_confs: dict[str, StreamConf] = field(default_factory=dict)
+    redis_conf: Optional[RedisConf] = None
+    registry_conf: Optional[RegistryInfo] = field(
+        default_factory=lambda: RegistryInfo(name="RedisRegistry", model_expiry_sec=172800)
+    )
+    prometheus_conf: Optional[PrometheusConf] = None
+    druid_conf: Optional[DruidConf] = None
+
+
+def load_pipeline_conf(*paths: str) -> PipelineConf:
+    confs = []
+    for _path in paths:
+        try:
+            conf = OmegaConf.load(_path)
+        except FileNotFoundError:
+            _logger.warning("Config file path: %s not found. Skipping...", _path)
+            continue
+        confs.append(conf)
+
+    if not confs:
+        _err_msg = f"None of the given conf paths exist: {paths}"
+        raise ConfigNotFoundError(_err_msg)
+
+    schema = OmegaConf.structured(PipelineConf)
+    conf = OmegaConf.merge(schema, *confs)
+    return OmegaConf.to_object(conf)
