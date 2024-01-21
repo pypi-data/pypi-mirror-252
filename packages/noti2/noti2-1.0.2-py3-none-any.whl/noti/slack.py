a8b624d09375ac17cd238e9d9b1ab185b228a6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:59 +0000

    Add package noti2/1.0.2/noti2-1.0.2-py3-none-any.whl

---
+++ packages/noti2/noti2-1.0.2-py3-none-any.whl/noti/slack.py
@@ -0,0 +1,43 @@
+import socket
+import time
+from contextlib import contextmanager
+
+import click
+import requests
+
+from noti.config import NotiConfig
+
+DEFAULT_MSG = "Task completed"
+
+
+class Slack:
+    def __init__(self, prefix: str = ""):
+        self.noti = NotiConfig.load()
+        self.prefix = prefix
+        if len(self.prefix) > 0:
+            self.prefix += " "
+
+    @contextmanager
+    def watch(self, msg: str = DEFAULT_MSG):
+        start = time.time()
+        try:
+            yield None
+        finally:
+            end = time.time()
+            self(f"{msg} (took {end - start:.2f}s)")
+
+    def __call__(self, msg: str = DEFAULT_MSG):
+        requests.post(self.noti.slack_webhook_url, json={"text": self.prefix + msg})
+
+
+slack = Slack(prefix=f"`{socket.gethostname()}`")
+
+
+@click.command()
+@click.argument("msg", default=DEFAULT_MSG)
+def slack_send_cmd(msg: str = DEFAULT_MSG):
+    slack(msg)
+
+
+if __name__ == "__main__":
+    slack_send_cmd()
