b8a773a571f31b839420e61bd0debac25b577cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:50 +0000

    Add package pyfracgen/0.2.0/pyfracgen-0.2.0.tar.gz

---
+++ packages/pyfracgen/pyfracgen-0.2.0.tar.gz/pyfracgen-0.2.0/pyfracgen/__init__.py
@@ -0,0 +1,23 @@
+from .buddhabrot import buddhabrot
+from .common import Canvas, Result
+from .images import images
+from .iterfuncs import funcs
+from .julia import julia
+from .lyapunov import lyapunov
+from .mandelbrot import mandelbrot
+from .randomwalk import construct_moves, randomwalk
+
+__all__ = [
+    "buddhabrot",
+    "images",
+    "julia",
+    "julia_series",
+    "lyapunov",
+    "mandelbrot",
+    "randomwalk",
+    "construct_moves",
+    "Result",
+    "Canvas",
+    "funcs",
+]
+__version__ = "0.2.0"
