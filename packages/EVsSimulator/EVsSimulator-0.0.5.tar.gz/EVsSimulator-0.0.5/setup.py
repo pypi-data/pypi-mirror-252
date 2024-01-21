532aa30a12bcda0528968221b47faca79b76a11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:38 +0000

    Add package EVsSimulator/0.0.5/EVsSimulator-0.0.5.tar.gz

---
+++ packages/EVsSimulator/EVsSimulator-0.0.5.tar.gz/EVsSimulator-0.0.5/setup.py
@@ -0,0 +1,34 @@
+# This is a setup file for the package
+
+from setuptools import find_packages
+from setuptools import setup
+
+# print(find_packages())
+# exit()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='EVsSimulator',
+    version='0.0.5',
+    description='A realistic V2X environment using gym',
+    author='Stavros Orfanoudakis',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='gym, Reinforcement Learning, V2X, EVs, EVsSimulator, Electric Vehicles, Electric Vehicle Simulator',
+    # package_dir = {"": "EVsSimulator"},
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        '': ['*.csv'],  # Include all CSV files in any directory
+        '': ['*.yaml'],  # Include all YAML files in any directory
+        '': ['*.json'],  # Include all PKL files in any directory
+    },
+    include_package_data=True,
+)
