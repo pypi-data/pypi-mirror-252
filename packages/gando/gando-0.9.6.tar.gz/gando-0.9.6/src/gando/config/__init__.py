936eb3e85ef3f4c9e847ce793baf5ce91de704ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:55 +0000

    Add package gando/0.9.6/gando-0.9.6.tar.gz

---
+++ packages/gando/gando-0.9.6.tar.gz/gando-0.9.6/src/gando/config/__init__.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel as Base
+from functools import lru_cache
+
+from django.conf import settings
+
+
+class Gando(Base):
+    MONITOR_KEYS: list = list()
+    DEBUG: bool = True
+    CACHING: bool = False
+    MESSAGES_RESPONSE_DISPLAYED: bool = True
+    MONITOR: dict = dict()
+
+
+@lru_cache()
+def __get_settings():
+    try:
+        input_conf = settings.GANDO
+    except:
+        input_conf = {}
+    try:
+        input_conf['DEBUG'] = settings.DEBUG
+    except:
+        pass
+    return Gando(**input_conf)
+
+
+SETTINGS = __get_settings()
