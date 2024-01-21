c66e089f722986f6494559d6d5672d44282aa959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:11 +0000

    Add package echarts-china-misc-pypkgkg/0.0.1/echarts-china-misc-pypkgkg-0.0.1.tar.gz

---
+++ packages/echarts-china-misc-pypkgkg/echarts-china-misc-pypkgkg-0.0.1.tar.gz/echarts-china-misc-pypkgkg-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='echarts-china-misc-pypkgkg', #package name
+      version='0.0.1',
+      description='Python Package By jordin',
+      author='jordin',
+      license='MIT',
+      zip_safe=False
+      )
