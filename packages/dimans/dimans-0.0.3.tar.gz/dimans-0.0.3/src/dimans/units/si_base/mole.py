7a625cc0a2c8f08b53fcec9e270e0bc415a60a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:32 +0000

    Add package dimans/0.0.3/dimans-0.0.3.tar.gz

---
+++ packages/dimans/dimans-0.0.3.tar.gz/dimans-0.0.3/src/dimans/units/si_base/mole.py
@@ -0,0 +1,33 @@
+from fractions import Fraction as _Fraction
+
+from ... import BaseUnit as _BaseUnit
+from ..metric_utils import make_metric_units as _make_metric_units
+from ...dimension import dimensions as _dimensions
+
+mole = _BaseUnit("mol", _dimensions["amount of substance"], _Fraction(1))
+
+(
+    quettamole,
+    yottamole,
+    zettamole,
+    examole,
+    petamole,
+    teramole,
+    gigamole,
+    megamole,
+    kilomole,
+    hectomole,
+    decamole,
+    decimole,
+    centimole,
+    millimole,
+    micromole,
+    nanomole,
+    picomole,
+    femtomole,
+    attomole,
+    zeptomole,
+    yoctomole,
+    rontomole,
+    quectomole,
+) = _make_metric_units(mole)
