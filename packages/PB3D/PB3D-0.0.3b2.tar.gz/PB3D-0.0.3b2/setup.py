7bc807d51f5f02bbf31523e548aa948464ba78bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:04 +0000

    Add package PB3D/0.0.3b2/PB3D-0.0.3b2.tar.gz

---
+++ packages/PB3D/PB3D-0.0.3b2.tar.gz/PB3D-0.0.3b2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="PB3D",
+    version="0.0.3b2",
+    author="InvincibleSiwoo",
+    author_email="siukk@gmail.com",
+    description="A small engine",
+    long_description="""# PB3D
+                        This is pre-test of engine.
+                        Thank you for your interest!""",
+    long_description_content_type="text/markdown",
+    url="https://github.com/InvincibleSiwoo/engine/tree/master",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
