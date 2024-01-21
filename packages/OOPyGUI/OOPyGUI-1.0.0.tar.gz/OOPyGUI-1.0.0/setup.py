7e7100deef7665eac743a5a48c4a5ad7ec6999a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:10 +0000

    Add package OOPyGUI/1.0.0/OOPyGUI-1.0.0.tar.gz

---
+++ packages/OOPyGUI/OOPyGUI-1.0.0.tar.gz/OOPyGUI-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="OOPyGUI",
+    version="1.0.0",
+    author="artandfi (Artem Fisunenko)",
+    author_email="artyom.fisunenko@gmail.com",
+    description="OOPyGUI is an object-oriented wrapper for DearPyGUI (https://github.com/hoffstadt/DearPyGui).",
+    packages=find_packages(),
+    install_requires=[line.strip() for line in open("requirements.txt", "r").readlines()],
+    keywords=[
+        "Python3",
+        "GUI"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
