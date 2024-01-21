2f6cb42e4569cc0b7d9804e008bc4bf794658e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:39 +0000

    Add package octoflow/0.0.11/octoflow-0.0.11-py3-none-any.whl

---
+++ packages/octoflow/octoflow-0.0.11-py3-none-any.whl/octoflow/__init__.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from octoflow import logging
+from octoflow.config import Config
+from octoflow.core.plugins import Package
+from octoflow.tracking import Client, Experiment, Run, Value
+
+default_plugins_package: Optional[Package]
+try:
+    from octoflow_plugins import package as default_plugins_package  # type: ignore
+except ImportError:
+    default_plugins_package = None
+
+__version__ = "0.0.11"
+
+__all__ = [
+    "Client",
+    "Experiment",
+    "Run",
+    "Value",
+    "Config",
+    "LoggingFactory",
+    "logging",
+]
+
+
+if default_plugins_package is not None:
+    default_plugins_package.import_modules()
+
+config = Config({
+    "resources": {
+        "path": "~/.octoflow",
+    },
+    "logging": {
+        "level": "INFO",
+        "format": "%(asctime)s %(levelname)s %(name)s %(message)s",
+    },
+})
+
+
+logger = logging.get_logger(
+    name=next(iter(__package__.split("."))),
+    level=config.logging.level,
+    formatter=config.logging.format,
+)
---
+++ packages/octoflow/octoflow-0.0.11-py3-none-any.whl/octoflow/data/__init__.py
@@ -0,0 +1,7 @@
+from octoflow.data.compute import Expression
+from octoflow.data.dataset import Dataset
+
+__all__ = [
+    "Dataset",
+    "Expression",
+]
