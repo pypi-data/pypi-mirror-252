c11cbeddfbacf0108beaf311a08b029a6853a451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:16 +0000

    Add package topsis-aaryan-102103053/0.2/topsis-aaryan-102103053-0.2.tar.gz

---
+++ packages/topsis-aaryan-102103053/topsis-aaryan-102103053-0.2.tar.gz/topsis-aaryan-102103053-0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'topsis-aaryan-102103053',         # How you named your package folder (MyLib)
+  packages = ['topsis-aaryan-102103053'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Library for dealing with Multiple Criteria Decision Making (MCDM) problems by applying Technique for Order of Preference by Similarity to Ideal Solution (TOPSIS).',   # Give a short description about your library
+  author = 'Aaryan Gupta',                   # Type in your name
+  author_email = 'aaryang991@gmail.com',      # Type in your E-Mail
+  url = '',   # Provide either the link to your github or to your website
+  download_url = '',    # I explain this later on
+  keywords = ['TOPSIS', 'MCDM'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+  ],
+)
