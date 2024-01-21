c7ee730a445e001fcd95294851e3d6ec50a83459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:24 +0000

    Add package cli-tool-audit/3.0.1/cli_tool_audit-3.0.1-py3-none-any.whl

---
+++ packages/cli-tool-audit/cli_tool_audit-3.0.1-py3-none-any.whl/cli_tool_audit/__about__.py
@@ -0,0 +1,24 @@
+"""Metadata for cli_tool_audit.
+"""
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__description__",
+    "__author__",
+    "__author_email__",
+    "__keywords__",
+    "__status__",
+    "__license__",
+    "__homepage__",
+]
+
+__title__ = "cli_tool_audit"
+__version__ = "3.0.1"
+__description__ = "Audit for existence and version number of cli tools."
+__author__ = "Matthew Martin"
+__author_email__ = "matthewdeanmartin@gmail.com"
+__keywords__ = ["cli tooling", "version numbers"]
+__status__ = "4 - Beta"
+__license__ = "MIT"
+__homepage__ = "https://github.com/matthewdeanmartin/cli_tool_audit"
