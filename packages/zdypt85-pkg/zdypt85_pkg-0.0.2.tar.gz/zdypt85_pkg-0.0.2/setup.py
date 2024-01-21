a8dd0d8a89f0ac72bb91c9c4e3698230ddaf9045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:57 +0000

    Add package zdypt85-pkg/0.0.2/zdypt85_pkg-0.0.2.tar.gz

---
+++ packages/zdypt85-pkg/zdypt85_pkg-0.0.2.tar.gz/zdypt85_pkg-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='zdypt85_pkg',
+    version='0.0.1',
+    author='ZDPYT85',
+    packages=find_packages(),
+    include_package_data=True,
+    description="Test zdpyt85 package"
+)
