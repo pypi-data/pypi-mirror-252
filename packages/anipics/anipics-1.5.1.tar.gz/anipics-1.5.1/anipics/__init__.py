21cf7af46402a8ec7fa03ca83a87cdc24a7b3f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:24 +0000

    Add package anipics/1.5.1/anipics-1.5.1.tar.gz

---
+++ packages/anipics/anipics-1.5.1.tar.gz/anipics-1.5.1/anipics/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Daniel <D4n13l3k00@yandex.ru>.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+__author__ = "Daniel <D4n13l3k00@yandex.ru>"
+__version__ = "1.5.1"
+
+__all__ = ["Models", "AniPics", "NekosLife"]
+
+from .models import Models
+from .services import AniPics, NekosLife
