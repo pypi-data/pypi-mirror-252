3891c53037d0478727eab6390f25b9d18864dc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:12 +0000

    Add package glhmm/0.2/glhmm-0.2.tar.gz

---
+++ packages/glhmm/glhmm-0.2.tar.gz/glhmm-0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='glhmm',
+    version='0.2',
+    description='Gaussian Linear Hidden Markov Model',
+    url='https://github.com/vidaurre/glhmm',
+    author='Diego Vidaurre',
+    author_email = "dvidaurre@cfin.au.dk",
+    readme = "README.md",
+    install_requires=['scipy','numpy','scikit-learn','matplotlib','numba','seaborn', 'pandas','igraph'],
+    packages=["glhmm"],
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3"]
+    )
+
