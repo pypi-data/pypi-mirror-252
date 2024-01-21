3a949dc09526c03907c56746cc499cd254445f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:50 +0000

    Add package dimans/0.0.10/dimans-0.0.10.tar.gz

---
+++ packages/dimans/dimans-0.0.10.tar.gz/dimans-0.0.10/src/dimans/units/metric/electron_volt.py
@@ -0,0 +1,38 @@
+from fractions import Fraction as _Fraction
+
+from ..si_derived.joule import (joule as _joule)
+from ..metric_utils import make_metric_units as _make_metric_units
+
+
+electron_volt = (
+    _Fraction(
+        1_602_176_634,
+        1_000_000_000 * 10 ** 19
+    ) * _joule
+).as_derived_unit("eV")
+
+(
+    quettaelectron_volt,
+    yottaelectron_volt,
+    zettaelectron_volt,
+    exaelectron_volt,
+    petaelectron_volt,
+    teraelectron_volt,
+    gigaelectron_volt,
+    megaelectron_volt,
+    kiloelectron_volt,
+    hectoelectron_volt,
+    decaelectron_volt,
+    decielectron_volt,
+    centielectron_volt,
+    millielectron_volt,
+    microelectron_volt,
+    nanoelectron_volt,
+    picoelectron_volt,
+    femtoelectron_volt,
+    attoelectron_volt,
+    zeptoelectron_volt,
+    yoctoelectron_volt,
+    rontoelectron_volt,
+    quectoelectron_volt,
+) = _make_metric_units(electron_volt)
