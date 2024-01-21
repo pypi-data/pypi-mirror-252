9f7c5c5cb403c422f5866e907c8ee99d37220360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:52 +0000

    Add package doot/0.4.0/doot-0.4.0.tar.gz

---
+++ packages/doot/doot-0.4.0.tar.gz/doot-0.4.0/doot/actions/control_flow.py
@@ -0,0 +1,112 @@
+## base_action.py -*- mode: python -*-
+"""
+Actions for task control flow.
+ie: Early exit from a task if a file exists
+"""
+##-- imports
+from __future__ import annotations
+
+# import abc
+import datetime
+# import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+# from copy import deepcopy
+# from dataclasses import InitVar, dataclass, field
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
+                    Iterable, Iterator, Mapping, Match, MutableMapping,
+                    Protocol, Sequence, Tuple, TypeAlias, TypeGuard, TypeVar,
+                    cast, final, overload, runtime_checkable)
+# from uuid import UUID, uuid1
+# from weakref import ref
+
+##-- end imports
+
+printer = logmod.getLogger("doot._printer")
+
+from time import sleep
+import sh
+import shutil
+import doot
+from doot.errors import DootTaskError, DootTaskFailed
+from doot._abstract import Action_p
+from doot.mixins.importer import ImporterMixin
+from doot.enums import ActionResponseEnum as ActRE
+from doot.structs import DootKey, DootCodeReference
+
+##-- expansion keys
+MSG          : Final[DootKey] = DootKey.make("msg")
+OLD          : Final[DootKey] = DootKey.make("old")
+NEW          : Final[DootKey] = DootKey.make("new")
+LEVEL        : Final[DootKey] = DootKey.make("level")
+PRED         : Final[DootKey] = DootKey.make("pred")
+FILE_TARGET  : Final[DootKey] = DootKey.make("file")
+
+##-- end expansion keys
+
+@doot.check_protocol
+class CancelOnPredicateAction(Action_p):
+    """
+      Get a predicate using the kwarg `pred`,
+      call it with the action spec and task state.
+      return its result for the task runner to handle
+
+    """
+    _toml_kwargs = [PRED]
+
+    def __call__(self, spec, task_state:dict) -> dict|bool|None:
+        val       = PRED.expand(spec, state)
+        ref       = DootCodeReference.from_str(val)
+        predicate = ref.try_import()
+        return predicate(spec,task_state)
+
+@doot.check_protocol
+class SkipIfFileExists(Action_p):
+
+    _toml_kwargs = ["args"]
+
+    def __call__(self, spec, state:dict) -> dict|bool|None:
+        for arg in spec.args:
+            key = DootKey.make(arg, explicit=True)
+            path = key.to_path(spec, state, on_fail=None)
+            if path and path.exists():
+                printer.info("Target Exists: %s", path)
+                return ActRE.SKIP
+
+@doot.check_protocol
+class LogAction(Action_p):
+
+    _toml_kwargs = [MSG, LEVEL]
+
+    def __call__(self, spec, task_state):
+        level_name   = LEVEL.to_type(spec, task_state, type_=str|None) or "INFO"
+        level        = logmod.getLevelName(level_name)
+        msg          = MSG.expand(spec, task_state, rec=True)
+        printer.log(level, "%s", msg)
+
+@doot.check_protocol
+class StalenessCheck(Action_p):
+    _toml_kwargs = [OLD, NEW]
+
+    def __call__(self, spec, task_state:dict) -> dict|bool|None:
+        old_loc = OLD.to_path(spec, task_state)
+        new_loc = NEW.to_path(spec, task_state)
+
+        if new_loc.exists() and old_loc.stat().st_mtime_ns <= new_loc.stat().st_mtime_ns:
+            return ActRE.SKIP
+
+@doot.check_protocol
+class AssertInstalled:
+    """
+    Easily check a program can be found and used
+    """
+    _toml_kwargs = []
+
+    def __call__(self, spec, task_state:dict) -> dict|bool|None:
+        raise NotImplementedError()
+        return ActRE.FAIL
---
+++ packages/doot/doot-0.4.0.tar.gz/doot-0.4.0/doot/actions/shell.py
@@ -0,0 +1,108 @@
+##-- imports
+from __future__ import annotations
+import logging as logmod
+##-- end imports
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+printer = logmod.getLogger("doot._printer")
+
+import sys
+import sh
+import doot
+from doot.errors import DootTaskError
+from doot._abstract import Action_p
+from doot.actions.base_action import DootBaseAction
+from doot.structs import DootKey
+
+BACKGROUND = DootKey.make("background")
+
+@doot.check_protocol
+class DootShellAction(Action_p):
+    """
+    For actions in subshells.
+    all other arguments are passed directly to the program, using `sh`
+
+    The arguments of the action are held in spec
+
+    TODO : handle shell output redirection, and error code ignoring (use action spec dict)
+    """
+    _toml_kwargs = [BACKGROUND]
+
+    def __call__(self, spec, state:dict) -> dict|bool|None:
+        result = None
+        try:
+            cmd      = getattr(sh, DootKey.make(spec.args[0], explicit=True).expand(spec, state))
+            args     = spec.args[1:]
+            keys     = [DootKey.make(x, explicit=True) for x in args]
+            expanded = [x.expand(spec, state) for x in keys]
+            result   = cmd(*expanded, _return_cmd=True, _bg=spec.kwargs.on_fail(False, bool).background())
+            assert(result.exit_code == 0)
+            printer.debug("(%s) Shell Cmd: %s, Args: %s, Result:", result.exit_code, spec.args[0], spec.args[1:])
+            printer.info("%s", result, extra={"colour":"reset"})
+            return True
+        except sh.CommandNotFound as err:
+            printer.error("Shell Commmand '%s' Not Action: %s", err.args[0], spec.args)
+            return False
+        except sh.ErrorReturnCode as err:
+            printer.error("Shell Command '%s' exited with code: %s", err.full_cmd, err.exit_code)
+            if bool(err.stdout):
+                printer.error("%s", err.stdout.decode())
+
+            printer.info("")
+            if bool(err.stderr):
+                printer.error("%s", err.stderr.decode())
+
+            return False
+
+@doot.check_protocol
+class DootInteractiveAction(Action_p):
+    """
+      An interactive command, which uses the self.interact method as a callback for sh.
+    """
+    _toml_args = ["background"]
+    aggregated = ""
+    prompt     = ">>> "
+    cont       = "... "
+
+    def __call__(self, spec, state:dict) -> dict|bool|None:
+        try:
+            self.prompt = spec.kwargs.on_fail(DootInteractiveAction.prompt, str).prompt()
+            self.cont   = spec.kwargs.on_fail(DootInteractiveAction.cont, str).cont()
+
+            cmd      = getattr(sh, spec.args[0])
+            args     = spec.args[1:]
+            keys     = [DootKey.make(x, explicit=True) for x in args]
+            expanded = [x.expand(spec, state) for x in keys]
+            result   = cmd(*expanded, _return_cmd=True, _bg=spec.kwargs.on_fail(False, bool).background(), _out=self.interact, _out_bufsize=0, _tty_in=True, _unify_ttys=True)
+            assert(result.exit_code == 0)
+            printer.debug("(%s) Shell Cmd: %s, Args: %s, Result:", result.exit_code, spec.args[0], spec.args[1:])
+            printer.info("%s", result, extra={"colour":"reset"})
+            return True
+        except sh.CommandNotFound as err:
+            printer.error("Shell Commmand '%s' Not Action: %s", err.args[0], spec.args)
+            return False
+        except sh.ErrorReturnCode:
+            printer.error("Shell Command '%s' exited with code: %s for args: %s", spec[0], result.exit_code, spec.args)
+            return False
+
+    def interact(self, char, stdin):
+        # TODO possibly add a custom interupt handler
+        self.aggregated += str(char)
+        if self.aggregated.endswith("\n"):
+            printer.info(self.aggregated.strip())
+            self.aggregated = ""
+
+        if self.aggregated.startswith(self.prompt) :
+            prompt = self.aggregated[:] + ": "
+            self.aggregated = ""
+            stdin.put(input(prompt) + "\n")
+        elif self.aggregated.startswith(self.cont):
+            self.aggregated = ""
+            val = input(self.cont)
+            if bool(val):
+                stdin.put("    " + input(self.cont) + "\n")
+            else:
+                stdin.put(input(self.cont) + "\n")
