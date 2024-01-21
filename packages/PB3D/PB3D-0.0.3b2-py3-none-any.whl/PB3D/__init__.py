94ea54c9723e03128e8b01b659ccd62b72338ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:04 +0000

    Add package PB3D/0.0.3b2/PB3D-0.0.3b2-py3-none-any.whl

---
+++ packages/PB3D/PB3D-0.0.3b2-py3-none-any.whl/PB3D/__init__.py
@@ -0,0 +1,6 @@
+from .window import init, init_2d, loop, Event, turn
+from .shape import Shape, mouse_pos, entities, Shape2d, Light
+from .event import button_dict, event_get
+from .math import Vec3, HSV, RGB, Vec4
+from .texture import Texture
+from .camera import Camera
