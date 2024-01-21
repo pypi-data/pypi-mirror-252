0af2f2dd606218aa8dd5a809b7a3d94536ea69e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:04:28 +0000

    Add package eredesscraper/0.1.9/eredesscraper-0.1.9-py3-none-any.whl

---
+++ packages/eredesscraper/eredesscraper-0.1.9-py3-none-any.whl/README.md
@@ -0,0 +1,117 @@
+# E-REDES Scraper
+## Description
+This is a web scraper that collects data from the E-REDES website and can upload it to a database.
+Since there is no exposed interface to the data, this web scraper was developed as approach to collect it programatically.
+A high-level of the process is:
+1. The scraper collects the data from the E-REDES website.
+2. A file with the energy consumption readings is downloaded.
+3. [ Optional ] The file is parsed and the data is uploaded to the selected database. 
+4. [ Optional ] A feature supporting only the insertion of "deltas" is available.
+
+> This package supports E-REDES website available at time of writing 23/10/2023. 
+> The entrypoint for the scraper is the page https://balcaodigital.e-redes.pt/login.
+
+## Installation
+The package can be installed using pip:
+```bash
+pip install eredesscraper
+```
+
+## Configuration
+Usage is based on a YAML configuration file.  
+`config.yml` holds the credentials for the E-REDES website and 
+the database connection. Currently, **only InfluxDB is supported** as a database sink.  
+
+### Template `config.yml`:
+```yaml
+eredes:
+  # eredes credentials
+  nif: <my-eredes-nif>
+  pwd: <my-eredes-password>
+  # CPE to monitor. e.g. PT00############04TW (where # is a digit). CPE can be found in your bill details
+  cpe: PT00############04TW
+
+
+influxdb:
+  # url to InfluxDB.  e.g. http://localhost or https://influxdb.my-domain.com
+  host: http://localhost
+  # default port is 8086
+  port: 8086
+  bucket: <my-influx-bucket>
+  org: <my-influx-org>
+  # access token with write access
+  token: <token>
+```
+
+## Usage
+### CLI:
+```bash
+ers config load "/path/to/config.yml"
+
+# get current month readings
+ers run -d influxdb
+
+# get only deltas from last month readings 
+ers run -w previous -d influxdb --delta
+
+# get readings from May
+ers run -w select -d influxdb -m 5
+```
+
+### Docker:
+```bash
+# get readings from May
+
+# docker args
+docker run --rm -v config.yml:/config.yml \
+  # latest `ers` image
+  ghcr.io/rf-santos/eredesscraper:latest \
+  # calling `ers` 
+  ers run -w current -d influxdb
+```
+
+### Python:
+
+```python
+from eredesscraper.workflows import switchboard
+from pathlib import Path
+
+# get deltas from current month readings
+switchboard(name="current",
+            db="influxdb",
+            config_path=Path("./config.yml")y
+            delta=True)
+
+# get readings from May 2023
+switchboard(name="select",
+            db="influxdb",
+            config_path=Path("./config.yml"),
+            month=5,
+            year=2023)
+```
+
+## Features
+### Available workflows:
+- `current`: Collects the current month consumption.
+- `previous`: Collects the previous month consumption data.
+- `select`: Collects the consumption data from an arbitrary month parsed by the user.
+
+### Available databases:
+- `influxdb`: Loads the data in an InfluxDB database. (https://docs.influxdata.com/influxdb/v2/get-started/)
+
+## Roadmap
+- [X] ~~Add workflow for retrieving previous month data.~~
+- [X] ~~Add workflow for retrieving data form an arbitrary month.~~
+- [X] ~~Build CLI~~.
+- [ ] Containerize app.
+- [ ] Documentation.
+- [ ] Add CI/CD.
+- [ ] Add logging.
+- [ ] Add tests.
+- [ ] Add runtime support for multiple CPEs.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+See [LICENSE](LICENSE) file.
---
+++ packages/eredesscraper/eredesscraper-0.1.9-py3-none-any.whl/eredesscraper/cli.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+from typing import Optional
+
+import typer
+import yaml
+
+from eredesscraper.utils import parse_config, flatten_config, struct_config, infer_type
+from eredesscraper.workflows import switchboard
+from eredesscraper.meta import cli_header, supported_workflows, supported_databases
+from eredesscraper._version import get_version
+
+appdir = typer.get_app_dir(app_name="ers")
+config_path = Path(appdir) / "cache" / "config.yml"
+
+app = typer.Typer(name="ers",
+                  help=cli_header,
+                  add_completion=False,
+                  add_help_option=True,
+                  no_args_is_help=True,
+                  epilog="For more information, please visit https://github.com/rf-santos/eredes-scraper",
+                  context_settings={"allow_extra_args": True})
+
+@app.command(help="Show the current version")
+def version():
+    """Show the current version"""
+    typer.echo(f"Version: {get_version()}")
+
+# TODO: Preform an initial step to guide users through the setup process
+# @app.command(help="Initialize the program with a CLI wizard")
+# def init():
+#     """Initialize the program with a CLI wizard"""
+#
+#     typer.echo("Welcome to E-REDES Scraper!")
+#     typer.echo("Please follow the instructions to set up the program.")
+#     typer.echo("Press Ctrl+C at any time to exit the wizard.")
+#     typer.echo("")
+#     typer.echo("First, we need to set up the config file. "
+#                "See the template `config.yml` at github.com/rf-santos/eredes-scraper for reference.")
+#     typer.echo("")
+#     typer.echo("Please enter the path to the config file:")
+#     config_path = typer.prompt("Path to config file", default="config.yml")
+#     assert Path(config_path).exists(), "File not found. Please check the path and try again."
+#     assert Path(config_path).is_file(), "Please enter a valid file path."
+#     load(config_path)
+#     typer.echo("Config file loaded successfully.")
+
+
+@app.command(help="Run the scraper workflow. Can directly load data onto supported databases.")
+def run(workflow: str = typer.Option("current",
+                                     "--workflow", "-w",
+                                     help=f"Specify one of the supported workflows: {supported_workflows}"),
+        db: str = typer.Option(None,
+                               "--database", "--db", "-d",
+                               help=f"Specify one of the supported databases: {supported_databases}"),
+        month: Optional[int] = typer.Option(None,
+                                            "--month", "-m",
+                                            help="Specify the month to load (1-12). "
+                                                 "[Required for `select` workflow]",
+                                            show_default=False),
+        year: Optional[int] = typer.Option(None, "--year", "-y",
+                                           help="Specify the year to load (YYYY). "
+                                                "[Required for `select` workflow]",
+                                           show_default=False),
+        delta: Optional[bool] = typer.Option(False,
+                                             "--delta", "-D",
+                                             help="Load only the most recent data points")):
+    """Run a workflow from a config file"""
+    config = Path(appdir) / "cache" / "config.yml"
+    assert Path(
+        config).exists(), f"Config file not found. "
+    f"Run {typer.style('ers config load </path/to/config.yml>', fg=typer.colors.GREEN)} to load it."
+
+    if db is None:
+        db = ""
+
+    switchboard(
+        config_path=config.resolve(),
+        name=workflow,
+        db=db,
+        month=month,
+        year=year,
+        delta=delta
+    )
+
+
+config_app = typer.Typer(name="config",
+                         help="E-REDES Scraper CLI configuration",
+                         add_completion=False,
+                         add_help_option=True,
+                         no_args_is_help=True)
+
+app.add_typer(config_app, name="config", help="Interact with the program configuration")
+
+
+@config_app.command(help="Loads a config file into the program")
+def load(config: str = typer.Argument(resolve_path=True,
+                                      exists=True,
+                                      file_okay=True,
+                                      dir_okay=False,
+                                      writable=True,
+                                      readable=True,
+                                      help="Path to the config file"
+                                      )
+         ):
+    try:
+        config_path = Path(config).resolve()
+
+        Path.mkdir(Path(appdir) / "cache", parents=True, exist_ok=True)
+
+        cache = Path(appdir) / "cache"
+
+        typer.echo(f"⚙️\tLoading configuration from file: {config_path}")
+
+        with open(config_path, "r") as f:
+            tmp_config = yaml.safe_load(f)
+
+        with open(cache / "config.yml", "w") as f:
+            yaml.dump(tmp_config, f)
+
+        typer.echo("✅\tConfig file loaded successfully.")
+
+    except FileNotFoundError:
+        typer.echo("💥\tFile not found. Please check the path and try again.")
+        raise typer.Exit(code=1)
+
+
+@config_app.command(help="Show the current configuration")
+def show():
+    """Show the current configuration"""
+    try:
+        config = parse_config(Path(appdir) / "cache" / "config.yml")
+        typer.echo("")
+        typer.echo("⚙️\tCurrent configuration (cached):")
+        typer.echo("")
+        typer.echo(yaml.dump(config), color=True)
+    except FileNotFoundError:
+        typer.echo("No config file found. Please run `ers config load /path/to/config.yml` to set a config file.")
+        raise typer.Exit(code=1)
+
+
+@config_app.command(help="Set a configuration value.\nUsage: ers config set <key>[.<subkey>] <value>",
+                    options_metavar="<KEY>[.<SUBKEY>] <VALUE>",
+                    no_args_is_help=True,
+                    epilog="Example: ers config set influxdb.host \"http://localhost\"")
+def set(key: str, value: str):
+    """
+    Set a configuration value
+
+    This command is not idempotent, meaning that it will overwrite the current value of the key in the cached config.
+
+    Infers the type of the value.
+    """
+    value = infer_type(value)
+
+    try:
+        config = flatten_config(parse_config(Path(appdir) / "cache" / "config.yml"))
+    except FileNotFoundError:
+        typer.echo("No config file found. Please run `ers config file </path/to/config.yml>` to set a config file.")
+        raise typer.Exit(code=1)
+
+    assert key in config.keys(), f"Unsupported key {key} not found in config file.\nKeys: {list(config.keys())}"
+
+    config[key] = value
+
+    with open(Path(appdir) / "cache" / "config.yml", "w") as f:
+        yaml.dump(struct_config(config), f)
+
+    typer.echo(f"✅\tKey {typer.style(key, fg=typer.colors.GREEN)} set to {typer.style(value, fg=typer.colors.GREEN)}.")
+
+
+if __name__ == "__main__":
+    app()
