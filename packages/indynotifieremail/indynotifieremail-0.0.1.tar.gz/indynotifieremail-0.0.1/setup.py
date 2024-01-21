ca326df8ecede6952db4a7fd4678f0ae8c265126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:11 +0000

    Add package indynotifieremail/0.0.1/indynotifieremail-0.0.1.tar.gz

---
+++ packages/indynotifieremail/indynotifieremail-0.0.1.tar.gz/indynotifieremail-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='indynotifieremail', #package name
+      version='0.0.1',
+      description='Python Package By jordin',
+      author='jordin',
+      license='MIT',
+      zip_safe=False
+      )
