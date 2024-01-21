2e2a985b061f67936ec09df85febbfb5f693c1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:03:34 +0000

    Add package smtp2paperless/0.0.1/smtp2paperless-0.0.1.tar.gz

---
+++ packages/smtp2paperless/smtp2paperless-0.0.1.tar.gz/smtp2paperless-0.0.1/flake.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 Matthias Riße <matthias.risze@t-online.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+{
+  description = "A bridge between SMTP and the Paperless API";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ (python3.withPackages (ps: with ps; [ pip build ])) reuse ruff ];
+            shellHook = ''
+              # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+              # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+              export PIP_PREFIX=$(pwd)/_build/pip_packages
+              export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+              export PATH="$PIP_PREFIX/bin:$PATH"
+              unset SOURCE_DATE_EPOCH
+            '';
+          };
+        });
+    };
+}
