9dc2630d92a8528daa61def02b6c9924ed4c7432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 15:03:28 +0000

    Add package qingping-ble/0.10.0/qingping_ble-0.10.0-py3-none-any.whl

---
+++ packages/qingping-ble/qingping_ble-0.10.0-py3-none-any.whl/qingping_ble/__init__.py
@@ -0,0 +1,32 @@
+"""Parser for Qingping BLE advertisements."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import QingpingBluetoothDeviceData
+
+__version__ = "0.10.0"
+
+__all__ = [
+    "QingpingBluetoothDeviceData",
+    "BinarySensorDeviceClass",
+    "BinarySensorValue",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
