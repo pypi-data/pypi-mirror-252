fefd2660fc863d840df867b41101dafb0ca24ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:00 +0000

    Add package quool/4.1.6/quool-4.1.6-py3-none-any.whl

---
+++ packages/quool/quool-4.1.6-py3-none-any.whl/quool/core/__init__.py
@@ -0,0 +1,22 @@
+from .backtrade import (
+    Strategy,
+    Indicator,
+    Analyzer,
+    Observer
+)
+
+from .request import (
+    Request
+)
+
+from .table import (
+    Table
+)
+
+from .util import (
+    Logger,
+    parse_commastr,
+    reduce_mem_usage
+)
+
+__version__ = "0.1.3"
