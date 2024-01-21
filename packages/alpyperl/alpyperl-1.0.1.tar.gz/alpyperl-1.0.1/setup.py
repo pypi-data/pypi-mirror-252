0d423afa6b392e2c9a5a7aebae21e8d9545e4d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:23 +0000

    Add package alpyperl/1.0.1/alpyperl-1.0.1.tar.gz

---
+++ packages/alpyperl/alpyperl-1.0.1.tar.gz/alpyperl-1.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='alpyperl',
+      version='1.0.1',
+      license='Apache License, Version 2.0',
+      author='Marc Escandell Mari',
+      author_email='marcescandellmari@gmail.com',
+      description='An open source library for connecting AnyLogic models with Reinforcement Learning frameworks through OpenAI Gymnasium ',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      python_requires='>=3.9, <3.11',
+      packages=find_packages(),
+      url='https://github.com/MarcEscandell/ALPypeRL',
+      keywords='alpyperl',
+      install_requires=[
+            'gymnasium>=0.28',
+            'py4j',
+            'ray>=2.9',
+            'ray[rllib]>=2.3.0',
+            'tensorflow',
+            'torch',
+            'uvicorn',
+            'fastapi'
+      ],
+      extras_require={
+            'docs': [
+                'sphinx_rtd_theme'
+            ]
+        }
+)
