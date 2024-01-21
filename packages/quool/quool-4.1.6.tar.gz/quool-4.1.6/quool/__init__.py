8fa758240d18b919e4aa57f7e3d972e8eb530aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:00 +0000

    Add package quool/4.1.6/quool-4.1.6.tar.gz

---
+++ packages/quool/quool-4.1.6.tar.gz/quool-4.1.6/quool/__init__.py
@@ -0,0 +1,31 @@
+from .core import (
+    Strategy,
+    Indicator,
+    Analyzer,
+    Observer,
+    Request,
+    Table,
+    Logger,
+    parse_commastr,
+    reduce_mem_usage,
+)
+
+from .backtrade import (
+    reweight_strategy,
+    rebalance_strategy,
+    RebalanceStrategy,
+    TradeOrderRecorder,
+    CashValueRecorder,
+    Cerebro,
+)
+
+from .request import (
+    WeChat,
+)
+from .table import (
+    FrameTable,
+    PanelTable,
+)
+
+
+__version__ = "4.1.6"
