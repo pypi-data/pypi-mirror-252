367d1d3962636a38d24bbd95603808c841eebf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:56 +0000

    Add package wordtm/0.3.4/wordtm-0.3.4-py3-none-any.whl

---
+++ packages/wordtm/wordtm-0.3.4-py3-none-any.whl/wordtm/viz.py
@@ -0,0 +1,54 @@
+﻿# viz.py
+#    
+# WordTM: Show a wordcloud for a precribed range of Scripture
+#
+# Copyright (C) 2022-2023 WordTM Project
+# Author: Johnny Cheng <drjohnnycheng@gmail.com>
+# Updated: 21 Dec. 2023
+#
+# URL:  nil
+# For license information, see LICENSE.TXT
+
+import numpy as np
+from importlib_resources import files
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from PIL import Image
+
+from wordtm import util
+
+
+def plot_cloud(wordcloud):
+    """Plot the prepared wordcloud"""
+    plt.figure(figsize=(15, 10))
+    plt.imshow(wordcloud) 
+    plt.axis("off");
+
+
+def show_wordcloud(text, image='heart.jpg', mask=None):
+    """Prepare and show a wordcloud"""
+    if image:
+        img_file = files('wordtm.images').joinpath(image)
+        mask = np.array(Image.open(img_file))
+
+    wordcloud = WordCloud(background_color='black', colormap='Set2', mask=mask) \
+                    .generate(text)
+
+    plot_cloud(wordcloud)
+
+
+def chi_wordcloud(df, image='heart.jpg', mask=None):
+    """Prepare and show a Chinese wordcloud"""
+    util.set_lang('chi')
+    diction = util.get_diction(df)
+
+    if image:
+        img_file = files('wordtm.images').joinpath(image)
+        mask = np.array(Image.open(img_file))
+
+    font = 'msyh.ttc'
+    wordcloud = WordCloud(background_color='black', colormap='Set2', 
+                          mask=mask, font_path=font) \
+                    .generate_from_frequencies(frequencies=diction)
+
+    plot_cloud(wordcloud)
