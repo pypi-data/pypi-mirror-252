0db4bd387ac2466c5d4477a7712035404a694551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 03:04:02 +0000

    Add package EntityExtractor/0.4/EntityExtractor-0.4.tar.gz

---
+++ packages/EntityExtractor/EntityExtractor-0.4.tar.gz/EntityExtractor-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EntityExtractor',
+    author='Ankit Mor',
+    version='0.4',
+    packages=find_packages(),
+    description='Extract specific entities from a text. Give text and get a JSON formatted output data.',
+    long_description="""Run following before using : \n nltk.download('punkt') \n , nltk.download('stopwords') \n , spacy.cli.download('web_en_core_lg')""",
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'pandas>=2.0.3',
+        'numpy>=1.24.3',
+        'nltk>=3.8.1',
+        'requests',
+        'BeautifulSoup4',
+        'spacy>=3.7.2',
+    ],
+)
