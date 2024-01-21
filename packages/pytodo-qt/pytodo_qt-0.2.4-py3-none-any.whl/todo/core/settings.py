ef795fda430e4ad8582cbc5c03963b1c3e6bb3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:52 +0000

    Add package pytodo-qt/0.2.4/pytodo_qt-0.2.4-py3-none-any.whl

---
+++ packages/pytodo-qt/pytodo_qt-0.2.4-py3-none-any.whl/todo/core/settings.py
@@ -0,0 +1,37 @@
+"""settings.py
+
+This module creates To-Do core global variables and functions.
+"""
+
+import sys
+
+from pathlib import Path
+
+from ..core.Logger import Logger
+
+
+logger = Logger(__name__)
+
+
+__version__ = "0.2.4"
+options = {}
+DB = None
+
+home_dir = Path.home()
+if home_dir is not None:
+    todo_dir = Path.joinpath(home_dir, ".todo")
+else:
+    logger.log.exception("Unable to write log file, exiting")
+    sys.exit(1)
+
+if not Path.exists(todo_dir):
+    try:
+        Path.mkdir(todo_dir)
+    except OSError as e:
+        logger.log.exception("Error creating To-Do configuration directory: %s", e)
+        sys.exit(1)
+
+
+# private files
+ini_fn = Path.joinpath(todo_dir, "todo.ini")
+lists_fn = Path.joinpath(todo_dir, "todo_lists.json")
