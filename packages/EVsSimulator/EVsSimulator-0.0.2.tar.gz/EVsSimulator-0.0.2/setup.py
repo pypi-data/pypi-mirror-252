b5539359dc37a1b300ed174749ce9bcc226b1e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:11 +0000

    Add package EVsSimulator/0.0.2/EVsSimulator-0.0.2.tar.gz

---
+++ packages/EVsSimulator/EVsSimulator-0.0.2.tar.gz/EVsSimulator-0.0.2/setup.py
@@ -0,0 +1,29 @@
+# This is a setup file for the package
+
+from setuptools import find_packages
+from setuptools import setup
+
+# print(find_packages())
+# exit()
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='EVsSimulator',
+    version='0.0.2',
+    description='A realistic V2X environment using gym',
+    author='Stavros Orfanoudakis',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    keywords='gym, Reinforcement Learning, V2X, EVs, EVsSimulator, Electric Vehicles, Electric Vehicle Simulator',
+    # package_dir = {"": "EVsSimulator"},
+    packages = find_packages(),
+    python_requires = ">=3.6",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
+                      
