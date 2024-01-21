1565b646a66397279f2db7828fac3fd7e4c641ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:10 +0000

    Add package aiovodafone/0.5.3/aiovodafone-0.5.3-py3-none-any.whl

---
+++ packages/aiovodafone/aiovodafone-0.5.3-py3-none-any.whl/aiovodafone/const.py
@@ -0,0 +1,27 @@
+"""Constants for Vodafone Station."""
+import logging
+from enum import Enum
+
+_LOGGER = logging.getLogger(__package__)
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+    "Accept-Language": "en-GB,en;q=0.5",
+    "X-Requested-With": "XMLHttpRequest",
+}
+
+LOGIN = [
+    "-",
+    "logged",
+    "already logged",
+    "credential error",
+    "credential error",
+    "password mismatch",
+]
+
+USER_ALREADY_LOGGED_IN = "MSG_LOGIN_150"
+
+
+class DeviceType(str, Enum):
+    SERCOMM = "Sercomm"
+    TECHNICOLOR = "Technicolor"
