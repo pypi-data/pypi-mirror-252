5911e7954a83872284ca973661b33f204826c08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 04:03:51 +0000

    Add package mdblewis-project-1/0.2/mdblewis_project_1-0.2.tar.gz

---
+++ packages/mdblewis-project-1/mdblewis_project_1-0.2.tar.gz/mdblewis_project_1-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mdblewis_project_1',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='mdblewis',
+    author_email='mdblewis@protonmail.com',
+    description='a test project',
+    url='https://github.com/mathewdblewis/testingPyPi',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
