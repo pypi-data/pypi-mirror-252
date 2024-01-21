6f3b14cbadd6343174c24d9738e3417124133daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 14:03:45 +0000

    Add package xdatbus/0.0.167/xdatbus-0.0.167-py3-none-any.whl

---
+++ packages/xdatbus/xdatbus-0.0.167-py3-none-any.whl/xdatbus/resources/sum_hills.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Process ID : $$"
+
+# Get the name of the current conda environment and directory
+current_env=$(conda info --envs | grep '*' | awk '{print $1}')
+current_dir=$(pwd)
+
+# Check if the 'plumed' package is installed in the current Conda environment
+if conda list | grep -q "^plumed "; then
+    echo "The 'plumed' package is installed in the current environment."
+else
+    echo "The 'plumed' package is not installed in the current environment."
+fi
+
+echo "Began running plumed!"
+
+# Create a clean results folder
+if [ ! -d "fes" ]; then
+    mkdir "fes"
+else
+    find "fes" -mindepth 1 -delete
+fi
+
+# Use arguments from the Python script
+HILLS_FILE=$1
+OUTFILE=$2
+MINTOZERO=$3
+MIN=$4
+MAX=$5
+BIN=$6
+
+# Run Plumed with the given arguments
+plumed sum_hills --hills $HILLS_FILE --outfile $OUTFILE --mintozero $MINTOZERO --min $MIN --max $MAX --bin $BIN
