d7f6efa29149f195e41bb594f91269f4a41a3e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:00 +0000

    Add package streamtranslator/1.1.0/streamtranslator-1.1.0.tar.gz

---
+++ packages/streamtranslator/streamtranslator-1.1.0.tar.gz/streamtranslator-1.1.0/README.md
@@ -0,0 +1,34 @@
+<p>
+  <h1>Real Time Translation for Live Streams</h1>
+</p>
+
+
+## Setup
+
+### 1. Install with pip
+
+Available for Python 3
+
+```
+pip install streamtranslator
+```
+
+### 2. Quick start - Example
+
+Play any audio from your desktop and run this example
+
+```
+import streamtranslator
+desktop_audio=streamtranslator.Translate('en-US','es-ES')
+desktop_audio.start()
+```
+
+### 3. Languages
+
+| Languages   |    Code     | 
+|:-----------:|:-----------:|
+| English     |    en-US    |
+| Spanish     |    es-ES    |
+
+
+
---
+++ packages/streamtranslator/streamtranslator-1.1.0.tar.gz/streamtranslator-1.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_des=f.read()
+    
+setup(
+    name='streamtranslator',
+    version='1.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={'voicetranslator': ['*.pyd', '*.exe','*.txt']},
+    description="Real time translation for live streams",
+    long_description=long_des,
+    long_description_content_type="text/markdown",
+    url="https://github.com/voicetranslator/voicetranslator",
+    project_urls={"Documentation":"https://github.com/voicetranslator/voicetranslator"},
+    install_requires=[
+        # Lista de dependencias requeridas para tu proyecto
+    ],
+    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+)
+
+    
+    
