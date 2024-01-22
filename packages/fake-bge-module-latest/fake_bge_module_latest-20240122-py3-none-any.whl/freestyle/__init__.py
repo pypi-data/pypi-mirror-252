923643170da50f444bc2ec60ffcd6621a1809bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:27 +0000

    Add package fake-bge-module-latest/20240122/fake_bge_module_latest-20240122-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240122-py3-none-any.whl/freestyle/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import types
+from . import chainingiterators
+from . import functions
+from . import predicates
+from . import utils
+from . import shaders
+
+GenericType = typing.TypeVar("GenericType")
