8313895b97a0fd914e108b06c40f8cd87aff9cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:44 +0000

    Add package kimm/0.1.3/kimm-0.1.3-py3-none-any.whl

---
+++ packages/kimm/kimm-0.1.3-py3-none-any.whl/kimm/layers/attention_test.py
@@ -0,0 +1,18 @@
+from absl.testing import parameterized
+from keras.src import testing
+
+from kimm.layers.attention import Attention
+
+
+class AttentionTest(testing.TestCase, parameterized.TestCase):
+    def test_attention_basic(self):
+        self.run_layer_test(
+            Attention,
+            init_kwargs={"hidden_dim": 20, "num_heads": 2},
+            input_shape=(1, 10, 20),
+            expected_output_shape=(1, 10, 20),
+            expected_num_trainable_weights=3,
+            expected_num_non_trainable_weights=0,
+            expected_num_losses=0,
+            supports_masking=False,
+        )
