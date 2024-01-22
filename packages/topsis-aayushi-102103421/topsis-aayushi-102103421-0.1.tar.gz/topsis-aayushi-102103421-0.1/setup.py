df441ed2571065ceb86107808701ebbcaa1a22f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:39 +0000

    Add package topsis-aayushi-102103421/0.1/topsis-aayushi-102103421-0.1.tar.gz

---
+++ packages/topsis-aayushi-102103421/topsis-aayushi-102103421-0.1.tar.gz/topsis-aayushi-102103421-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='topsis-aayushi-102103421',
+ version='0.1',
+ author="Aayushi Bareja",
+ author_email="abareja_be21@thapar.edu",
+ description="Topsis Implementation",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
