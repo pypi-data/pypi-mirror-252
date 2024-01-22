aefa95657c7cc44b1be94d1f5a52756263938f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:03:59 +0000

    Add package cirro-api-client/0.0.1a0/cirro_api_client-0.0.1a0-py3-none-any.whl

---
+++ packages/cirro-api-client/cirro_api_client-0.0.1a0-py3-none-any.whl/cirro_api_client/api/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import apis into api package
+from cirro_api_client.api.billing_api import BillingApi
+from cirro_api_client.api.dashboards_api import DashboardsApi
+from cirro_api_client.api.datasets_api import DatasetsApi
+from cirro_api_client.api.execution_api import ExecutionApi
+from cirro_api_client.api.file_api import FileApi
+from cirro_api_client.api.metadata_api import MetadataApi
+from cirro_api_client.api.metrics_api import MetricsApi
+from cirro_api_client.api.notebooks_api import NotebooksApi
+from cirro_api_client.api.processes_api import ProcessesApi
+from cirro_api_client.api.projects_api import ProjectsApi
+from cirro_api_client.api.references_api import ReferencesApi
+from cirro_api_client.api.system_api import SystemApi
+from cirro_api_client.api.users_api import UsersApi
+
