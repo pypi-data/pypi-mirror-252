a0abd21a4d3792f53dbf8db41e73e69beadf5c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:07 +0000

    Add package trustce/0.2.7/trustce-0.2.7.tar.gz

---
+++ packages/trustce/trustce-0.2.7.tar.gz/trustce-0.2.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trustce",
+    version="0.2.7",
+    packages=find_packages(),
+    package_data={
+        "trustce": ["input_instance/*.json", "config/*.yaml", "config/*.json", "examples_notebooks_demos/*.ipynb", "models/*.pkl", "models/*.pth"]
+    },
+    data_files=[("", ["LICENSE", 'README.md'])],
+    install_requires=[
+        "numpy==1.26.1",
+        "pandas==2.1.1",
+        "scikit-learn==1.3.1",
+        "scipy==1.11.3",
+        "pydiffmap==0.2.0.1",
+        "Pillow==10.0.1",
+        "PyYAML==6.0.1",
+        "requests==2.31.0",
+        "ipykernel>=6.0.3",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.4.3",
+            "ipykernel>=6.0.3",
+        ],
+        "sklearn": ["scikit-learn>=0.24.1"],
+        "tensorflow": ["tensorflow>=2.14.1"],
+        "pytorch": ["torch>=1.8.1"],
+        "gpgomea": ["gpgomea>=0.1.0"],
+        "homeloan": [],
+        "diabetes": [],
+        "income": []
+    }
+)
