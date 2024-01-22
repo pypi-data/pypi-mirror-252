0bb2947232c3e69fc387858ff22f608d6781d8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:30 +0000

    Add package Topsis-arpit-102103463/0.0.1/Topsis_arpit_102103463-0.0.1.tar.gz

---
+++ packages/Topsis-arpit-102103463/Topsis_arpit_102103463-0.0.1.tar.gz/Topsis_arpit_102103463-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'topsis package'
+
+
+# Setting up
+setup(
+    name="Topsis_arpit_102103463",
+    version=VERSION,
+    author="arpit",
+    author_email="adubey_be21@thapar.edu",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    install_requires=['logging', 'numpy', 'pandas'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
