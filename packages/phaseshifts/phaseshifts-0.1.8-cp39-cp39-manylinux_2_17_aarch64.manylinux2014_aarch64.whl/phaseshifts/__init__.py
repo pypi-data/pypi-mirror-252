557f10a44761ca895da3424340fc153aa2f6b99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:40 +0000

    Add package phaseshifts/0.1.8/phaseshifts-0.1.8-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/phaseshifts/phaseshifts-0.1.8-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/phaseshifts/__init__.py
@@ -0,0 +1,6 @@
+__project__ = "phaseshifts"
+__version__ = "0.1.8"
+__homepage__ = "https://github.com/Liam-Deacon/phaseshifts"
+__contact__ = __homepage__ + "/issues"
+
+VERSION = __project__ + "-" + __version__
