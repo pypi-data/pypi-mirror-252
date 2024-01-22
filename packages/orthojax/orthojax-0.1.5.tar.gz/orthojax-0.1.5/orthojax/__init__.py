1bb9e5f9c38ba5ba79f7a56e27b9acba30906d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:03:51 +0000

    Add package orthojax/0.1.5/orthojax-0.1.5.tar.gz

---
+++ packages/orthojax/orthojax-0.1.5.tar.gz/orthojax-0.1.5/orthojax/__init__.py
@@ -0,0 +1,12 @@
+"""
+orthjax.
+
+Orthogonal polynomials in jax.
+"""
+
+__version__ = "0.1.5"
+__author__ = 'Ilias Bilionis'
+__credits__ = 'Purdue University, School of Mechanical Engineering'
+
+from .orthpol import *
+from .tensor_product import *
---
+++ packages/orthojax/orthojax-0.1.5.tar.gz/orthojax-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='orthojax',
+    version='0.1.5',    
+    description='Orthogonal polynomials in Jxax',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PredictiveScienceLab/orthojax',
+    author='Iilias Bilionis',
+    author_email='ibilion@purdue.edu',
+    license='MIT License',
+    packages=['orthojax'],
+    install_requires=['jax>=0.4.19',
+                      'numpy', 
+                      'equinox>=0.11.2'                    
+                      ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3'
+    ],
+)
