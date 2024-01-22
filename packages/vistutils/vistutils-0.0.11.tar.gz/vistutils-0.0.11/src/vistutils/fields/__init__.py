9c619b3a67b5248ecc94136d7e7336846ac61148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:27 +0000

    Add package vistutils/0.0.11/vistutils-0.0.11.tar.gz

---
+++ packages/vistutils/vistutils-0.0.11.tar.gz/vistutils-0.0.11/src/vistutils/fields/__init__.py
@@ -0,0 +1,8 @@
+"""The fields module provide a collection of descriptor classes. """
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._abstract_field import AbstractField
+from ._abstract_decorator import AbstractDecorator
+from ._typed_field import TypedField
