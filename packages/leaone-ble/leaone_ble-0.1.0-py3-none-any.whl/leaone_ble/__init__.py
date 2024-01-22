f8c387d388a5004e1fa364a5b0e03000ecca26b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:12 +0000

    Add package leaone-ble/0.1.0/leaone_ble-0.1.0-py3-none-any.whl

---
+++ packages/leaone-ble/leaone_ble-0.1.0-py3-none-any.whl/leaone_ble/__init__.py
@@ -0,0 +1,35 @@
+"""
+Parser for Leaone BLE advertisements.
+
+This file is shamelessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/leaone.py
+
+MIT License applies.
+"""
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import LeaoneBluetoothDeviceData
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "LeaoneBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
