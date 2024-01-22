d4c2c39695a861c232cb356b3ff4b051c9b1184f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:04:11 +0000

    Add package Topsis-Aditya-102103546/1.0.0/Topsis_Aditya_102103546-1.0.0.tar.gz

---
+++ packages/Topsis-Aditya-102103546/Topsis_Aditya_102103546-1.0.0.tar.gz/Topsis_Aditya_102103546-1.0.0/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis_Aditya_102103546",
+    version="1.0.0",
+    description="This Python script implements the TOPSIS (Technique for Order Preference by Similarity to Ideal Solution) algorithm.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BROWNcoder4946/Topsis_pypi_package",
+    author="Aditya Vashishta",
+    author_email="adityavashishta3911@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["Topsis"],
+    include_package_data=True,
+    install_requires=['pandas','numpy','inspect'],
+    entry_points={
+        "console_scripts": [
+            "Topsis=Topsis.__main__:TOPSIS",
+        ]
+    },
+)
