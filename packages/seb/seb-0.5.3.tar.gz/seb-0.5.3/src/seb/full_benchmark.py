cf6c1696b52c19b35312aec89250ba2779992ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:12 +0000

    Add package seb/0.5.3/seb-0.5.3.tar.gz

---
+++ packages/seb/seb-0.5.3.tar.gz/seb-0.5.3/src/seb/full_benchmark.py
@@ -0,0 +1,46 @@
+"""
+This is the specification for the full benchmark. Running the code here will reproduce the results.
+"""
+
+
+from pathlib import Path
+from typing import Optional
+
+from .benchmark import Benchmark
+from .interfaces.model import EmbeddingModel
+from .registries import get_all_models
+from .result_dataclasses import BenchmarkResults
+
+BENCHMARKS = {
+    "Mainland Scandinavian": ["da", "sv", "nn", "nb"],
+    "Danish": ["da"],
+    "Norwegian": ["nn", "nb"],
+    "Swedish": ["sv"],
+}
+
+
+def run_benchmark(
+    use_cache: bool = True,
+    run_models: bool = True,
+    raise_errors: bool = True,
+    cache_dir: Optional[Path] = None,
+) -> dict[str, list[BenchmarkResults]]:
+    """
+    Run the full SEB benchmark.
+    """
+    models: list[EmbeddingModel] = get_all_models()
+
+    results = {}
+    for subset, langs in BENCHMARKS.items():
+        benchmark = Benchmark(languages=langs)
+        bm_results = benchmark.evaluate_models(
+            models=models,
+            use_cache=use_cache,
+            run_model=run_models,
+            raise_errors=raise_errors,
+            cache_dir=cache_dir,
+        )
+
+        results[subset] = bm_results
+
+    return results
