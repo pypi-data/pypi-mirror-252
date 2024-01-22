0b6be12f0d8844692d9d144be4ad8f2e5663af11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:30 +0000

    Add package Topsis-Sehaj-102103290/1.1.0/Topsis-Sehaj-102103290-1.1.0.tar.gz

---
+++ packages/Topsis-Sehaj-102103290/Topsis-Sehaj-102103290-1.1.0.tar.gz/Topsis-Sehaj-102103290-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="Topsis-Sehaj-102103290",
+    version="1.1.0",
+    description="TOPSIS",
+    long_description="Topsis-Sehaj-102103290",
+    long_description_content_type="text/markdown",
+    author="Sehaj",
+    author_email="bainssehaj73@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=[""],
+    include_package_data=True,
+    install_requires=[],
+    
+)
