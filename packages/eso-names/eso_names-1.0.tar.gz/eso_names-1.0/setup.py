439f38697cacf6137f2a7098662693fd592d29a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:03:50 +0000

    Add package eso-names/1.0/eso_names-1.0.tar.gz

---
+++ packages/eso-names/eso_names-1.0.tar.gz/eso_names-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eso_names',
+    version='1.0',
+    include_package_data=True,
+    package_data={'': ['db/*.csv','model/*pth']},
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'filelock==3.13.1',
+        'fsspec==2023.12.2',
+        'Jinja2==3.1.3',
+        'Levenshtein==0.23.0',
+        'MarkupSafe==2.1.4',
+        'mpmath==1.3.0',
+        'networkx==3.2.1',
+        'numpy==1.26.3',
+        'pandas==2.2.0',
+        'python-dateutil==2.8.2',
+        'pytz==2023.3.post1',
+        'rapidfuzz==3.6.1',
+        'six==1.16.0',
+        'sympy==1.12',
+        'torch==2.1.2',
+        'typing_extensions==4.9.0',
+        'tzdata==2023.4',
+    ],
+)
