2c1361390fae3fc00b8f4e7b42cbb0f2bbc662b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:04:12 +0000

    Add package django-silk/5.1.0/django-silk-5.1.0.tar.gz

---
+++ packages/django-silk/django-silk-5.1.0.tar.gz/django-silk-5.1.0/project/tests/test_view_summary_view.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from silk.views.summary import SummaryView
+
+from .test_lib.mock_suite import MockSuite
+
+mock_suite = MockSuite()
+
+
+class TestSummaryView(TestCase):
+    def test_longest_query_by_view(self):
+        [mock_suite.mock_request() for _ in range(0, 10)]
+        print([x.time_taken for x in SummaryView()._longest_query_by_view([])])
