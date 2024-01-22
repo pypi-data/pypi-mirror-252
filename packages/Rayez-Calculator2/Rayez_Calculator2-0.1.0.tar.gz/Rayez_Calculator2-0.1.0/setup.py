4325ad23bdf865aacd7b000a5db65b706a729b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:03:59 +0000

    Add package Rayez-Calculator2/0.1.0/Rayez_Calculator2-0.1.0.tar.gz

---
+++ packages/Rayez-Calculator2/Rayez_Calculator2-0.1.0.tar.gz/Rayez_Calculator2-0.1.0/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='Rayez_Calculator2',
+    version='0.1.0',
+    packages=['Rayez_Calculator'],
+    install_requires=[],  # Add any dependencies here
+)
