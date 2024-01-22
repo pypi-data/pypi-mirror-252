0b1d910429d713325583211503dbaa4602d141d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:19 +0000

    Add package runtimepy/3.3.0/runtimepy-3.3.0-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-3.3.0-py3-none-any.whl/runtimepy/commands/all.py
@@ -0,0 +1,44 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=2fec0392db71770da35d98fdbc8ce142
+# =====================================
+
+"""
+A module aggregating package commands.
+"""
+
+# built-in
+from typing import List as _List
+from typing import Tuple as _Tuple
+
+# third-party
+from vcorelib.args import CommandRegister as _CommandRegister
+
+# internal
+from runtimepy.commands.arbiter import add_arbiter_cmd
+from runtimepy.commands.server import add_server_cmd
+from runtimepy.commands.tui import add_tui_cmd
+
+
+def commands() -> _List[_Tuple[str, str, _CommandRegister]]:
+    """Get this package's commands."""
+
+    return [
+        (
+            "arbiter",
+            "run a connection-arbiter application from a config",
+            add_arbiter_cmd,
+        ),
+        (
+            "server",
+            "run a server for a specific connection factory",
+            add_server_cmd,
+        ),
+        (
+            "tui",
+            "run a terminal interface for the channel environment",
+            add_tui_cmd,
+        ),
+        ("noop", "command stub (does nothing)", lambda _: lambda _: 0),
+    ]
