e038fd9cb4cfdb5bcc54b2bf00345b5ebf89d72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:09 +0000

    Add package mathfortrand/1.2.7/mathfortrand-1.2.7.tar.gz

---
+++ packages/mathfortrand/mathfortrand-1.2.7.tar.gz/mathfortrand-1.2.7/mathfortrand/__init__.py
@@ -0,0 +1,14 @@
+from mathfortrand import DF1N
+from mathfortrand import DF2N
+from mathfortrand import DF3N
+from mathfortrand import DF4N
+from mathfortrand import DF1S
+from mathfortrand import DF2S
+from mathfortrand import DF3S
+from mathfortrand import DF4S
+from mathfortrand import out_pl
+from mathfortrand import mathcn
+from mathfortrand import mathcs
+from mathfortrand import mathpcn
+from mathfortrand import mathpcs
+
---
+++ packages/mathfortrand/mathfortrand-1.2.7.tar.gz/mathfortrand-1.2.7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import setup
+from glob import glob
+
+setuptools.setup(
+    name="mathfortrand",
+    version="1.2.7",
+    author="bug404",
+    author_email="z19040042@s.upc.edu.cn",
+    description="Some environments for reinforcement learning.",
+    long_description="Some environments for reinforcement learning.",
+    long_description_content_type="text/markdown",
+    include_package_data = True,
+    packages=['.'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
