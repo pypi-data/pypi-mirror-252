bd7942a59a63ef96523f6320b5100075bf09ca10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:22 +0000

    Add package streamlit-project-template/0.1.2/streamlit-project-template-0.1.2.tar.gz

---
+++ packages/streamlit-project-template/streamlit-project-template-0.1.2.tar.gz/streamlit-project-template-0.1.2/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+import setuptools
+
+setuptools.setup(
+    name="streamlit-project-template",
+    version="0.1.2",
+    description="A solid project structure from basit to advanced streamlit applications.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="Mehmet Can Demir",
+    author_email="mhmtcndmr01@gmail.com",
+    licance="MIT",
+    project_urls={
+        "Documentation": \
+        "https://github.com/mcandemir/streamlit-project-template/blob/master/README.md",
+        "Repository": "https://github.com/mcandemir/streamlit-project-template"
+    },
+    classifiers={
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.12"
+    },
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit"
+    ],
+    extras_require={
+        "extras": [
+            "streamlit-extras"
+        ]
+    },
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "streamlit-project-template = streamlit_project_template:main",
+            ]
+        }
+)
