f981db234e5033653abc58c6c54ccd3751f12bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:03:54 +0000

    Add package scratchclientImproved/1.2.1/scratchclientImproved-1.2.1.tar.gz

---
+++ packages/scratchclientImproved/scratchclientImproved-1.2.1.tar.gz/scratchclientImproved-1.2.1/scratchclientImproved/__init__.py
@@ -0,0 +1,10 @@
+from .ScratchSession import ScratchSession
+
+# Version Check, Not Sure If I Wanna Implement It, But I Have The Code
+#import re, requests
+
+#current_version = "1.2"
+#newest_version = re.findall(r"v(\d+\.\d+\.\d+)", requests.get("https://github.com/StellarSt0rm/scratchclientImproved/releases/latest"))>
+
+#if current_version != newest_version:
+#        print("Info: Newer Version Is Available!")
