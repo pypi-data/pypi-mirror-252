6806c36f07a0678e95384fe9c91587b206513f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:03:44 +0000

    Add package py-riot-standard-calls/0.1/py-riot-standard-calls-0.1.tar.gz

---
+++ packages/py-riot-standard-calls/py-riot-standard-calls-0.1.tar.gz/py-riot-standard-calls-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-riot-standard-calls',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Dependências necessárias
+    ],
+)
