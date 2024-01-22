47191feb7c1f3acd6832e3dd1eb7e246d5afc29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:03:52 +0000

    Add package pyside-ui-backpack/0.1.2/pyside_ui_backpack-0.1.2-py3-none-any.whl

---
+++ packages/pyside-ui-backpack/pyside_ui_backpack-0.1.2-py3-none-any.whl/ps_ui/version.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------------------
+# 0.1.0 01/2024 - Initial Test Release
+# ----------------------------------------------------------------------------------------
+
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION_PATCH = 2
+
+__version__ = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
+
+app_name = 'pyside-ui-backpack'
