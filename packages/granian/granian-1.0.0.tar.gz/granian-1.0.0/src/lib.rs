0cfea3b445cdbb2262a8641c22912d477bcd1ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:21 +0000

    Add package granian/1.0.0/granian-1.0.0.tar.gz

---
+++ packages/granian/granian-1.0.0.tar.gz/granian-1.0.0/src/lib.rs
@@ -0,0 +1,28 @@
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+use pyo3::prelude::*;
+
+mod asgi;
+mod callbacks;
+mod conversion;
+mod http;
+mod io;
+mod rsgi;
+mod runtime;
+mod tcp;
+mod tls;
+mod utils;
+mod workers;
+mod ws;
+mod wsgi;
+
+#[pymodule]
+fn _granian(py: Python, module: &PyModule) -> PyResult<()> {
+    asgi::init_pymodule(module)?;
+    rsgi::init_pymodule(py, module)?;
+    tcp::init_pymodule(module)?;
+    workers::init_pymodule(module)?;
+    wsgi::init_pymodule(py, module)?;
+    Ok(())
+}
