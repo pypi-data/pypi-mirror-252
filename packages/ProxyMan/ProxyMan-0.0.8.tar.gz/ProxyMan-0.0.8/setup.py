8c1adf6bcea6c28a4587adbf80d215aecfc5cbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:17 +0000

    Add package ProxyMan/0.0.8/ProxyMan-0.0.8.tar.gz

---
+++ packages/ProxyMan/ProxyMan-0.0.8.tar.gz/ProxyMan-0.0.8/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+# Function to read the version from __init__.py
+def get_version():
+    with open("ProxyMan/__init__.py", "r", encoding="utf-8") as f:
+        for line in f:
+            if line.startswith('__version__'):
+                # Strip the version string and return it
+                return line.split('=')[-1].strip().strip("'").strip('"')
+        raise RuntimeError("Unable to find version string.")
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Get version from init file
+setup(
+    name='ProxyMan',
+    version=get_version(),
+    description='A simple proxy manager for Python with WebShare API support',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rawandahmad698/ProxyMan',
+    author='Rawand Ahmed Shaswar',
+    author_email='rawa@rawa.dev',
+    license='BSD 3-Clause License',
+    packages=['ProxyMan'],
+    install_requires=['aiohttp'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
