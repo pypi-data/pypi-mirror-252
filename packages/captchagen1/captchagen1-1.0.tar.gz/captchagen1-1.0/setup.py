b6c3d1ca05bbba7e5e6454baf237d81429ba40bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:19 +0000

    Add package captchagen1/1.0/captchagen1-1.0.tar.gz

---
+++ packages/captchagen1/captchagen1-1.0.tar.gz/captchagen1-1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='captchagen1',
+	version='1.0',
+	author='danon',
+	author_email='genry.stivensighu@gmail.com',
+	description='',
+	packages=['captchagen'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
