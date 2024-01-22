5a5e4fce4dbbfbaf4cde7e32617791d0735950d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:03:59 +0000

    Add package spinesTS/0.4.10/spinesTS-0.4.10.tar.gz

---
+++ packages/spinesTS/spinesTS-0.4.10.tar.gz/spinesTS-0.4.10/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+    name='spinesTS',
+    version="0.4.10",
+    description='spinesTS, a powerful timeseries toolsets.',
+    keywords='machine learning',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    url='https://github.com/BirchKwok/spinesTS',
+    author='Birch Kwok',
+    author_email='birchkwok@gmail.com',
+    install_requires=[
+        'scikit-learn>=1.0.2',
+        'torch>=2.0.0',
+        'scipy>=1.7.0',
+        'numpy>=1.17.0',
+        'pandas>=2.0.0',
+        'tabulate>=0.8',
+        'matplotlib>=3.5.1',
+        'spinesUtils>=0.3.13'
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
---
+++ packages/spinesTS/spinesTS-0.4.10.tar.gz/spinesTS-0.4.10/spinesTS/base/__init__.py
@@ -0,0 +1,2 @@
+from ._base_mixin import ForecastingMixin, TableFeatureGenerateMixin
+from ._torch_mixin import TorchModelMixin, detect_available_device
