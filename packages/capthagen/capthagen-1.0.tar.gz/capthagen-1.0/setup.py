2b56354068bc6d793f5ef19b9094c38e3434f090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:20 +0000

    Add package capthagen/1.0/capthagen-1.0.tar.gz

---
+++ packages/capthagen/capthagen-1.0.tar.gz/capthagen-1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='capthagen',
+	version='1.0',
+	author='danon',
+	author_email='henry.hurnistavt@gmail.com',
+	description='',
+	packages=['captchagen'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
