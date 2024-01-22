74fe773ab2dda28d500636e6ff568958167d7c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:11 +0000

    Add package Topsis-Jaisika-102103378/2.2.2/Topsis-Jaisika-102103378-2.2.2.tar.gz

---
+++ packages/Topsis-Jaisika-102103378/Topsis-Jaisika-102103378-2.2.2.tar.gz/Topsis-Jaisika-102103378-2.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Jaisika-102103378",
+    version="2.2.2",
+    description="A simple Python package for math operations and geometry calculations",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
