88c2ebe3071269281980f70cea2560478717b76c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:28 +0000

    Add package polylith-cli/0.5.0/polylith_cli-0.5.0-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-0.5.0-py3-none-any.whl/polylith_cli/polylith/toml/__init__.py
@@ -0,0 +1,2 @@
+from polylith_cli.polylith.toml.core import get_project_dependencies, get_project_package_includes, get_project_packages_from_polylith_section, read_toml_document
+__all__ = ['get_project_dependencies', 'get_project_package_includes', 'get_project_packages_from_polylith_section', 'read_toml_document']
