2b7d131f0215d86a068da5a5549b426510b10b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:04:06 +0000

    Add package localtileserver/0.10.0rc1/localtileserver-0.10.0rc1.tar.gz

---
+++ packages/localtileserver/localtileserver-0.10.0rc1.tar.gz/localtileserver-0.10.0rc1/localtileserver/web/wsgi.py
@@ -0,0 +1,10 @@
+import logging
+
+from localtileserver.web import create_app
+
+logging.getLogger("werkzeug").setLevel(logging.DEBUG)
+logging.getLogger("gdal").setLevel(logging.DEBUG)
+logging.getLogger("rasterio").setLevel(logging.DEBUG)
+logging.getLogger("rio_tiler").setLevel(logging.DEBUG)
+
+app = create_app()
