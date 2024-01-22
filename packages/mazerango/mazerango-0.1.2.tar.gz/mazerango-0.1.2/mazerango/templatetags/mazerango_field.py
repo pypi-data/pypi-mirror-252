63d001212e88b952682c7ebd7836ccbfa9fd6f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:28 +0000

    Add package mazerango/0.1.2/mazerango-0.1.2.tar.gz

---
+++ packages/mazerango/mazerango-0.1.2.tar.gz/mazerango-0.1.2/mazerango/templatetags/mazerango_field.py
@@ -0,0 +1,28 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def mazerango_class(field, css_class):
+    attr = field.field.widget.attrs
+    is_required = field.field.required
+    widget = field.field.widget
+    if hasattr(widget, 'input_type') and widget.input_type == 'select':
+        field.field.widget.template_name = 'mazerango/widgets/select.html'
+        return field
+    if 'class' in attr:
+        attr_dict = {"class": f"{css_class} {attr['class']}"}
+        attr_dict.update({
+            "data-parsley-required": "true" if is_required else "false"
+        })
+        return field.as_widget(attrs=attr_dict)
+    return field.as_widget(attrs={"class": f"{css_class}"})
+
+
+@register.filter
+def mazerango_checkbox(field):
+    return field.as_widget(attrs={"class": "form-check-input"})
+
+@register.filter
+def mazerango_label(field):
+    return field.field.label_tag(attrs={"class": "form-label"})
