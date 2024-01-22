c401d078974ea44c1ea2a7095f4f4234817acca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:03 +0000

    Add package llm-wrap/0.1.1/llm_wrap-0.1.1.tar.gz

---
+++ packages/llm-wrap/llm_wrap-0.1.1.tar.gz/llm_wrap-0.1.1/llm_wrap/llm_response.py
@@ -0,0 +1,20 @@
+from .llms.gpt_api import chatCompletion
+
+
+def get_response(llm_api='openai', **kwargs):
+    """
+    This function acts as an interface for consumer libraries to get AI's response.
+    It currently supports OpenAI's GPT API, but can be extended to support other APIs.
+
+    Parameters:
+    api (str): The API to use. Default is 'openai'.
+    **kwargs: Arbitrary keyword arguments. These will be passed to the API function.
+
+    Returns:
+    The response from the AI.
+    """
+    if llm_api == 'openai':
+        response = chatCompletion(**kwargs)
+        return response.choices[0].message.content
+    else:
+        raise ValueError(f"Unsupported API: {llm_api}")
