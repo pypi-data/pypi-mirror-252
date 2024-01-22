34896243ed6fbd8702c19acb278f5e267dac3725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:03:44 +0000

    Add package officialeye/1.1.4/officialeye-1.1.4-py3-none-any.whl

---
+++ packages/officialeye/officialeye-1.1.4-py3-none-any.whl/officialeye/_internal/io/__init__.py
@@ -0,0 +1,5 @@
+"""
+Module responsible for handling IO (input and output).
+In this context, IO means everything related to the way we output complex data.
+Simply logging does not count as
+"""
---
+++ packages/officialeye/officialeye-1.1.4-py3-none-any.whl/officialeye/_internal/matching/__init__.py
@@ -0,0 +1,4 @@
+"""
+Module handling everything related to matching, i.e., to the process of finding correspondences
+between the template and a given image.
+"""
