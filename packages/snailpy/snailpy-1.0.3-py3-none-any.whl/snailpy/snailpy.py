1ad70b9502fe857bbad34b540f8dc666d9be5a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:27 +0000

    Add package snailpy/1.0.3/snailpy-1.0.3-py3-none-any.whl

---
+++ packages/snailpy/snailpy-1.0.3-py3-none-any.whl/snailpy/snailpy.py
@@ -0,0 +1,50 @@
+import requests
+
+# This will be changed when we get the new server
+api_url = 'https://snailshare-backend.glitch.me'
+
+def get_follower_count(username):
+    url = f'{api_url}/api/users/getFollowerCount?username={username}'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            follower_count = int(response.text)
+            return follower_count
+        else:
+            print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def id_to_name(args):
+    url = f'{api_url}/api/pmWrapper/getProject?id={args["WHO"]}'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            json_data = response.json()
+            return json_data.get('name', '')
+        else:
+            return ""
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return ""
+
+def id_to_owner(args):
+    url = f'{api_url}/api/pmWrapper/getProject?id={args["WHO"]}'
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            json_data = response.json()
+            return json_data.get('author', {}).get('username', '')
+        else:
+            return ""
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return ""
