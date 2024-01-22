ea6f384cd4a1808fff32c983aaa3171ae8ef91fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:30 +0000

    Add package tavily-python/0.3.1/tavily-python-0.3.1.tar.gz

---
+++ packages/tavily-python/tavily-python-0.3.1.tar.gz/tavily-python-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tavily-python',
+    version='0.3.1',
+    url='https://github.com/assafelovic/tavily-python',
+    author='Assaf Elovic',
+    author_email='assaf.elovic@gmail.com',
+    description='Python wrapper for the Tavily API',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=find_packages(),
+    install_requires=['requests', 'tiktoken==0.5.2'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
