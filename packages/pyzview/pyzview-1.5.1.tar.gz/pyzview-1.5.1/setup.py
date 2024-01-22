eeee88c19962e4b50684870f27e8ccec4bfe23a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:25 +0000

    Add package pyzview/1.5.1/pyzview-1.5.1.tar.gz

---
+++ packages/pyzview/pyzview-1.5.1.tar.gz/pyzview-1.5.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension
+__version__ = "1.5.1"
+
+module = Pybind11Extension("zview_module",
+                       sources=["pyzview/zview_inf.cpp"],
+                       libraries = ["zview_inf"],   
+                       
+              )
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+setup(
+     name='pyzview',  
+     version=__version__,
+     author="Ohad Menashe",
+     author_email="ohad.men@gmail.com",
+     description="zview python inferface",
+     long_description_content_type="text/markdown",
+     long_description=long_description,
+     url="https://github.com/ohadmen/pyzview",
+     packages=find_packages(),
+     ext_modules=[module],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    install_requires=[
+        'numpy>=1.24.4'  ],
+ )
