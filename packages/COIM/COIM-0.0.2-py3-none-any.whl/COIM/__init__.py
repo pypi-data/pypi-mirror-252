a7cc1214359880c14ddb7bc27395a921b1750443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:26 +0000

    Add package COIM/0.0.2/COIM-0.0.2-py3-none-any.whl

---
+++ packages/COIM/COIM-0.0.2-py3-none-any.whl/COIM/__init__.py
@@ -0,0 +1,6 @@
+from .operator import *
+from .constrain import *
+from .constrain_custom import *
+from .constrain_add_scalar import *
+from .constrain_mul_scalar import *
+from .constrain_constant_sum import *
