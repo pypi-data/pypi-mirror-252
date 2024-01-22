809c206c898ecb309518a0f8a4eaaf8f6b512ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:19 +0000

    Add package tallyerp/0.0.10/tallyerp-0.0.10-py3-none-any.whl

---
+++ packages/tallyerp/tallyerp-0.0.10-py3-none-any.whl/tallyerp/usecases/master/ledger/createLedger/useCase.py
@@ -0,0 +1,16 @@
+from .dto import CreateLedgerRequest, CreateLedgerResponse
+from tallyerp.repositories.masters.Ledger import LedgerRepository
+
+
+class CreateLedgerUseCase:
+    def __init__(self, req: CreateLedgerRequest):
+        self.request = req
+        self.repo = LedgerRepository()
+
+    def execute(self) -> CreateLedgerResponse:
+        res = self.repo.create(self.request.ledger)
+        return CreateLedgerResponse(
+            created=res.BODY.DATA.IMPORTRESULT.CREATED,
+            altered=res.BODY.DATA.IMPORTRESULT.ALTERED,
+            deleted=res.BODY.DATA.IMPORTRESULT.DELETED,
+        )
