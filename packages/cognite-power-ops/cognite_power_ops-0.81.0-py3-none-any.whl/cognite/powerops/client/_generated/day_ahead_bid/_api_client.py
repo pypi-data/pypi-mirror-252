e88cc7a694cae4b3d8376209a8697753b7e34195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:48 +0000

    Add package cognite-power-ops/0.81.0/cognite_power_ops-0.81.0-py3-none-any.whl

---
+++ packages/cognite-power-ops/cognite_power_ops-0.81.0-py3-none-any.whl/cognite/powerops/client/_generated/day_ahead_bid/_api_client.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from cognite.client import ClientConfig, CogniteClient, data_modeling as dm
+from cognite.client.credentials import OAuthClientCredentials
+
+from ._api.alert import AlertAPI
+from ._api.basic_bid_matrix import BasicBidMatrixAPI
+from ._api.bid_document import BidDocumentAPI
+from ._api.bid_matrix import BidMatrixAPI
+from ._api.bid_method import BidMethodAPI
+from ._api.multi_scenario_matrix import MultiScenarioMatrixAPI
+from ._api.price_area import PriceAreaAPI
+from ._api.shop_multi_scenario import SHOPMultiScenarioAPI
+from ._api.water_value_based import WaterValueBasedAPI
+from . import data_classes
+
+
+class DayAheadBidAPI:
+    """
+    DayAheadBidAPI
+
+    Generated with:
+        pygen = 0.33.0
+        cognite-sdk = 7.8.6
+        pydantic = 2.5.3
+
+    Data Model:
+        space: power-ops-day-ahead-bid
+        externalId: DayAheadBid
+        version: 1
+    """
+
+    def __init__(self, config_or_client: CogniteClient | ClientConfig):
+        if isinstance(config_or_client, CogniteClient):
+            client = config_or_client
+        elif isinstance(config_or_client, ClientConfig):
+            client = CogniteClient(config_or_client)
+        else:
+            raise ValueError(f"Expected CogniteClient or ClientConfig, got {type(config_or_client)}")
+        # The client name is used for aggregated logging of Pygen Usage
+        client.config.client_name = "CognitePygen:0.33.0"
+
+        view_by_read_class = {
+            data_classes.Alert: dm.ViewId("power-ops-shared", "Alert", "1"),
+            data_classes.BasicBidMatrix: dm.ViewId("power-ops-day-ahead-bid", "BasicBidMatrix", "1"),
+            data_classes.BidDocument: dm.ViewId("power-ops-day-ahead-bid", "BidDocument", "1"),
+            data_classes.BidMatrix: dm.ViewId("power-ops-day-ahead-bid", "BidMatrix", "1"),
+            data_classes.BidMethod: dm.ViewId("power-ops-day-ahead-bid", "BidMethod", "1"),
+            data_classes.MultiScenarioMatrix: dm.ViewId("power-ops-day-ahead-bid", "MultiScenarioMatrix", "1"),
+            data_classes.PriceArea: dm.ViewId("power-ops-day-ahead-bid", "PriceArea", "1"),
+            data_classes.SHOPMultiScenario: dm.ViewId("power-ops-day-ahead-bid", "SHOPMultiScenario", "1"),
+            data_classes.WaterValueBased: dm.ViewId("power-ops-day-ahead-bid", "WaterValueBased", "1"),
+        }
+
+        self.alert = AlertAPI(client, view_by_read_class)
+        self.basic_bid_matrix = BasicBidMatrixAPI(client, view_by_read_class)
+        self.bid_document = BidDocumentAPI(client, view_by_read_class)
+        self.bid_matrix = BidMatrixAPI(client, view_by_read_class)
+        self.bid_method = BidMethodAPI(client, view_by_read_class)
+        self.multi_scenario_matrix = MultiScenarioMatrixAPI(client, view_by_read_class)
+        self.price_area = PriceAreaAPI(client, view_by_read_class)
+        self.shop_multi_scenario = SHOPMultiScenarioAPI(client, view_by_read_class)
+        self.water_value_based = WaterValueBasedAPI(client, view_by_read_class)
+
+    @classmethod
+    def azure_project(
+        cls, tenant_id: str, client_id: str, client_secret: str, cdf_cluster: str, project: str
+    ) -> DayAheadBidAPI:
+        credentials = OAuthClientCredentials.default_for_azure_ad(tenant_id, client_id, client_secret, cdf_cluster)
+        config = ClientConfig.default(project, cdf_cluster, credentials)
+
+        return cls(config)
+
+    @classmethod
+    def from_toml(cls, file_path: Path | str, section: str | None = "cognite") -> DayAheadBidAPI:
+        import toml
+
+        toml_content = toml.load(file_path)
+        if section is not None:
+            try:
+                toml_content = toml_content[section]
+            except KeyError as e:
+                raise ValueError(f"Could not find section '{section}' in {file_path}") from e
+
+        return cls.azure_project(**toml_content)
---
+++ packages/cognite-power-ops/cognite_power_ops-0.81.0-py3-none-any.whl/cognite/powerops/client/_generated/day_ahead_bid/data_classes/__init__.py
@@ -0,0 +1,149 @@
+from ._core import (
+    DomainModel,
+    DomainModelCore,
+    DomainModelApply,
+    DomainModelList,
+    DomainRelationApply,
+    ResourcesApply,
+    ResourcesApplyResult,
+)
+from ._alert import Alert, AlertApply, AlertApplyList, AlertFields, AlertList, AlertTextFields
+from ._basic_bid_matrix import (
+    BasicBidMatrix,
+    BasicBidMatrixApply,
+    BasicBidMatrixApplyList,
+    BasicBidMatrixFields,
+    BasicBidMatrixList,
+    BasicBidMatrixTextFields,
+)
+from ._bid_document import (
+    BidDocument,
+    BidDocumentApply,
+    BidDocumentApplyList,
+    BidDocumentFields,
+    BidDocumentList,
+    BidDocumentTextFields,
+)
+from ._bid_matrix import (
+    BidMatrix,
+    BidMatrixApply,
+    BidMatrixApplyList,
+    BidMatrixFields,
+    BidMatrixList,
+    BidMatrixTextFields,
+)
+from ._bid_method import (
+    BidMethod,
+    BidMethodApply,
+    BidMethodApplyList,
+    BidMethodFields,
+    BidMethodList,
+    BidMethodTextFields,
+)
+from ._multi_scenario_matrix import (
+    MultiScenarioMatrix,
+    MultiScenarioMatrixApply,
+    MultiScenarioMatrixApplyList,
+    MultiScenarioMatrixFields,
+    MultiScenarioMatrixList,
+    MultiScenarioMatrixTextFields,
+)
+from ._price_area import (
+    PriceArea,
+    PriceAreaApply,
+    PriceAreaApplyList,
+    PriceAreaFields,
+    PriceAreaList,
+    PriceAreaTextFields,
+)
+from ._shop_multi_scenario import (
+    SHOPMultiScenario,
+    SHOPMultiScenarioApply,
+    SHOPMultiScenarioApplyList,
+    SHOPMultiScenarioFields,
+    SHOPMultiScenarioList,
+    SHOPMultiScenarioTextFields,
+)
+from ._water_value_based import (
+    WaterValueBased,
+    WaterValueBasedApply,
+    WaterValueBasedApplyList,
+    WaterValueBasedFields,
+    WaterValueBasedList,
+    WaterValueBasedTextFields,
+)
+
+BasicBidMatrix.model_rebuild()
+BasicBidMatrixApply.model_rebuild()
+BidDocument.model_rebuild()
+BidDocumentApply.model_rebuild()
+BidMatrix.model_rebuild()
+BidMatrixApply.model_rebuild()
+MultiScenarioMatrix.model_rebuild()
+MultiScenarioMatrixApply.model_rebuild()
+PriceArea.model_rebuild()
+PriceAreaApply.model_rebuild()
+
+__all__ = [
+    "ResourcesApply",
+    "DomainModel",
+    "DomainModelCore",
+    "DomainModelApply",
+    "DomainModelList",
+    "DomainRelationApply",
+    "ResourcesApplyResult",
+    "Alert",
+    "AlertApply",
+    "AlertList",
+    "AlertApplyList",
+    "AlertFields",
+    "AlertTextFields",
+    "BasicBidMatrix",
+    "BasicBidMatrixApply",
+    "BasicBidMatrixList",
+    "BasicBidMatrixApplyList",
+    "BasicBidMatrixFields",
+    "BasicBidMatrixTextFields",
+    "BidDocument",
+    "BidDocumentApply",
+    "BidDocumentList",
+    "BidDocumentApplyList",
+    "BidDocumentFields",
+    "BidDocumentTextFields",
+    "BidMatrix",
+    "BidMatrixApply",
+    "BidMatrixList",
+    "BidMatrixApplyList",
+    "BidMatrixFields",
+    "BidMatrixTextFields",
+    "BidMethod",
+    "BidMethodApply",
+    "BidMethodList",
+    "BidMethodApplyList",
+    "BidMethodFields",
+    "BidMethodTextFields",
+    "MultiScenarioMatrix",
+    "MultiScenarioMatrixApply",
+    "MultiScenarioMatrixList",
+    "MultiScenarioMatrixApplyList",
+    "MultiScenarioMatrixFields",
+    "MultiScenarioMatrixTextFields",
+    "PriceArea",
+    "PriceAreaApply",
+    "PriceAreaList",
+    "PriceAreaApplyList",
+    "PriceAreaFields",
+    "PriceAreaTextFields",
+    "SHOPMultiScenario",
+    "SHOPMultiScenarioApply",
+    "SHOPMultiScenarioList",
+    "SHOPMultiScenarioApplyList",
+    "SHOPMultiScenarioFields",
+    "SHOPMultiScenarioTextFields",
+    "WaterValueBased",
+    "WaterValueBasedApply",
+    "WaterValueBasedList",
+    "WaterValueBasedApplyList",
+    "WaterValueBasedFields",
+    "WaterValueBasedTextFields",
+]
