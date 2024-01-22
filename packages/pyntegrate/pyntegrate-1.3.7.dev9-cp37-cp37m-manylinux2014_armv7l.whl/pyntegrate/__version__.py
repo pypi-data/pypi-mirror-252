36d41e092252e904f38b0514118667fa89480072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:29 +0000

    Add package pyntegrate/1.3.7.dev9/pyntegrate-1.3.7.dev9-cp37-cp37m-manylinux2014_armv7l.whl

---
+++ packages/pyntegrate/pyntegrate-1.3.7.dev9-cp37-cp37m-manylinux2014_armv7l.whl/pyntegrate/__version__.py
@@ -0,0 +1,20 @@
+__title__ = 'pyntegrate'
+__description__ = 'Pyntegrate is an example of various sorting algorithms and other functions using C Extensions.'
+__url__ = 'https://github.com/rtsfred3/pyntegrate'
+__download_url__ = 'https://pypi.org/project/pyntegrate/'
+__version__ = '1.3.7.dev9'
+__author__ = 'Ryan Fredrickson'
+__author_email__ = 'rtsfred3@gmail.com'
+__project_urls__ = {
+    'Bug Tracker': 'https://github.com/rtsfred3/pyntegrate/issues',
+    'Source Code': 'https://github.com/rtsfred3/pyntegrate',
+}
+__classifiers__ = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: C",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+]
+__keywords__ = 'pyntegrate Python C CPython'
+__license__ = 'Apache 2.0'
