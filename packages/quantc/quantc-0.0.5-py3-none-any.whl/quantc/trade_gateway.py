1a5642c77e69ac9a704f8510c24d71063554ed4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:05:07 +0000

    Add package quantc/0.0.5/quantc-0.0.5-py3-none-any.whl

---
+++ packages/quantc/quantc-0.0.5-py3-none-any.whl/quantc/trade_gateway.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Optional
+
+from pandas import DataFrame
+
+from .data_object import AccInfo, Position
+
+
+class TradeGateway(ABC):
+    @abstractmethod
+    async def get_position(self, code, acc_id: int, strategy_id: int = None) -> Optional[Position]:
+        pass
+
+    @abstractmethod
+    async def get_acc_info(self, acc_id: int) -> AccInfo:
+        pass
+
+    # @abstractmethod
+    # async def create_acc_info(self, acc_id: str, cash: float) -> AccInfo:
+    #     pass
+
+    # @abstractmethod
+    # def get_cash_mode(self, strategy_id: int = None):
+    #     pass
+
+    @abstractmethod
+    async def get_trade_record(
+            self,
+            acc_id: int,
+            code: str,
+            start: datetime = None,
+            end: datetime = None,
+            strategy_id: int = None,
+            limit: int = 1,
+    ) -> DataFrame:
+        pass
+
+    @abstractmethod
+    async def get_position_list(self, acc_id: int, strategy_id: int) -> list[Position]:
+        pass
+
+    @abstractmethod
+    async def sell(
+            self,
+            code: str,
+            price: float,
+            qty: int,
+            acc_id: int,
+            remark: str = None,
+            trade_count: int = None,
+            strategy_id: int = None,
+    ):
+        pass
+
+    @abstractmethod
+    async def buy(
+            self,
+            code: str,
+            price: float,
+            qty: int,
+            acc_id: int,
+            remark: str = None,
+            trade_count: int = None,
+            strategy_id: int = None,
+    ):
+        pass
+
+    async def update_position_acc_with_new_price(self, code: str, price: float, strategy_id=None):
+        pass
+
+    async def update_position_acc_with_kline_df(self, kline_df: DataFrame):
+        pass
+
+    async def get_position_count(self, acc_id: int, strategy_id: int = None):
+        pass
