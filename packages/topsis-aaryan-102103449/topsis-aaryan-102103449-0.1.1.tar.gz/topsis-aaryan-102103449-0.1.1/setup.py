3a8f6331b46ed804f0e6d799f0bce3f6d6872efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:19 +0000

    Add package topsis-aaryan-102103449/0.1.1/topsis-aaryan-102103449-0.1.1.tar.gz

---
+++ packages/topsis-aaryan-102103449/topsis-aaryan-102103449-0.1.1.tar.gz/topsis-aaryan-102103449-0.1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='topsis-aaryan-102103449',
+ version='0.1.1',
+ author="Aaryan Duggal",
+ author_email="aaryanduggal05@gmail.com",
+ description="Topsis Implementation",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
