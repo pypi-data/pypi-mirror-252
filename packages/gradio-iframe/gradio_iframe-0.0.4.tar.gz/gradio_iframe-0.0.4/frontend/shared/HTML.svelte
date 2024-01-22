65e42ecb05efb5472cfd0b5ba7f7ecec5b096bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:47 +0000

    Add package gradio-iframe/0.0.4/gradio_iframe-0.0.4.tar.gz

---
+++ packages/gradio-iframe/gradio_iframe-0.0.4.tar.gz/gradio_iframe-0.0.4/frontend/shared/HTML.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	export let elem_classes: string[] = [];
+	export let value: string;
+	export let visible = true;
+	export let min_height = false;
+
+	export let height = "100%";
+
+	const dispatch = createEventDispatcher<{ change: undefined }>();
+
+	$: value, dispatch("change");
+</script>
+
+<div
+	class="prose {elem_classes.join(' ')}"
+	class:min={min_height}
+	class:hide={!visible}
+>
+	<iframe title="iframe component" width="100%" height={height} srcdoc={value} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+<style>
+	.min {
+		min-height: var(--size-24);
+	}
+	.hide {
+		display: none;
+	}
+</style>
