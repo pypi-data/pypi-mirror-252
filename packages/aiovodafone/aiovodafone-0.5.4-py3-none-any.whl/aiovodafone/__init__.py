5a45da645315640808e4c7d4e7683025836b70d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:23 +0000

    Add package aiovodafone/0.5.4/aiovodafone-0.5.4-py3-none-any.whl

---
+++ packages/aiovodafone/aiovodafone-0.5.4-py3-none-any.whl/aiovodafone/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.5.1"
+
+from .api import (
+    VodafoneStationDevice,
+    VodafoneStationSercommApi,
+    VodafoneStationTechnicolorApi,
+)
+from .exceptions import (
+    AlreadyLogged,
+    CannotAuthenticate,
+    CannotConnect,
+    ModelNotSupported,
+    VodafoneError,
+)
+
+__all__ = [
+    "VodafoneStationDevice",
+    "VodafoneStationSercommApi",
+    "VodafoneStationTechnicolorApi",
+    "VodafoneError",
+    "AlreadyLogged",
+    "CannotConnect",
+    "CannotAuthenticate",
+    "ModelNotSupported",
+]
