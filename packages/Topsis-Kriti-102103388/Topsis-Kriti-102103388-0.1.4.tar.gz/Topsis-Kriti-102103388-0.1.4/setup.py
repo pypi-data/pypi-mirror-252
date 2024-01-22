171b7bccaaf852acaecbe3048e21a5985bde1413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:21 +0000

    Add package Topsis-Kriti-102103388/0.1.4/Topsis-Kriti-102103388-0.1.4.tar.gz

---
+++ packages/Topsis-Kriti-102103388/Topsis-Kriti-102103388-0.1.4.tar.gz/Topsis-Kriti-102103388-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Kriti-102103388",
+    version="0.1.4",
+    description="A simple Python package for performing topsis",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
