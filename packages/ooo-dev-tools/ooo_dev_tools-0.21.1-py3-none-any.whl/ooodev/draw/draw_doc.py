c715c91809930d32dc94ced8810546640f6a4af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 14:03:55 +0000

    Add package ooo-dev-tools/0.21.1/ooo_dev_tools-0.21.1-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.21.1-py3-none-any.whl/ooodev/draw/draw_doc.py
@@ -0,0 +1,240 @@
+from __future__ import annotations
+from typing import Any, cast, TYPE_CHECKING, overload
+import uno
+from com.sun.star.util import XCloseable
+
+from ooodev.adapter.document.document_event_events import DocumentEventEvents
+from ooodev.adapter.drawing.drawing_document_comp import DrawingDocumentComp
+from ooodev.adapter.frame.storable2_partial import Storable2Partial
+from ooodev.adapter.util.modify_events import ModifyEvents
+from ooodev.adapter.view.print_job_events import PrintJobEvents
+from ooodev.events.args.listener_event_args import ListenerEventArgs
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.utils import lo as mLo
+from ooodev.utils.inst.lo.lo_inst import LoInst
+from ooodev.utils.partial.gui_partial import GuiPartial
+from ooodev.utils.partial.prop_partial import PropPartial
+from ooodev.utils.partial.qi_partial import QiPartial
+from ooodev.adapter.util.close_events import CloseEvents
+from ooodev.utils.type_var import PathOrStr
+from ooodev.utils.partial.service_partial import ServicePartial
+from ooodev.utils.partial.lo_open_doc import LoOpenPartial
+from .draw_pages import DrawPages
+from .partial.draw_doc_partial import DrawDocPartial
+
+if TYPE_CHECKING:
+    from com.sun.star.lang import XComponent
+
+
+class DrawDoc(
+    DrawDocPartial["DrawDoc"],
+    DrawingDocumentComp,
+    DocumentEventEvents,
+    ModifyEvents,
+    PrintJobEvents,
+    CloseEvents,
+    Storable2Partial,
+    QiPartial,
+    PropPartial,
+    GuiPartial,
+    ServicePartial,
+    StylePartial,
+    LoOpenPartial,
+):
+    def __init__(self, doc: XComponent, lo_inst: LoInst | None = None) -> None:
+        if lo_inst is None:
+            self._lo_inst = mLo.Lo.current_lo
+        else:
+            self._lo_inst = lo_inst
+
+        DrawDocPartial.__init__(self, owner=self, component=doc, lo_inst=self._lo_inst)
+        DrawingDocumentComp.__init__(self, doc)
+        generic_args = self._ComponentBase__get_generic_args()  # type: ignore
+        DocumentEventEvents.__init__(self, trigger_args=generic_args, cb=self._on_document_event_add_remove)
+        # ModifyEvents.__init__(self, trigger_args=generic_args, cb=self._on_modify_events_add_remove)
+        PrintJobEvents.__init__(self, trigger_args=generic_args, cb=self._on_print_job_add_remove)
+        CloseEvents.__init__(self, trigger_args=generic_args, cb=self._on_print_job_add_remove)
+        Storable2Partial.__init__(self, component=doc, interface=None)  # type: ignore
+        QiPartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        PropPartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        GuiPartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        StylePartial.__init__(self, component=doc)
+        ServicePartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        LoOpenPartial.__init__(self, lo_inst=self._lo_inst)
+        self._pages = None
+
+    # region Lazy Listeners
+
+    def _on_modify_events_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addModifyListener(self.events_listener_modify)
+        event.remove_callback = True
+
+    def _on_document_event_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addDocumentEventListener(self.events_listener_document_event)
+        event.remove_callback = True
+
+    def _on_print_job_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addPrintJobListener(self.events_listener_print_job)
+        event.remove_callback = True
+
+    def _on_close_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addCloseListener(self.events_listener_close)  # type: ignore
+        event.remove_callback = True
+
+    # endregion Lazy Listeners
+
+    # region Overrides
+    def get_slides(self) -> DrawPages:
+        """
+        Gets the draw pages of a document.
+
+        Args:
+            doc (XComponent): Document.
+
+        Raises:
+            DrawPageMissingError: If there are no draw pages.
+            DrawPageError: If any other error occurs.
+
+        Returns:
+            DrawPages: Draw Pages.
+        """
+        return self.slides
+
+    def delete_slide(self, idx: int) -> bool:
+        """
+        Deletes a slide
+
+        Args:
+            idx (int): Index. Can be a negative value to delete from the end of the document.
+                For example, -1 will delete the last slide.
+
+        Returns:
+            bool: ``True`` on success; Otherwise, ``False``
+        """
+        if idx < 0:
+            idx = len(self.slides) + idx
+            if idx < 0:
+                raise IndexError("list index out of range")
+
+        return super().delete_slide(idx=idx)
+
+    # endregion Overrides
+
+    # region save_doc
+
+    @overload
+    def save_doc(self, fnm: PathOrStr) -> bool:
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+
+        Returns:
+            bool: ``False`` if ``DOC_SAVING`` event is canceled; Otherwise, ``True``
+        """
+        ...
+
+    @overload
+    def save_doc(self, fnm: PathOrStr, password: str) -> bool:
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+            password (str): Password to save document with.
+
+
+        Returns:
+            bool: ``False`` if ``DOC_SAVING`` event is canceled; Otherwise, ``True``
+        """
+        ...
+
+    @overload
+    def save_doc(self, fnm: PathOrStr, password: str, format: str) -> bool:  # pylint: disable=W0622
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+            password (str): Password to save document with.
+            format (str): document format such as 'odt' or 'xml'.
+
+        Returns:
+            bool: ``False`` if ``DOC_SAVING`` event is canceled; Otherwise, ``True``.
+        """
+        ...
+
+    def save_doc(self, fnm: PathOrStr, password: str | None = None, format: str | None = None) -> bool:
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+            password (str, optional): password to save document with.
+            format (str, optional): document format such as 'odt' or 'xml'.
+
+        Raises:
+            MissingInterfaceError: If doc does not implement XStorable interface.
+
+        Returns:
+            bool: ``False`` if DOC_SAVING event is canceled; Otherwise, ``True``
+
+        :events:
+            .. cssclass:: lo_event
+
+                - :py:attr:`~.events.lo_named_event.LoNamedEvent.DOC_SAVING` :eventref:`src-docs-event-cancel`
+                - :py:attr:`~.events.lo_named_event.LoNamedEvent.DOC_SAVED` :eventref:`src-docs-event`
+
+        Note:
+            Event args ``event_data`` is a dictionary containing all method parameters.
+
+        Attention:
+            :py:meth:`~.utils.lo.Lo.store_doc` method is called along with any of its events.
+
+        See Also:
+            :ref:`ch02_save_doc`
+
+        .. versionadded:: 0.20.2
+        """
+        return self._lo_inst.save_doc(self.component, fnm, password, format)  # type: ignore
+
+    # endregion save_doc
+
+    def close(self, deliver_ownership=True) -> None:
+        """
+        Try to close the Document.
+
+        Nobody can guarantee real closing of called object - because it can disagree with that if any still running processes can't be canceled yet.
+        It's not allowed to block this call till internal operations will be finished here.
+
+        See Also:
+            See LibreOffice API: `XCloseable.close() <https://api.libreoffice.org/docs/idl/ref/interfacecom_1_1sun_1_1star_1_1util_1_1XCloseable.html#af3d34677f1707b1904f8e07be4408592>`__
+        """
+        self.qi(XCloseable, True).close(deliver_ownership)
+
+    # region Properties
+
+    @property
+    def slides(self) -> DrawPages[DrawDoc]:
+        """
+        Returns:
+            Any: Draw Pages.
+        """
+        if self._pages is None:
+            self._pages = DrawPages(owner=self, slides=self.component.getDrawPages())
+        return cast("DrawPages[DrawDoc]", self._pages)
+
+    @property
+    def lo_inst(self) -> LoInst:
+        """
+        Returns:
+            LoInst: LibreOffice instance.
+        """
+        return self._lo_inst
+
+    # endregion Properties
---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.21.1-py3-none-any.whl/ooodev/draw/impress_doc.py
@@ -0,0 +1,403 @@
+from __future__ import annotations
+from typing import Any, cast, TYPE_CHECKING, List, overload
+import uno
+
+from ooodev.adapter.document.document_event_events import DocumentEventEvents
+from ooodev.adapter.presentation.presentation_document_comp import PresentationDocumentComp
+from ooodev.adapter.util.modify_events import ModifyEvents
+from ooodev.adapter.view.print_job_events import PrintJobEvents
+from ooodev.events.args.listener_event_args import ListenerEventArgs
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.office import draw as mDraw
+from ooodev.utils import lo as mLo
+from ooodev.utils.inst.lo.lo_inst import LoInst
+from ooodev.utils.partial.gui_partial import GuiPartial
+from ooodev.utils.partial.lo_open_doc import LoOpenPartial
+from ooodev.utils.partial.prop_partial import PropPartial
+from ooodev.utils.partial.qi_partial import QiPartial
+from ooodev.utils.partial.service_partial import ServicePartial
+from ooodev.utils.type_var import PathOrStr
+from .partial.draw_doc_partial import DrawDocPartial
+from . import impress_page as mImpressPage
+from . import master_draw_page as mMasterDrawPage
+from .draw_pages import DrawPages
+
+if TYPE_CHECKING:
+    from com.sun.star.lang import XComponent
+    from com.sun.star.drawing import XDrawPage
+    from com.sun.star.drawing import XDrawPages
+    from com.sun.star.presentation import XPresentation2
+    from com.sun.star.presentation import XSlideShowController
+
+
+class ImpressDoc(
+    DrawDocPartial["ImpressDoc"],
+    PresentationDocumentComp,
+    DocumentEventEvents,
+    ModifyEvents,
+    PrintJobEvents,
+    QiPartial,
+    PropPartial,
+    GuiPartial,
+    ServicePartial,
+    StylePartial,
+    LoOpenPartial,
+):
+    def __init__(self, doc: XComponent, lo_inst: LoInst | None = None) -> None:
+        if lo_inst is None:
+            self._lo_inst = mLo.Lo.current_lo
+        else:
+            self._lo_inst = lo_inst
+        DrawDocPartial.__init__(self, owner=self, component=doc, lo_inst=self._lo_inst)
+        PresentationDocumentComp.__init__(self, doc)
+        generic_args = self._ComponentBase__get_generic_args()  # type: ignore
+        DocumentEventEvents.__init__(self, trigger_args=generic_args, cb=self._on_document_event_add_remove)
+        ModifyEvents.__init__(self, trigger_args=generic_args, cb=self._on_modify_events_add_remove)
+        PrintJobEvents.__init__(self, trigger_args=generic_args, cb=self._on_print_job_add_remove)
+        QiPartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        PropPartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        GuiPartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        ServicePartial.__init__(self, component=doc, lo_inst=self._lo_inst)
+        StylePartial.__init__(self, component=doc)
+        LoOpenPartial.__init__(self, lo_inst=self._lo_inst)
+        self._pages = None
+
+    # region Lazy Listeners
+
+    def _on_modify_events_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addModifyListener(self.events_listener_modify)
+        event.remove_callback = True
+
+    def _on_document_event_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addDocumentEventListener(self.events_listener_document_event)
+        event.remove_callback = True
+
+    def _on_print_job_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.component.addPrintJobListener(self.events_listener_print_job)
+        event.remove_callback = True
+
+    # endregion Lazy Listeners
+    def add_slide(self) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Add a slide to the end of the document.
+
+        Raises:
+            DrawPageMissingError: If unable to get pages.
+            DrawPageError: If any other error occurs.
+
+        Returns:
+            mImpressPage: The slide that was inserted at the end of the document.
+        """
+        result = mDraw.Draw.add_slide(doc=self.component)
+        return mImpressPage.ImpressPage(owner=self, component=result, lo_inst=self._lo_inst)
+
+    def duplicate(self, idx: int) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Duplicates a slide
+
+        Args:
+            idx (int): Index of slide to duplicate.
+
+        Raises:
+            DrawError If unable to create duplicate.
+
+        Returns:
+            ImpressPage: Duplicated slide.
+        """
+        page = mDraw.Draw.duplicate(self.component, idx)
+        return mImpressPage.ImpressPage(owner=self, component=page, lo_inst=self._lo_inst)
+
+    # region get_slide()
+    @overload
+    def get_slide(self) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets draw page by page at index ``0``.
+
+        Returns:
+            ImpressPage: Draw Page.
+        """
+        ...
+
+    @overload
+    def get_slide(self, *, idx: int) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets draw page by page index
+
+        Args:
+            idx (int): Index of draw page. Default ``0``
+
+        Returns:
+            ImpressPage: Draw Page.
+        """
+        ...
+
+    @overload
+    def get_slide(self, *, slides: XDrawPages) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets draw page at index ``0`` from ``slides``.
+
+        Args:
+            slides (XDrawPages): Draw Pages
+
+        Returns:
+            ImpressPage: Draw Page.
+        """
+        ...
+
+    @overload
+    def get_slide(self, *, slides: XDrawPages, idx: int) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets draw page by page index from ``slides``.
+
+        Args:
+            slides (XDrawPages): Draw Pages
+            idx (int): Index of slide. Default ``0``
+
+        Returns:
+            ImpressPage: Slide as Draw Page.
+        """
+        ...
+
+    def get_slide(self, **kwargs) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets slide
+
+        Args:
+            slides (XDrawPages): Draw Pages
+            idx (int): Index of slide. Default ``0``
+
+        Raises:
+            IndexError: If ``idx`` is out of bounds
+            DrawError: If any other error occurs.
+
+        Returns:
+            ImpressPage: Slide as Draw Page.
+        """
+        if not kwargs:
+            result = mDraw.Draw.get_slide(doc=self.component)
+            return mImpressPage.ImpressPage(owner=self, component=result, lo_inst=self._lo_inst)
+        if "slides" not in kwargs:
+            kwargs["doc"] = self.component
+        result = mDraw.Draw.get_slide(**kwargs)
+        return mImpressPage.ImpressPage(owner=self, component=result, lo_inst=self._lo_inst)
+
+    # endregion get_slide()
+
+    def get_slides_list(self) -> List[mImpressPage.ImpressPage[ImpressDoc]]:
+        """
+        Gets all the slides as a list of XDrawPage
+
+        Returns:
+            List[ImpressPage[_T]]: List of pages
+        """
+        slides = mDraw.Draw.get_slides_list(self.component)
+        return [mImpressPage.ImpressPage(owner=self, component=slide, lo_inst=self._lo_inst) for slide in slides]
+
+    def get_viewed_page(self) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets viewed page
+
+        Raises:
+            DrawPageError: If error occurs.
+
+        Returns:
+            ImpressPage: Draw Page
+        """
+        page = mDraw.Draw.get_viewed_page(self.component)
+        return mImpressPage.ImpressPage(owner=self, component=page, lo_inst=self._lo_inst)
+
+    def get_handout_master_page(self) -> mMasterDrawPage.MasterDrawPage[ImpressDoc]:
+        """
+        Gets handout master page for an impress document.
+
+        Raises:
+            DrawError: If unable to get hand-out master page.
+            DrawPageMissingError: If Draw Page is ``None``.
+
+        Returns:
+            MasterDrawPage: Impress Page
+        """
+        page = mDraw.Draw.get_handout_master_page(self.component)
+        return mMasterDrawPage.MasterDrawPage(owner=self, component=page, lo_inst=self._lo_inst)
+
+    def get_notes_page_by_index(self, idx: int) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Gets notes page by index.
+
+        Each draw page has a notes page.
+
+        Args:
+            idx (int): Index
+
+        Raises:
+            DrawPageError: If error occurs.
+
+        Returns:
+            ImpressPage: Notes Page.
+
+        See Also:
+            :py:meth:`~.draw.Draw.get_notes_page`
+        """
+        page = mDraw.Draw.get_notes_page_by_index(self.component, idx)
+        return mImpressPage.ImpressPage(owner=self, component=page, lo_inst=self._lo_inst)
+
+    def get_show(self) -> XPresentation2:
+        """
+        Gets Slide show Presentation.
+
+        Raises:
+            DrawError: If error occurs.
+
+        Returns:
+            XPresentation2: Slide Show Presentation.
+        """
+        return mDraw.Draw.get_show(self.component)
+
+    def get_show_controller(self) -> XSlideShowController:
+        """
+        Gets slide show controller
+
+        Args:
+            show (XPresentation2): Slide Show Presentation
+
+        Raises:
+            DrawError: If error occurs.
+
+        Returns:
+            XSlideShowController: Slide Show Controller.
+
+        Note:
+            It may take a little bit for the slides show to start.
+            For this reason this method will wait up to five seconds.
+        """
+        show = self.get_show()
+        return mDraw.Draw.get_show_controller(show)
+
+    def insert_slide(self, idx: int) -> mImpressPage.ImpressPage[ImpressDoc]:
+        """
+        Inserts a slide at the given position in the document
+
+        Args:
+            idx (int): Index
+
+        Raises:
+            DrawPageMissingError: If unable to get pages.
+            DrawPageError: If any other error occurs.
+
+        Returns:
+            DrawPage: New slide that was inserted.
+        """
+        slide = mDraw.Draw.insert_slide(doc=self.component, idx=idx)
+        return mImpressPage.ImpressPage(owner=self, component=slide, lo_inst=self._lo_inst)
+
+    def remove_master_page(self, slide: XDrawPage) -> None:
+        """
+        Removes a master page.
+
+        Args:
+            slide (XDrawPage): Draw page to remove.
+
+        Raises:
+            DrawError: If unable to remove master page.
+
+        Returns:
+            None:
+        """
+        mDraw.Draw.remove_master_page(doc=self.component, slide=slide)
+
+    # region save_doc
+
+    @overload
+    def save_doc(self, fnm: PathOrStr) -> bool:
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+
+        Returns:
+            bool: ``False`` if ``DOC_SAVING`` event is canceled; Otherwise, ``True``
+        """
+        ...
+
+    @overload
+    def save_doc(self, fnm: PathOrStr, password: str) -> bool:
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+            password (str): Password to save document with.
+
+
+        Returns:
+            bool: ``False`` if ``DOC_SAVING`` event is canceled; Otherwise, ``True``
+        """
+        ...
+
+    @overload
+    def save_doc(self, fnm: PathOrStr, password: str, format: str) -> bool:  # pylint: disable=W0622
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+            password (str): Password to save document with.
+            format (str): document format such as 'odt' or 'xml'.
+
+        Returns:
+            bool: ``False`` if ``DOC_SAVING`` event is canceled; Otherwise, ``True``.
+        """
+        ...
+
+    def save_doc(self, fnm: PathOrStr, password: str | None = None, format: str | None = None) -> bool:
+        """
+        Save document.
+
+        Args:
+            fnm (PathOrStr): file path to save as.
+            password (str, optional): password to save document with.
+            format (str, optional): document format such as 'odt' or 'xml'.
+
+        Raises:
+            MissingInterfaceError: If doc does not implement XStorable interface.
+
+        Returns:
+            bool: ``False`` if DOC_SAVING event is canceled; Otherwise, ``True``
+
+        :events:
+            .. cssclass:: lo_event
+
+                - :py:attr:`~.events.lo_named_event.LoNamedEvent.DOC_SAVING` :eventref:`src-docs-event-cancel`
+                - :py:attr:`~.events.lo_named_event.LoNamedEvent.DOC_SAVED` :eventref:`src-docs-event`
+
+        Note:
+            Event args ``event_data`` is a dictionary containing all method parameters.
+
+        Attention:
+            :py:meth:`~.utils.lo.Lo.store_doc` method is called along with any of its events.
+
+        See Also:
+            :ref:`ch02_save_doc`
+
+        .. versionadded:: 0.20.2
+        """
+        return self._lo_inst.save_doc(self.component, fnm, password, format)  # type: ignore
+
+    # endregion save_doc
+
+    # region Properties
+    @property
+    def slides(self) -> DrawPages[ImpressDoc]:
+        """
+        Returns:
+            Any: Draw Pages.
+        """
+        if self._pages is None:
+            self._pages = DrawPages(owner=self, slides=self.component.getDrawPages(), lo_inst=self._lo_inst)
+        return cast("DrawPages[ImpressDoc]", self._pages)
+
+    # endregion Properties
