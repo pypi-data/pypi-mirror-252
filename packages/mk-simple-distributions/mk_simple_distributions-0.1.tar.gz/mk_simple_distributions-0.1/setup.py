fc1abf800b869113f400eedcb24dd1f766138aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:05:13 +0000

    Add package mk-simple-distributions/0.1/mk_simple_distributions-0.1.tar.gz

---
+++ packages/mk-simple-distributions/mk_simple_distributions-0.1.tar.gz/mk_simple_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mk_simple_distributions',
+      version='0.1',
+      description='Simple Gaussian and Binomial distributions',
+      packages=['mk_simple_distributions'],
+      zip_safe=False)
