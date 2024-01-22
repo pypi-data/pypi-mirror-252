33c4ed7f32f9f0e41d6a7abae17f26b4f7623db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:03:52 +0000

    Add package mathpachesfortrana/0.0.3/mathpachesfortrana-0.0.3.tar.gz

---
+++ packages/mathpachesfortrana/mathpachesfortrana-0.0.3.tar.gz/mathpachesfortrana-0.0.3/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 20:04:21 2023
+
+@author: JL
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mathpachesfortrana",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        # 在这里列出你的库所需的其他Python包
+    ],
+
+    author="Durant_Johnson57",
+    author_email="zjbj2030@163.com",
+    description="A short description of your awesome package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/yourusername/my-awesome-package",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
