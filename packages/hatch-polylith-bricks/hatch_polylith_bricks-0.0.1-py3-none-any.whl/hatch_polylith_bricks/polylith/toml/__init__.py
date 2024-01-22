7488b134df50c8906906f743eb9c55e026cc7683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 03:04:32 +0000

    Add package hatch-polylith-bricks/0.0.1/hatch_polylith_bricks-0.0.1-py3-none-any.whl

---
+++ packages/hatch-polylith-bricks/hatch_polylith_bricks-0.0.1-py3-none-any.whl/hatch_polylith_bricks/polylith/toml/__init__.py
@@ -0,0 +1,2 @@
+from hatch_polylith_bricks.polylith.toml.core import get_project_dependencies, get_project_package_includes, get_project_packages_from_polylith_section, read_toml_document
+__all__ = ['get_project_dependencies', 'get_project_package_includes', 'get_project_packages_from_polylith_section', 'read_toml_document']
