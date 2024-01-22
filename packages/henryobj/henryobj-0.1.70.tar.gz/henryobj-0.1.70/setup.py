d46c727e6628b1a0325c3921c6c8bcc0897d5369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:10 +0000

    Add package henryobj/0.1.70/henryobj-0.1.70.tar.gz

---
+++ packages/henryobj/henryobj-0.1.70.tar.gz/henryobj-0.1.70/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="henryobj",
+    version="0.1.70", # need to increment this everytime otherwise Pypi will not accept the new version
+     url='https://github.com/HenryObj/mypip',
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "tiktoken",
+        "requests",
+        "bs4"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+
+# "somepackage==1.2.3",  # if a specific version is required. Here we do it in a way where any version (latest stable) should work
