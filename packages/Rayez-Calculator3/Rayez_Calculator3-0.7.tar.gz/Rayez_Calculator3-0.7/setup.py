6d3fbcff2112724f1521b760c46d1de47c52addb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:03 +0000

    Add package Rayez-Calculator3/0.7/Rayez_Calculator3-0.7.tar.gz

---
+++ packages/Rayez-Calculator3/Rayez_Calculator3-0.7.tar.gz/Rayez_Calculator3-0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Rayez_Calculator3',
+    version='0.7',
+    description='Short description of your package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/your_username/your_package_name',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
