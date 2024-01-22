cfd7ba240ffaaf06270943ee281620bcaa00ed52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 16:04:26 +0000

    Add package srcdit/0.1.3/srcdit-0.1.3.tar.gz

---
+++ packages/srcdit/srcdit-0.1.3.tar.gz/srcdit-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='srcdit',
+    version='0.1.3',
+    author='Arpit Jain',
+    # description='Edit dotfiles from terminal',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
