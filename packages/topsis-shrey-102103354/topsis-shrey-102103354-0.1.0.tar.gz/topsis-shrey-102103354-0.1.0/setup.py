2938b0ad79c9a1056b00801aa91381d3c3885479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:30 +0000

    Add package topsis-shrey-102103354/0.1.0/topsis-shrey-102103354-0.1.0.tar.gz

---
+++ packages/topsis-shrey-102103354/topsis-shrey-102103354-0.1.0.tar.gz/topsis-shrey-102103354-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis-shrey-102103354',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'topsis = topsis.topsis:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
