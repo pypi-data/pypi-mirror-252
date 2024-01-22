3b4fc43b2220e0b075f9502e06a89e155bf4100f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:03 +0000

    Add package ezDnsShim/0.9.0/ezDnsShim-0.9.0.tar.gz

---
+++ packages/ezDnsShim/ezDnsShim-0.9.0.tar.gz/ezDnsShim-0.9.0/ezDnsShim/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+"""
+ezDnsDhim v1.0.0
+          ____            ____  _     _
+  ___ ___|  _ \ _ __  ___/ ___|| |__ (_)_ __ ___
+ / _ \_  / | | | '_ \/ __\___ \| '_ \| | '_ ` _ \
+|  __// /| |_| | | | \__ \___) | | | | | | | | | |
+ \___/___|____/|_| |_|___/____/|_| |_|_|_| |_| |_|
+                Easy DnsShim Python Library - 2024
+ 
+Author: Ricardo Abuchaim - ricardoabuchaim@gmail.com
+        https://github.com/rabuchaim/ezdnsshim/
+
+License: MIT
+
+ezDnsShim is based on Pydnsshim v1.5 library created by Registro.br
+https://ftp.registro.br/pub/dnsshim/pydnsshim_manual_en.pdf
+https://www.registro.br/dnsshim
+
+THIS  SOFTWARE  IS BASED ON ANOTHER SOFTWARE DEVELOPED BY REGISTRO.BR,
+AND  BOTH  AUTHORS PROVIDE THIS SOURCE CODE ''AS IS AND ANY EXPRESS OR
+IMPLIED  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES
+WARRANTY  OF  FITNESS  FOR  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT   SHALL   REGISTRO.BR   BE  LIABLE  FOR  ANY  DIRECT,  INDIRECT,
+INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE  OF  THIS  SOFTWARE,  EVEN  IF  ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+"""
+from ezdnsshim.ezdnsshim import *
