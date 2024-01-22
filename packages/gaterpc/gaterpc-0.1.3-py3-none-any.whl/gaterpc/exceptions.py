258fd1e3cc1d9847d494d32447cad45b98231f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:00 +0000

    Add package gaterpc/0.1.3/gaterpc-0.1.3-py3-none-any.whl

---
+++ packages/gaterpc/gaterpc-0.1.3-py3-none-any.whl/gaterpc/exceptions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Author      : ShiFan
+# Created Date: 2023/8/10 17:38
+from gettext import gettext as _
+
+
+class ServiceUnAvailableError(Exception):
+    def __init__(self, name):
+        super().__init__(
+            _("The {name} Service unavailable.").format(name=name),
+        )
+
+
+class BuysWorkersError(Exception):
+    def __init__(self):
+        super().__init__(_("No idle workers."))
+
+
+class RemoteException(Exception):
+    def __init__(self, except_class, except_value, except_traceback: list):
+        message = (f"\n"
+                   f"  RemoteTraceback ({except_class}):\n"
+                   f"  {except_value}\n"
+                   f"  {except_traceback}")
+        super().__init__(message)
+
+
+class BadGzip(OSError):
+    pass
+
+
+class HugeDataException(OSError):
+    pass
