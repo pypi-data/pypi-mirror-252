4d7f77836bae8af11b3affd4baf2f686673ff90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 04:04:16 +0000

    Add package neetbox/0.4.10/neetbox-0.4.10-py3-none-any.whl

---
+++ packages/neetbox/neetbox-0.4.10-py3-none-any.whl/neetbox/server/_daemon_server_launch_script.py
@@ -0,0 +1,26 @@
+import argparse
+import json
+import sys
+
+
+def run(argv):
+    if len(argv) <= 1:
+        print("_daemon_: Warning: empty daemon_config")
+        daemon_config = None
+    else:
+        ap = argparse.ArgumentParser()
+        ap.add_argument("--config")
+        args = ap.parse_args()
+        print(type(args.config))
+        print(args.config)
+        daemon_config = json.loads(args.config)
+        print("Daemon started with config:", daemon_config)
+    from .server_process import server_process
+
+    server_process(daemon_config)
+
+
+if __name__ == "__main__":
+    print("daemon server starting.\n name:\t", __name__, "\nargs:\t", sys.argv)
+    run(sys.argv)
+    print("daemon server exiting.")
