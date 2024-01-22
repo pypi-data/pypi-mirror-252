1307d825ae7602bdfdc4a1adafbcd2c2beb1dfaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 05:04:32 +0000

    Add package tableofcontents/2.0.0/tableofcontents-2.0.0.tar.gz

---
+++ packages/tableofcontents/tableofcontents-2.0.0.tar.gz/tableofcontents-2.0.0/tests/test_all.py
@@ -0,0 +1,24 @@
+import unittest
+from toc.toc import Toc
+
+# example test
+class TestToc(unittest.TestCase):
+    def test_set_character(self):
+        test_cases = [
+            ("file.c", "//"),
+            ("file.ini", ";"),
+            ("file.tex", "%"),
+            ("file.sql", "--"),
+            ("file.unknown", "#"),
+            ("file.", "#"),
+            ("file", "#"),
+        ]
+
+        for file, expected_character in test_cases:
+            with self.subTest(file=file, expected_character=expected_character):
+                toc = Toc(file=file)
+                character = toc.set_character()
+                self.assertEqual(character, expected_character)
+
+if __name__ == "__main__":
+    unittest.main()
