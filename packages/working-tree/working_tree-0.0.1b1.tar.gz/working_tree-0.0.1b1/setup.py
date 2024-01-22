355998d3c692acd022be293889b1576fd2b9de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:25 +0000

    Add package working-tree/0.0.1b1/working_tree-0.0.1b1.tar.gz

---
+++ packages/working-tree/working_tree-0.0.1b1.tar.gz/working_tree-0.0.1b1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='working_tree',
+    version='0.0.1b1',
+    license='MIT',
+    author="Jahongir Ibragimov",
+    author_email='chogirmali.yigit@gmail.com',
+    packages=['working_tree'],
+    url='https://github.com/ChogirmaliYigit/working-tree',
+    keywords='working tree',
+    install_requires=[
+        'gitignore-parser==0.1.11',
+        "pyyaml==6.0.1",
+    ],
+)
