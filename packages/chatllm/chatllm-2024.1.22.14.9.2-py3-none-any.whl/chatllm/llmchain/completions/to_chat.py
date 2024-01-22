fe6533f1fb0a09f976acb1efb1022e1537f96d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:02 +0000

    Add package chatllm/2024.1.22.14.9.2/chatllm-2024.1.22.14.9.2-py3-none-any.whl

---
+++ packages/chatllm/chatllm-2024.1.22.14.9.2-py3-none-any.whl/chatllm/llmchain/completions/to_chat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : to_chat
+# @Time         : 2024/1/22 12:09
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+
+
+class Completions(object):
+    pass
