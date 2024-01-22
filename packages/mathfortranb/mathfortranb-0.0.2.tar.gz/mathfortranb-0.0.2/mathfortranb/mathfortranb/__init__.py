309e5e88794a860a94c6d4e075b66a9de8a39fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-22 15:04:04 +0000

    Add package mathfortranb/0.0.2/mathfortranb-0.0.2.tar.gz

---
+++ packages/mathfortranb/mathfortranb-0.0.2.tar.gz/mathfortranb-0.0.2/mathfortranb/mathfortranb/__init__.py
@@ -0,0 +1,4 @@
+from mathfortranb import mathcn
+from mathfortranb import mathcs
+from mathfortranb import mathpcn
+from mathfortranb import mathpcs
---
+++ packages/mathfortranb/mathfortranb-0.0.2.tar.gz/mathfortranb-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="mathfortranb",
+    version="0.0.2",
+    author="bug404",
+    author_email="z19040042@s.upc.edu.cn",
+    description="Some environments for reinforcement learning.",
+    long_description="Some environments for reinforcement learning.",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
