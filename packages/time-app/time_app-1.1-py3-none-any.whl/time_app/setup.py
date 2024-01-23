7775c98844d9b86df9776319d6ccbcfe788230da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:35 +0000

    Add package time-app/1.1/time_app-1.1-py3-none-any.whl

---
+++ packages/time-app/time_app-1.1-py3-none-any.whl/time_app/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='capcha.widget_py',
+	version='1.5',
+	author='BornTheHell',
+	author_email='anp50158@gmail.com',
+	description='capcha.widget_py',
+	packages=['capcha.widget_py'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
