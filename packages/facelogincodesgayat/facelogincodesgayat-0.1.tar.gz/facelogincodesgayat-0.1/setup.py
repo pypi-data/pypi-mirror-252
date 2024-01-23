653663c0955767be57c21ea212cbbffd28abd111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:03:51 +0000

    Add package facelogincodesgayat/0.1/facelogincodesgayat-0.1.tar.gz

---
+++ packages/facelogincodesgayat/facelogincodesgayat-0.1.tar.gz/facelogincodesgayat-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open ("README.md") as f:
+    description = f.read()
+    
+setup(
+    name='facelogincodesgayat',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your package may have
+        "face-recognition",
+        "face-recognition-models",
+        "opencv-python",
+        "cmake",
+        "dlib",
+        "numpy",
+    ],
+)
+
+# "face-recognition==1.3.0",
+#         "face-recognition-models==0.3.0",
+#         "opencv-python==4.8.1.78",
+#         "cmake==3.27.7",
+#         "dlib==19.24.1",
+#         "numpy==1.26.2",
