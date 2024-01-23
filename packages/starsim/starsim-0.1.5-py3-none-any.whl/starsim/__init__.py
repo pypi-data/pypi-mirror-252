7827fba07fc4b29dada93b6e63899031bf3835ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:24 +0000

    Add package starsim/0.1.5/starsim-0.1.5-py3-none-any.whl

---
+++ packages/starsim/starsim-0.1.5-py3-none-any.whl/starsim/__init__.py
@@ -0,0 +1,25 @@
+from .version import __version__, __versiondate__, __license__
+from .settings      import *
+from .parameters    import *
+from .utils         import *
+from .distributions import *
+from .states        import *
+from .random        import *
+from .people        import *
+from .modules       import *
+from .networks      import *
+from .results       import *
+from .demographics  import *
+from .interventions import *
+from .analyzers     import *
+from .demographics  import *
+from .connectors    import *
+from .diseases      import *
+from .sim           import *
+
+import sciris as sc
+root = sc.thispath(__file__).parent
+
+# Import the version and print the license
+if options.verbose:
+    print(__license__)
---
+++ packages/starsim/starsim-0.1.5-py3-none-any.whl/starsim/diseases/__init__.py
@@ -0,0 +1,5 @@
+from .disease       import *
+from .hiv           import *
+from .gonorrhea     import *
+from .examples      import *
+from .syphilis      import *
