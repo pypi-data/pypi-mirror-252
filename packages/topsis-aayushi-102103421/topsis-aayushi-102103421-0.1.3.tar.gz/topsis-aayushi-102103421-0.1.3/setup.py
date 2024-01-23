58ec580c72f3148418496338f4bdba32b9f9f283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:32 +0000

    Add package topsis-aayushi-102103421/0.1.3/topsis-aayushi-102103421-0.1.3.tar.gz

---
+++ packages/topsis-aayushi-102103421/topsis-aayushi-102103421-0.1.3.tar.gz/topsis-aayushi-102103421-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='topsis-aayushi-102103421',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        "pandas","numpy"
+    ],
+    author='Aayushi Bareja',
+    author_email='aayushi.bareja@gmail.com',
+    description='Topsis Implementation Package',
+    long_description=open('README.md', encoding='utf-8').read(),
+
+    long_description_content_type='text/markdown',
+    
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
