5429fe51eb9eaa15e878ada25c992da92dcc23f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:07 +0000

    Add package customTestCalculator/1.0.0/customTestCalculator-1.0.0-py3-none-any.whl

---
+++ packages/customTestCalculator/customTestCalculator-1.0.0-py3-none-any.whl/customTestCalculator/__init__.py
@@ -0,0 +1,1 @@
+from .calc import customAddition, customSubtraction, customMultiplication, customDivision
---
+++ packages/customTestCalculator/customTestCalculator-1.0.0-py3-none-any.whl/customTestCalculator/calc.py
@@ -0,0 +1,11 @@
+def customAddition(a,b):
+    return a + b
+
+def customSubtraction(a,b):
+    return a - b
+
+def customMultiplication(a,b):
+    return a * b
+
+def customDivision(a,b):
+    return a / b
