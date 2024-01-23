77c8adf0cd196f9834091ff41ab645ddc250a59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:25 +0000

    Add package ampel-interface/0.8.12/ampel_interface-0.8.12-py3-none-any.whl

---
+++ packages/ampel-interface/ampel_interface-0.8.12-py3-none-any.whl/ampel/secret/AmpelVault.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                Ampel-interface/ampel/secret/AmpelVault.py
+# License:             BSD-3-Clause
+# Author:              valery brinnel <firstname.lastname@gmail.com>
+# Date:                21.06.2021
+# Last Modified Date:  22.06.2021
+# Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
+
+from typing import Any, overload
+from ampel.abstract.AbsSecretProvider import AbsSecretProvider
+from ampel.secret.Secret import Secret, T
+from ampel.secret.NamedSecret import NamedSecret
+
+
+class AmpelVault:
+	""" Collection of secret providers """
+
+	def __init__(self, providers: list[AbsSecretProvider]) -> None:
+		self.providers = providers
+
+	def resolve_secret(self, secret: Secret, ValueType: type) -> bool:
+
+		for sp in self.providers:
+			if sp.tell(secret, ValueType):
+				return True
+		return False
+
+	@overload
+	def get_named_secret(self, label: str) -> None | NamedSecret[Any]:
+		...
+	
+	@overload
+	def get_named_secret(self, label: str, ValueType: type[T]) -> None | NamedSecret[T]:
+		...
+	
+	def get_named_secret(self, label, ValueType=object):
+		""" Returns a resolved NamedSecret using provided label """
+		ns = NamedSecret(label=label)
+		if self.resolve_secret(ns, ValueType):
+			return ns
+		return None
