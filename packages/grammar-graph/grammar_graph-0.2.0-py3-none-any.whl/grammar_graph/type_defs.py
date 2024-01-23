2953ebbe29fe42f99057d19dc7775bd8f166da8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:15 +0000

    Add package grammar-graph/0.2.0/grammar_graph-0.2.0-py3-none-any.whl

---
+++ packages/grammar-graph/grammar_graph-0.2.0-py3-none-any.whl/grammar_graph/type_defs.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Tuple, TypeVar, Optional, List, Iterator, Dict
+
+T = TypeVar('T')
+Path = Tuple[int, ...]
+Tree = Tuple[T, Optional[List['Tree[T]']]]
+
+NonterminalType = str
+Grammar = Dict[NonterminalType, List[str]]
+ImmutableGrammar = Tuple[Tuple[str, Tuple[str, ...]], ...]
+
+
+class ParseTree(ABC):
+    @classmethod
+    def __subclasshook__(cls, C):
+        return hasattr(C, "__iter__") and hasattr(C, "__getitem__")
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[str | List['ParseTree'] | None]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def __getitem__(self, item: int) -> str | List['ParseTree'] | None:
+        raise NotImplementedError()
