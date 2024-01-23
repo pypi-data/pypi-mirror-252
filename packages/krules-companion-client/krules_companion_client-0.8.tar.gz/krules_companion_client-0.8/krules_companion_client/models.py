fd140800bd0285bafe9e60f50eb1005765376616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:36 +0000

    Add package krules-companion-client/0.8/krules_companion_client-0.8.tar.gz

---
+++ packages/krules-companion-client/krules_companion_client-0.8.tar.gz/krules_companion_client-0.8/krules_companion_client/models.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from types import NoneType
+
+from enum import Enum
+
+from typing import Dict, Generic, TypeVar, Sequence
+from pydantic import BaseModel
+
+T = TypeVar("T")
+
+
+class EventType(str, Enum):
+    EntityCreated = "io.krules.streams.entity.v1.created"
+    EntityUpdated = "io.krules.streams.entity.v1.updated"
+    EntityDeleted = "io.krules.streams.entity.v1.deleted"
+    EntityCallback = "io.krules.streams.entity.v1.callback"
+
+
+class EntityUpdatedEvent(BaseModel, Generic[T]):
+    id: str
+    group: str
+    subscription: int
+    changed_properties: Sequence[str]
+    state: T
+    old_state: T
+
+
+class EntityCreatedEvent(BaseModel, Generic[T]):
+    id: str
+    group: str
+    subscription: int
+    changed_properties: Sequence[str]
+    state: T
+    old_state: NoneType
+
+
+class EntityDeletedEvent(BaseModel, Generic[T]):
+    id: str
+    group: str
+    subscription: int
+    changed_properties: Sequence[str]
+    state: NoneType
+    old_state: T
+
+
+class EntityCallbackEvent(BaseModel, Generic[T]):
+    last_updated: datetime
+    task_id: str
+    id: str
+    group: str
+    subscription: int
+    state: T
+    message: str
