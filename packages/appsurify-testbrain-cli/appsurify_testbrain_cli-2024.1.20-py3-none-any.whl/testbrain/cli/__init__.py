1558ca0511856aa446bf797001fe77e1226a4a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:19 +0000

    Add package appsurify-testbrain-cli/2024.1.20/appsurify_testbrain_cli-2024.1.20-py3-none-any.whl

---
+++ packages/appsurify-testbrain-cli/appsurify_testbrain_cli-2024.1.20-py3-none-any.whl/testbrain/cli/__init__.py
@@ -0,0 +1,9 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)  # noqa
+
+__title__ = "Appsurify Testbrain CLI"
+__name__ = "appsurify-testbrain-cli"
+__prog__ = "testbrain-cli"
+__version__ = "2024.1.20"
+__build__ = "undefined"
---
+++ packages/appsurify-testbrain-cli/appsurify_testbrain_cli-2024.1.20-py3-none-any.whl/testbrain/cli/cli.py
@@ -0,0 +1,32 @@
+import logging
+
+import click
+
+import testbrain.cli
+from testbrain.cli.apps.auth.cli import auth
+from testbrain.cli.apps.repository.cli import repository
+from testbrain.cli.core.command import TestbrainContext, TestbrainGroup
+
+logger = logging.getLogger(__name__)
+
+
+@click.group(
+    name=testbrain.cli.__prog__,
+    cls=TestbrainGroup,
+    default_if_no_args=True,
+    no_args_is_help=True,
+)
+@click.version_option(  # TODO: "%(package)s (%(prog)s %(version)s)"
+    package_name=testbrain.cli.__name__,
+    prog_name=testbrain.cli.__prog__,
+    version=testbrain.cli.__version__,
+    message="%(package)s (%(version)s) [%(prog)s]",
+)
+@click.pass_context
+def app(ctx: TestbrainContext, **kwargs):
+    logger.debug(f"testbrain run with {ctx} {kwargs}")
+
+
+# TODO: Will be needed refactoring
+app.add_command(auth, "auth")
+app.add_command(repository, "repository")
