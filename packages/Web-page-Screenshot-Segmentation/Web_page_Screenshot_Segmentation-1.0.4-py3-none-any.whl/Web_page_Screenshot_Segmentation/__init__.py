96e42ec4cba930c7fb210eee26009ba6831869f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:03:52 +0000

    Add package Web-page-Screenshot-Segmentation/1.0.4/Web_page_Screenshot_Segmentation-1.0.4-py3-none-any.whl

---
+++ packages/Web-page-Screenshot-Segmentation/Web_page_Screenshot_Segmentation-1.0.4-py3-none-any.whl/Web_page_Screenshot_Segmentation/__init__.py
@@ -0,0 +1,5 @@
+from .blank_spliter import find_height_spliter
+from .color_spliter import color_height_spliter
+from .drawer import draw_line
+from .spliter import split_and_save_image, split_and_save_image_pil
+from .master import split_heights
