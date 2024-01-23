0718f7b422f9692d4b2977eb1aac001c236da9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:22 +0000

    Add package ooo-dev-tools/0.21.3/ooo_dev_tools-0.21.3-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.21.3-py3-none-any.whl/ooodev/write/write_paragraph_cursor.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import Any
+import uno
+
+from com.sun.star.text import XParagraphCursor
+
+from ooodev.adapter.text.paragraph_cursor_partial import ParagraphCursorPartial
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.utils import lo as mLo
+from ooodev.utils.inst.lo.lo_inst import LoInst
+from .write_text_cursor import WriteTextCursor
+
+
+class WriteParagraphCursor(WriteTextCursor, ParagraphCursorPartial, StylePartial):
+    """Represents a writer Paragraph cursor."""
+
+    def __init__(self, owner: Any, component: XParagraphCursor, lo_inst: LoInst | None = None) -> None:
+        """
+        Constructor
+
+        Args:
+            owner (WriteDoc): Doc that owns this component.
+            component (XParagraphCursor): A UNO object that supports ``com.sun.star.text.XParagraphCursor`` interface.
+            lo_inst (LoInst, optional): Lo instance. Defaults to ``None``.
+        """
+        if lo_inst is None:
+            self._lo_inst = mLo.Lo.current_lo
+        else:
+            self._lo_inst = lo_inst
+        WriteTextCursor.__init__(self, owner=owner, component=component, lo_inst=self._lo_inst)
+        ParagraphCursorPartial.__init__(self, component, None)  # type: ignore
+        StylePartial.__init__(self, component=component)
---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.21.3-py3-none-any.whl/ooodev/write/write_text_frame.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, TypeVar, Generic
+import uno
+
+
+if TYPE_CHECKING:
+    from com.sun.star.text import XTextFrame
+
+from ooodev.adapter.text.text_frame_comp import TextFrameComp
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.proto.component_proto import ComponentT
+from ooodev.utils import lo as mLo
+from ooodev.utils.partial.prop_partial import PropPartial
+from ooodev.utils.partial.qi_partial import QiPartial
+from ooodev.utils.inst.lo.lo_inst import LoInst
+
+T = TypeVar("T", bound="ComponentT")
+
+
+class WriteTextFrame(Generic[T], TextFrameComp, QiPartial, PropPartial, StylePartial):
+    """Represents writer text content."""
+
+    def __init__(self, owner: T, component: XTextFrame, lo_inst: LoInst | None = None) -> None:
+        """
+        Constructor
+
+        Args:
+            owner (T): Owner of this component.
+            component (XTextFrame): UNO object that supports ``com.sun.star.text.TextFrame`` service.
+            lo_inst (LoInst, optional): Lo instance. Defaults to ``None``.
+        """
+        if lo_inst is None:
+            self._lo_inst = mLo.Lo.current_lo
+        else:
+            self._lo_inst = lo_inst
+        self.__owner = owner
+        TextFrameComp.__init__(self, component)  # type: ignore
+        QiPartial.__init__(self, component=component, lo_inst=self._lo_inst)  # type: ignore
+        PropPartial.__init__(self, component=component, lo_inst=self._lo_inst)  # type: ignore
+        StylePartial.__init__(self, component=component)
+
+    # region Properties
+    @property
+    def owner(self) -> T:
+        """Owner of this component."""
+        return self.__owner
+
+    # endregion Properties
