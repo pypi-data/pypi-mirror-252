ac4641ced0f6c80fdbe8f49a36d9e82accd899f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:09 +0000

    Add package Topsis-Arshiya-102103150/1.3.1/Topsis-Arshiya-102103150-1.3.1.tar.gz

---
+++ packages/Topsis-Arshiya-102103150/Topsis-Arshiya-102103150-1.3.1.tar.gz/Topsis-Arshiya-102103150-1.3.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name="Topsis-Arshiya-102103150",
+	version='1.3.1',
+	author='Arshiya Sethi;',
+	author_email='arshiyasethi55@yahoo.com',
+	description='A python package to implement TOPSIS on a given dataset',
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[            
+          'numpy',
+          'pandas',
+      ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',    
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+    python_requires='>=3.6',
+	)
