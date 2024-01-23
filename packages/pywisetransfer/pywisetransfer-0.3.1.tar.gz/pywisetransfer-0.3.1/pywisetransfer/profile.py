4921a65bfd141012cdabe1515bc9a8b5ae8dba0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:04:10 +0000

    Add package pywisetransfer/0.3.1/pywisetransfer-0.3.1.tar.gz

---
+++ packages/pywisetransfer/pywisetransfer-0.3.1.tar.gz/pywisetransfer-0.3.1/pywisetransfer/profile.py
@@ -0,0 +1,26 @@
+from typing import Any, Optional
+
+from apiron import JsonEndpoint
+from munch import munchify
+
+from pywisetransfer import Client
+from pywisetransfer.base import Base
+
+
+class ProfileService(Base):
+    list = JsonEndpoint(path="/v1/profiles")
+    get = JsonEndpoint(path="/v1/profiles/{profile_id}")
+
+
+class Profile:
+    def __init__(self, client: Client):
+        self.service = ProfileService(client=client)
+
+    def list(self, type: Optional[str] = None) -> list[Any]:
+        profiles: list[Any] = munchify(self.service.list())
+        if type is None:
+            return profiles
+        return [p for p in profiles if p.type == type]
+
+    def get(self, profile_id: str) -> Any:
+        return munchify(self.service.get(profile_id=profile_id))
