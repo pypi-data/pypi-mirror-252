f8ae139e2c9c6d6c1491adc1220308bf372987cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:31 +0000

    Add package stamina-pipeline/0.0.13/stamina_pipeline-0.0.13-py3-none-any.whl

---
+++ packages/stamina-pipeline/stamina_pipeline-0.0.13-py3-none-any.whl/stamina/src/desktop/desktop.py
@@ -0,0 +1,20 @@
+from ..core.logging import log
+from ..core import config
+from ..core import enums
+from ..core.update import check_update
+from ..core.socketUtils import disconnect
+
+import subprocess
+
+APP = enums.apps.Desktop
+
+
+def main():
+    log(f'Stamina Desktop Started! Version: {config.STAMINA_VERSION}')
+
+    disconnect()
+
+
+if __name__ == '__main__':
+    main()
+
