8773d090b3d466869220ff065955c737a0ca9ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:26 +0000

    Add package sqlmesh/0.68.1/sqlmesh-0.68.1.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.68.1.tar.gz/sqlmesh-0.68.1/web/client/src/library/components/documentation/Documentation.tsx
@@ -0,0 +1,333 @@
+import React from 'react'
+import { Disclosure, Tab } from '@headlessui/react'
+import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/solid'
+import { EnumFileExtensions } from '@models/file'
+import {
+  PATH_SEPARATOR,
+  isArrayNotEmpty,
+  isFalse,
+  isString,
+  isTrue,
+  toDateFormat,
+  truncate,
+} from '@utils/index'
+import clsx from 'clsx'
+import { type ModelSQLMeshModel } from '@models/sqlmesh-model'
+import { ModelColumns } from '@components/graph/Graph'
+import { useLineageFlow } from '@components/graph/context'
+import TabList from '@components/tab/Tab'
+import { useSQLMeshModelExtensions } from '@components/editor/hooks'
+import {
+  CodeEditorDefault,
+  CodeEditorRemoteFile,
+} from '@components/editor/EditorCode'
+
+const Documentation = function Documentation({
+  model,
+  withModel = true,
+  withDescription = true,
+  withColumns = true,
+  withCode = true,
+  withQuery = true,
+}: {
+  model: ModelSQLMeshModel
+  withCode?: boolean
+  withQuery?: boolean
+  withModel?: boolean
+  withDescription?: boolean
+  withColumns?: boolean
+}): JSX.Element {
+  const { handleClickModel } = useLineageFlow()
+
+  const modelExtensions = useSQLMeshModelExtensions(model.path, model => {
+    handleClickModel?.(model.name)
+  })
+
+  return (
+    <Container>
+      {withModel && (
+        <Section
+          headline="Model"
+          defaultOpen={true}
+        >
+          <ul className="px-2 w-full">
+            <DetailsItem
+              name="Path"
+              value={model.path
+                .split(PATH_SEPARATOR)
+                .slice(0, -1)
+                .join(PATH_SEPARATOR)}
+            />
+            <DetailsItem
+              name="Name"
+              title={model.displayName}
+              value={truncate(model.displayName, 50, 25)}
+            />
+            <DetailsItem
+              name="Dialect"
+              value={model.dialect}
+            />
+            <DetailsItem
+              name="Type"
+              value={model.type}
+            />
+            {Object.entries(model.details ?? {}).map(([key, value]) => (
+              <DetailsItem
+                key={key}
+                name={key.replaceAll('_', ' ')}
+                value={value}
+                isCapitalize={true}
+              />
+            ))}
+          </ul>
+        </Section>
+      )}
+      {withDescription && (
+        <Section
+          headline="Description"
+          defaultOpen={true}
+        >
+          {model.description ?? 'No description'}
+        </Section>
+      )}
+      {withColumns && (
+        <Section headline="Columns">
+          <ModelColumns
+            className="max-h-[15rem]"
+            nodeId={model.name}
+            columns={model.columns}
+            disabled={model?.isModelPython}
+            withHandles={false}
+            withSource={false}
+            withDescription={true}
+            limit={10}
+          />
+        </Section>
+      )}
+      {(withCode || withQuery) && (
+        <Section headline="SQL">
+          <CodeEditorRemoteFile path={model.path}>
+            {({ file }) => (
+              <Tab.Group defaultIndex={withQuery ? 1 : 0}>
+                <TabList
+                  list={
+                    [
+                      withCode && 'Source Code',
+                      withQuery && 'Compiled Query',
+                    ].filter(Boolean) as string[]
+                  }
+                  className="!justify-center"
+                />
+
+                <Tab.Panels className="h-full w-full overflow-hidden bg-neutral-10 mt-4 rounded-lg">
+                  {withCode && (
+                    <Tab.Panel
+                      unmount={false}
+                      className={clsx(
+                        'flex flex-col w-full h-full relative px-2 overflow-hidden p-2',
+                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                      )}
+                    >
+                      <CodeEditorDefault
+                        content={file.content}
+                        type={file.extension}
+                        extensions={modelExtensions}
+                        className="text-xs"
+                      />
+                    </Tab.Panel>
+                  )}
+                  {withQuery && (
+                    <Tab.Panel
+                      unmount={false}
+                      className="w-full h-full ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2 p-2"
+                    >
+                      <CodeEditorDefault
+                        type={EnumFileExtensions.SQL}
+                        content={model.sql ?? ''}
+                        extensions={modelExtensions}
+                        className="text-xs"
+                      />
+                    </Tab.Panel>
+                  )}
+                </Tab.Panels>
+              </Tab.Group>
+            )}
+          </CodeEditorRemoteFile>
+        </Section>
+      )}
+    </Container>
+  )
+}
+
+function Headline({ headline }: { headline: string }): JSX.Element {
+  return (
+    <div
+      className="text-lg font-bold whitespace-nowrap w-full"
+      id={headline}
+    >
+      <h3 className="py-2">{headline}</h3>
+    </div>
+  )
+}
+
+function NotFound(): JSX.Element {
+  return (
+    <Container className="font-bold whitespace-nowrap w-full h-full">
+      <div className="flex items-center justify-center w-full h-full">
+        Documentation Not Found
+      </div>
+    </Container>
+  )
+}
+
+function Container({
+  children,
+  className,
+}: {
+  className?: string
+  children: React.ReactNode
+}): JSX.Element {
+  return (
+    <div className={clsx('w-full h-full rounded-xl', className)}>
+      <div className="w-full h-full py-4 rounded-xl">
+        <div className="w-full h-full overflow-auto hover:scrollbar scrollbar--vertical scrollbar--horizontal">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Section({
+  children,
+  className,
+  headline,
+  defaultOpen = false,
+}: {
+  headline: string
+  children: React.ReactNode
+  className?: string
+  defaultOpen?: boolean
+}): JSX.Element {
+  return (
+    <div className="px-4">
+      <Disclosure defaultOpen={defaultOpen}>
+        {({ open }) => (
+          <>
+            <Disclosure.Button
+              className={clsx(
+                'flex items-center justify-between rounded-lg text-left w-full bg-neutral-10 px-3 mb-2 overflow-hidden',
+                className,
+              )}
+            >
+              <Headline headline={headline} />
+              <div>
+                {open ? (
+                  <MinusCircleIcon className="h-6 w-6 text-neutral-50" />
+                ) : (
+                  <PlusCircleIcon className="h-6 w-6 text-neutral-50" />
+                )}
+              </div>
+            </Disclosure.Button>
+            <Disclosure.Panel className="pb-2 overflow-hidden">
+              <div className="px-4 mb-2 text-xs">{children}</div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  )
+}
+
+function DetailsItem<TValue = Record<string, Primitive>>({
+  className,
+  name,
+  value,
+  isHighlighted = false,
+  isCapitalize = false,
+  children,
+  title,
+}: {
+  name: string
+  title?: string
+  value: Primitive | TValue[]
+  className?: string
+  isHighlighted?: boolean
+  isCapitalize?: boolean
+  children?: React.ReactNode
+}): JSX.Element {
+  return (
+    <li
+      className={clsx('w-full border-b border-primary-10 py-1 mb-1', className)}
+    >
+      {isArrayNotEmpty<TValue>(value) ? (
+        <>
+          <strong
+            title={title}
+            className="mr-2 text-xs capitalize"
+          >
+            {name}
+          </strong>
+          {value.map((item, idx) => (
+            <span
+              className="w-full items-center flex ml-2 mb-1"
+              key={idx}
+            >
+              <ul className="w-full flex ml-3 whitespace-nowrap">
+                {Object.entries<Primitive>(
+                  item as Record<string, Primitive>,
+                ).map(([key, val]) => (
+                  <li
+                    key={key}
+                    className="flex"
+                  >
+                    <div className="flex text-xs">
+                      <strong className="mr-2">{key}:</strong>
+                      <p className="text-xs rounded text-neutral-500 dark:text-neutral-400">
+                        {getValue(val)}
+                      </p>
+                      <span className="px-2 text-neutral-20">|</span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </span>
+          ))}
+        </>
+      ) : (
+        <div className="flex justify-between text-xs whitespace-nowrap">
+          <strong
+            className={clsx(
+              'mr-2',
+              isCapitalize && 'capitalize',
+              isHighlighted && 'text-brand-500',
+            )}
+          >
+            {name}
+          </strong>
+          <p className="text-xs rounded text-neutral-500 dark:text-neutral-400">
+            {getValue(value)}
+          </p>
+        </div>
+      )}
+      <p className="text-xs ">{children}</p>
+    </li>
+  )
+}
+
+Documentation.NotFound = NotFound
+Documentation.Container = Container
+
+export default Documentation
+
+function getValue(value: Primitive): string {
+  const maybeDate = new Date(value as string)
+  const isDate = isString(value) && !isNaN(maybeDate.getTime())
+  const isBoolean = typeof value === 'boolean'
+
+  if (isBoolean && isTrue(value)) return 'True'
+  if (isBoolean && isFalse(value)) return 'False'
+  if (isDate) return toDateFormat(maybeDate)
+
+  return String(value)
+}
---
+++ packages/sqlmesh/sqlmesh-0.68.1.tar.gz/sqlmesh-0.68.1/web/client/src/library/components/report/ReportErrors.tsx
@@ -0,0 +1,215 @@
+import { Disclosure, Popover, Transition } from '@headlessui/react'
+import pluralize from 'pluralize'
+import clsx from 'clsx'
+import React, { useState, Fragment, useEffect } from 'react'
+import { useIDE, type ErrorIDE, type ErrorKey } from '../../pages/ide/context'
+import { isNotNil, toDate, toDateFormat } from '@utils/index'
+import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/solid'
+import { Divider } from '@components/divider/Divider'
+import { useStoreContext } from '@context/context'
+import { Button } from '@components/button/Button'
+import { EnumSize, EnumVariant } from '~/types/enum'
+
+export default function ReportErrors(): JSX.Element {
+  const { errors, clearErrors } = useIDE()
+
+  const [isShow, setIsShow] = useState(false)
+
+  useEffect(() => {
+    if (errors.size < 1) {
+      setIsShow(false)
+    }
+  }, [errors])
+
+  const hasError = errors.size > 0
+
+  return (
+    <Popover
+      onMouseEnter={() => {
+        setIsShow(hasError)
+      }}
+      onMouseLeave={() => {
+        setIsShow(false)
+      }}
+      className="flex"
+    >
+      {() => (
+        <>
+          <span
+            className={clsx(
+              'block ml-1 px-2 first-child:ml-0 rounded-full border whitespace-nowrap dark:text-neutral-100 text-xs text-center',
+              hasError
+                ? 'border-danger-500 text-danger-100 bg-danger-500 cursor-pointer'
+                : 'border-neutral-500 cursor-default text-neutral-700',
+            )}
+          >
+            {hasError ? (
+              <span>
+                {errors.size} {pluralize('Error', errors.size)}
+              </span>
+            ) : (
+              <span>No Errors</span>
+            )}
+          </span>
+          <Transition
+            show={isShow}
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel
+              className={clsx(
+                'absolute top-20 right-2 z-[1000] rounded-md bg-light transform text-danger-700 shadow-2xl',
+                'w-[90vw] max-h-[80vh]',
+              )}
+            >
+              <div className="flex justify-end mx-1 mt-2">
+                <Button
+                  size={EnumSize.sm}
+                  variant={EnumVariant.Neutral}
+                  onClick={() => clearErrors()}
+                >
+                  Clear
+                </Button>
+              </div>
+              <ul
+                className={clsx(
+                  'w-full p-2 overflow-auto hover:scrollbar scrollbar--vertical scrollbar--horizontal',
+                  'max-w-[90vw] max-h-[80vh]',
+                )}
+              >
+                {Array.from(errors)
+                  .reverse()
+                  .map(error => (
+                    <li
+                      key={error.id}
+                      className="bg-danger-10 mb-4 last:m-0 p-2 rounded-md"
+                    >
+                      <DisplayError
+                        scope={error.key}
+                        error={error}
+                      />
+                    </li>
+                  ))}
+              </ul>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+  )
+}
+
+export function DisplayError({
+  error,
+  scope,
+}: {
+  scope: ErrorKey
+  error: ErrorIDE
+}): JSX.Element {
+  const { removeError } = useIDE()
+
+  const version = useStoreContext(s => s.version)
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="flex w-full h-full">
+        <div className="w-[20rem]">
+          <div className="flex aline-center mb-2">
+            <small className="block mr-3">
+              <small className="py-0.5 px-2 bg-danger-500 text-danger-100 rounded-md text-xs">
+                {error.status ?? 'UI'}
+              </small>
+            </small>
+            <small className="block">{scope}</small>
+          </div>
+          <div className={clsx('text-sm px-2')}>
+            <small className="block text-xs font-mono">
+              {toDateFormat(
+                toDate(error.timestamp),
+                'yyyy-mm-dd hh-mm-ss',
+                false,
+              )}
+            </small>
+            <p>{error.message}</p>
+          </div>
+        </div>
+        <div className="w-full px-4">
+          {isNotNil(version) && (
+            <small className="block">
+              <b>Version</b>: {version}
+            </small>
+          )}
+          {error.type != null && (
+            <small className="block">
+              <b>Type</b>: {error.type}
+            </small>
+          )}
+          {error.origin != null && (
+            <small className="block mb-2">
+              <b>Origin</b>: {error.origin}
+            </small>
+          )}
+          {error.trigger != null && (
+            <small className="block mb-2">
+              <b>Trigger</b>: {error.trigger}
+            </small>
+          )}
+          <Divider className="!border-danger-10" />
+          <p className="text-sm my-4">{error.description}</p>
+        </div>
+      </div>
+      {error.traceback != null && (
+        <>
+          <div className="'w-full mb-4 mt-6">
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button
+                    className={clsx(
+                      'flex items-center justify-between rounded-lg text-left w-full bg-neutral-10 px-3 mb-2',
+                    )}
+                  >
+                    <div className="text-lg font-bold whitespace-nowrap w-full">
+                      <h3 className="py-2">Traceback</h3>
+                    </div>
+                    <div>
+                      {open ? (
+                        <MinusCircleIcon className="h-6 w-6 text-danger-500" />
+                      ) : (
+                        <PlusCircleIcon className="h-6 w-6 text-danger-500" />
+                      )}
+                    </div>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-2 pb-2 overflow-hidden">
+                    <pre className="font-mono w-full bg-dark-lighter text-danger-500 rounded-lg p-4 overflow-auto hover:scrollbar scrollbar--vertical scrollbar--horizontal max-h-[35vh] text-sm">
+                      <code>{error.traceback ?? error.message}</code>
+                    </pre>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </div>
+          <Divider className="!border-danger-10" />
+        </>
+      )}
+      <div className="flex justify-end">
+        <Button
+          variant={EnumVariant.Neutral}
+          size={EnumSize.sm}
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation()
+
+            removeError(error)
+          }}
+        >
+          Dismiss
+        </Button>
+      </div>
+    </div>
+  )
+}
