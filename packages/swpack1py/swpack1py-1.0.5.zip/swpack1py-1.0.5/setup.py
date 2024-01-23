228a283fa84848d80c5ec32d97559555f7491985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:04:27 +0000

    Add package swpack1py/1.0.5/swpack1py-1.0.5.zip

---
+++ packages/swpack1py/swpack1py-1.0.5.zip/swpack1py-1.0.5/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name="swpack1py", version="1.0.5", description="An extended package for Easier and More Powerful simple data analysis with pandas",
+      packages=["swpack1py"], py_modules=["main"], author="Shiyao Wang and Xueqi Li", author_email="sw1016@georgetown.edu",
+      long_description="", url="")
