ce2bca4b311951e6bccc6bd4abd036fef2df5ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:48 +0000

    Add package imfusion-sdk/0.0.0/imfusion-sdk-0.0.0.tar.gz

---
+++ packages/imfusion-sdk/imfusion-sdk-0.0.0.tar.gz/imfusion-sdk-0.0.0/imfusion-sdk/main.py
@@ -0,0 +1,1 @@
+print("Hello from ImFusion, this is a placeholder package")
---
+++ packages/imfusion-sdk/imfusion-sdk-0.0.0.tar.gz/imfusion-sdk-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imfusion-sdk",
+    version="0.0.0",
+    author="ImFusion GmbH",
+    author_email="info@imfusion.com",
+    packages=find_packages(),
+    python_requires=">=3.7, <4",
+    project_urls={"Company Home Page": "https://www.imfusion.com",},
+)
+from abc import ABC
+ABC.register
