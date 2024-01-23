4948a33217dbf1965602d038eedd1bdcf8dfde51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 07:05:45 +0000

    Add package fal/0.12.1/fal-0.12.1.tar.gz

---
+++ packages/fal/fal-0.12.1.tar.gz/fal-0.12.1/openapi-fal-rest/openapi_fal_rest/models/customer_details.py
@@ -0,0 +1,91 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+import attr
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="CustomerDetails")
+
+
+@attr.s(auto_attribs=True)
+class CustomerDetails:
+    """
+    Attributes:
+        user_id (str):
+        soft_monthly_budget (Union[Unset, int]):
+        hard_monthly_budget (Union[Unset, int]):
+        current_balance (Union[Unset, int]):
+        is_paying (Union[Unset, bool]):
+    """
+
+    user_id: str
+    soft_monthly_budget: Union[Unset, int] = UNSET
+    hard_monthly_budget: Union[Unset, int] = UNSET
+    current_balance: Union[Unset, int] = 0
+    is_paying: Union[Unset, bool] = False
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        user_id = self.user_id
+        soft_monthly_budget = self.soft_monthly_budget
+        hard_monthly_budget = self.hard_monthly_budget
+        current_balance = self.current_balance
+        is_paying = self.is_paying
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "user_id": user_id,
+            }
+        )
+        if soft_monthly_budget is not UNSET:
+            field_dict["soft_monthly_budget"] = soft_monthly_budget
+        if hard_monthly_budget is not UNSET:
+            field_dict["hard_monthly_budget"] = hard_monthly_budget
+        if current_balance is not UNSET:
+            field_dict["current_balance"] = current_balance
+        if is_paying is not UNSET:
+            field_dict["is_paying"] = is_paying
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        user_id = d.pop("user_id")
+
+        soft_monthly_budget = d.pop("soft_monthly_budget", UNSET)
+
+        hard_monthly_budget = d.pop("hard_monthly_budget", UNSET)
+
+        current_balance = d.pop("current_balance", UNSET)
+
+        is_paying = d.pop("is_paying", UNSET)
+
+        customer_details = cls(
+            user_id=user_id,
+            soft_monthly_budget=soft_monthly_budget,
+            hard_monthly_budget=hard_monthly_budget,
+            current_balance=current_balance,
+            is_paying=is_paying,
+        )
+
+        customer_details.additional_properties = d
+        return customer_details
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
