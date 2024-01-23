0b8bf016bfe86517d35cc49e28b2f1880d99f96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:03:33 +0000

    Add package NT.py/4.3.2/NT.py-4.3.2.tar.gz

---
+++ packages/NT.py/NT.py-4.3.2.tar.gz/NT.py-4.3.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NT.py",
+    version="4.3.2",
+    authors = ["adl212", "Malakai"],
+    author_email="emailforpythoncoding@gmail.com",
+    description="A package to use the nitrotype api and get player or team stats",
+    long_description=long_description, # don't touch this, this is your README.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/adl212/NT.py",
+    packages=['nitrotype'],
+    data_files=['nitrotype/scrapers.json'],
+    include_package_data = True,
+    install_requires=['cloudscraper', 'jsonpickle'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
