f8287d8a52a947f8ace37388c707f6c02e4f4db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:04:32 +0000

    Add package soraxas-toolbox/0.1.4/soraxas_toolbox-0.1.4.tar.gz

---
+++ packages/soraxas-toolbox/soraxas_toolbox-0.1.4.tar.gz/soraxas_toolbox-0.1.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="soraxas_toolbox",
+    version="0.1.4",
+    author="soraxas",
+    author_email="oscar@tinyiu.com",
+    description="A simple packaged toolbox for various scenario",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/soraxas/pytoolbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
