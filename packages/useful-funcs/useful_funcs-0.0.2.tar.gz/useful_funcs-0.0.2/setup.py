40a79f95b0410fcdb7e70ea5b5ef92ba2ea0a7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:59 +0000

    Add package useful-funcs/0.0.2/useful_funcs-0.0.2.tar.gz

---
+++ packages/useful-funcs/useful_funcs-0.0.2.tar.gz/useful_funcs-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Useful Functions'
+LONG_DESCRIPTION = 'Useful functions for various applications'
+
+# Setting up
+setup(
+    name="useful_funcs",
+    version=VERSION,
+    author="SuryaSudh",
+    author_email="<suryasudhakar7@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['datetime', 'numpy'],
+    keywords=['python', 'useful', 'useful functions', 'unit conversion', 'date conversion', 'calendar functions'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+
+)
