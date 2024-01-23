87846764d3c7265b339340c66ed0e091c1e43d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:15 +0000

    Add package turms/0.5.0/turms-0.5.0.tar.gz

---
+++ packages/turms/turms-0.5.0.tar.gz/turms-0.5.0/turms/processors/base.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+
+from pydantic import BaseModel, BaseSettings
+from turms.config import GeneratorConfig
+
+
+class ProcessorConfig(BaseSettings):
+    type: str
+
+    class Config:
+        extra = "forbid"
+
+
+class Processor(BaseModel):
+    """Base class for all processors
+
+    Processors are used to modify the generated python code before it is written to the file.
+    You can use processors to enforce specific styles on the generated python code like (black
+    or isort) or to add additional code to the generated python code.
+    """
+
+    config: ProcessorConfig
+
+    @abstractmethod
+    def run(gen_file: str, config: GeneratorConfig):
+        ...  # pragma: no cover
