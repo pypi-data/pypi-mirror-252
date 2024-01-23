c6468d3ac09cfe775853cf3d8d0cc7636b85666d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 06:04:09 +0000

    Add package api-omie/0.2.4/api_omie-0.2.4-py3-none-any.whl

---
+++ packages/api-omie/api_omie-0.2.4-py3-none-any.whl/omieapi/omie.py
@@ -0,0 +1,8 @@
+from .scripts import CodigoAutogerado
+
+
+class Omie(CodigoAutogerado):
+        """
+        :param omie_app_key:              Chave api da omie
+        :param omie_app_secret:           APi Secret da omie
+        """
