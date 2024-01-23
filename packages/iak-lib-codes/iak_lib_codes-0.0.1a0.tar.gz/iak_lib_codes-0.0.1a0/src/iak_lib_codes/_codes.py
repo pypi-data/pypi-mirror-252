47eb6065dd5076c99e149334be0065a3b16c6c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:06:08 +0000

    Add package iak-lib-codes/0.0.1a0/iak_lib_codes-0.0.1a0.tar.gz

---
+++ packages/iak-lib-codes/iak_lib_codes-0.0.1a0.tar.gz/iak_lib_codes-0.0.1a0/src/iak_lib_codes/_codes.py
@@ -0,0 +1,14 @@
+"""Private module of ``iak-lib-codes`` with codes."""
+from enum import IntEnum
+
+
+__all__ = ["Code"]
+
+
+class Code(IntEnum):
+    """
+    Represents the codes.
+    """
+
+    # System codes
+    EMPTY = 0
