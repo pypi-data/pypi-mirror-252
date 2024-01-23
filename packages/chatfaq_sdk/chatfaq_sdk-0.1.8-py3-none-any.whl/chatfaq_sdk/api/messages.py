bf03757a159db3de71b085188ae22380c8e6fb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:04:20 +0000

    Add package chatfaq_sdk/0.1.8/chatfaq_sdk-0.1.8-py3-none-any.whl

---
+++ packages/chatfaq_sdk/chatfaq_sdk-0.1.8-py3-none-any.whl/chatfaq_sdk/api/messages.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    fsm_def = "fsm_def"
+    rpc_request = "rpc_request"
+    rpc_result = "rpc_result"
+    llm_request = "llm_request"
+    llm_request_result = "llm_request_result"
+    error = "error"
+
+
+class RPCNodeType(Enum):
+    action = "action"
+    condition = "condition"
+
+
+class DataSourceType(Enum):
+    data_source_parsers = "data_source_parsers"
