b4f22b96e98512aa190207939eb91e6168ad5ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:05:19 +0000

    Add package pyminerva/0.0.23/pyminerva-0.0.23.tar.gz

---
+++ packages/pyminerva/pyminerva-0.0.23.tar.gz/pyminerva-0.0.23/tests/test_minerva.py
@@ -0,0 +1,40 @@
+import unittest
+import pyminerva as mi
+
+
+
+def test(tickers):
+    mi.timing_strategy(tickers, 20, 200)
+
+
+
+'''
+Main Fuction
+'''
+
+if __name__ == "__main__":
+    
+    tickers = ['SPY', 'QQQ', 'TLT']
+    test(tickers)
+
+
+
+
+
+
+
+
+# from pyminerva import PyMinerva
+
+# class Test(unittest.TestCase):
+#     def test_lower_method(self):
+#         self.assertEqual(PyMinerva.lower("TEST"), "test")
+#         self.assertNotEqual(PyMinerva.lower("test"), "TEST")
+
+#     def test_upper_method(self):
+#         self.assertEqual(PyMinerva.upper("test"), "TEST")
+#         self.assertNotEqual(PyMinerva.upper("TEST"), "test")
+
+#     def test_title_method(self):
+#         # self.assertEqual(PyMinerva.title("hello world"), "Hello world")
+#         self.assertNotEqual(PyMinerva.title("hELLO wORLD"), "hello world")
