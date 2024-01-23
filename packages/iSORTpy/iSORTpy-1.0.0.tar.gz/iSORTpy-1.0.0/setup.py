d7d2ffa39718aa6b6a9de768e7361fed3494efbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:32 +0000

    Add package iSORTpy/1.0.0/iSORTpy-1.0.0.tar.gz

---
+++ packages/iSORTpy/iSORTpy-1.0.0.tar.gz/iSORTpy-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="iSORTpy",
+    version="1.0.0",
+    description='iSORT: an Integrative Method for Reconstructing Spatial Organization of Cells via Transfer Learning',
+    author='Yecheng Tan',
+    author_email='yctan21@m.fudan.edu.cn',
+    url='https://github.com/xiaojierzi/iSORT',
+    packages=['iSORT'],
+    install_requires=[],
+    python_requires='>=3.8',
+    license='GNU General Public License v3.0',
+)
+
+
