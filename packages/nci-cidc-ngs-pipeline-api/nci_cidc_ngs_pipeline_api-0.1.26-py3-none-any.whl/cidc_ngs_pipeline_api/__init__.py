fc961e77e222d70a6ac249d0b6860fd8608867b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:03:47 +0000

    Add package nci-cidc-ngs-pipeline-api/0.1.26/nci_cidc_ngs_pipeline_api-0.1.26-py3-none-any.whl

---
+++ packages/nci-cidc-ngs-pipeline-api/nci_cidc_ngs_pipeline_api-0.1.26-py3-none-any.whl/cidc_ngs_pipeline_api/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os
+from json import load
+
+
+__author__ = """NCI"""
+__email__ = "nci-cidc-tools-admin@mail.nih.gov"
+__version__ = "0.1.26"
+
+
+_API_ENDING = "_output_API.json"
+_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+_SCHEMA_PATH = os.path.join(_BASE_DIR, "output_API.schema.json")
+
+
+try:
+    with open(_SCHEMA_PATH, "r") as f:
+        METASCHEMA = load(f)
+except Exception as e:
+    raise Exception(f"Failed loading json {_SCHEMA_PATH}") from e
+
+OUTPUT_APIS = {}
+for dname, _, files in os.walk(_BASE_DIR):
+    for fname in files:
+        if fname.endswith(_API_ENDING):
+            analysis = fname[: -len(_API_ENDING)]
+            with open(os.path.join(dname, fname), "rb") as f:
+                OUTPUT_APIS[analysis] = load(f)
