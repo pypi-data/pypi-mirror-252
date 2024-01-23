4a8cfcc2467a8d2946cf24a78510c8ec9608e32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:16 +0000

    Add package pypwsqc/0.0.1/pypwsqc-0.0.1.tar.gz

---
+++ packages/pypwsqc/pypwsqc-0.0.1.tar.gz/pypwsqc-0.0.1/src/pypwsqc/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2024 Christian Chwala. All rights reserved.
+
+pypwsqc: Quality control for PWS data
+"""
+
+
+from __future__ import annotations
+
+__version__ = "0.0.1"
+
+__all__ = ["__version__"]
