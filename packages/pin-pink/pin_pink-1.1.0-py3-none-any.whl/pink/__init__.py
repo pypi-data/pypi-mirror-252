c74b7eec54053dc138df47ff54aa4e2b1189f71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:03:54 +0000

    Add package pin-pink/1.1.0/pin_pink-1.1.0-py3-none-any.whl

---
+++ packages/pin-pink/pin_pink-1.1.0-py3-none-any.whl/pink/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Stéphane Caron
+
+"""Python inverse kinematics for your robot model based on Pinocchio."""
+
+from .configuration import Configuration
+from .solve_ik import build_ik, solve_ik
+from .tasks import Task
+from .utils import custom_configuration_vector
+
+__version__ = "1.1.0"
+
+__all__ = [
+    "Configuration",
+    "build_ik",
+    "custom_configuration_vector",
+    "solve_ik",
+    "Task",
+]
