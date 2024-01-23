bce3c97c59c7716eaf610cd5dbbfa683618b22fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:04:55 +0000

    Add package ft-package-string-operations/0.0.1/ft_package_string_operations-0.0.1.tar.gz

---
+++ packages/ft-package-string-operations/ft_package_string_operations-0.0.1.tar.gz/ft_package_string_operations-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package_string_operations',
+    version='0.0.1',
+    author='nali',
+    author_email='nali@student.42.ae',
+    description='String Operations',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+"""
+PyPI, short for Python Package Index, is the official third-party package repository for the Python programming language. 
+It is a central repository that hosts and distributes software packages for Python developers to use.
+"""
