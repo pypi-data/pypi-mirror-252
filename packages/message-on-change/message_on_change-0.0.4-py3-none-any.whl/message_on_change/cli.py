e53d213a93152ceeb5c2c354058c4d93b9e85b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:12 +0000

    Add package message-on-change/0.0.4/message_on_change-0.0.4-py3-none-any.whl

---
+++ packages/message-on-change/message_on_change-0.0.4-py3-none-any.whl/message_on_change/cli.py
@@ -0,0 +1,32 @@
+#Message on change fetches data from a website, and notifies the user when the data has changed.
+#Copyright (C) 2024  Rūdolfs Driķis
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+import argparse
+import main
+import sys
+
+print(sys.path)
+
+parser = argparse.ArgumentParser(
+    prog="Comparator",
+    description="compares a downloaded version of a webpage, to the hosted one on a specified URL at a given interval, to signal to you when a change has been made."
+)
+parser.add_argument('URL')
+parser.add_argument('--delay', nargs=1)
+parser.add_argument('--open-after')
+parser.add_argument('--cli')
+
+args = parser.parse_args()
+
+if args.cli is None:
+    import guic
