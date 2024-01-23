551dabf359b71e6fb77bbb4f1883e9fa773b43f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:05:22 +0000

    Add package wtpsplit/1.3.0/wtpsplit-1.3.0.tar.gz

---
+++ packages/wtpsplit/wtpsplit-1.3.0.tar.gz/wtpsplit-1.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="wtpsplit",
+    version="1.3.0",
+    packages=["wtpsplit"],
+    description="Robust, adaptible sentence segmentation for 85 languages",
+    author="Benjamin Minixhofer",
+    author_email="bminixhofer@gmail.com",
+    install_requires=[
+        "onnxruntime>=1.13.1",
+        "transformers>=4.22.2",
+        "numpy>=1",
+        "scikit-learn>=1",
+        "tqdm",
+        "skops",
+        "pandas>=1",
+        "cached_property",  # for Py37
+    ],
+    url="https://github.com/bminixhofer/wtpsplit",
+    package_data={"wtpsplit": ["data/*"]},
+    include_package_data=True,
+    license="MIT",
+)
