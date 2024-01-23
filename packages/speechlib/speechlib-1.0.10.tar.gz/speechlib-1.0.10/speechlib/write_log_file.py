71593db50593d74de4143c23ec26cdc4c08f11dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:05:29 +0000

    Add package speechlib/1.0.10/speechlib-1.0.10.tar.gz

---
+++ packages/speechlib/speechlib-1.0.10.tar.gz/speechlib-1.0.10/speechlib/write_log_file.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+
+def write_log_file(common_segments, log_folder):
+
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+
+    file_name = "output"
+    current_datetime = datetime.now().strftime("%Y-%m-%d")
+
+    #---------------------log file part-------------------------
+
+    log_file = log_folder + "/" + file_name + "_" + current_datetime + ".txt"
+    
+    lf=open(log_file,"wb")
+
+    entry = ""
+    
+    for segment in common_segments:
+        start = segment[0]
+        end = segment[1]
+        text = segment[2]
+        speaker = segment[3]
+        
+        entry += f"{speaker} ({start} : {end}) : {text}\n"
+        
+    lf.write(bytes(entry.encode('utf-8')))      
+    lf.close()
+
+    # -------------------------log file end-------------------------
