0ff662b216e1fb1bc308adf74822120d1e3d79a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:05 +0000

    Add package takion-api/0.1.32/takion_api-0.1.32-py3-none-any.whl

---
+++ packages/takion-api/takion_api-0.1.32-py3-none-any.whl/takion_api/__init__.py
@@ -0,0 +1,19 @@
+from .datadome import TakionAPIDatadome
+from .incapsula import TakionAPIUtmvc, TakionAPIReese84
+
+from .exceptions import (
+    TakionAPIException,
+    IpBanException,
+    BadResponseException
+)
+
+__all__ = [
+    "TakionAPIDatadome",
+    "TakionAPIUtmvc",
+    "TakionAPIReese84",
+    "TakionAPIException",
+    "IpBanException",
+    "BadResponseException"
+]
+
+__version__ = "0.1.3"
