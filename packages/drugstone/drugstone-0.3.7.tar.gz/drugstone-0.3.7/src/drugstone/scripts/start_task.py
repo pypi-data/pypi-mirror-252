eb01093ca101ec194223a67461b82f0f3cc871ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:16 +0000

    Add package drugstone/0.3.7/drugstone-0.3.7.tar.gz

---
+++ packages/drugstone/drugstone-0.3.7.tar.gz/drugstone-0.3.7/src/drugstone/scripts/start_task.py
@@ -0,0 +1,27 @@
+"""
+drugstone.scripts.start_task
+
+This module implements the start_task function.
+
+:copyright: 2022 Institute for Computational Systems Biology by Prof. Dr. Jan Baumbach
+:author: Ugur Turhan
+"""
+
+import requests
+from .constants.url import Url
+
+
+def start_task(params: dict) -> str:
+    """Starts a task and returns the token for it.
+
+    If there is no token for any reason, an empty str gets returned.
+    """
+    start_task_response = requests.post(
+        Url.TASK,
+        verify=False,
+        json=params
+    )
+
+    token_obj = start_task_response.json()
+
+    return token_obj.get("token", "")
