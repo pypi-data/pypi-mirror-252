3a7c90375a0d3d2e8c60b9da8469bad4db4cb485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:17 +0000

    Add package Topsis-Mohak-102103144/1.1.2/Topsis-Mohak-102103144-1.1.2.tar.gz

---
+++ packages/Topsis-Mohak-102103144/Topsis-Mohak-102103144-1.1.2.tar.gz/Topsis-Mohak-102103144-1.1.2/README.md
@@ -0,0 +1,3 @@
+# Topsis Python Package
+
+Topsis package made by Mohak 102103144
---
+++ packages/Topsis-Mohak-102103144/Topsis-Mohak-102103144-1.1.2.tar.gz/Topsis-Mohak-102103144-1.1.2/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis-Mohak-102103144",
+    version="1.1.2",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Mohak Singh",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=Topsis.__main__:main",
+        ],
+    },
+)
