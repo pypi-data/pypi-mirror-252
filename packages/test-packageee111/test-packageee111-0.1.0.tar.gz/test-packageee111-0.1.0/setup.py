d4bddeaaa1881534c8bfe00ff8bf5782c9a2bb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:36 +0000

    Add package test-packageee111/0.1.0/test-packageee111-0.1.0.tar.gz

---
+++ packages/test-packageee111/test-packageee111-0.1.0.tar.gz/test-packageee111-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="practice1",
+    description="for pactice",
+    name="test-packageee111", # this is the name of the package when using "pip install test-package". when importing, "import test_pack" (the actual directory names). (refer to for_test_of_deployment)
+    packages=find_packages(include=["test_pack", "test_pack.*"]),
+    version="0.1.0",
+    install_requires = ['numpy >=1.10', 'pandas'] # this better cover as many versions as possible (different from requirements.txt, which is "exact" version of installation via "pip freeze > requirements.txt", then install via "pip install -r requirements.txt")
+)
