1c88525dd97b45ad4cef52800d4b5125e2361413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:04:10 +0000

    Add package viggocore/1.2.4/viggocore-1.2.4-py3-none-any.whl

---
+++ packages/viggocore/viggocore-1.2.4-py3-none-any.whl/viggocore/subsystem/constant_for_calculation/__init__.py
@@ -0,0 +1,5 @@
+from viggocore.common import subsystem
+from viggocore.subsystem.constant_for_calculation \
+    import resource
+
+subsystem = subsystem.Subsystem(resource=resource.ConstantForCalculation)
