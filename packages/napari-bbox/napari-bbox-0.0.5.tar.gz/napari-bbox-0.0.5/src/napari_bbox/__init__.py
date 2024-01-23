a605d766786e1799d6beac526476f913e2ad2168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:27 +0000

    Add package napari-bbox/0.0.5/napari-bbox-0.0.5.tar.gz

---
+++ packages/napari-bbox/napari-bbox-0.0.5.tar.gz/napari-bbox-0.0.5/src/napari_bbox/__init__.py
@@ -0,0 +1,50 @@
+__version__ = "0.0.3"
+
+from napari import layers
+layers.NAMES.add("bounding_boxes")
+
+from napari import types
+types.Bounding_BoxesData = types.NewType("Bounding_BoxesData", types.ArrayBase)
+
+
+from .boundingbox import BoundingBoxLayer
+from ._reader import napari_get_reader
+from ._writer import write_single_bbox
+
+from ._widget import BoundingBoxCreator
+
+from napari import Viewer
+def add_bounding_boxes(self, *args, **kwargs):
+    layer = BoundingBoxLayer(*args, **kwargs)
+    self.layers.append(layer)
+    return layer
+Viewer.add_bounding_boxes = add_bounding_boxes
+
+import npe2.manifest.contributions._writers
+from enum import Enum
+
+
+class LayerType(str, Enum):
+    image = "image"
+    labels = "labels"
+    points = "points"
+    shapes = "shapes"
+    surface = "surface"
+    tracks = "tracks"
+    vectors = "vectors"
+    bounding_boxes = "bounding_boxes"
+
+
+# npe2.manifest.contributions._writers.LayerType = LayerType
+
+# This is an ugly solution to register every component correctly
+from .boundingbox import register_layer_control
+from .boundingbox import register_layer_visual
+register_layer_control(BoundingBoxLayer)
+register_layer_visual(BoundingBoxLayer)
+# register_bounding_boxes_actions(BoundingBoxLayer)
+import sys
+layers.__dict__["BoundingBoxLayer"] = BoundingBoxLayer
+layers.__dict__["bounding_boxes"] = sys.modules[__name__]
+
+__all__ = ["BoundingBoxCreator", "BoundingBoxLayer", "napari_get_reader", "write_single_bbox"]
