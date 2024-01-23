dfbcef3280e684f691f8a0ee91d90b11a90a65c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 06:03:59 +0000

    Add package nautobot/2.1.2/nautobot-2.1.2-py3-none-any.whl

---
+++ packages/nautobot/nautobot-2.1.2-py3-none-any.whl/nautobot/core/graphql/types.py
@@ -0,0 +1,34 @@
+from django.contrib.contenttypes.models import ContentType
+import graphene
+import graphene_django_optimizer as gql_optimizer
+
+
+class OptimizedNautobotObjectType(gql_optimizer.OptimizedDjangoObjectType):
+    url = graphene.String()
+
+    def resolve_url(self, info):
+        return self.get_absolute_url(api=True)
+
+    class Meta:
+        abstract = True
+
+
+class ContentTypeType(OptimizedNautobotObjectType):
+    """
+    Graphene-Django object type for ContentType records.
+
+    Needed because ContentType is a built-in model, not one that we own and can auto-generate types for.
+    """
+
+    class Meta:
+        model = ContentType
+
+
+class JSON(graphene.Scalar):
+    @staticmethod
+    def serialize_data(dt):
+        return dt
+
+    serialize = serialize_data
+    parse_value = serialize_data
+    parse_literal = serialize_data
