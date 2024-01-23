7da51edcaed61a348d715900fce334505307268e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:20 +0000

    Add package aind-ng-link/1.0.15/aind-ng-link-1.0.15.tar.gz

---
+++ packages/aind-ng-link/aind-ng-link-1.0.15.tar.gz/aind-ng-link-1.0.15/src/ng_link/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package for the generation of neuroglancer links
+"""
+__version__ = "1.0.15"
+
+# flake8: noqa: F401
+from ng_link.ng_layer import NgLayer
+from ng_link.ng_state import NgState
