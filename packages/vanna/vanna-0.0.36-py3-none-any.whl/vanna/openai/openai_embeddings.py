b2a8d8edd96e822096985270a5a7211a722818f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:04:27 +0000

    Add package vanna/0.0.36/vanna-0.0.36-py3-none-any.whl

---
+++ packages/vanna/vanna-0.0.36-py3-none-any.whl/vanna/openai/openai_embeddings.py
@@ -0,0 +1,41 @@
+from abc import abstractmethod
+
+from openai import OpenAI
+
+from ..base import VannaBase
+
+
+class OpenAI_Embeddings(VannaBase):
+    def __init__(self, config=None):
+        VannaBase.__init__(self, config=config)
+
+        self.client = OpenAI()
+
+        if config is None:
+            return
+
+        if "api_type" in config:
+            self.client.api_type = config["api_type"]
+
+        if "api_base" in config:
+            self.client.api_base = config["api_base"]
+
+        if "api_version" in config:
+            self.client.api_version = config["api_version"]
+
+        if "api_key" in config:
+            self.client.api_key = config["api_key"]
+
+    def generate_embedding(self, data: str, **kwargs) -> list[float]:
+        if self.config is not None and "engine" in self.config:
+            embedding = self.client.embeddings.create(
+                engine=self.config["engine"],
+                input=data,
+            )
+        else:
+            embedding = self.client.embeddings.create(
+                model="text-embedding-ada-002",
+                input=data,
+            )
+
+        return embedding.get("data")[0]["embedding"]
