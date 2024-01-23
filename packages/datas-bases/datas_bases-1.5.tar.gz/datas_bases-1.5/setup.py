f74bb8a7920ceff5f60f445fd7c683665e384132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:24 +0000

    Add package datas-bases/1.5/datas_bases-1.5.tar.gz

---
+++ packages/datas-bases/datas_bases-1.5.tar.gz/datas_bases-1.5/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='datas_bases',
+	version='1.5',
+	author='BornTheHell',
+	author_email='anp50158@gmail.com',
+	description='',
+	packages=['datas_bases'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
