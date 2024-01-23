b5e6f852a25d1f4c472f508eea04a8430e02d2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:04:08 +0000

    Add package llfmt/2024.1.23/llfmt-2024.1.23.tar.gz

---
+++ packages/llfmt/llfmt-2024.1.23.tar.gz/llfmt-2024.1.23/llfmt/__main__.py
@@ -0,0 +1,15 @@
+import glob
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    paths = [path for pattern in args for path in glob.glob(pattern)]
+    if not paths:
+        return
+    paths.sort(key=lambda s: s.split('/'))
+    print("\n".join([f"### {path} ###\n{open(path, 'r').read()}" for path in paths]))
+
+
+if __name__ == '__main__':
+    main()
