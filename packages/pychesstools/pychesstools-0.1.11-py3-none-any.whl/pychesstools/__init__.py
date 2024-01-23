ee8ab27b02163784b5483c9a487c0883d19d1138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 07:05:54 +0000

    Add package pychesstools/0.1.11/pychesstools-0.1.11-py3-none-any.whl

---
+++ packages/pychesstools/pychesstools-0.1.11-py3-none-any.whl/pychesstools/__init__.py
@@ -0,0 +1,17 @@
+"""A game of chess."""
+
+from importlib.metadata import PackageNotFoundError, version
+from importlib.util import find_spec
+from pathlib import Path
+
+__version__: str | None
+
+try:
+    __version__ = version("pychesstools")
+except PackageNotFoundError:
+    __version__ = None
+
+__WORKING_DIRECTORY__ = (
+    Path(__file__).parent if "__file__" in globals() else Path("pychesstools")
+)
+__RICH_AVAILABLE__ = bool(find_spec("rich"))
