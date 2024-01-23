00f8f9ecaf4c26a6fa6f0fe59e70fc3032b776e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:12 +0000

    Add package gguf-connector/0.1/gguf_connector-0.1.tar.gz

---
+++ packages/gguf-connector/gguf_connector-0.1.tar.gz/gguf_connector-0.1/src/gguf_connector/__init__.py
@@ -0,0 +1,13 @@
+# print("Please select a connector:\n1. ctransformers\n2. llama.cpp")
+# choice = input("Enter your choice (1 to 2): ")
+
+# if choice=="1":
+#     from .c import *
+# elif choice=="2":
+#     from .cpp import *
+# else:
+#     print("Not a valid number.")
+
+"""Return the pathname of main directory"""
+
+__version__ = "0.1"
