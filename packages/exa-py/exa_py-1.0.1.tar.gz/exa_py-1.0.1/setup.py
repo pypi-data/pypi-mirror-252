34830caacd53ea7567ee4d875a9379bd73520150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:31 +0000

    Add package exa-py/1.0.1/exa_py-1.0.1.tar.gz

---
+++ packages/exa-py/exa_py-1.0.1.tar.gz/exa_py-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exa_py',
+    version='1.0.1',
+    description='Python SDK for Exa API.',
+    author='Exa',
+    author_email='hello@exa.ai',
+    package_data={"exa_py": ["py.typed"]},
+    url='https://github.com/exa-labs/exa-py',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Typing :: Typed",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
