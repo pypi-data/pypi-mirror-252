7637e19a4356499765c6a002986f631ab122b6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:05:21 +0000

    Add package cosmosdb/0.1.6/cosmosdb-0.1.6-py3-none-any.whl

---
+++ packages/cosmosdb/cosmosdb-0.1.6-py3-none-any.whl/cosmosdb/patch/patch.py
@@ -0,0 +1,31 @@
+from typing import Literal, Annotated, Any
+from pydantic import BaseModel, StringConstraints
+from azure.cosmos.aio import CosmosClient
+import ramda as R
+from .. import KEY, ENDPOINT
+from ..util import with_client
+
+Path = Annotated[str, StringConstraints(pattern='/.*')]
+
+class ValueOp(BaseModel):
+    op: Literal['add', 'replace', 'set', 'incr', 'remove']
+    value: str | dict[str, Any]
+    path: Path
+
+class MoveOp(BaseModel):
+    op: Literal['move']
+    from_: Path
+    to: Path
+
+Op = ValueOp | MoveOp
+
+@with_client
+async def item(
+    database: str, container: str, id: str, ops: list[Op], partition_key: str | None = None,
+    *, client: CosmosClient | None = None, url: str | None = ENDPOINT, key: str | None = KEY,
+) -> dict[str]:
+    """Partial update (PATCH) a given item. See [Azure's docs](https://learn.microsoft.com/en-us/azure/cosmos-db/partial-document-update) and [python SDK docs](https://learn.microsoft.com/en-us/azure/cosmos-db/partial-document-update-getting-started?tabs=python) for details
+    - Returns the patched object"""
+    cc = client.get_database_client(database).get_container_client(container)
+    dict_ops = R.map(BaseModel.model_validate, ops)
+    return await cc.patch_item(item=id, partition_key=partition_key or id, patch_operations=dict_ops)
