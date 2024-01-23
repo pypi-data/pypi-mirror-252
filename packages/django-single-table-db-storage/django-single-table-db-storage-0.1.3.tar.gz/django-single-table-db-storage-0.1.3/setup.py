3e5978e1694822ea8044ff71ae17577b07392953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:03:59 +0000

    Add package django-single-table-db-storage/0.1.3/django-single-table-db-storage-0.1.3.tar.gz

---
+++ packages/django-single-table-db-storage/django-single-table-db-storage-0.1.3.tar.gz/django-single-table-db-storage-0.1.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    longdesc = f.read()
+
+setup(
+    name="django-single-table-db-storage",
+    version="0.1.3",
+    description="Provides a Django storage implementation that uses a single database table.",
+    long_description=longdesc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/waterimp/django-single-table-db-storage",
+    author="Lee Bush",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+    ],
+    keywords="django storage database",
+    packages=['django_single_table_db_storage', 'django_single_table_db_storage.migrations'],
+    package_dir={'django_single_table_db_storage': 'django_single_table_db_storage'},
+    package_data={},
+    include_package_data=True,
+    scripts=[],
+    install_requires=[
+          'Django>=3.2',
+          'django-extensions>=3.2',
+      ],
+    python_requires=">=3.5",
+)
