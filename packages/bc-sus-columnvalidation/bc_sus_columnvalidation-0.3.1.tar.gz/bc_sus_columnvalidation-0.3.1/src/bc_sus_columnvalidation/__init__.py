d3d156efa2cd33623d4586785d5f4c3d9d609a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:04:34 +0000

    Add package bc-sus-columnvalidation/0.3.1/bc_sus_columnvalidation-0.3.1.tar.gz

---
+++ packages/bc-sus-columnvalidation/bc_sus_columnvalidation-0.3.1.tar.gz/bc_sus_columnvalidation-0.3.1/src/bc_sus_columnvalidation/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from .validation_scripts.missing_data_check import missing_data_check
+from .validation_scripts.datatype_check_integer import datatype_check_integer
+from .validation_scripts.datatype_check_bool import datatype_check_bool
+
+
+
+
+
