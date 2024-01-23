9d936c22d5546c1b8083b3cbe7f1d4e278db5f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:04:38 +0000

    Add package vine-test-importer/1.0.16/vine_test_importer-1.0.16.tar.gz

---
+++ packages/vine-test-importer/vine_test_importer-1.0.16.tar.gz/vine_test_importer-1.0.16/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'pip',
+    'numpy',
+    'requests',
+    'pandas',
+    'curlify',
+    'PyYAML',
+    'psycopg2-binary',
+    'pymysql',
+    'pyhive',
+    'thrift',
+    'sasl'
+]
+
+setup(
+    name='vine_test_importer',
+    version='1.0.16',
+    author='vine_wang',
+    author_email='vine_wc@163.com',
+    packages=find_packages('../', include=["../importers*"]),  # 从importers目录中查找所有包
+    include_package_data=True,
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'clear_user=importers.clear_user:main',
+            'data_importer=importers.data_importer:main',
+            'format_importer=importers.format_importer:main',
+            'meta_importer=importers.meta_importer:main',
+        ],
+    },
+)
