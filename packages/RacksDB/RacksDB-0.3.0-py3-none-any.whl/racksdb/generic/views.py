2f92e6a954cfe25bb6d26c373a2eba7b8a397ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:04:32 +0000

    Add package RacksDB/0.3.0/RacksDB-0.3.0-py3-none-any.whl

---
+++ packages/RacksDB/RacksDB-0.3.0-py3-none-any.whl/racksdb/generic/views.py
@@ -0,0 +1,162 @@
+# Copyright (c) 2023 Rackslab
+#
+# This file is part of RacksDB.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from typing import List, Dict, Union, Literal
+from itertools import chain
+
+from .errors import DBViewError
+
+
+class DBActionParameter:
+    def __init__(
+        self,
+        name,
+        description,
+        short=None,
+        nargs=None,
+        positional=False,
+        required=False,
+        choices=None,
+        default=None,
+        _type=None,
+        body=None,
+        specific: Union[None, Literal["cli", "web"]] = None,
+    ):
+        self.name = name
+        self.description = description
+        self.short = short
+        self.nargs = nargs
+        self.positional = positional
+        self.required = required
+        self.choices = choices
+        self.default = default
+        self.type = _type
+        self.body = body
+        self.specific = specific
+
+
+class DBActionResponse:
+    def __init__(self, mimetype, binary=False, object_name=None):
+        self.mimetype = mimetype
+        self.binary = binary
+        self.object = object_name
+
+
+class DBActionError:
+    def __init__(self, code: int, description):
+        self.code = code
+        self.description = description
+
+
+class DBAction:
+    def __init__(
+        self,
+        name,
+        path,
+        description,
+        method="get",
+        parameters=[],
+        responses=[],
+        errors=[],
+    ):
+        self.name = name
+        self.path = path
+        self.description = description
+        self.method = method
+        self.parameters = parameters
+        self.responses = responses
+        self.errors = errors
+
+    def inpath(self, parameter: DBActionParameter) -> bool:
+        """Return True if a DBActionParameter is in DBAction path"""
+        return f"<{parameter.name}>" in self.path
+
+
+class DBViewParameter(DBActionParameter):
+    def __init__(
+        self, name, description, short=None, nargs=None, choices=None, default=None
+    ):
+        super().__init__(
+            name,
+            description,
+            short=short,
+            nargs=nargs,
+            choices=choices,
+            default=default,
+        )
+
+
+class DBViewFilter(DBActionParameter):
+    def __init__(
+        self, name: str, description: str, nargs: Union[str, int, None] = None
+    ):
+        super().__init__(name, description, nargs=nargs)
+
+
+class DBView:
+    def __init__(
+        self,
+        content: str,
+        objects_name: str,
+        description: str,
+        filters: List[DBViewFilter],
+        objects_map: Dict[str, Union[None, str]],
+    ):
+        self.content = content
+        self.objects_name = objects_name
+        self.description = description
+        self.filters = filters
+        self.objects_map = objects_map
+
+
+class DBViewSet:
+    def __iter__(self):
+        for view in self.VIEWS:
+            yield view
+
+    def views_actions(self):
+        """Generate the list of generic DBActions (with their DBActionParameters and
+        DBActionResponses) corresponding to the list of DBViews attached this
+        DBViewSet."""
+        actions = []
+        for view in self:
+            # Merge view filters and generic parameters to form the full list of
+            # parameters.
+            parameters = [
+                parameter for parameter in chain(view.filters, self.parameters())
+            ]
+            if not len(parameters):
+                parameters = None
+            # List of responses
+            responses = []
+            for mimetype in ["application/json", "application/x-yaml"]:
+                responses.append(
+                    DBActionResponse(mimetype, object_name=view.objects_name)
+                )
+            actions.append(
+                DBAction(
+                    name=view.content,
+                    path=f"/{view.content}",
+                    description=view.description,
+                    parameters=parameters,
+                    responses=responses,
+                )
+            )
+        return actions
+
+    def parameters(self):
+        for parameter in self.PARAMETERS:
+            yield parameter
+
+    def actions(self):
+        for action in self.ACTIONS:
+            yield action
+
+    def __getitem__(self, key):
+        for view in self.VIEWS:
+            if view.content == key:
+                return view
+        raise DBViewError(f"Unable to find view for '{key}' content")
---
+++ packages/RacksDB/RacksDB-0.3.0-py3-none-any.whl/racksdb/views.py
@@ -0,0 +1,161 @@
+# Copyright (c) 2023 Rackslab
+#
+# This file is part of RacksDB.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from pathlib import Path
+
+from .drawers.parameters import DrawingParameters
+from .generic.views import (
+    DBViewSet,
+    DBView,
+    DBViewFilter,
+    DBViewParameter,
+    DBAction,
+    DBActionParameter,
+    DBActionResponse,
+    DBActionError,
+)
+
+
+class RacksDBViews(DBViewSet):
+    VIEWS = [
+        DBView(
+            content="datacenters",
+            objects_name="RacksDBDatacenter",
+            description="Get information about datacenters",
+            filters=[
+                DBViewFilter(name="name", description="Filter datacenters by name"),
+                DBViewFilter(
+                    name="tags", description="Filter datacenters by tag", nargs="*"
+                ),
+            ],
+            objects_map={
+                "RacksDBDatacenter": None,
+                "RacksDBDatacenterRoom": "name",
+                "RacksDBRacksRow": "name",
+                "RacksDBRack": "name",
+                "RacksDBRack.nodes": None,
+            },
+        ),
+        DBView(
+            content="infrastructures",
+            objects_name="RacksDBInfrastructure",
+            description="Get information about infrastructures",
+            filters=[
+                DBViewFilter(name="name", description="Filter infrastructures by name"),
+                DBViewFilter(
+                    name="tags",
+                    description="Filter infrastructures by tag",
+                    nargs="*",
+                ),
+            ],
+            objects_map={
+                "RacksDBDatacenter": "name",
+                "RacksDBDatacenterRoom": "name",
+                "RacksDBRack": "name",
+                "RacksDBInfrastructure": None,
+            },
+        ),
+        DBView(
+            content="nodes",
+            objects_name="RacksDBNode",
+            description="Get information about nodes",
+            filters=[
+                DBViewFilter(name="name", description="Filter nodes by name"),
+                DBViewFilter(
+                    name="infrastructure",
+                    description="Filter nodes by infrastructure",
+                ),
+                DBViewFilter(name="tags", description="Filter nodes by tag", nargs="*"),
+            ],
+            objects_map={
+                "RacksDBGroupRack": "name",
+                "RacksDBDatacenter": "name",
+                "RacksDBDatacenterRoom": "name",
+                "RacksDBRacksRow": "name",
+                "RacksDBInfrastructure": "name",
+                "RacksDBRack.nodes": None,
+            },
+        ),
+        DBView(
+            content="racks",
+            objects_name="RacksDBRack",
+            description="Get information about racks",
+            filters=[DBViewFilter(name="name", description="Filter racks by name")],
+            objects_map={
+                "RacksDBDatacenter": "name",
+                "RacksDBDatacenterRoom": "name",
+                "RacksDBRacksRow": "name",
+                "RacksDBRack": None,
+                "RacksDBNodeType": "id",
+                "RacksDBInfrastructure": "name",
+            },
+        ),
+    ]
+    PARAMETERS = [
+        DBViewParameter(
+            "list",
+            "Get list of object names instead of full objects",
+            short="l",
+            nargs=0,
+        ),
+        DBViewParameter("fold", "Fold expandable objects", short="f", nargs=0),
+        DBViewParameter(
+            "with_objects_types", "Report object types in YAML dumps", nargs=0
+        ),
+        DBViewParameter(
+            "format",
+            "Select output format",
+            choices=["yaml", "json"],
+        ),
+    ]
+    ACTIONS = [
+        DBAction(
+            name="draw",
+            path="/draw/<entity>/<name>.<format>",
+            description="Draw an entity",
+            method="post",
+            parameters=[
+                DBActionParameter(
+                    "entity",
+                    description="Type of entity to draw",
+                    choices=["infrastructure", "room"],
+                    positional=True,
+                ),
+                DBActionParameter("name", description="Name of entity", required=True),
+                DBActionParameter(
+                    "format",
+                    description="Format of the generated image",
+                    choices=["png", "svg", "pdf"],
+                    default="png",
+                ),
+                DBActionParameter(
+                    "parameters",
+                    description="Drawing parameters",
+                    body="Drawings_content",
+                ),
+                DBActionParameter(
+                    "drawings_schema",
+                    description="Schema of drawing parameters",
+                    _type=Path,
+                    default=DrawingParameters.DEFAULT_SCHEMA,
+                    specific="cli",
+                ),
+            ],
+            responses=[
+                DBActionResponse("image/png", binary=True),
+                DBActionResponse("image/svg+xml"),
+                DBActionResponse("application/pdf", binary=True),
+            ],
+            errors=[
+                DBActionError(400, "Unable to parse drawing parameters in JSON format"),
+                DBActionError(
+                    415,
+                    "Unsupported drawing parameters format or unable to load drawing "
+                    "parameters schema",
+                ),
+            ],
+        )
+    ]
