81274f335793ffa9a48c5dbf34d7da7b91cdd72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:15 +0000

    Add package AreaOfFig24test/0.0.2/AreaOfFig24test-0.0.2-py3-none-any.whl

---
+++ packages/AreaOfFig24test/AreaOfFig24test-0.0.2-py3-none-any.whl/first_package/Area_Fig.py
@@ -0,0 +1,10 @@
+import math
+
+def square_area(side_length):
+    return print("Area of Square:",side_length ** 2)
+
+def rectangle_area(length, width):
+    return print("Area of Rectangle:",length * width)
+
+def circle_area(radius):
+    return print("Area of Circle:",math.pi * radius ** 2)
