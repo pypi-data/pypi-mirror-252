6f5005a629ea3213b8e427170bb5c05cd87996bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 07:06:09 +0000

    Add package api-omie/0.2.5/api-omie-0.2.5.tar.gz

---
+++ packages/api-omie/api-omie-0.2.5.tar.gz/api-omie-0.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open('README.md', 'rt', encoding='latin-1') as arq:
+      readme = arq.read()
+
+keywords = ['Api omie', 'omie', 'api do omie', 'omieapi']
+
+setup(name='api-omie',
+      url='https://github.com/MikalROn/ApiOmie-nao-oficial',
+      version='0.2.5',
+      license='MIT license',
+      author='Daniel Coêlho',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author_email='heromon.9010@gmail.com',
+      keywords=keywords,
+      description='Ferramenta para api do omie não oficial',
+      packages=['omieapi'],
+      install_requires=['requests', 'beautifulsoup4'],
+      python_requires='>=3',
+      project_urls={
+            'Demonstrações': 'https://github.com/MikalROn/ApiOmie-nao-oficial/tree/main/demos',
+      }
+)
