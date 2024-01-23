99f350ea149154e3902ee5f78dd64640762860b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:05:01 +0000

    Add package polars/0.20.6rc1/polars-0.20.6rc1.tar.gz

---
+++ packages/polars/polars-0.20.6rc1.tar.gz/polars-0.20.6rc1/crates/polars-core/src/chunked_array/array/mod.rs
@@ -0,0 +1,83 @@
+//! Special fixed-size-list utility methods
+
+mod iterator;
+
+use crate::prelude::*;
+
+impl ArrayChunked {
+    /// Get the inner data type of the fixed size list.
+    pub fn inner_dtype(&self) -> DataType {
+        match self.dtype() {
+            DataType::Array(dt, _size) => *dt.clone(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        match self.dtype() {
+            DataType::Array(_dt, size) => *size,
+            _ => unreachable!(),
+        }
+    }
+
+    /// # Safety
+    /// The caller must ensure that the logical type given fits the physical type of the array.
+    pub unsafe fn to_logical(&mut self, inner_dtype: DataType) {
+        debug_assert_eq!(inner_dtype.to_physical(), self.inner_dtype());
+        let width = self.width();
+        let fld = Arc::make_mut(&mut self.field);
+        fld.coerce(DataType::Array(Box::new(inner_dtype), width))
+    }
+
+    /// Get the inner values as `Series`
+    pub fn get_inner(&self) -> Series {
+        let ca = self.rechunk();
+        let inner_dtype = self.inner_dtype().to_arrow(true);
+        let arr = ca.downcast_iter().next().unwrap();
+        unsafe {
+            Series::_try_from_arrow_unchecked(
+                self.name(),
+                vec![(arr.values()).clone()],
+                &inner_dtype,
+            )
+            .unwrap()
+        }
+    }
+
+    /// Ignore the list indices and apply `func` to the inner type as [`Series`].
+    pub fn apply_to_inner(
+        &self,
+        func: &dyn Fn(Series) -> PolarsResult<Series>,
+    ) -> PolarsResult<ArrayChunked> {
+        // Rechunk or the generated Series will have wrong length.
+        let ca = self.rechunk();
+        let inner_dtype = self.inner_dtype().to_arrow(true);
+
+        let chunks = ca.downcast_iter().map(|arr| {
+            let elements = unsafe {
+                Series::_try_from_arrow_unchecked(
+                    self.name(),
+                    vec![(*arr.values()).clone()],
+                    &inner_dtype,
+                )
+                .unwrap()
+            };
+
+            let expected_len = elements.len();
+            let out: Series = func(elements)?;
+            polars_ensure!(
+                out.len() == expected_len,
+                ComputeError: "the function should apply element-wise, it removed elements instead"
+            );
+            let out = out.rechunk();
+            let values = out.chunks()[0].clone();
+
+            let inner_dtype =
+                FixedSizeListArray::default_datatype(out.dtype().to_arrow(true), ca.width());
+            let arr = FixedSizeListArray::new(inner_dtype, values, arr.validity().cloned());
+            Ok(arr)
+        });
+
+        ArrayChunked::try_from_chunk_iter(self.name(), chunks)
+    }
+}
