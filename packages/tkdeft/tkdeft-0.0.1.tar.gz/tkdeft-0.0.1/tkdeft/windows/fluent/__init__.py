f049faf36e18bed6e02d7b76ac41525c3b07a307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:05:15 +0000

    Add package tkdeft/0.0.1/tkdeft-0.0.1.tar.gz

---
+++ packages/tkdeft/tkdeft-0.0.1.tar.gz/tkdeft-0.0.1/tkdeft/windows/fluent/__init__.py
@@ -0,0 +1,11 @@
+"""、
+
+默认的组件库（模板）
+
+-------------
+作者：XiangQinxi
+-------------
+"""
+
+from .button import DButtonDraw, DButtonCanvas, DButton, DDarkButton, DAccentButton, DDarkAccentButton
+from .frame import DFrameDraw, DFrameCanvas, DFrame, DDarkFrame
