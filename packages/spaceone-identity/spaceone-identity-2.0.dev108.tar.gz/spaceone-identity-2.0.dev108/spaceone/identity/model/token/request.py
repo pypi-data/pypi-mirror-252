db27129904c605cb609e88ad3a5a3bb932b4e45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:37 +0000

    Add package spaceone-identity/2.0.dev108/spaceone-identity-2.0.dev108.tar.gz

---
+++ packages/spaceone-identity/spaceone-identity-2.0.dev108.tar.gz/spaceone-identity-2.0.dev108/spaceone/identity/model/token/request.py
@@ -0,0 +1,24 @@
+from typing import Union, Literal
+from pydantic import BaseModel
+
+__all__: ["TokenIssueRequest"]
+
+AuthType = Literal["LOCAL", "EXTERNAL", "MFA"]
+GrantType = Literal["REFRESH_TOKEN"]
+Scope = Literal["SYSTEM", "DOMAIN", "WORKSPACE", "USER"]
+
+
+class TokenIssueRequest(BaseModel):
+    credentials: dict
+    auth_type: AuthType
+    timeout: Union[int, None] = None
+    verify_code: Union[str, None] = None
+    domain_id: str
+
+
+class TokenGrantRequest(BaseModel):
+    grant_type: str
+    token: str
+    scope: Scope
+    timeout: Union[int, None] = None
+    workspace_id: Union[str, None] = None
