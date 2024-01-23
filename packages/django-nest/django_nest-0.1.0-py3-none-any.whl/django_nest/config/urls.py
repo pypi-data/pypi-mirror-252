4dcfb93dedb6480b75daae27b16166d0c9e83984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:40 +0000

    Add package django-nest/0.1.0/django_nest-0.1.0-py3-none-any.whl

---
+++ packages/django-nest/django_nest-0.1.0-py3-none-any.whl/django_nest/config/urls.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from authentication.views import JWTTokenView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="silver@t3ch4good.com"),
+        license=openapi.License(name="Private License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/swagger', permanent=False)),
+    path('api/token/', JWTTokenView.as_view(), name='token'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
