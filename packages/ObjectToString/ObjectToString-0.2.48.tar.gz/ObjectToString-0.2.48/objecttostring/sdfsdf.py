7f5c0a8d1a6955e1d2ec9a8d46e7b0e2b698708b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 04:06:38 +0000

    Add package ObjectToString/0.2.48/ObjectToString-0.2.48.tar.gz

---
+++ packages/ObjectToString/ObjectToString-0.2.48.tar.gz/ObjectToString-0.2.48/objecttostring/sdfsdf.py
@@ -0,0 +1,17 @@
+class sdfsdf:
+	def __init__(self):
+		self.a=None
+		self.c=None
+		self.d=None
+	def seta(self,a):
+		self.a=a
+	def geta(self):
+		return self.a
+	def setc(self,c):
+		self.c=c
+	def getc(self):
+		return self.c
+	def setd(self,d):
+		self.d=d
+	def getd(self):
+		return self.d
