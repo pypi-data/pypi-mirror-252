bc7d0113dcb179cde12cc9aff1d3e28fc4e2e10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 07:05:40 +0000

    Add package queue-local/0.0.39/queue-local-0.0.39.tar.gz

---
+++ packages/queue-local/queue-local-0.0.39.tar.gz/queue-local-0.0.39/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+PACKAGE_NAME = "queue-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+setuptools.setup(
+    name=PACKAGE_NAME,  # https://pypi.org/project/queue-local
+    version='0.0.39',
+    author="Circles",
+    author_email="info@circles.life",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f'{package_dir}/src'},
+    package_data={package_dir: ['*.py']},
+    long_description="queue-local",
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pytest>=7.4.1",
+                      "database-mysql-local>=0.0.120",
+                      "logger-local>=0.0.59",
+                      "user-context-remote>=0.0.30",
+                      ]
+)
