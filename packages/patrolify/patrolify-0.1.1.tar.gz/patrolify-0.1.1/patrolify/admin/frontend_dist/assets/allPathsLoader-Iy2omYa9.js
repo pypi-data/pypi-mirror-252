da5f62b4efa8378eb34d9eabd3bfa4535890ecec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:04:40 +0000

    Add package patrolify/0.1.1/patrolify-0.1.1.tar.gz

---
+++ packages/patrolify/patrolify-0.1.1.tar.gz/patrolify-0.1.1/patrolify/admin/frontend_dist/assets/allPathsLoader-Iy2omYa9.js
@@ -0,0 +1,7 @@
+import{_ as o,a as n,b as i}from"./index-joqSclU6.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-0UJUX5G3.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-0UJUX5G3.js","assets/index-6QKa2Ynx.js","assets/index-s_3b95Qx.js","assets/index-joqSclU6.js","assets/index-tafDgpBb.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
