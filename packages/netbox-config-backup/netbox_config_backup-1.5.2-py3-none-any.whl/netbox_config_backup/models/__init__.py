744d454bad9bafeae78777911e7c60ff1d486e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:03:52 +0000

    Add package netbox-config-backup/1.5.2/netbox_config_backup-1.5.2-py3-none-any.whl

---
+++ packages/netbox-config-backup/netbox_config_backup-1.5.2-py3-none-any.whl/netbox_config_backup/models/__init__.py
@@ -0,0 +1,12 @@
+from netbox_config_backup.models.backups import Backup, BackupCommit, BackupFile, BackupObject, BackupCommitTreeChange
+from netbox_config_backup.models.jobs import BackupJob
+
+
+__all__ = (
+    'Backup',
+    'BackupCommit',
+    'BackupFile',
+    'BackupObject',
+    'BackupCommitTreeChange',
+    'BackupJob',
+)
