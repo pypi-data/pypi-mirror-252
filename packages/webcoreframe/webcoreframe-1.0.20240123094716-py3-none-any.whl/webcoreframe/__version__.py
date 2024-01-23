085f331a44e1725ae1b8c534735c9a89db3a0d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:04:31 +0000

    Add package webcoreframe/1.0.20240123094716/webcoreframe-1.0.20240123094716-py3-none-any.whl

---
+++ packages/webcoreframe/webcoreframe-1.0.20240123094716-py3-none-any.whl/webcoreframe/__version__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# code build user: Administrator
+# code build date: 2024-01-23 10:22:15
+# code release date: 2024-01-23 09:47:16
+# code commit hash: 84f527a2759a26206a01030c4f7abd3d5b29d7c0
+
+
+VERSION = "1.0.20240123094716"
+
