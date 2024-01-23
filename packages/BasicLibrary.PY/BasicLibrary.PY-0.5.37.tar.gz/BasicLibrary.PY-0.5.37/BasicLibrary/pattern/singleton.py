819bb908e9576acb24ddd9bafde355fa5555e0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 15:04:23 +0000

    Add package BasicLibrary.PY/0.5.37/BasicLibrary.PY-0.5.37.tar.gz

---
+++ packages/BasicLibrary.PY/BasicLibrary.PY-0.5.37.tar.gz/BasicLibrary.PY-0.5.37/BasicLibrary/pattern/singleton.py
@@ -0,0 +1,24 @@
+"""
+ * @file   : singleton.py
+ * @time   : 13:56
+ * @date   : 2022/1/6
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+
+
+class Singleton(object):
+    """
+    单例装饰器
+    """
+    def __init__(self, cls):
+        self._cls = cls
+        self._instance = {}
+
+    def __call__(self):
+        if self._cls not in self._instance:
+            self._instance[self._cls] = self._cls()
+        pass
+
+        return self._instance[self._cls]
