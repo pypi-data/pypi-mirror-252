accf17105f21c0092cbd020d9e1bb716fd84f8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:19 +0000

    Add package snapctl/0.22.1/snapctl-0.22.1-py3-none-any.whl

---
+++ packages/snapctl/snapctl-0.22.1-py3-none-any.whl/snapctl/config/endpoints.py
@@ -0,0 +1,8 @@
+"""
+This file contains the endpoints for the Snapser API.
+"""
+END_POINTS = {
+    'DEV': 'https://gateway.dev.snapser.io/snapser',
+    'PLAYTEST': 'https://gateway.dev.snapser.io/playtest',
+    'PROD': 'https://gateway.snapser.com/snapser'
+}
