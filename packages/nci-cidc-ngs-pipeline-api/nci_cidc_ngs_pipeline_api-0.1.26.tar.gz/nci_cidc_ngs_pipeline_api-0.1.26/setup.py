541c186a87cde59ea441bf82ae60ce8ac85c20e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:03:48 +0000

    Add package nci-cidc-ngs-pipeline-api/0.1.26/nci_cidc_ngs_pipeline_api-0.1.26.tar.gz

---
+++ packages/nci-cidc-ngs-pipeline-api/nci_cidc_ngs_pipeline_api-0.1.26.tar.gz/nci_cidc_ngs_pipeline_api-0.1.26/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+from cidc_ngs_pipeline_api import __author__, __email__, __version__
+
+setup(
+    author=__author__,
+    author_email=__email__,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="The NCI CIDC NGS Pipeline output APIs",
+    python_requires=">=3.9",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="cidc_ngs_pipeline_api",
+    name="nci_cidc_ngs_pipeline_api",
+    packages=find_packages(include=["cidc_ngs_pipeline_api"]),
+    test_suite="tests",
+    url="https://github.com/NCI-CIDC/cidc-ngs-pipeline-api",
+    version=__version__,
+    zip_safe=False,
+)
