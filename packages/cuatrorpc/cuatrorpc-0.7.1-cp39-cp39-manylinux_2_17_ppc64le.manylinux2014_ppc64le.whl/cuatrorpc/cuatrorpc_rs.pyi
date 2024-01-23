a9a568af9de5ccdf68572d125c771022d90cf0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 06:03:58 +0000

    Add package cuatrorpc/0.7.1/cuatrorpc-0.7.1-cp39-cp39-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/cuatrorpc/cuatrorpc-0.7.1-cp39-cp39-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/cuatrorpc/cuatrorpc_rs.pyi
@@ -0,0 +1,32 @@
+from typing import List, Any, Optional
+
+class RpcClient:
+    def __new__(
+        self,
+        username: str,
+        password: str,
+        port: int,
+        host: Optional[str] = "localhost",
+        use_https: Optional[bool] = False,
+        timeout: Optional[int] = 120,
+    ): ...
+    def callrpc(
+        self,
+        method: str,
+        params: Optional[List[Any]] = None,
+        wallet: Optional[str] = "",
+    ) -> Any: ...
+
+class RpcClientCLI:
+    def __new__(
+        self,
+        cli_bin: str,
+        data_dir: str,
+        daemon_conf: str,
+    ): ...
+    def callrpc(
+        self,
+        method: str,
+        call_args: Optional[List[Any]] = None,
+        wallet: Optional[str] = "",
+    ) -> Any: ...
