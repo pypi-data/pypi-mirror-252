45e4c8ea15b66cb4c2637d94cf71ba2b7bb16aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:04:51 +0000

    Add package hybrid-sort/0.1.0/hybrid-sort-0.1.0.tar.gz

---
+++ packages/hybrid-sort/hybrid-sort-0.1.0.tar.gz/hybrid-sort-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hybrid-sort',  # my package name
+    version='0.1.0',  # my desired version
+    author='Pandey Abhishek Nath Roy',
+    author_email='princeabhi00985@gmail.com',
+    description='A hybrid sorting algorithm using Lomuto and Hoare partitioning',
+    packages=find_packages(),
+    install_requires=[]  # List any dependencies here
+)
