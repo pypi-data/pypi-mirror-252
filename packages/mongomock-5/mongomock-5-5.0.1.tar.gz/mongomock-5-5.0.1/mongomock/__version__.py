04ea762f0233bee970aac8b0d3492a07e8941412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:04:21 +0000

    Add package mongomock-5/5.0.1/mongomock-5-5.0.1.tar.gz

---
+++ packages/mongomock-5/mongomock-5-5.0.1.tar.gz/mongomock-5-5.0.1/mongomock/__version__.py
@@ -0,0 +1,10 @@
+from platform import python_version_tuple
+
+python_version = python_version_tuple()
+
+if (int(python_version[0]) == 3 and int(python_version[1]) >= 8) or int(python_version[0]) >= 4:
+    from importlib.metadata import version
+    __version__ = version('mongomock')
+else:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution('mongomock').version
