44b0c2d5e5fc1c86904004df665f7139bd706413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 14:04:24 +0000

    Add package multiparlog/1.0.8/multiparlog-1.0.8.tar.gz

---
+++ packages/multiparlog/multiparlog-1.0.8.tar.gz/multiparlog-1.0.8/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_namespace_packages
+
+# with open("README.rst", "rb") as f: # 
+#     long_descr = f.read().decode("utf-8")
+
+setup(
+    name = "multiparlog",
+    package_dir = {'': 'src'},
+    packages = ['multiparlog'],
+    # packages = find_namespace_packages(include=['src.multiparlog.*']),
+    # entry_points = {
+    #     "console_scripts": []
+    #     },
+    version = "1.0.4",
+    # description = "A flexible multi-processing safe logging tool",
+    # # long_description = long_descr,
+    # author = "Ralf Kotulla",
+    # author_email = "ralf.kotulla@gmail.com",
+    # url = "https://github.com/rkotulla/multiparallel_logging",
+    )
