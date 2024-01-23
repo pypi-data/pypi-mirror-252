7f5e5d5459fa598b10c5e985ae01e927f6bbb327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:19 +0000

    Add package growingnn/0.0.2/growingnn-0.0.2.tar.gz

---
+++ packages/growingnn/growingnn-0.0.2.tar.gz/growingnn-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import pkg_resources
+import codecs
+import os
+#python setup.py sdist bdist_wheel
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+with open('README.md') as f:
+    readme = f.read()
+VERSION = '0.0.2'
+DESCRIPTION = 'growingnn is a cutting-edge Python package that introduces a dynamic neural network architecture learning algorithm. This innovative approach allows the neural network to adapt its structure during training, optimizing both weights and architecture. Leveraging a Stochastic Gradient Descent-based optimizer and guided Monte Carlo tree search, the package provides a powerful tool for enhancing model performance.'
+
+
+# Setting up
+setup(
+    name="growingnn",
+    version=VERSION,
+    author="Szymon Świderski",
+    author_email="<pjuralszymqn@gmail.com>",
+    description=DESCRIPTION,
+    license=license,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_reqs = ["numpy", "matplotlib", "opencv-python", "scipy", "imgaug", "sklearn", "scikit-learn", "pyvis"],
+    #pkg_resources.parse_requirements('requirements.txt'),# numpy, matplotlib opencv-python scipy imgaug sklearn scikit-learn pyvis
+    keywords=['python', 'neural network', 'growing neural network', 'growing'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"])
