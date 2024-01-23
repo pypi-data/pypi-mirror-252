cf5574af55f3a0dca9af2061a214e9f7f4bb4f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 06:04:19 +0000

    Add package xingu/1.6.5/xingu-1.6.5-py3-none-any.whl

---
+++ packages/xingu/xingu-1.6.5-py3-none-any.whl/xingu/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['Model', 'Coach', 'Estimator', 'DataProviderFactory', 'DataProvider', 'ConfigManager']
+
+from .dataprovider          import DataProvider
+from .config_manager        import ConfigManager
+from .dataproviderfactory   import DataProviderFactory
+from .estimator             import Estimator
+from .coach                 import Coach
+from .model                 import Model
