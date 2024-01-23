35efe3d134bcb109aefb1d1188b5fe893655a342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:04:22 +0000

    Add package movoid-log/1.1.0/movoid_log-1.1.0-py3-none-any.whl

---
+++ packages/movoid-log/movoid_log-1.1.0-py3-none-any.whl/movoid_log/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/11 22:52
+# Description   : 
+"""
+from .log import Log, LogSub, LogElement, LogError
+
+if __name__ == '__main__':
+    pass
