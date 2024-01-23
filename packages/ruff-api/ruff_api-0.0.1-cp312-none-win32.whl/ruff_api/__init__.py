8850a7325723b39497a2145d2de2da2d516ed0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 16:05:12 +0000

    Add package ruff-api/0.0.1/ruff_api-0.0.1-cp312-none-win32.whl

---
+++ packages/ruff-api/ruff_api-0.0.1-cp312-none-win32.whl/ruff_api/__init__.py
@@ -0,0 +1,12 @@
+# Copyright Amethyst Reese
+# Licensed under the MIT license
+
+"""
+Experimental Python API for Ruff
+"""
+
+from .__version__ import __version__
+
+__author__ = "Amethyst Reese"
+
+from ._rust import format_string, FormatOptions
