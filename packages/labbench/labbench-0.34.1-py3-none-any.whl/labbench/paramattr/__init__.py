8ef7dcc9c4b65c76080b5247f64ec0d294ad15d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 07:05:29 +0000

    Add package labbench/0.34.1/labbench-0.34.1-py3-none-any.whl

---
+++ packages/labbench/labbench-0.34.1-py3-none-any.whl/labbench/paramattr/__init__.py
@@ -0,0 +1,18 @@
+from . import method, method_kwarg, property, value
+from ._bases import (
+    HasParamAttrs,
+    KeyAdapterBase,
+    ParamAttr,
+    Undefined,
+    adjust,
+    get_class_attrs,
+    hold_attr_notifications,
+    observe,
+    unobserve,
+)
+from ._key_adapters import message_keying, visa_keying
+
+for _obj in dict(locals()).values():
+    if getattr(_obj, '__module__', '').startswith('labbench.paramattr.'):
+        _obj.__module__ = 'labbench.paramattr'
+del _obj
