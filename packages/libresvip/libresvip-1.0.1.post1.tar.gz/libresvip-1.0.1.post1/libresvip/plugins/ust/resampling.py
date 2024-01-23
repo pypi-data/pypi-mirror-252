abe6cf30a0a258d8fdc2c8d1491203be5e537ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 03:03:43 +0000

    Add package libresvip/1.0.1.post1/libresvip-1.0.1.post1.tar.gz

---
+++ packages/libresvip/libresvip-1.0.1.post1.tar.gz/libresvip-1.0.1.post1/libresvip/plugins/ust/resampling.py
@@ -0,0 +1,28 @@
+from collections.abc import Callable
+from operator import attrgetter
+
+from more_itertools import minmax
+
+from libresvip.model.point import Point
+
+
+def resampled(
+    data: list[Point],
+    interval: int,
+    interpolate_method: Callable[[Point, Point, int], float],
+) -> list[Point]:
+    result = []
+    left_point, right_point = minmax(data, key=attrgetter("x"), default=(Point(0, 0), Point(0, 0)))
+    for current in range(left_point.x, right_point.x + 1, interval):
+        prev = next((p for p in reversed(data) if p.x <= current), None)
+        next_ = next((p for p in data if p.x >= current), None)
+        result.append(Point(x=current, y=interpolate_method(prev, next_, current)))
+    return result
+
+
+def dot_resampled(data: list[Point], interval: int) -> list[Point]:
+    return resampled(
+        data,
+        interval,
+        lambda prev, next_, _: prev[1] if prev and prev[1] is not None else next_[1],
+    )
