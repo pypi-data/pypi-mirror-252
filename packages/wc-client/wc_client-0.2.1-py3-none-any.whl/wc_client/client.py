51a820b94082745a78366d196baa709cec777967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 06:04:17 +0000

    Add package wc-client/0.2.1/wc_client-0.2.1-py3-none-any.whl

---
+++ packages/wc-client/wc_client-0.2.1-py3-none-any.whl/wc_client/client.py
@@ -0,0 +1,43 @@
+import base64
+from typing import Dict
+
+from wc_client.request import WCRequest
+
+
+class WCClient:
+    """
+    A client for interacting with a WooCommerce API.
+    """
+
+    def __init__(self, domain: str, consumer_key: str, consumer_secret: str):
+        self.domain = domain
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+
+    def _get_token(self) -> str:
+        """
+        Returns the authentication token.
+
+        Returns:
+            str: The authentication token
+        """
+        auth_str = f"{self.consumer_key}:{self.consumer_secret}"
+        enconded_auth = base64.b64encode(auth_str.encode("utf-8")).decode("utf-8")
+        return f"Basic {enconded_auth}"
+
+    @property
+    def _default_headers(self) -> Dict[str, str]:
+        """
+        Set the default headers for WooCommerce API call
+
+        Returns:
+            Dict: The headers
+        """
+        return {
+            "Accept": "application/json",
+            "User-Agent": "WooCommerce-Python-REST-API/wc/v3",
+            "Authorization": self._get_token(),
+        }
+
+    def __getattr__(self, name):
+        return WCRequest(self.domain, self._default_headers, name)
---
+++ packages/wc-client/wc_client-0.2.1-py3-none-any.whl/wc_client/request.py
@@ -0,0 +1,118 @@
+from typing import Any, Dict, List
+
+import httpx
+
+
+class WCRequest:
+    """
+    A request builder for WooCommerce API.
+    """
+
+    METHODS = {"delete", "get", "patch", "post", "put"}
+    DEFAULT_PAGE_LIMIT = 20
+
+    def __init__(self, base_url: str, headers: Dict, *args):
+        """
+        Construct the WooCommerce request builder object.
+            (e.g. WCRequest("https://example.com", {"Accept": "application/json"}, "consumer", "orders", "1"))
+
+        Args:
+            base_url (str): The base URL for the request
+            headers (Dict): The headers for the request
+            *args: The path for the request
+        """
+        self.base_url = base_url
+        self.args = list(map(str, args))
+        self.headers = headers
+
+        self._url_path = [base_url]
+        self._url_path.extend(self.args)
+
+        self.client = httpx.Client(headers=headers)
+
+    def _build_url(self) -> str:
+        """
+        Build the final URL for the request.
+
+        Returns:
+            str: The URL for the request
+        """
+        return "/".join(self._url_path)
+
+    def _update_headers(self, headers):
+        """
+        Update the headers for the request.
+
+        Args:
+            headers (Dict): The headers to update
+        """
+        self.headers.update(headers)
+
+    def _(self, resource: str) -> "WCRequest":
+        """
+        Build a new request with the given resource.
+
+        Args:
+            resource (str): The resource to append to the request
+
+        Returns:
+            WCRequest: The new request"""
+        return WCRequest(self.base_url, self.headers, *self.args, resource)
+
+    def __getattr__(self, resource: str) -> Any:
+        """
+        Adds method calls to the url path.
+            (e.g. WCRequest().consumer.orders.get() -> {base_url}/consumer/orders/{variable})
+
+        Args:
+            resource (str): The resource to append to the request
+        """
+        if resource in self.METHODS:
+
+            def make_request(body=None, query_params=None, headers=None):
+                if headers:
+                    self._update_headers(headers)
+
+                return self.client.request(
+                    method=resource,
+                    url=self._build_url(),
+                    data=body,
+                    params=query_params,
+                    headers=self.headers,
+                )
+
+            return make_request
+
+        elif resource == "paginated":
+
+            def make_request_paginated(query_params={}, headers=None) -> List[Dict]:
+                if headers:
+                    self._update_headers(headers)
+
+                query_params["per_page"] = self.DEFAULT_PAGE_LIMIT
+                query_params["page"] = 1
+
+                data = []
+
+                with self.client as client:
+                    while True:
+                        res = client.get(
+                            url=self._build_url(),
+                            headers=self.headers,
+                            params=query_params.copy(),
+                        )
+
+                        res_json = res.json()
+                        data.extend(res_json)
+
+                        if len(res_json) == 0:
+                            break
+
+                        query_params["page"] += 1
+
+                return data
+
+            return make_request_paginated
+
+        else:
+            return self._(resource)
