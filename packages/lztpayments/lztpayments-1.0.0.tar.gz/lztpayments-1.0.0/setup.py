1a8ee31c731414a05a563a847b4e65d8743b36f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 05:03:56 +0000

    Add package lztpayments/1.0.0/lztpayments-1.0.0.tar.gz

---
+++ packages/lztpayments/lztpayments-1.0.0.tar.gz/lztpayments-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='lztpayments',
+  version='1.0.0',
+  author='its_niks',
+  url='https://zelenka.guru/threads/4206679/',
+  packages=find_packages(),
+)
