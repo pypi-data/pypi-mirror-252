a253b4df82af662827aae1318e33a0f9c621f7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-23 06:04:06 +0000

    Add package plone.registry/2.0.1/plone.registry-2.0.1.tar.gz

---
+++ packages/plone.registry/plone.registry-2.0.1.tar.gz/plone.registry-2.0.1/setup.py
@@ -0,0 +1,64 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+version = "2.0.1"
+
+description = "Registry for application settings (like debconf/ about:config)"
+long_description = (
+    read("README.rst")
+    + "\n"
+    + read("plone", "registry", "registry.rst")
+    + "\n"
+    + read("plone", "registry", "events.rst")
+    + "\n"
+    + read("plone", "registry", "field.rst")
+    + "\n"
+    + read("CHANGES.rst")
+    + "\n"
+)
+
+setup(
+    name="plone.registry",
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="configuration registry",
+    author="Martin Aspeli, Wichert Akkerman, Hanno Schlichting",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.registry",
+    license="GPL",
+    packages=find_packages(),
+    namespace_packages=["plone"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "Zope",
+    ],
+    extras_require={"test": ["plone.schema"]},
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
