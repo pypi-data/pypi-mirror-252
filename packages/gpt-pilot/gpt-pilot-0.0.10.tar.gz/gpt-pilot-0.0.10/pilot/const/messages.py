18cf5d40220c13701b16ee34e3d0439b094812d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:09 +0000

    Add package gpt-pilot/0.0.10/gpt-pilot-0.0.10.tar.gz

---
+++ packages/gpt-pilot/gpt-pilot-0.0.10.tar.gz/gpt-pilot-0.0.10/pilot/const/messages.py
@@ -0,0 +1,4 @@
+CHECK_AND_CONTINUE = 'Please inform me of any issues, or type "continue" to proceed.'
+WHEN_USER_DONE = 'Once you have completed, enter "continue"'
+AFFIRMATIVE_ANSWERS = ['', 'y', 'yes', 'ok', 'okay', 'sure', 'absolutely', 'indeed', 'correct', 'affirmative']
+NEGATIVE_ANSWERS = ['n', 'no', 'skip', 'negative', 'not now', 'cancel', 'decline', 'stop']
