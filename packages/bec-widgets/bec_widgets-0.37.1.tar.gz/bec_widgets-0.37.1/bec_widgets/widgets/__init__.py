2b3c007210b001624d38d94bdc7b32140ee86907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:06:28 +0000

    Add package bec-widgets/0.37.1/bec_widgets-0.37.1.tar.gz

---
+++ packages/bec-widgets/bec_widgets-0.37.1.tar.gz/bec_widgets-0.37.1/bec_widgets/widgets/__init__.py
@@ -0,0 +1,5 @@
+from .monitor import BECMonitor, ConfigDialog
+from .motor_map import MotorMap
+from .scan_control import ScanControl
+from .toolbar import ModularToolBar
+from .editor import BECEditor
