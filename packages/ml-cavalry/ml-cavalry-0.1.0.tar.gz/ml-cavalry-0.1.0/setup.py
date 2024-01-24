c91649e519010fc473e563189f264e6879d1c5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:23 +0000

    Add package ml-cavalry/0.1.0/ml-cavalry-0.1.0.tar.gz

---
+++ packages/ml-cavalry/ml-cavalry-0.1.0.tar.gz/ml-cavalry-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml-cavalry',
+    version='0.1.0',
+    author='Andron00e',
+    author_email='semenov.andrei.v@gmail.com',
+    description='Custom tools for Machine Learning and Optimization',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
