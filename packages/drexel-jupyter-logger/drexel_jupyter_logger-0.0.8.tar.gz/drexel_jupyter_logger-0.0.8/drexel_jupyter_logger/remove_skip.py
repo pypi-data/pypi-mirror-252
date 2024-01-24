0c2b8d1682896b35b3c3e786d5ebe4b733d50c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:05:25 +0000

    Add package drexel-jupyter-logger/0.0.8/drexel_jupyter_logger-0.0.8.tar.gz

---
+++ packages/drexel-jupyter-logger/drexel_jupyter_logger-0.0.8.tar.gz/drexel_jupyter_logger-0.0.8/drexel_jupyter_logger/remove_skip.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import nbformat
+import sys
+
+def main(notebook_path):
+   # Load the Jupyter Notebook
+   notebook = nbformat.read(notebook_path, as_version=4)
+
+   # Check and remove "skip execution" tags from each cell
+   for cell in notebook.cells:
+      if 'metadata' in cell and 'tags' in cell.metadata:
+         if 'skip-execution' in cell.metadata.tags:
+               if "from friendly.jupyter import *" in cell.source:
+                  pass
+               else:
+                  cell.metadata.tags.remove('skip-execution')
+
+   # Save the modified notebook
+   nbformat.write(notebook, notebook_path)
+   return notebook_path
+
+if __name__ == "__main__":
+   if len(sys.argv) != 1:
+      print("Usage: python <notebook_path>")
+      sys.exit(1)
+
+   input_notebook_path = sys.argv[0]
+   output_notebook_path = main(input_notebook_path)
