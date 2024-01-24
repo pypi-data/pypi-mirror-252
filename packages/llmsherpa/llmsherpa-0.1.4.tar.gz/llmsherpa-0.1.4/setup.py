0f5692cefe598065652e7435506b140b56a5c600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:05:07 +0000

    Add package llmsherpa/0.1.4/llmsherpa-0.1.4.tar.gz

---
+++ packages/llmsherpa/llmsherpa-0.1.4.tar.gz/llmsherpa-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llmsherpa',
+    version='0.1.4',    
+    description='Strategic APIs to Accelerate LLM Use Cases',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nlmatics/llmsherpa',
+    author='Ambika Sukla',
+    author_email='ambika.sukla@nlmatics.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        "urllib3"
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Legal Industry',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only'        
+    ],
+)
