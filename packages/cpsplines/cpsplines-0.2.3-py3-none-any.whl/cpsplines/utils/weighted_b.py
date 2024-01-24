d409b9c110b98980aa46006559f9cc0edcca43e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:16 +0000

    Add package cpsplines/0.2.3/cpsplines-0.2.3-py3-none-any.whl

---
+++ packages/cpsplines/cpsplines-0.2.3-py3-none-any.whl/cpsplines/utils/weighted_b.py
@@ -0,0 +1,28 @@
+from typing import Iterable, Tuple
+
+
+from cpsplines.psplines.bspline_basis import BsplineBasis
+
+
+def get_idx_fitting_region(bspline_bases: Iterable[BsplineBasis]) -> Tuple[slice]:
+    """
+    Get the fitting region indices on the expanded sample regressor vector
+    (containing the extra knots from the extended B-spline basis and the sample
+    regressor vector).
+
+    Parameters
+    ----------
+    bspline_bases : Iterable[BsplineBasis]
+        The B-spline bases.
+
+    Returns
+    -------
+    Tuple[slice]
+        Tuple of slices ranging from the first to the last index in the fitting
+        region, one by axis.
+    """
+
+    return tuple(
+        slice(bsp.int_back, bsp.int_back + len(bsp.xsample), None)
+        for bsp in bspline_bases
+    )
