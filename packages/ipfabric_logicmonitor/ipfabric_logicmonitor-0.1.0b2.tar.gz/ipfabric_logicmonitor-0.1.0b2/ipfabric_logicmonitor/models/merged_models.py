7e2f92648a4cc3006ca2f6a2c33110138a7866c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 06:04:51 +0000

    Add package ipfabric_logicmonitor/0.1.0b2/ipfabric_logicmonitor-0.1.0b2.tar.gz

---
+++ packages/ipfabric_logicmonitor/ipfabric_logicmonitor-0.1.0b2.tar.gz/ipfabric_logicmonitor-0.1.0b2/ipfabric_logicmonitor/models/merged_models.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class MergedDevice:
+    ipf_hostname: Optional[str] = None
+    ipf_fqdn: Optional[str] = None
+    ipf_login_ip: Optional[str] = None
+    ipf_site: Optional[str] = None
+    ipf_vendor: Optional[str] = None
+    ipf_family: Optional[str] = None
+    ipf_platform: Optional[str] = None
+    ipf_model: Optional[str] = None
+    ipf_serial_number: Optional[str] = None
+    ipf_hw_serial_number: Optional[str] = None
+    ipf_version: Optional[str] = None
+    ipf_device_type: Optional[str] = None
+    lm_display_name: Optional[str] = None
+    lm_name: Optional[str] = None
+    lm_hostname: Optional[str] = None
+    lm_sysname: Optional[str] = None
+    lm_description: Optional[str] = None
+    lm_host_status: Optional[str] = None
+    lm_groups: Optional[str] = None
+    lm_id: Optional[int] = None
+    lm_network_address: Optional[str] = None
+    lm_endpoint_serial_number: Optional[str] = None
+    lm_entphysical_serialnum: Optional[str] = None
+    lm_url: Optional[str] = None
