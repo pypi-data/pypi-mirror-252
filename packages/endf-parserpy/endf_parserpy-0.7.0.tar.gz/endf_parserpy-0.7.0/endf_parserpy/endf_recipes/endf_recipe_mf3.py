0b2eae8ffe3550a9187f24d4e18c94ccadf27691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:05:13 +0000

    Add package endf-parserpy/0.7.0/endf_parserpy-0.7.0.tar.gz

---
+++ packages/endf-parserpy/endf_parserpy-0.7.0.tar.gz/endf_parserpy-0.7.0/endf_parserpy/endf_recipes/endf_recipe_mf3.py
@@ -0,0 +1,17 @@
+############################################################
+#
+# Author(s):       Georg Schnabel
+# Email:           g.schnabel@iaea.org
+# Creation date:   2022/05/30
+# Last modified:   2022/05/30
+# License:         MIT
+# Copyright (c) 2022 International Atomic Energy Agency (IAEA)
+#
+############################################################
+
+ENDF_RECIPE_MF3 = """
+
+[MAT, 3, MT/ ZA, AWR, 0, 0, 0, 0] HEAD
+[MAT, 3, MT/ QM, QI, 0, LR, NR, NP / E / xs]TAB1 (xstable)
+SEND
+"""
