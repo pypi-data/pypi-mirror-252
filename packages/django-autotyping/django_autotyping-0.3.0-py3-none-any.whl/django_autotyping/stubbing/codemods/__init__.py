c08c3b184af8ee27061804c318d85bfd9bb89e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 06:04:23 +0000

    Add package django-autotyping/0.3.0/django_autotyping-0.3.0-py3-none-any.whl

---
+++ packages/django-autotyping/django_autotyping-0.3.0-py3-none-any.whl/django_autotyping/stubbing/codemods/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import Container, Literal
+
+from django_autotyping._compat import TypeAlias
+
+from .auth_functions_codemod import AuthFunctionsCodemod
+from .base import StubVisitorBasedCodemod
+from .create_overload_codemod import CreateOverloadCodemod
+from .forward_relation_overload_codemod import ForwardRelationOverloadCodemod
+from .get_model_overload_codemod import GetModelOverloadCodemod
+from .query_lookups_overload_codemod import QueryLookupsOverloadCodemod
+from .reverse_overload_codemod import ReverseOverloadCodemod
+from .settings_codemod import SettingCodemod
+
+__all__ = (
+    "StubVisitorBasedCodemod",
+    "CreateOverloadCodemod",
+    "ForwardRelationOverloadCodemod",
+    "GetModelOverloadCodemod",
+    "QueryLookupsOverloadCodemod",
+    "ReverseOverloadCodemod",
+)
+
+RulesT: TypeAlias = Literal["DJAS001", "DJAS002", "DJAS010", "DJAS011", "DJAS015", "DJAS016"]
+
+rules: list[tuple[RulesT, type[StubVisitorBasedCodemod]]] = [
+    ("DJAS001", ForwardRelationOverloadCodemod),
+    ("DJAS002", CreateOverloadCodemod),
+    # ("DJAS003", QueryLookupsOverloadCodemod),
+    ("DJAS010", GetModelOverloadCodemod),
+    ("DJAS011", AuthFunctionsCodemod),
+    ("DJAS015", ReverseOverloadCodemod),
+    ("DJAS016", SettingCodemod),
+]
+
+
+def gather_codemods(
+    ignore: Container[RulesT] = [], include: Container[RulesT] = []
+) -> list[type[StubVisitorBasedCodemod]]:
+    if include:
+        return [rule[1] for rule in rules if rule[0] in include]
+    return [rule[1] for rule in rules if rule[0] not in ignore]
