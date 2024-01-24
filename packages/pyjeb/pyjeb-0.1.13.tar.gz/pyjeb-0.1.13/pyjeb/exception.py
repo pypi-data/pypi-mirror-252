7dd6b7292a888ccb7f47cd84be18f06f7a4f3e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:48 +0000

    Add package pyjeb/0.1.13/pyjeb-0.1.13.tar.gz

---
+++ packages/pyjeb/pyjeb-0.1.13.tar.gz/pyjeb-0.1.13/pyjeb/exception.py
@@ -0,0 +1,7 @@
+"""PyJeb Exceptions"""
+
+class InvalidParameterException(Exception):
+    """Invalid parameter exception"""
+
+class CustomFunctionException(Exception):
+    """Custom parameter exception"""
