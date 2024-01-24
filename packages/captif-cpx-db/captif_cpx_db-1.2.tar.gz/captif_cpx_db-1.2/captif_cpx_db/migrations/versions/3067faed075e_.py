216eea2da6ddbf932866146aa2121040ef7f9685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:24 +0000

    Add package captif-cpx-db/1.2/captif_cpx_db-1.2.tar.gz

---
+++ packages/captif-cpx-db/captif_cpx_db-1.2.tar.gz/captif_cpx_db-1.2/captif_cpx_db/migrations/versions/3067faed075e_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 3067faed075e
+Revises:
+Create Date: 2024-01-23 03:17:01.187268
+
+"""
+from typing import Sequence, Union
+
+from alembic import op  # noqa: F401
+import sqlalchemy as sa  # noqa: F401
+
+
+# revision identifiers, used by Alembic.
+revision: str = "3067faed075e"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
