049a73fec02480c989811e23c1cb3c0005ddebcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:17 +0000

    Add package eis1600/1.4.1/eis1600-1.4.1.tar.gz

---
+++ packages/eis1600/eis1600-1.4.1.tar.gz/eis1600-1.4.1/eis1600/models/POSTagger.py
@@ -0,0 +1,17 @@
+from threading import Lock
+from typing import List, Optional
+
+from camel_tools.disambig.mle import MLEDisambiguator
+from camel_tools.tagger.default import DefaultTagger
+
+
+class POSTagger:
+    def __init__(self) -> None:
+        self.model = DefaultTagger(MLEDisambiguator.pretrained(), 'pos')
+        self.lock = Lock()
+
+    def get_pos(self, tokens: List[str], debug: Optional[bool] = False) -> List[str]:
+        with self.lock:
+            if debug:
+                print('POS')
+            return self.model.tag(tokens)
