3304e9b6871a7e5b3941842e5f8a2ba90a188461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:37 +0000

    Add package arize-phoenix/2.6.0/arize_phoenix-2.6.0.tar.gz

---
+++ packages/arize-phoenix/arize_phoenix-2.6.0.tar.gz/arize_phoenix-2.6.0/src/phoenix/config.py
@@ -0,0 +1,88 @@
+import os
+import tempfile
+from pathlib import Path
+from typing import List, Optional
+
+# Phoenix environment variables
+ENV_PHOENIX_PORT = "PHOENIX_PORT"
+ENV_PHOENIX_HOST = "PHOENIX_HOST"
+ENV_NOTEBOOK_ENV = "PHOENIX_NOTEBOOK_ENV"
+ENV_PHOENIX_COLLECTOR_ENDPOINT = "PHOENIX_COLLECTOR_ENDPOINT"
+"""
+The endpoint traces and evals are sent to. This must be set if the Phoenix
+server is running on a remote instance.
+"""
+
+
+def _get_temp_path() -> Path:
+    """Get path to  directory in which to store temp phoenix server files."""
+    return Path(tempfile.gettempdir()) / ".arize-phoenix"
+
+
+def get_pids_path() -> Path:
+    """Get path to directory in which to store temp phoenix instance pid files.
+    This directory is used to track any currently running instances of Arize Phoenix
+    on the host machine. The directory will be created if it does not exist.
+    """
+    path = _get_temp_path() / "pids"
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
+def get_running_pid() -> Optional[int]:
+    for file in get_pids_path().iterdir():
+        if file.name.isnumeric():
+            return int(file.name)
+    return None
+
+
+for path in (
+    ROOT_DIR := Path.home().resolve() / ".phoenix",
+    EXPORT_DIR := ROOT_DIR / "exports",
+    DATASET_DIR := ROOT_DIR / "datasets",
+    TRACE_DATASET_DIR := ROOT_DIR / "trace_datasets",
+):
+    path.mkdir(parents=True, exist_ok=True)
+
+PHOENIX_DIR = Path(__file__).resolve().parent
+# Server config
+SERVER_DIR = PHOENIX_DIR / "server"
+# The host the server will run on after launch_app is called
+HOST = "0.0.0.0"
+# The port the server will run on after launch_app is called
+PORT = 6006
+# The prefix of datasets that are auto-assigned a name
+GENERATED_DATASET_NAME_PREFIX = "phoenix_dataset_"
+
+
+def get_exported_files(directory: Path) -> List[Path]:
+    """
+    Yields the list of paths of exported files.
+
+    Parameters
+    ----------
+    directory: Path
+        Disk location to search exported files.
+
+    Returns
+    -------
+    list: List[Path]
+        List of paths of the exported files.
+    """
+    return list(directory.glob("*.parquet"))
+
+
+def get_env_port() -> int:
+    return (
+        int(port)
+        if isinstance(port := os.getenv(ENV_PHOENIX_PORT), str) and port.isnumeric()
+        else PORT
+    )
+
+
+def get_env_host() -> str:
+    return os.getenv(ENV_PHOENIX_HOST) or HOST
+
+
+def get_env_collector_endpoint() -> Optional[str]:
+    return os.getenv(ENV_PHOENIX_COLLECTOR_ENDPOINT)
