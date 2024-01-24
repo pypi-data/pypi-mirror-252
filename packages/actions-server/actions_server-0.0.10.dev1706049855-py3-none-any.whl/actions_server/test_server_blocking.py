bb738e5bc4680e20d2f5f678f73bf7efe39f9e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:19 +0000

    Add package actions-server/0.0.10.dev1706049855/actions_server-0.0.10.dev1706049855-py3-none-any.whl

---
+++ packages/actions-server/actions_server-0.0.10.dev1706049855-py3-none-any.whl/actions_server/test_server_blocking.py
@@ -0,0 +1,56 @@
+import logging
+import threading
+import time
+
+import pytest
+import requests
+import os
+
+from .server import JsonGet, http_server
+
+logging.basicConfig()
+
+PORT = 9999
+SLEEP_TIME = 10
+
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+class RequestThenStopThread(threading.Thread):
+    def __init__(self, server):
+        threading.Thread.__init__(self)
+        self.response = None
+        self.server = server
+
+    def run(self):
+        time.sleep(SLEEP_TIME)
+        result = requests.get(f"http://localhost:{PORT}/test")
+        result.raise_for_status()
+        self.response = result.json()
+        self.server.stop()
+
+
+class TestServer:
+
+    def teardown_method(self, method) -> None:
+        if self.server is not None:
+            self.server.stop()
+
+    @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
+    def test_should_block_main_thread(self):
+        # given
+        time_before_server_start = time.time()
+
+        self.server = http_server(PORT, [JsonGet("/test", lambda params: {'status': 'ok'})], thread_count=2)
+
+        request_thread = RequestThenStopThread(self.server)
+        request_thread.start()
+
+        self.server.start(block_caller_thread=True)
+
+        # when
+        time_after_server_start = time.time()
+
+        # then
+        assert SLEEP_TIME < time_after_server_start - time_before_server_start
+        assert 'ok' == request_thread.response['status']
