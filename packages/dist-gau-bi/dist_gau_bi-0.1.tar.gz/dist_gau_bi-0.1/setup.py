c9c31898223ca069aa9028522b12e0460f300a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:38 +0000

    Add package dist-gau-bi/0.1/dist_gau_bi-0.1.tar.gz

---
+++ packages/dist-gau-bi/dist_gau_bi-0.1.tar.gz/dist_gau_bi-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_gau_bi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_gau_bi'],
+      author='jahidul39306',
+      zip_safe=False)
