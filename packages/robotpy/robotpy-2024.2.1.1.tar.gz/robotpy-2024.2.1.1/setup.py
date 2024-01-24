3b813bc50010505239f811acd0d59fee6a5fad6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:11 +0000

    Add package robotpy/2024.2.1.1/robotpy-2024.2.1.1.tar.gz

---
+++ packages/robotpy/robotpy-2024.2.1.1.tar.gz/robotpy-2024.2.1.1/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+from pathlib import Path
+import re
+
+setup_dir = Path(__file__).parent
+
+
+def get_reqs_from_path(path):
+    content = Path(path).read_text()
+
+    # Do not include in all requirements if this special package is in there
+    in_all = re.search(r"^error-(.*)-not-available-yet$", content, re.MULTILINE) is None
+    return [
+        req for req in content.splitlines() if req and not req.startswith("#")
+    ], in_all
+
+
+install_requires, _ = get_reqs_from_path(setup_dir / "requirements.txt")
+all_reqs = set(install_requires)
+
+extras_require = {}
+
+for fpath in setup_dir.glob("*-requirements.txt"):
+    key = fpath.stem[:-13]  # stem doesn't have .txt
+
+    reqs, in_all = get_reqs_from_path(fpath)
+    if reqs:
+        extras_require[key] = reqs
+        if in_all:
+            all_reqs |= set(reqs)
+
+extras_require["all"] = all_reqs
+
+long_description = (setup_dir / "README.md").read_text()
+
+setup(
+    name="robotpy",
+    description="Meta package to make installing robotpy easier",
+    long_description=long_description,
+    author="RobotPy Development Team",
+    author_email="robotpy@googlegroups.com",
+    url="https://github.com/robotpy/robotpy-meta",
+    license="BSD-3-Clause",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    python_requires=">=3.8,<3.13",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Testing",
+    ],
+    use_scm_version=True,
+)
