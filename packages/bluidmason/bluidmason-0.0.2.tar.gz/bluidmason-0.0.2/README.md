9ed8b1d060053661438152a0a87089c2db0c2a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:06:59 +0000

    Add package bluidmason/0.0.2/bluidmason-0.0.2.tar.gz

---
+++ packages/bluidmason/bluidmason-0.0.2.tar.gz/bluidmason-0.0.2/README.md
@@ -0,0 +1,2 @@
+# BuildMason
+
---
+++ packages/bluidmason/bluidmason-0.0.2.tar.gz/bluidmason-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='bluidmason',
+    version='0.0.2',
+    author="Abhishek Chaudhary",
+    author_email="abhishek20144047@gmail.com",
+    license='MIT',
+    description="A command-line tool for generating scaffolding projects with different configurations. ",
+    long_description="BuildMason is a command-line tool for generating Flask projects with different configurations. It currently supports creating a basic Flask project, a Flask REST API project, and a Flask project with Blueprints.",
+    keywords='cli abhishek chaudhary flask project development software blueprint',
+    url='https://github.com/Worm4047/BuildMason',
+    py_modules=['buildmason'],
+    install_requires=['Click', 'cookiecutter', ],
+    entry_points={
+        'console_scripts': [
+            'buildmason = buildmason.app:cli'
+        ]
+    },
+)
