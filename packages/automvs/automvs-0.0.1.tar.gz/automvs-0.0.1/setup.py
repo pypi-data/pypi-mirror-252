2a2de4ddb919a6ffac2163168f1f20f91e3faef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:40 +0000

    Add package automvs/0.0.1/automvs-0.0.1.tar.gz

---
+++ packages/automvs/automvs-0.0.1.tar.gz/automvs-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='automvs',
+    version='0.0.1',    
+    description='Python library for MVS/CE automation',
+    url='https://github.com/MVS-sysgen/automvs',
+    author='Philip Young',
+    author_email='mainframed767@gmail.com',
+    license='MIT',
+    packages=['automvs'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',    
+        'Programming Language :: Python :: 3',
+    ],
+)
