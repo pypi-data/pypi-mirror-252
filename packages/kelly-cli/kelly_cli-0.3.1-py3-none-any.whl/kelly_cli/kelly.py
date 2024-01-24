71f73e386d16c7f3eba251822883918b3e173c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 06:04:31 +0000

    Add package kelly-cli/0.3.1/kelly_cli-0.3.1-py3-none-any.whl

---
+++ packages/kelly-cli/kelly_cli-0.3.1-py3-none-any.whl/kelly_cli/kelly.py
@@ -0,0 +1,49 @@
+import click
+from pybet import Odds
+from pybet.staking import kelly as pykelly
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--decimal",
+    "odds_type",
+    flag_value="decimal",
+    default=True,
+    help="interpret inputs as decimal odds (default)",
+)
+@click.option(
+    "-p",
+    "--prob",
+    "odds_type",
+    flag_value="prob",
+    help="interpret inputs as probabilities",
+)
+@click.option(
+    "-c",
+    "--percent",
+    "odds_type",
+    flag_value="percent",
+    help="interpret inputs as percentages",
+)
+@click.argument("true_odds", type=click.FLOAT)
+@click.argument("market_odds", type=click.FLOAT)
+@click.option(
+    "-b", "--bank", default=100, help="the size of betting bank (default 100)"
+)
+@click.option(
+    "-m",
+    "--commission",
+    default=0,
+    help="the percentage commission to deduct (default 0)",
+)
+def kelly(odds_type, true_odds, market_odds, bank, commission):
+    constructor = {
+        "decimal": Odds,
+        "prob": Odds.probability,
+        "percent": Odds.percentage,
+    }[odds_type]
+
+    stake = pykelly(constructor(true_odds), constructor(market_odds), bank, commission)
+    color = "bright_green" if stake > 0 else "red"
+    click.secho(f"£{round(stake, 2)}", fg=color)
