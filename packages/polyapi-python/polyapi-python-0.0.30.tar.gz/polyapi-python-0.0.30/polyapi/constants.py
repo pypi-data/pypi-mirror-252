121838ed641a6acfe57ea0f80650b7d0267dbd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:06:59 +0000

    Add package polyapi-python/0.0.30/polyapi-python-0.0.30.tar.gz

---
+++ packages/polyapi-python/polyapi-python-0.0.30.tar.gz/polyapi-python-0.0.30/polyapi/constants.py
@@ -0,0 +1,18 @@
+JSONSCHEMA_TO_PYTHON_TYPE_MAP = {
+    "integer": "int",
+    "number": "float",
+    "string": "str",
+    "boolean": "bool",
+    "array": "List",
+    "object": "Dict",
+}
+
+
+PYTHON_TO_JSONSCHEMA_TYPE_MAP = {
+    "int": "integer",
+    "float": "number",
+    "str": "string",
+    "bool": "boolean",
+    "List": "array",
+    "Dict": "object",
+}
