3a015e1b07d964ee0bbca4c2ac631489a1f19516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:06:01 +0000

    Add package appsurify-testbrain-contrib/2024.1.10/appsurify_testbrain_contrib-2024.1.10-py3-none-any.whl

---
+++ packages/appsurify-testbrain-contrib/appsurify_testbrain_contrib-2024.1.10-py3-none-any.whl/testbrain/contrib/__init__.py
@@ -0,0 +1,9 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)  # noqa
+
+__title__ = "Appsurify Testbrain Contrib"
+__name__ = "appsurify-testbrain-contrib"
+__prog__ = "appsurify-testbrain-contrib"
+__version__ = "2024.1.10"
+__build__ = "undefined"
