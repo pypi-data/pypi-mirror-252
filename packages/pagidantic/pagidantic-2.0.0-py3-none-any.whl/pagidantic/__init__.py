7c2d9849fa508eb4c010f47be86905f19645e333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:07:07 +0000

    Add package pagidantic/2.0.0/pagidantic-2.0.0-py3-none-any.whl

---
+++ packages/pagidantic/pagidantic-2.0.0-py3-none-any.whl/pagidantic/__init__.py
@@ -0,0 +1,26 @@
+"""
+Pagidantic is a Python package for pagination using Pydantic.
+
+It's easy to use, lightweight, and easy to integrate with existing projects.
+
+It helps in creating efficient pagination solution with minimal code, while maintaining type checking and data validation with Pydantic.
+"""
+
+
+__version__ = "2.0.0"
+
+from pagidantic.dict import PagidanticDict
+from pagidantic.factory import PagidanticFactory
+from pagidantic.page import Page
+from pagidantic.pagidantic import pagidantic
+from pagidantic.paginator import Paginator
+from pagidantic.set import PagidanticSet
+
+__all__ = [
+    "PagidanticFactory",
+    "Paginator",
+    "pagidantic",
+    "PagidanticDict",
+    "PagidanticSet",
+    "Page",
+]
