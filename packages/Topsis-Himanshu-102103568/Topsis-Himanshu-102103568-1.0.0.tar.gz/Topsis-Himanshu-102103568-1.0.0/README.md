f1654951342742e0ecd05152f08d5a470d5cc895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:07:07 +0000

    Add package Topsis-Himanshu-102103568/1.0.0/Topsis-Himanshu-102103568-1.0.0.tar.gz

---
+++ packages/Topsis-Himanshu-102103568/Topsis-Himanshu-102103568-1.0.0.tar.gz/Topsis-Himanshu-102103568-1.0.0/README.md
@@ -0,0 +1,27 @@
+```markdown
+# Project Description
+for: Assignment-1(UCS654)
+Submitted by: Himanshu Bansal
+Roll no: 102103568
+Group: 3COE20
+
+```markdown
+# What is TOPSIS?
+
+TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution) is a multi-criteria decision analysis method that helps in making decisions based on multiple criteria. This Python package provides a command-line interface for performing TOPSIS analysis on a given dataset.
+
+## Installation
+
+To install the TOPSIS package, you can use the following command:
+
+```bash
+pip install Topsis-Himanshu-102103568
+```
+
+### Parameters
+
+- `<InputDataFile>`: CSV file containing the input data.
+- `<Weights>`: Comma-separated weights for each criterion.
+- `<Impacts>`: Comma-separated impact values for each criterion (`+` for benefit, `-` for cost).
+- `<ResultFileName>`: Name of the file to save the TOPSIS result.
+
---
+++ packages/Topsis-Himanshu-102103568/Topsis-Himanshu-102103568-1.0.0.tar.gz/Topsis-Himanshu-102103568-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Implementation of Topsis'
+
+setup(
+    name="Topsis-Himanshu-102103568",
+    version=VERSION,
+    author="Himanshu Bansal",
+    author_email="himu90505@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['Topsis', 'Topsis-Himanshu-102103568', 'Himanshu', 'Topsis-Himanshu', '102103568'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
