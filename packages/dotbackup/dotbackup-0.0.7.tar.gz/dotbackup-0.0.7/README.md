95bfd8c090ccbd6961c5d615218847c7cd6d7054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:04:46 +0000

    Add package dotbackup/0.0.7/dotbackup-0.0.7.tar.gz

---
+++ packages/dotbackup/dotbackup-0.0.7.tar.gz/dotbackup-0.0.7/README.md
@@ -0,0 +1,174 @@
+# dotbackup
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/dotbackup)
+[![PyPI - Version](https://img.shields.io/pypi/v/dotbackup)](https://pypi.org/project/dotbackup)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/dotbackup)](https://pypi.org/project/dotbackup)
+[![AUR version](https://img.shields.io/aur/version/dotbackup)](https://aur.archlinux.org/packages/dotbackup)
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Lint: flake8](https://img.shields.io/badge/lint-flake8-blueviolet)](https://github.com/PyCQA/flake8)
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1)](https://pycqa.github.io/isort)
+[![Test: pytest](https://img.shields.io/badge/test-pytest-orange)](https://pytest.org)
+[![Codecov](https://codecov.io/gh/jaxvanyang/dotbackup/graph/badge.svg)](https://codecov.io/gh/jaxvanyang/dotbackup)
+
+YAML config based backup utility. Easy to use yet flexible. With a primary focus on
+dotfile backup & setup, but not limited to dotfiles.
+
+## Features
+
+- Simple configuration.
+- Custom hooks.
+
+## Installation
+
+You can download [the script](./src/dotbackup.py) and place it to somewhere you like:
+
+```bash
+curl -L -o ~/.local/bin/dotbackup \
+    "https://github.com/jaxvanyang/dotbackup/raw/main/src/dotbackup.py"
+chmod +x ~/.local/bin/dotbackup
+```
+
+Or you can install from one of these package managers:
+
+- [PyPI](https://pypi.org/project/dotbackup)
+- [AUR](https://aur.archlinux.org/packages/dotbackup)
+
+## Configuration
+
+The default configuration file path is `~/.config/dotbackup/dotbackup.yml`, however you
+can use the `-c` option to specify another configuration file. Configuration files use
+YAML syntax. If you are new to YAML and want to learn more, see [Learn yaml in Y Minutes](https://learnxinyminutes.com/docs/yaml).
+If you want a quick start, you can take [this](./examples/dotbackup.yml) as an example.
+Following are the configuration keyword definitions, undefined keywords are ignored
+(maybe I'll write a validator someday).
+
+### `backup_dir`
+
+Required. The directory where backup files be stored.
+
+Example:
+
+```yaml
+backup_dir: ~/dotfiles
+```
+
+### `clean`
+
+Optional. Whether to do clean backup/setup, i.e., delete target files before backup or
+setup. The default is `false`. This may be overridden by the command line option
+`--clean`.
+
+Example:
+
+```yaml
+clean: true
+```
+
+### `ignore`
+
+Optional. The global ignored file patterns. Files that matches these patterns will be
+ignored in backup and setup. But files which are directly specified in [`apps.<app>.files`](#appsappfiles)
+will not be ignored.
+
+Example:
+
+```yaml
+apps:
+  nvim:
+    files: [~/.config/nvim]
+ignore: ['.git']
+```
+
+### `apps.<app>.files`
+
+Optional. The files to be backed up of the application `<app>`, `<app>` can be any
+string. File paths MUST be under the `$HOME` directory due to implementation. You can
+use [custom hooks](#appsapppre_backuppost_backuppre_setuppost_setup) to back up other
+files.
+
+Example:
+
+```yaml
+apps:
+  nvim:
+    files:
+      - ~/.config/nvim/init.lua
+      - ~/.config/nvim/lua
+```
+
+### `apps.<app>.ignore`
+
+Optional. The Application ignored file patterns. Files that matches these patterns will
+be ignored in backup and setup. But files which are directly specified in [`apps.<app>.files`](#appsappfiles)
+will not be ignored.
+
+Example:
+
+```yaml
+apps:
+  nvim:
+    files: [~/.config/nvim]
+    ignore: ['*.json']
+```
+
+### `apps.<app>.<pre_backup|post_backup|pre_setup|post_setup>`
+
+Optional. The hooks to be executed before/after `<app>` backup/setup, `<app>` can be any
+string. Each hook accept a list of commands, command are passed to `sh -c`. You can use
+these hooks to do advance operation.
+
+Example:
+
+> FIXME: defining `BACKUP_DIR` is not implemented yet.
+
+```yaml
+apps:
+  dotbackup:
+    pre_backup:
+      - cp /etc/dotbackup/dotbackup.yml "$BACKUP_DIR/dotbackup/dotbackup.yml"
+    post_backup:
+      - cd "$BACKUP_DIR" && git add dotbackup
+    pre_setup:
+      - pip install --user dotbackup || true
+    post_setup:
+      - sudo cp "$BACKUP_DIR/dotbackup/dotbackup.yml" /etc/dotbackup/dotbackup.yml
+```
+
+### `<pre_backup|post_backup|pre_setup|post_setup>`
+
+Optional. The hooks to be executed before/after all backup/setup. Each hook accept a
+list of commands, command are passed to `sh -c`. You can use these hooks to do advance
+operation.
+
+Example:
+
+> FIXME: defining `BACKUP_DIR` is not implemented yet.
+
+```yaml
+pre_backup:
+  - date > "$BACKUP_DIR/backup_date.txt"
+post_backup:
+  - |
+    cd "$BACKUP_DIR"
+    git add backup_date.txt
+    git commit -m "update backups"
+    git push
+pre_setup:
+  - cd "$BACKUP_DIR" && git pull
+post_setup:
+  - paru -Syu base-devel
+```
+
+## Usage
+
+Some examples:
+
+```bash
+dotbackup nvim tmux
+dotbackup backup nvim tmux
+dotbackup setup nvim tmux
+dotbackup setup
+```
+
+Run `dotbackup -h` for more information.
---
+++ packages/dotbackup/dotbackup-0.0.7.tar.gz/dotbackup-0.0.7/tests/helper.py
@@ -0,0 +1,203 @@
+import filecmp
+import os
+import random
+import shutil
+import string
+import sys
+
+import dotbackup
+
+TEST_CONFIG_DIR = f"{os.path.dirname(__file__)}/configs"
+TEST_HOME = "/tmp/dotbackup_test"
+CONFIG_DIR = f"{TEST_HOME}/.config"
+CONFIG_FILE = f"{CONFIG_DIR}/dotbackup/dotbackup.yml"
+BACKUP_DIR = f"{TEST_HOME}/backup"
+
+BASIC_CONFIG_DICT = {
+    "backup_dir": "~/backup",
+    "apps": {
+        "app_a": {
+            "files": ["~/.config/app_a"],
+            "pre_backup": ["echo app_a pre_backup"],
+            "post_backup": ["echo app_a post_backup"],
+            "pre_setup": ["echo app_a pre_setup"],
+            "post_setup": ["echo app_a post_setup"],
+        },
+        "app_b": {
+            "files": ["~/.config/app_b/b1.txt", "~/.config/app_b/b2.txt"],
+            "pre_backup": ["echo app_b pre_backup"],
+            "post_backup": ["echo app_b post_backup"],
+            "pre_setup": ["echo app_b pre_setup"],
+            "post_setup": ["echo app_b post_setup"],
+        },
+    },
+    "pre_backup": ["echo pre_backup"],
+    "post_backup": ["echo post_backup"],
+    "pre_setup": ["echo pre_setup"],
+    "post_setup": ["echo post_setup"],
+}
+BASIC_CONFIG = dotbackup.Config(BASIC_CONFIG_DICT)
+ONLY_HOOKS_DICT = {
+    "backup_dir": "~/backup",
+    "apps": {
+        "app_a": {
+            "pre_backup": ["echo app_a pre_backup"],
+            "post_backup": ["echo app_a post_backup"],
+            "pre_setup": ["echo app_a pre_setup"],
+            "post_setup": ["echo app_a post_setup"],
+        },
+        "app_b": {
+            "pre_backup": ["echo app_b pre_backup"],
+            "post_backup": ["echo app_b post_backup"],
+            "pre_setup": ["echo app_b pre_setup"],
+            "post_setup": ["echo app_b post_setup"],
+        },
+    },
+    "pre_backup": ["echo pre_backup"],
+    "post_backup": ["echo post_backup"],
+    "pre_setup": ["echo pre_setup"],
+    "post_setup": ["echo post_setup"],
+}
+ONLY_HOOKS_CONFIG = dotbackup.Config(ONLY_HOOKS_DICT)
+
+APPS_CHOICE = [[], ["app_a"], ["app_b"], ["app_a", "app_b"], ["app_b", "app_a"]]
+CONFIG_PATHS = [
+    "basic.yml",
+    "clean.yml",
+    "complex_script.yml",
+    "ignore.yml",
+    "only_hooks.yml",
+]
+
+
+class CleanConfig:
+    dict = {
+        "backup_dir": "~/backup",
+        "clean": True,
+    }
+    config = dotbackup.Config(dict)
+
+
+class IgnoreConfig:
+    dict = {
+        "backup_dir": "~/backup",
+        "apps": {
+            "app": {
+                "files": [
+                    "~/.config/app",
+                    "~/.config/app/global_ignore",
+                    "~/.config/app/app_ignore",
+                ],
+                "ignore": ["app_ignore"],
+            },
+        },
+        "ignore": ["global_ignore"],
+    }
+    config = dotbackup.Config(dict)
+
+    global_ignore_file = f"{CONFIG_DIR}/app/ignore/global_ignore"
+    app_ignore_file = f"{CONFIG_DIR}/app/ignore/app_ignore"
+    global_noignore_file = f"{CONFIG_DIR}/app/global_ignore"
+    app_noignore_file = f"{CONFIG_DIR}/app/app_ignore"
+    files = (
+        global_ignore_file,
+        app_ignore_file,
+        global_noignore_file,
+        app_noignore_file,
+    )
+
+    global_ignore_backup = f"{BACKUP_DIR}/app/.config/app/ignore/global_ignore"
+    app_ignore_backup = f"{BACKUP_DIR}/app/.config/app/ignore/app_ignore"
+    global_noignore_backup = f"{BACKUP_DIR}/app/.config/app/global_ignore"
+    app_noignore_backup = f"{BACKUP_DIR}/app/.config/app/app_ignore"
+    backups = (
+        global_ignore_backup,
+        app_ignore_backup,
+        global_noignore_backup,
+        app_noignore_backup,
+    )
+
+
+def rm_test_home():
+    if not os.path.exists(TEST_HOME):
+        return
+
+    if os.path.isfile(TEST_HOME):
+        os.unlink(TEST_HOME)
+
+    shutil.rmtree(TEST_HOME)
+
+
+def prepare_test(monkeypatch):
+    monkeypatch.setattr(dotbackup, "HOME", TEST_HOME)
+    monkeypatch.setattr(sys, "argv", sys.argv[:0])
+    rm_test_home()
+
+
+def get_config_path(path):
+    test_config_path = f"{TEST_CONFIG_DIR}/{path}"
+
+    if os.path.isfile(test_config_path):
+        return test_config_path
+
+    return path
+
+
+def get_config(path):
+    if path == "basic.yml":
+        return BASIC_CONFIG
+    elif path == "only_hooks.yml":
+        return ONLY_HOOKS_CONFIG
+    elif path == "clean.yml":
+        return CleanConfig.config
+    elif path == "ignore.yml":
+        return IgnoreConfig.config
+
+    return dotbackup.parse_config(get_config_path(path))
+
+
+def random_str(length=0):
+    length = length or random.randint(1, 50)
+    return "".join(random.choices(string.ascii_uppercase, k=length))
+
+
+def cp(src, dst):
+    if os.path.isfile(src):
+        os.makedirs(os.path.dirname(dst), exist_ok=True)
+        shutil.copy2(src, dst)
+    elif os.path.isdir(src):
+        shutil.copytree(src, dst, dirs_exist_ok=True)
+    else:
+        raise FileNotFoundError(src)
+
+
+def mkdir(path):
+    os.makedirs(path, exist_ok=True)
+
+
+def create_file(path, content=""):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, mode="w") as fp:
+        fp.write(content)
+
+
+def read_file(path):
+    with open(path) as fp:
+        return fp.read()
+
+
+def generate_hook_out(command="backup", apps=[]):
+    if apps == []:
+        apps = ["app_a", "app_b"]
+
+    out = f"pre_{command}\n"
+    for app in apps:
+        out += f"{app} pre_{command}\n{app} post_{command}\n"
+    out += f"post_{command}\n"
+
+    return out
+
+
+def dirdiff(dir1, dir2):
+    dcmp = filecmp.dircmp(dir1, dir2)
+    return not (dcmp.left_only or dcmp.right_only or dcmp.diff_files)
