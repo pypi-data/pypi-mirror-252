44474d177d03454ddf2767ac42823494ab2d90ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 06:04:55 +0000

    Add package marimo/0.1.81/marimo-0.1.81.tar.gz

---
+++ packages/marimo/marimo-0.1.81.tar.gz/marimo-0.1.81/marimo/_ast/app.py
@@ -0,0 +1,310 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from collections.abc import Sequence
+from dataclasses import asdict, dataclass
+from typing import Any, Callable, Iterable, Literal, Optional, Union, cast
+
+from marimo._ast.cell import (
+    CellConfig,
+    CellFunction,
+    CellFuncType,
+    CellFuncTypeBound,
+    CellId_t,
+    execute_cell,
+)
+from marimo._ast.compiler import cell_factory
+from marimo._ast.errors import (
+    CycleError,
+    DeleteNonlocalError,
+    MultipleDefinitionError,
+    UnparsableError,
+)
+from marimo._output.rich_help import mddoc
+from marimo._runtime.dataflow import DirectedGraph, topological_sort
+
+
+@dataclass
+class _AppConfig:
+    """Program-specific configuration.
+
+    Configuration for frontends or runtimes that is specific to
+    a single marimo program.
+    """
+
+    width: Literal["normal", "full"] = "normal"
+
+    # The file path of the layout file, relative to the app file.
+    layout_file: Optional[str] = None
+
+    def asdict(self) -> dict[str, Any]:
+        return asdict(self)
+
+    def update(self, updates: dict[str, Any]) -> None:
+        config_dict = asdict(self)
+        for key in updates:
+            if key in config_dict:
+                self.__setattr__(key, updates[key])
+
+
+@dataclass
+class CellData:
+    """A cell together with some metadata"""
+
+    cell_id: CellId_t
+    # User code comprising the cell
+    code: str
+    # User-provided name for cell (or default)
+    name: str
+    # Cell config
+    config: CellConfig
+
+    # Callable cell, or None if cell was not parsable
+    cell_function: Optional[CellFunction[CellFuncType]]
+
+
+@mddoc
+class App:
+    """A marimo app.
+
+    A marimo app is a dataflow graph, with each node computing a Python
+    function.
+
+    This class has no public API, but this may change in the future.
+    """
+
+    def __init__(self, **kwargs: Any) -> None:
+        # Take `AppConfig` as kwargs for forward/backward compatibility;
+        # unrecognized settings will just be dropped, instead of raising
+        # a TypeError.
+        self._config = _AppConfig()
+        for key in asdict(self._config):
+            if key in kwargs:
+                self._config.__setattr__(key, kwargs.pop(key))
+
+        self._cell_data: dict[CellId_t, CellData] = {}
+        self._registration_order: list[CellId_t] = []
+
+        self._cell_id_counter = 0
+        self._unparsable = False
+        self._initialized = False
+
+    def _create_cell_id(
+        self, cell_function: Optional[CellFunction[CellFuncTypeBound]]
+    ) -> CellId_t:
+        del cell_function
+        cell_id = str(self._cell_id_counter)
+        self._registration_order.append(cell_id)
+        self._cell_id_counter += 1
+        return str(cell_id)
+
+    def cell(
+        self,
+        func: Optional[CellFuncTypeBound] = None,
+        *,
+        disabled: bool = False,
+        hide_code: bool = False,
+        **kwargs: Any,
+    ) -> Union[
+        Callable[[CellFuncType], CellFunction[CellFuncTypeBound]],
+        CellFunction[CellFuncTypeBound],
+    ]:
+        """A decorator to add a cell to the app
+
+        This decorator can be called with or without parentheses. Each of the
+        following is valid:
+
+        ```
+        @app.cell
+        def __(mo):
+            # ...
+
+        @app.cell()
+        def __(mo):
+            # ...
+
+        @app.cell(disabled=True)
+        def __(mo):
+            # ...
+        ```
+
+        Args:
+        - func: The decorated function
+        - disabled: Whether to disable the cell
+        - kwargs: For forward-compatibility with future arguments
+        """
+        del kwargs
+
+        cell_config = CellConfig(disabled=disabled, hide_code=hide_code)
+
+        if func is None:
+            # If the decorator was used with parentheses, func will be None,
+            # and we return a decorator that takes the decorated function as an
+            # argument
+            def decorator(
+                func: CellFuncTypeBound,
+            ) -> CellFunction[CellFuncTypeBound]:
+                cell_function = cell_factory(
+                    func, cell_id=str(self._cell_id_counter)
+                )
+                cell_function.cell.configure(cell_config)
+                self._register_cell(cell_function)
+                return cell_function
+
+            return cast(
+                Callable[[CellFuncType], CellFunction[CellFuncTypeBound]],
+                decorator,
+            )
+        else:
+            # If the decorator was used without parentheses, func will be the
+            # decorated function
+            cell_function = cell_factory(
+                func, cell_id=str(self._cell_id_counter)
+            )
+            cell_function.cell.configure(cell_config)
+            self._register_cell(cell_function)
+            return cell_function
+
+    def _register_cell(
+        self, cell_function: CellFunction[CellFuncTypeBound]
+    ) -> None:
+        cell_id = self._create_cell_id(cell_function)
+        self._cell_data[cell_id] = CellData(
+            cell_id=cell_id,
+            code=cell_function.cell.code,
+            name=cell_function.__name__,
+            config=cell_function.cell.config,
+            cell_function=cast(CellFunction[CellFuncType], cell_function),
+        )
+
+    def _names(self) -> Iterable[str]:
+        return (cell_data.name for cell_data in self._cell_data.values())
+
+    def _codes(self) -> Iterable[str]:
+        return (cell_data.code for cell_data in self._cell_data.values())
+
+    def _configs(self) -> Iterable[CellConfig]:
+        return (cell_data.config for cell_data in self._cell_data.values())
+
+    def _cell_functions(
+        self,
+    ) -> Iterable[Optional[CellFunction[CellFuncType]]]:
+        return (
+            cell_data.cell_function for cell_data in self._cell_data.values()
+        )
+
+    def _unparsable_cell(
+        self,
+        code: str,
+        name: Optional[str] = None,
+        **config: Any,
+    ) -> None:
+        cell_id = self._create_cell_id(None)
+        name = name if name is not None else "__"
+        # - code.split("\n")[1:-1] disregards first and last lines, which are
+        #   empty
+        # - line[4:] removes leading indent in multiline string
+        # - replace(...) unescapes double quotes
+        # - rstrip() removes an extra newline
+        code = "\n".join(
+            [line[4:].replace('\\"', '"') for line in code.split("\n")[1:-1]]
+        )
+        self._cell_data[cell_id] = CellData(
+            cell_id=cell_id,
+            code=code,
+            name=name,
+            config=CellConfig.from_dict(config),
+            cell_function=None,
+        )
+        self._unparsable = True
+
+    def _maybe_initialize(self) -> None:
+        assert not self._unparsable
+        if not self._initialized:
+            # ids of cells to add to the graph, in the order that they
+            # were registered with the app
+            self._cell_ids = [
+                # exclude unparsable cells from graph
+                cell_data.cell_id
+                for cell_data in self._cell_data.values()
+                if cell_data.cell_function is not None
+            ]
+            self._graph = DirectedGraph()
+            for cell_id in self._cell_ids:
+                cell_function = self._cell_data[cell_id].cell_function
+                assert cell_function is not None
+                self._graph.register_cell(cell_id, cell_function.cell)
+            self._defs = self._graph.definitions.keys()
+
+            # these two helper functions could be written as concise
+            # `any` expressions using assignment expressions, but
+            # that's a silly reason to make Python < 3.8 incompatible
+            # with marimo.
+            def get_multiply_defined() -> Optional[str]:
+                for name, definers in self._graph.definitions.items():
+                    if len(definers) > 1:
+                        return name
+                return None
+
+            def get_deleted_nonlocal_ref() -> Optional[str]:
+                for cell in self._graph.cells.values():
+                    for ref in cell.deleted_refs:
+                        if ref in self._graph.definitions:
+                            return ref
+                return None
+
+            try:
+                if self._graph.cycles:
+                    raise CycleError(
+                        "This app can't be run because it has cycles."
+                    )
+                name = get_multiply_defined()
+                if name is not None:
+                    raise MultipleDefinitionError(
+                        "This app can't be run because it has multiple "
+                        f"definitions of the name {name}"
+                    )
+                ref = get_deleted_nonlocal_ref()
+                if ref is not None:
+                    raise DeleteNonlocalError(
+                        "This app can't be run because at least one cell "
+                        f"deletes one of its refs (the ref's name is {ref})"
+                    )
+                self._execution_order = topological_sort(
+                    self._graph, self._cell_ids
+                )
+            finally:
+                self._initialized = True
+
+    def run(self) -> tuple[Sequence[Any], dict[str, Any]]:
+        if self._unparsable:
+            raise UnparsableError(
+                "This app can't be run because it has unparsable cells."
+            )
+
+        self._maybe_initialize()
+        glbls: dict[Any, Any] = {}
+        outputs = {
+            cid: execute_cell(cell_function.cell, glbls)
+            for cid in self._execution_order
+            if (cell_function := self._cell_data[cid].cell_function)
+            is not None
+        }
+        # Return
+        # - the outputs, sorted in the order that cells were added to the
+        #   graph
+        # - dict of defs -> values
+        return (
+            tuple(
+                outputs[cid]
+                for cid in self._registration_order
+                # exclude unparsable cells
+                if cid in outputs
+            ),
+            # omit defs that were never defined at runtime, eg due to
+            # conditional definitions like
+            #
+            # if cond:
+            #   x = 0
+            {name: glbls[name] for name in self._defs if name in glbls},
+        )
---
+++ packages/marimo/marimo-0.1.81.tar.gz/marimo-0.1.81/marimo/_server/sessions.py
@@ -0,0 +1,384 @@
+# Copyright 2024 Marimo. All rights reserved.
+"""Client session management
+
+This module encapsulates session management: each client gets a unique session,
+and each session wraps a Python kernel and a websocket connection through which
+the kernel can send messages to the frontend. Sessions do not share kernels or
+websockets.
+
+In run mode, in which we may have many clients connected to the server, a
+session is closed as soon as its websocket connection is severed. In edit mode,
+in which we have at most one connected client, a session may be kept around
+even if its socket is closed.
+"""
+from __future__ import annotations
+
+import multiprocessing as mp
+import os
+import queue
+import shutil
+import signal
+import subprocess
+import sys
+import threading
+from multiprocessing import connection
+from typing import Any, Optional
+from uuid import uuid4
+
+from marimo import _loggers
+from marimo._ast import codegen
+from marimo._ast.app import App, _AppConfig
+from marimo._messaging.ops import Alert, serialize
+from marimo._output.formatters.formatters import register_formatters
+from marimo._runtime import requests, runtime
+from marimo._server.model import (
+    ConnectionState,
+    SessionHandler,
+    SessionMode,
+)
+from marimo._server.utils import import_files, print_tabbed
+
+LOGGER = _loggers.marimo_logger()
+SESSION_MANAGER: Optional["SessionManager"] = None
+
+
+class Session:
+    """A client session.
+
+    Each session has its own Python kernel, for editing and running the app,
+    and its own websocket, for sending messages to the client.
+    """
+
+    TTL_SECONDS = 120
+
+    def __init__(
+        self,
+        session_handler: SessionHandler,
+    ) -> None:
+        """Initialize kernel and client connection to it."""
+        mpctx = mp.get_context("spawn")
+        mgr: SessionManager = get_manager()
+        self.session_handler = session_handler
+
+        # Control messages for the kernel (run, autocomplete,
+        # set UI element, set config, etc ) are sent through the control queue
+        self.control_queue: mp.Queue[requests.Request] | queue.Queue[
+            requests.Request
+        ]
+        # Input messages for the user's Python code are sent through the
+        # input queue
+        self.input_queue: mp.Queue[str] | queue.Queue[str]
+
+        self.kernel_task: threading.Thread | mp.Process
+        self.read_conn: connection.Connection
+
+        app = mgr.load_app()
+        configs = (
+            {cell_id: data.config for cell_id, data in app._cell_data.items()}
+            if app is not None
+            else {}
+        )
+        # Need to use a socket for windows compatibility
+        listener = connection.Listener(family="AF_INET")
+        is_edit_mode = mgr.mode == SessionMode.EDIT
+        if is_edit_mode:
+            # We use a process in edit mode so that we can interrupt the app
+            # with a SIGINT; we don't mind the additional memory consumption,
+            # since there's only one client session
+            self.control_queue = mpctx.Queue()
+            self.input_queue = mpctx.Queue(maxsize=1)
+            self.kernel_task = mp.Process(
+                target=runtime.launch_kernel,
+                args=(
+                    self.control_queue,
+                    self.input_queue,
+                    listener.address,
+                    is_edit_mode,
+                    configs,
+                ),
+                # The process can't be a daemon, because daemonic processes
+                # can't create children
+                # https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Process.daemon  # noqa: E501
+                daemon=False,
+            )
+        else:
+            # We use threads in run mode to minimize memory consumption;
+            # launching a process would copy the entire program state,
+            # which (as of writing) is around 150MB
+            self.control_queue = queue.Queue()
+            self.input_queue = queue.Queue(maxsize=1)
+
+            # We can't terminate threads, so we have to wait until they
+            # naturally exit before cleaning up resources
+            def launch_kernel_with_cleanup(*args: Any) -> None:
+                runtime.launch_kernel(*args)
+                self.read_conn.close()
+
+            # install formatter import hooks, which will be shared by all
+            # threads (in edit mode, the single kernel process installs
+            # formatters ...)
+            register_formatters()
+
+            # Make threads daemons so killing the server immediately brings
+            # down all client sessions
+            self.kernel_task = threading.Thread(
+                target=launch_kernel_with_cleanup,
+                args=(
+                    self.control_queue,
+                    self.input_queue,
+                    listener.address,
+                    is_edit_mode,
+                    configs,
+                ),
+                # daemon threads can create child processes, unlike
+                # daemon processes
+                daemon=True,
+            )
+        self.kernel_task.start()
+        # First thing kernel does is connect to the socket, so it's safe to
+        # call accept
+        self.read_conn = listener.accept()
+
+        def check_alive() -> None:
+            if not self.kernel_task.is_alive():
+                self.close()
+                print()
+                print_tabbed(
+                    "\033[31mThe Python kernel died unexpectedly.\033[0m"
+                )
+                print()
+                sys.exit()
+
+        session_handler.on_start(
+            mode=mgr.mode, connection=self.read_conn, check_alive=check_alive
+        )
+
+    def try_interrupt(self) -> None:
+        if (
+            isinstance(self.kernel_task, mp.Process)
+            and self.kernel_task.pid is not None
+        ):
+            LOGGER.debug("Sending SIGINT to kernel")
+            os.kill(self.kernel_task.pid, signal.SIGINT)
+
+    def close(self) -> None:
+        """Close kernel, sockets, and pipes."""
+        if isinstance(self.kernel_task, mp.Process):
+            # type ignores:
+            # guaranteed to be a multiprocessing Queue; annoying to assert
+            # this, because mp.Queue appears to be a function
+            #
+            # don't care if the queues still have things in it; don't make the
+            # child process wait for it to empty.
+            self.control_queue.cancel_join_thread()  # type: ignore
+            self.control_queue.close()  # type: ignore
+            self.input_queue.cancel_join_thread()  # type: ignore
+            self.input_queue.close()  # type: ignore
+            if self.kernel_task.is_alive():
+                # Explicitly terminate the process
+                self.kernel_task.terminate()
+            self.read_conn.close()
+        elif self.kernel_task.is_alive():
+            # kernel thread cleans up read/write conn and IOloop handler on
+            # exit; we don't join the thread because we don't want to block
+            self.control_queue.put(requests.StopRequest())
+
+        self.session_handler.on_stop(self.read_conn)
+
+
+class SessionManager:
+    """Mapping from client session IDs to sessions.
+
+    Maintains a mapping from client session IDs to client sessions;
+    there is exactly one session per client.
+
+    The SessionManager also encapsulates state common to all sessions:
+    - the app filename
+    - the app mode (edit or run)
+    - the server token
+    """
+
+    def __init__(
+        self,
+        filename: Optional[str],
+        mode: SessionMode,
+        port: int,
+        development_mode: bool,
+        quiet: bool,
+        include_code: bool,
+    ) -> None:
+        self.filename = filename
+        self.mode = mode
+        self.port = port
+        self.lsp_port = int(self.port * 10)
+        self.lsp_process: Optional[subprocess.Popen[bytes]] = None
+        self.development_mode = development_mode
+        self.quiet = quiet
+        self.sessions: dict[str, Session] = {}
+        self.app_config: Optional[_AppConfig]
+        self.include_code = include_code
+        # token uniquely identifying this server
+
+        if (app := self.load_app()) is not None:
+            self.app_config = app._config
+        else:
+            self.app_config = None
+
+        if mode == SessionMode.EDIT:
+            # In edit mode, the server gets a random token to prevent
+            # frontends that it didn't create from connecting to it and
+            # executing edit-only commands (such as overwriting the file).
+            self.server_token = str(uuid4())
+        else:
+            # Because run-mode is read-only, all that matters is that
+            # the frontend's app matches the server's app.
+            assert app is not None
+            self.server_token = str(
+                hash("".join(code for code in app._codes()))
+            )
+
+    def load_app(self) -> Optional[App]:
+        return codegen.get_app(self.filename)
+
+    def update_app_config(self, config: dict[str, Any]) -> None:
+        if self.app_config is not None:
+            self.app_config.update(config)
+        else:
+            self.app_config = _AppConfig(**config)
+
+    def rename(self, filename: Optional[str]) -> None:
+        """Register a change in filename.
+
+        Should be called if an api call renamed the current file on disk.
+        """
+        self.filename = filename
+
+    def create_session(
+        self, session_id: str, session_handler: SessionHandler
+    ) -> Session:
+        """Create a new session"""
+        LOGGER.debug("creating new session for id %s", session_id)
+        if session_id not in self.sessions:
+            s = Session(session_handler=session_handler)
+            self.sessions[session_id] = s
+            return s
+        else:
+            return self.sessions[session_id]
+
+    def get_session(self, session_id: str) -> Optional[Session]:
+        if session_id in self.sessions:
+            return self.sessions[session_id]
+        return None
+
+    def any_clients_connected(self) -> bool:
+        """Returns True if at least one client has an open socket."""
+        for session in self.sessions.values():
+            if (
+                session.session_handler.connection_state()
+                == ConnectionState.OPEN
+            ):
+                return True
+        return False
+
+    def start_lsp_server(self) -> None:
+        """Starts the lsp server if it is not already started.
+
+        Doesn't start in run mode.
+        """
+        if self.lsp_process is not None or self.mode == SessionMode.RUN:
+            LOGGER.debug("LSP server already started")
+            return
+
+        binpath = shutil.which("node")
+        if binpath is None:
+            LOGGER.error("Node.js not found; cannot start LSP server.")
+            for _, session in self.sessions.items():
+                session.session_handler.write_operation(
+                    Alert.name,
+                    serialize(
+                        Alert(
+                            title="Github Copilot: Connection Error",
+                            description="<span><a class='hyperlink' href='https://docs.marimo.io/getting_started/index.html#github-copilot'>Install Node.js</a> to use copilot.</span>",  # noqa: E501
+                            variant="danger",
+                        )
+                    ),
+                )
+            return
+
+        cmd = None
+        try:
+            LOGGER.debug("Starting LSP server at port %s...", self.lsp_port)
+            lsp_bin = os.path.join(
+                str(import_files("marimo").joinpath("_lsp")),
+                "index.js",
+            )
+            cmd = f"node {lsp_bin} --port {self.lsp_port}"
+            LOGGER.debug("... running command: %s", cmd)
+            self.lsp_process = subprocess.Popen(
+                cmd.split(),
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                stdin=subprocess.DEVNULL,
+            )
+            LOGGER.debug(
+                "... node process return code (`None` means success): %s",
+                self.lsp_process.returncode,
+            )
+            LOGGER.debug("Started LSP server at port %s", self.lsp_port)
+        except Exception as e:
+            LOGGER.error(
+                "When starting language server (%s), got error: %s",
+                cmd,
+                e,
+            )
+
+    def close_session(self, session_id: str) -> None:
+        LOGGER.debug("closing session %s", session_id)
+        session = self.get_session(session_id)
+        if session is not None:
+            session.close()
+            del self.sessions[session_id]
+
+    def close_all_sessions(self) -> None:
+        LOGGER.debug("Closing all sessions (sessions: %s)", self.sessions)
+        for session in self.sessions.values():
+            session.close()
+        LOGGER.debug("All sessions closed.")
+        self.sessions = {}
+
+    def shutdown(self) -> None:
+        LOGGER.debug("Shutting down")
+        self.close_all_sessions()
+        if self.lsp_process is not None:
+            self.lsp_process.terminate()
+
+    def should_send_code_to_frontend(self) -> bool:
+        """Returns True if the server can send messages to the frontend."""
+        return self.mode == SessionMode.EDIT or self.include_code
+
+
+def initialize_manager(
+    filename: Optional[str],
+    mode: SessionMode,
+    port: int,
+    development_mode: bool,
+    quiet: bool,
+    include_code: bool,
+) -> SessionManager:
+    """Must be called on server start."""
+    global SESSION_MANAGER
+    SESSION_MANAGER = SessionManager(
+        filename=filename,
+        mode=mode,
+        port=port,
+        development_mode=development_mode,
+        quiet=quiet,
+        include_code=include_code,
+    )
+    return SESSION_MANAGER
+
+
+def get_manager() -> SessionManager:
+    """Cannot be called until manager has been initialized."""
+    assert SESSION_MANAGER is not None
+    return SESSION_MANAGER
