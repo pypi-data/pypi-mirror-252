dc756f4dd1c7a896979d7541e27bb70cd8cab1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:23 +0000

    Add package actions-server/0.0.10.dev1706050157/actions-server-0.0.10.dev1706050157.tar.gz

---
+++ packages/actions-server/actions-server-0.0.10.dev1706050157.tar.gz/actions-server-0.0.10.dev1706050157/setup.py
@@ -0,0 +1,31 @@
+import os
+
+import setuptools
+import time
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = "0.0.10"
+
+postfix = "" if os.getenv("RELEASE", "0") == "1" else ".dev%s" % round(time.time())
+
+setuptools.setup(
+    name="actions-server",
+    version=f"{version}{postfix}",
+    description="A very simple, multi-threaded HTTP server",
+    author="Rafał Zarajczyk",
+    author_email="rzarajczyk@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rzarajczyk/actions-server",
+    keywords=["HTTP", "SERVER"],
+    packages=['actions_server'],
+    package_dir={'actions_server': './actions_server'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests>=2.31.0", "multipart>=0.2.4", "deser>=0.0.1.dev1706048327"],
+)
