aac628352e3e75e454ae448996982f861179ab50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 06:04:23 +0000

    Add package vectorview/1.0.1/vectorview-1.0.1.tar.gz

---
+++ packages/vectorview/vectorview-1.0.1.tar.gz/vectorview-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vectorview",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Vectorview",
+    author_email="lukas@vectorview.ai",
+    description="Vector database monitoring and analytics",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vectorview/vectorview_py",
+)
