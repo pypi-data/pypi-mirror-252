8b6b3b9809b2f98a391dd1f733607803b20f786b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:26 +0000

    Add package sws-api-wrapper/0.1.5/sws_api_wrapper-0.1.5.tar.gz

---
+++ packages/sws-api-wrapper/sws_api_wrapper-0.1.5.tar.gz/sws_api_wrapper-0.1.5/src/sws_api_wrapper/Course.py
@@ -0,0 +1,22 @@
+from .api import get_datetime_from_string
+from typing import Any
+
+class Course:
+    def __init__(self, json_course: dict[str, Any]) -> None:
+        
+        self.id: str = json_course['id']
+        self.name: str = json_course['name']
+        
+        # Just get the important id and name fields.
+        if ('place' in json_course.keys()):
+            self.place: dict[str, str] = {k:v for (k,v) in json_course['place'].items() if k in 'id name'}
+        
+        self.start = get_datetime_from_string(f"{json_course['date']}T{json_course['start'][:-6]}.0000Z")
+        self.end = get_datetime_from_string(f"{json_course['date']}T{json_course['end'][:-6]}.0000Z")
+        
+        
+    def __str__(self) -> str:
+        return f"Course({self.id}: {self.name} --- {self.start.strftime('%d-%m-%Y %H:%M')} - {self.end.strftime('%H:%M')})"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
