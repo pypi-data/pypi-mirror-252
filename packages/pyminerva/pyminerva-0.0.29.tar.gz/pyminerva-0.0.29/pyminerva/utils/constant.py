727e5a76a2409dfcef5ee888f7059662de0fec2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:05:16 +0000

    Add package pyminerva/0.0.29/pyminerva-0.0.29.tar.gz

---
+++ packages/pyminerva/pyminerva-0.0.29.tar.gz/pyminerva-0.0.29/pyminerva/utils/constant.py
@@ -0,0 +1,48 @@
+# Copyright 2023-2025 Jeongmin Kang, jarvisNim @ GitHub
+# See LICENSE for details.
+
+import sys, os
+
+utils_dir = os.getcwd() + '/batch/Utils'
+reports_dir = os.getcwd() + '/batch/reports'
+data_dir = os.getcwd() + '/batch/reports/data'
+database_dir = os.getcwd() + '/database'
+batch_dir = os.getcwd() + '/batch'
+sys.path.append(utils_dir)
+sys.path.append(reports_dir)
+sys.path.append(data_dir)
+sys.path.append(database_dir)
+sys.path.append(batch_dir)
+
+fred = Fred(api_key='0e836827495d195023016a96b5fe6e4a')
+bok_key = 'OLSJAN6H7R43WEYUEV5Q'
+fmp_key = 'f57bdcaa7d140c9de35806d47fbd2f91'
+
+TIMEFRAMES = ['1min', '1hour', '1day']
+
+ASSETS = ['stock', 'bond', 'commodity', 'cash']
+# 보유종목들
+MY_TICKERS = {
+    'US':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'KR':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],    
+    'EU':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'JP':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'CN':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'US':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'DE':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],    
+    'IN':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'SG':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+}
+
+# 관심종목들
+WATCH_TICKERS = {
+    'US':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'KR':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],    
+    'EU':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'JP':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'CN':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'US':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'DE':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],    
+    'IN':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+    'SG':[{'bond':['TLT']}, {'stock':['SPY','QQQ']}, {'commodity':['GLD']}, {'currency':['UUP']}, ],
+}
