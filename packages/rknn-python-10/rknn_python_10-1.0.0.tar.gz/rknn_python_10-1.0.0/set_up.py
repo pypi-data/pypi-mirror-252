540170f188c930d7c050bc586d0c301e8036cc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:06:31 +0000

    Add package rknn-python-10/1.0.0/rknn_python_10-1.0.0.tar.gz

---
+++ packages/rknn-python-10/rknn_python_10-1.0.0.tar.gz/rknn_python_10-1.0.0/set_up.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rknn_python_10',
+    version='1.0.0',
+    description='rknn_api',
+    author='ErnisMeshi',
+    author_email=' ernis.meshi@student.uni-pr.edu ',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
