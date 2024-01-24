3fc5094f8e7fe33b00ccf8456247095cda96c862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:05:38 +0000

    Add package dsp-tools/5.6.0.post55/dsp_tools-5.6.0.post55.tar.gz

---
+++ packages/dsp-tools/dsp_tools-5.6.0.post55.tar.gz/dsp_tools-5.6.0.post55/src/dsp_tools/utils/connection.py
@@ -0,0 +1,98 @@
+from typing import Any, Protocol
+
+
+class Connection(Protocol):
+    """
+    Protocol class/interface for the connection object.
+
+    Exposes:
+        - get
+        - put
+        - post
+        - delete
+    """
+
+    def get(
+        self,
+        route: str,
+        headers: dict[str, str] | None = None,
+    ) -> dict[str, Any]:
+        """
+        Make a HTTP GET request to the server to which this connection has been established.
+
+        Args:
+            route: route that will be called on the server
+            headers: headers for the HTTP request
+        """
+
+    def put(
+        self,
+        route: str,
+        data: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
+    ) -> dict[str, Any]:
+        """
+        Make a HTTP GET request to the server to which this connection has been established.
+
+        Args:
+            route: route that will be called on the server
+            data: payload of the HTTP request
+            headers: headers for the HTTP request
+        """
+
+    def post(
+        self,
+        route: str,
+        data: dict[str, Any] | None = None,
+        files: dict[str, tuple[str, Any]] | None = None,
+        headers: dict[str, str] | None = None,
+        timeout: int | None = None,
+    ) -> dict[str, Any]:
+        """
+        Make a HTTP POST request to the server to which this connection has been established.
+
+        Args:
+            route: route that will be called on the server
+            data: payload of the HTTP request
+            files: files to be uploaded, if any
+            headers: headers for the HTTP request
+            timeout: timeout of the HTTP request, or None if the default should be used
+        """
+
+    def delete(
+        self,
+        route: str,
+        headers: dict[str, str] | None = None,
+    ) -> dict[str, Any]:
+        """
+        Make a HTTP GET request to the server to which this connection has been established.
+
+        Args:
+            route: route that will be called on the server
+            headers: headers for the HTTP request
+        """
+
+    def get_token(self) -> str:
+        """
+        Return the token of this connection.
+
+        Raises:
+            BaseError: if no token is available
+        """
+
+    def login(self, email: str, password: str) -> None:
+        """
+        Retrieve a session token and store it as class attribute.
+
+        Args:
+            email: email address of the user
+            password: password of the user
+
+        Raises:
+            BaseError: if DSP-API returns no token with the provided user credentials
+        """
+
+    def logout(self) -> None:
+        """
+        Delete the token on the server and in this class.
+        """
