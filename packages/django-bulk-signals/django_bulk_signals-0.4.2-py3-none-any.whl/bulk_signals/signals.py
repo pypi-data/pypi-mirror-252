5987821dbe692add4fb9d9d1da86226af305d466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:08:14 +0000

    Add package django-bulk-signals/0.4.2/django_bulk_signals-0.4.2-py3-none-any.whl

---
+++ packages/django-bulk-signals/django_bulk_signals-0.4.2-py3-none-any.whl/bulk_signals/signals.py
@@ -0,0 +1,8 @@
+from django.dispatch import Signal
+
+pre_bulk_create = Signal()
+post_bulk_create = Signal()
+pre_bulk_update = Signal()
+post_bulk_update = Signal()
+pre_query_update = Signal()
+post_query_update = Signal()
