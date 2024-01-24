fa78294a2f357dd7bfddb34e4b62072509598a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:05:41 +0000

    Add package vesselPackage/0.0.1/vesselPackage-0.0.1.tar.gz

---
+++ packages/vesselPackage/vesselPackage-0.0.1.tar.gz/vesselPackage-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+   name="vesselPackage",
+   author="unknow",
+   description="unknow",
+   packages=find_packages(),
+   include_package_data=True,
+   classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+],
+   python_requires='>=3.6',
+   setup_requires=['setuptools-git-versioning'],
+   version_config={
+       "dirty_template": "{tag}",
+   }
+)
