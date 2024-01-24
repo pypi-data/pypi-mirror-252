df3a4d5fc4ca0df47d087d1a16f12135e14dfed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:05:11 +0000

    Add package vdk-jupyterlab-extension/0.1.1147938789/vdk_jupyterlab_extension-0.1.1147938789.tar.gz

---
+++ packages/vdk-jupyterlab-extension/vdk_jupyterlab_extension-0.1.1147938789.tar.gz/vdk_jupyterlab_extension-0.1.1147938789/src/__tests__/vdk-jupyterlab-extension.spec.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2021-2024 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
+ */
+
+describe('vdk-jupyterlab-extension', () => {
+  it('should be tested', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
