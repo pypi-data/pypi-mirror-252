66857d488dedf5eb3f7137f520e3583ee092fa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:05:13 +0000

    Add package edc-notification/0.3.24/edc-notification-0.3.24.tar.gz

---
+++ packages/edc-notification/edc-notification-0.3.24.tar.gz/edc-notification-0.3.24/runtests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+from edc_constants.constants import IGNORE
+from edc_test_utils import DefaultTestSettings, func_main
+
+app_name = "edc_notification"
+base_dir = Path(__file__).absolute().parent
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
+    EDC_AUTH_CODENAMES_WARN_ONLY=True,
+    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    EDC_SITES_REGISTER_DEFAULT=True,
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_export.apps.AppConfig",
+        "edc_randomization.apps.AppConfig",
+        "edc_dashboard.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_protocol.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+    ],
+    RANDOMIZATION_LIST_PATH=str(base_dir / app_name / "tests" / "test_randomization_list.csv"),
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    func_main(project_settings, f"{app_name}.tests")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
