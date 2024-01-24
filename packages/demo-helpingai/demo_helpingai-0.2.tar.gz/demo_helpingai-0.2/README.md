8157ffab90ab687d68b4e0c30f8963fb9629e9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:41 +0000

    Add package demo-helpingai/0.2/demo_helpingai-0.2.tar.gz

---
+++ packages/demo-helpingai/demo_helpingai-0.2.tar.gz/demo_helpingai-0.2/README.md
@@ -0,0 +1,29 @@
+# demo-helpingai
+
+HelpingAI most advance AI chatbot
+
+## Getting Started:
+
+    python -m pip install -U demo-helpingai
+
+Join my [Discord server](https://discord.gg/hf99YvT8qX ) for live chat, support, or if you have any issues with this package.
+
+
+## Support this repository:
+
+-   🎉 **Join my Discord Server:** Chat with me and others. [Join here](https://discord.gg/hf99YvT8qX ):
+
+[![DiscordWidget](https://discordapp.com/api/guilds/1076407776403787796/widget.png?style=banner2)](https://discord.gg/hf99YvT8qX)
+
+**Async:**
+
+```python
+
+from demo_helpingai import ChatBot
+
+bot = ChatBot()
+bot.query_chat()
+bot.close()
+
+```
+
---
+++ packages/demo-helpingai/demo_helpingai-0.2.tar.gz/demo_helpingai-0.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as f:
+    README = f.read()
+setup(
+    name="demo_helpingai",
+    version="0.2",
+    description="HelpingAI most advance AI chatbot.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HelpingAI/HelpingAI1",
+    author="OEvortex",
+    license="GPLv3",
+    keywords=[
+        "artificial-intelligence",
+        "chatbot",
+        "HelpingAI"
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    install_requires=[
+        "aiohttp",
+        "requests",
+        "curl-cffi",
+    ],
+    project_urls={
+        "Source": "https://github.com/HelpingAI/HelpingAI1",
+    },
+)
