330859e6c799aa93f8cc48797d9af41f4db997da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:49 +0000

    Add package clickhouse-s3-etl-tools/0.2.0/clickhouse_s3_etl_tools-0.2.0-py3-none-any.whl

---
+++ packages/clickhouse-s3-etl-tools/clickhouse_s3_etl_tools-0.2.0-py3-none-any.whl/clickhouse_s3_etl_tools/table_task_tree/config_module.py
@@ -0,0 +1,39 @@
+import argparse
+
+
+from clickhouse_s3_etl_tools.schema.schema_dependency_tree import DependencyTreeConfig
+
+
+def get_configuration() -> DependencyTreeConfig:
+    parser = argparse.ArgumentParser(
+        description="The script returns the dependency tree of ClickHouse tables to a file or stdout. It also "
+        "visualizes the tree in the console"
+        "Specify ClickHouse connection details, databases list and file_output"
+    )
+
+    parser.add_argument("--ch-url", type=str, help="ClickHouse URL")
+
+    parser.add_argument("--databases", type=str, help="Databases list")
+    parser.add_argument("--file_output", type=str, help="File output for dict result")
+    parser.add_argument(
+        "--excluded-databases",
+        type=str,
+        help="A list of excluded databases for the parent " "dependency.",
+    )
+    parser.add_argument(
+        "--log-level", type=str, default="INFO", help="Log Level"
+    )
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Construct raw config dictionary
+    raw_config = {
+        "CH_URL": args.ch_url,
+        "DATABASES": args.databases,
+        "FILE_OUTPUT": args.file_output,
+        "EXCLUDED_DATABASES": args.excluded_databases,
+        "LOG_LEVEL": args.log_level
+    }
+
+    return DependencyTreeConfig(**raw_config)
