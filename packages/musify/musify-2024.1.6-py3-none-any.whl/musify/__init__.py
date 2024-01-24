a4df53cdac7d101d8a28a3140c506bb63395b89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:36 +0000

    Add package musify/2024.1.6/musify-2024.1.6-py3-none-any.whl

---
+++ packages/musify/musify-2024.1.6-py3-none-any.whl/musify/__init__.py
@@ -0,0 +1,13 @@
+"""Welcome to Musify"""
+
+from os.path import basename, dirname
+
+PROGRAM_NAME = "Musify"
+__version__ = "2024.1.6"  # just a placeholder, version number is calculated dynamically at build time
+PROGRAM_OWNER_NAME = "George Martin Marino"
+PROGRAM_OWNER_USER = "geo-martino"
+PROGRAM_OWNER_EMAIL = f"gm.engineer+{PROGRAM_NAME.lower()}@pm.me"
+PROGRAM_URL = f"https://github.com/{PROGRAM_OWNER_USER}/{PROGRAM_NAME.lower()}"
+
+MODULE_ROOT: str = basename(dirname(__file__))
+PACKAGE_ROOT: str = dirname(dirname(__file__))
