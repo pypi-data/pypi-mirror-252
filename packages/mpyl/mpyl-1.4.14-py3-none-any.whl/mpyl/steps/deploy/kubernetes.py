1f0fb7f4630b25bc5fe2916f3b94f8342240c99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:06:40 +0000

    Add package mpyl/1.4.14/mpyl-1.4.14-py3-none-any.whl

---
+++ packages/mpyl/mpyl-1.4.14-py3-none-any.whl/mpyl/steps/deploy/kubernetes.py
@@ -0,0 +1,81 @@
+"""Deploys the docker image produced in the build stage to Kubernetes, using HELM. """
+import re
+from logging import Logger
+from typing import Optional
+
+from . import STAGE_NAME
+from .k8s import deploy_helm_chart, CustomResourceDefinition, DeployedHelmAppSpec
+from .k8s.chart import ChartBuilder, to_service_chart
+from .. import Step, Meta
+from ..models import (
+    Input,
+    Output,
+    ArtifactType,
+    input_to_artifact,
+)
+from ...project import Target
+
+
+class DeployKubernetes(Step):
+    def __init__(self, logger: Logger) -> None:
+        super().__init__(
+            logger,
+            Meta(
+                name="Kubernetes Deploy",
+                description="Deploy to k8s",
+                version="0.0.1",
+                stage=STAGE_NAME,
+            ),
+            produced_artifact=ArtifactType.NONE,
+            required_artifact=ArtifactType.DOCKER_IMAGE,
+        )
+
+    @staticmethod
+    def match_to_url(match: str) -> str:
+        return "https://" + next(iter(re.findall(r"`(.*)`", match.split(",")[-1])))
+
+    @staticmethod
+    def try_extract_hostname(
+        chart: dict[str, CustomResourceDefinition], service_name: str
+    ) -> Optional[str]:
+        ingress = chart.get(f"{service_name}-ingress-0-https")
+        if ingress:
+            routes = ingress.spec.get("routes", {})
+            if routes:
+                match = routes[0].get("match")
+                return DeployKubernetes.match_to_url(match)
+        return None
+
+    def execute(self, step_input: Input) -> Output:
+        properties = step_input.run_properties
+        builder = ChartBuilder(step_input)
+        chart = to_service_chart(builder)
+
+        deploy_result = deploy_helm_chart(
+            self._logger, chart, step_input, properties.target, builder.release_name
+        )
+        if deploy_result.success:
+            hostname = self.try_extract_hostname(chart, builder.project.name)
+            url = None
+            if hostname:
+                has_specific_routes_configured: bool = bool(
+                    builder.deployment.traefik is not None
+                    and properties.target == Target.PRODUCTION
+                )
+                self._logger.info(
+                    f"Service {step_input.project.name} reachable at: {hostname}"
+                )
+                endpoint = (
+                    "/" if has_specific_routes_configured else "/swagger/index.html"
+                )
+                url = f"{hostname}{endpoint}"
+            artifact = input_to_artifact(
+                ArtifactType.DEPLOYED_HELM_APP,
+                step_input,
+                spec=DeployedHelmAppSpec(url=f"{url}"),
+            )
+            return Output(
+                success=True, message=deploy_result.message, produced_artifact=artifact
+            )
+
+        return deploy_result
