04043a713b1bd304035ae9d1270dd36cbe75aefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:32 +0000

    Add package bailo/0.4.2/bailo-0.4.2-py3-none-any.whl

---
+++ packages/bailo/bailo-0.4.2-py3-none-any.whl/bailo/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__version__ = "0.4.2"
+
+from bailo.core.agent import Agent, PkiAgent, TokenAgent
+from bailo.core.client import Client
+from bailo.core.enums import ModelVisibility, Role, SchemaKind
+from bailo.helper.access_request import AccessRequest
+from bailo.helper.model import Model
+from bailo.helper.release import Release
+from bailo.helper.schema import Schema
