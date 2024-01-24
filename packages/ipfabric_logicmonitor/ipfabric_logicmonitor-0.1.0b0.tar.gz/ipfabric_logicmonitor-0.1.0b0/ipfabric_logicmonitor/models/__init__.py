2fe766063e243c2f95ef733c8ba58ab895d696fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:05:12 +0000

    Add package ipfabric_logicmonitor/0.1.0b0/ipfabric_logicmonitor-0.1.0b0.tar.gz

---
+++ packages/ipfabric_logicmonitor/ipfabric_logicmonitor-0.1.0b0.tar.gz/ipfabric_logicmonitor-0.1.0b0/ipfabric_logicmonitor/models/__init__.py
@@ -0,0 +1,19 @@
+from .ipf_models import (
+    IPFDevice,
+    ManagedIPs,
+    LogicalDevices,
+    SNMPSummary,
+    SNMPUser,
+    ConfiguredSNMPUser,
+)
+from .lm_models import LMDevice
+
+__all__ = [
+    "IPFDevice",
+    "ManagedIPs",
+    "LogicalDevices",
+    "SNMPSummary",
+    "SNMPUser",
+    "ConfiguredSNMPUser",
+    "LMDevice",
+]
