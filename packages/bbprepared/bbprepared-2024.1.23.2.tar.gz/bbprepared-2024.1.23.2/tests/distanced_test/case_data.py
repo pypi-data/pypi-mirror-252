7fecaa2d824ea22bd8d2a30be8c479a34e7ebfc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:05:38 +0000

    Add package bbprepared/2024.1.23.2/bbprepared-2024.1.23.2.tar.gz

---
+++ packages/bbprepared/bbprepared-2024.1.23.2.tar.gz/bbprepared-2024.1.23.2/tests/distanced_test/case_data.py
@@ -0,0 +1,19 @@
+import stk
+
+
+class CaseData:
+    """A test case."""
+
+    def __init__(
+        self,
+        molecule: stk.Molecule,
+        desired_functional_groups: int,
+        closest_ids: tuple[int, int],
+        furthest_ids: tuple[int, int],
+        name: str,
+    ) -> None:
+        self.molecule = molecule
+        self.desired_functional_groups = desired_functional_groups
+        self.closest_ids = closest_ids
+        self.furthest_ids = furthest_ids
+        self.name = name
