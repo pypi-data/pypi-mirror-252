7625b1349e824727b0d6cc8dd09a62c7adbe2b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:05:14 +0000

    Add package fleetfind/1.1.0/fleetfind-1.1.0.tar.gz

---
+++ packages/fleetfind/fleetfind-1.1.0.tar.gz/fleetfind-1.1.0/fleetfind/__init__.py
@@ -0,0 +1,3 @@
+from .main import fleetfind
+
+fleetfind = fleetfind
---
+++ packages/fleetfind/fleetfind-1.1.0.tar.gz/fleetfind-1.1.0/fleetfind/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class FleetFindAppConfig(AppConfig):
+    """App configuration for fleetfind."""
+    name = 'fleetfind'
+    default_auto_field = 'django.db.models.AutoField'
