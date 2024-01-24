e0302f4a343ba77be6bfa2bbe6f990edc5fc5ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:38 +0000

    Add package cilib/0.1.0/cilib-0.1.0.tar.gz

---
+++ packages/cilib/cilib-0.1.0.tar.gz/cilib-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cilib',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Empty example package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
