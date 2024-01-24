7d722029ab4e30c2fdc4cc59bff4ff2330bcf041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:07:12 +0000

    Add package pipez/0.0.9/pipez-0.0.9.tar.gz

---
+++ packages/pipez/pipez-0.0.9.tar.gz/pipez-0.0.9/pipez/watchdog.py
@@ -0,0 +1,56 @@
+from typing import List
+import logging
+
+from pipez.node import Node, NodeType, NodeStatus
+from pipez.batch import Batch, BatchStatus
+
+
+class WatchDog(Node):
+    def __init__(
+            self,
+            nodes: List[Node],
+            verbose_metrics: bool = True,
+            **kwargs
+    ) -> None:
+        super().__init__(name='WatchDog', type=NodeType.THREAD, nodes=nodes, timeout=1e-1)
+
+        self._nodes = self._kwargs['nodes']
+        self._verbose_metrics = verbose_metrics
+
+    def _print_metrics(self):
+        message = []
+        for node in self._nodes:
+            metrics = node.metrics
+            message.append(
+                '{}: {}[{:.2f}+-{:.2f} ms]'.format(
+                    node.name,
+                    metrics.sum('handled'),
+                    metrics.mean('duration') * 1000,
+                    metrics.std('duration') * 1000,
+                )
+            )
+        message = '\t'.join(message)
+        print('\r', message, flush=True, end='', sep='')
+
+    def work_func(
+            self,
+            data=None
+    ) -> Batch:
+        if self._verbose_metrics:
+            self._print_metrics()
+        if all([node.status == NodeStatus.FINISH for node in self._nodes]):
+            for node in self._nodes:
+                node.close()
+            logging.warning('WatchDog node got all finished nodes. This node will be finished.')
+            return Batch(data=list(), status=BatchStatus.END)
+        if any([node.status == NodeStatus.TERMINATE for node in self._nodes]):
+            logging.warning('WatchDog node got some terminated nodes. This node will be finished.')
+            for node in self._nodes:
+                node.terminate()
+                logging.warning(f'Node {node.name} was terminated by watchdog.')
+            return Batch(status=BatchStatus.END)
+        return Batch(status=BatchStatus.OK)
+
+    @property
+    def nodes(self) -> List[Node]:
+        return self._nodes
