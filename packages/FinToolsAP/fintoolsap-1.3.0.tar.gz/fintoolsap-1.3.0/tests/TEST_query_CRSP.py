6bea0e34590a3186437d0d676727f6def238b1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:38 +0000

    Add package FinToolsAP/1.3.0/fintoolsap-1.3.0.tar.gz

---
+++ packages/FinToolsAP/fintoolsap-1.3.0.tar.gz/fintoolsap-1.3.0/tests/TEST_query_CRSP.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import pathlib
+import shutil
+import time
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sys.path.insert(0, '../src/FinToolsAP/')
+
+import LocalDatabase
+import Decorators
+import LaTeXBuilder
+
+# set printing options
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', shutil.get_terminal_size()[0])
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+# directory for loacl wrds database 
+
+# linux
+LOCAL_WRDS_DB = pathlib.Path('/home/andrewperry/Documents')
+
+@Decorators.Performance
+def query_CRSP_performance(DB):
+    return(DB.queryDB(DB.DBP.CHAR, ticker = 'JPM', all_vars = True))
+
+def main():
+
+    DB = LocalDatabase.LocalDatabase(save_directory = LOCAL_WRDS_DB, 
+                                     database_name = 'LCLDB'
+                                    )
+
+    df = query_CRSP_performance(DB)
+    print(df.tail(20))
+    print(df.info())
+    
+
+if __name__ == "__main__":
+    main()
