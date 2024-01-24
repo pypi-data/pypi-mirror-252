2cdf1828d81566ce7a2298688eab5546cef00bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:06:25 +0000

    Add package jupyter-duckdb/0.9.2.4/jupyter_duckdb-0.9.2.4-py3-none-any.whl

---
+++ packages/jupyter-duckdb/jupyter_duckdb-0.9.2.4-py3-none-any.whl/duckdb_kernel/parser/elements/binary/__init__.py
@@ -0,0 +1,39 @@
+from .Cross import Cross
+from .Difference import Difference
+from .Intersection import Intersection
+from .Join import Join
+from .Union import Union
+
+from .Add import Add
+from .And import And
+from .ArrowLeft import ArrowLeft
+from .ConditionalSet import ConditionalSet
+from .Divide import Divide
+from .Equal import Equal
+from .GreaterThan import GreaterThan
+from .GreaterThanEqual import GreaterThanEqual
+from .LessThan import LessThan
+from .LessThanEqual import LessThanEqual
+from .Minus import Minus
+from .Multiply import Multiply
+from .Or import Or
+from .Unequal import Unequal
+
+# inverse binding strength
+LOGIC_BINARY_OPERATORS = sorted([
+    Or, And,
+    Equal, Unequal,
+    GreaterThan, GreaterThanEqual, LessThan, LessThanEqual,
+    ArrowLeft,
+    Add, Minus, Multiply, Divide
+], key=lambda x: x.order, reverse=True)
+
+RA_BINARY_OPERATORS = [
+    Difference,
+    Union,
+    Intersection,
+    Join,
+    Cross
+]
+
+DC_SET = ConditionalSet
