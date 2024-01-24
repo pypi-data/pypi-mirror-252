6cb4bdbb8704cd7ef20abed5acb81ac80389cd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:05:06 +0000

    Add package buildmason/0.1.0/buildmason-0.1.0-py3-none-any.whl

---
+++ packages/buildmason/buildmason-0.1.0-py3-none-any.whl/buildmason/__main__.py
@@ -0,0 +1,11 @@
+import click
+from . import commands
+
+@click.group()
+def cli():
+    pass
+
+# Add more commands for other project types
+cli.add_command(commands.basic_flask_project)
+cli.add_command(commands.rest_api)
+cli.add_command(commands.blueprint)
---
+++ packages/buildmason/buildmason-0.1.0-py3-none-any.whl/buildmason/commands.py
@@ -0,0 +1,36 @@
+import click
+from cookiecutter.main import cookiecutter
+
+def generate_project(template_path, output_path, extra_context=None):
+    """
+    Generate a project based on the provided template.
+
+    Parameters:
+        template_path (str): Path to the cookiecutter template.
+        output_path (str): Path to the output directory where the project will be generated.
+        extra_context (dict): Extra context for customizing the template.
+
+    Returns:
+        None
+    """
+    cookiecutter(template_path, output_dir=output_path, extra_context=extra_context)
+
+@click.command()
+@click.option('--output-path', prompt='Output directory path', help='Path where the project will be generated')
+def basic_flask_project(output_path):
+    click.echo("Creating a basic Flask project...")
+    generate_project('gh:Worm4047/buildmason-basic-flask', output_path)
+
+
+@click.command()
+@click.option('--output-path', prompt='Output directory path', help='Path where the project will be generated')
+def rest_api(output_path):
+    click.echo("Creating a Flask RESTful API project...")
+    generate_project('gh:Worm4047/buildmason-flask-rest', output_path)
+
+@click.command()
+@click.option('--output-path', prompt='Output directory path', help='Path where the project will be generated')
+def blueprint(output_path):
+    click.echo("Creating a Flask Blueprint project...")
+    generate_project('gh:Worm4047/buildmason-flask-blueprint', output_path)
+
