fdcf3c56835d90ecd1420f9cd43d2260bb909c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:05:11 +0000

    Add package hxsoup/0.4.1/hxsoup-0.4.1-py3-none-any.whl

---
+++ packages/hxsoup/hxsoup-0.4.1-py3-none-any.whl/hxsoup/exceptions.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from httpx import codes
+
+
+class EmptyResultError(Exception):
+    def __init__(self, error_message: str, selector: str | None = None, url=None, status_code: int | None = None) -> None:
+        error_message += (
+            ' This error happens probably because of invalid selector or URL. '
+            'Check whether selector and URL are both valid.\n'
+        )
+
+        if status_code is not None:
+            error_message += f"status code: HTTP {status_code} {codes(200).name}, "
+
+        if url is not None:
+            error_message += f"URL: {url}, "
+
+        error_message += f'selector: {selector!r}'
+
+        super().__init__(error_message)
