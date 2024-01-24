72d762e3bee35e828c05d044617fc670311c39ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 03:05:38 +0000

    Add package sql-mock/0.5.2/sql_mock-0.5.2.tar.gz

---
+++ packages/sql-mock/sql_mock-0.5.2.tar.gz/sql_mock-0.5.2/src/sql_mock/clickhouse/table_mocks.py
@@ -0,0 +1,23 @@
+from clickhouse_driver import Client
+
+from sql_mock.clickhouse.settings import ClickHouseSettings
+from sql_mock.table_mocks import BaseMockTable
+
+
+class ClickHouseTableMock(BaseMockTable):
+    _sql_dialect = "clickhouse"
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        self.settings = ClickHouseSettings()
+        super().__init__(*args, **kwargs)
+
+    def _get_results(self, query: str) -> list[dict]:
+        with Client(
+            host=self.settings.host, user=self.settings.user, password=self.settings.password, port=self.settings.port
+        ) as client:
+            res = client.query_dataframe(query)
+        return res.to_dict("records")
