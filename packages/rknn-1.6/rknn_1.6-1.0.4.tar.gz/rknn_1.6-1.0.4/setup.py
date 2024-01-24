6da9696f872c5a336cbbbca1898dd5c345838594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:38 +0000

    Add package rknn-1.6/1.0.4/rknn_1.6-1.0.4.tar.gz

---
+++ packages/rknn-1.6/rknn_1.6-1.0.4.tar.gz/rknn_1.6-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rknn_1.6',
+    version='1.0.4',
+    description='rknn_api',
+    author='ErnisMeshi',
+    author_email='ernis.meshi@student.uni-pr.edu',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    package_data={'': ['*.so']},  # Include all shared object files in the root package
+)
+
