02368dd09460ada2c6dd2a4275c898929369546a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:41 +0000

    Add package openark/0.0.3/openark-0.0.3.tar.gz

---
+++ packages/openark/openark-0.0.3.tar.gz/openark-0.0.3/examples/download_csv.py
@@ -0,0 +1,27 @@
+import argparse
+
+from openark import OpenArk
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='OpenARK',
+        description='OpenARK Python',
+    )
+    parser.add_argument(
+        'model',
+        type=str,
+        help='model name',
+    )
+    parser.add_argument(
+        'filename',
+        type=str,
+        help='a file name to store data',
+    )
+
+    args = parser.parse_args()
+    ark = OpenArk()
+    model = ark.get_model(args.model)
+
+    pl = model.to_polars()
+    pl.write_csv(args.filename)
---
+++ packages/openark/openark-0.0.3.tar.gz/openark-0.0.3/examples/query.py
@@ -0,0 +1,23 @@
+import argparse
+from pprint import pprint as print
+
+from openark import OpenArk
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='OpenARK',
+        description='OpenARK Python',
+    )
+    parser.add_argument(
+        'query',
+        type=str,
+        help='a SQL query to be executed',
+    )
+
+    args = parser.parse_args()
+    ark = OpenArk()
+    models = ark.get_global_namespace()
+
+    df = models.sql(args.query).collect()
+    print(df)
