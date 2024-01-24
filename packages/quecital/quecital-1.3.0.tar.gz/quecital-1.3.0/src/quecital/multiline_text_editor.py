ad845adfa413cb9f0da1a02e7d73e724d35a8c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:19 +0000

    Add package quecital/1.3.0/quecital-1.3.0.tar.gz

---
+++ packages/quecital/quecital-1.3.0.tar.gz/quecital-1.3.0/src/quecital/multiline_text_editor.py
@@ -0,0 +1,61 @@
+import click
+
+
+def get_multiline_edit(prompt, hint_shown=False):
+    """
+    Capture multiline input from the user using an interactive editor.
+
+    The function opens an interactive editor (specified by the user or the default)
+    for multiline input. The user can edit the content and save the changes.
+
+    Args:
+        prompt (str): The prompt to display to the user.
+
+    Returns:
+        List[str]: A list of strings representing the multiline input.
+    """
+    # Custom marker to separate prompt text from user's input
+    custom_marker = "# ---- Your Input Below ----\n"
+    default_content = f"{prompt}\n\n{custom_marker}"
+    hide_hint = '<DELETE FOR HINT>'
+    no_hint = 'Hint not provided'
+
+    edited_content = click.edit(
+        text=default_content,
+        editor=None,
+    )  # Let Click choose the default editor
+    # keep_open=False,  # Close the editor immediately after the user exits
+
+    # an attempt after viewing the hint
+    if edited_content and hint_shown is True:
+        # Find the position of the custom marker and extract user's input
+        marker_position = edited_content.find(custom_marker)
+        user_input = edited_content[marker_position + len(custom_marker):].strip()
+        return user_input
+
+    # check if the user wants to see the hint
+    if edited_content and hint_shown is False:
+        if hide_hint not in edited_content and no_hint not in edited_content:
+            return 'show_hint'
+    # an attempt without viewing the hint
+    if edited_content and hint_shown is False:
+        if hide_hint in edited_content or no_hint in edited_content:
+            # Find the position of the custom marker and extract user's input
+            marker_position = edited_content.find(custom_marker)
+            user_input = edited_content[marker_position + len(custom_marker):].strip()
+
+            return user_input
+
+    else:
+        click.echo("No changes were made.")
+        return ""
+
+
+if __name__ == "__main__":
+    # Example of using get_multiline_edit()
+    code_input = get_multiline_edit("Enter your code:")
+
+    if code_input:
+        click.echo("\nYou entered:")
+        for line in code_input:
+            click.echo(line)
---
+++ packages/quecital/quecital-1.3.0.tar.gz/quecital-1.3.0/src/quecital/recital.py
@@ -0,0 +1,57 @@
+import click
+from pathlib import Path
+import tomllib
+import random
+from quecital.multiline_text_editor import get_multiline_edit
+
+
+def main(data):
+    assets = preprocess(data)
+    attempt = get_attempt(assets)
+    display = f"\
+        \nThe prompt was:\
+        \n{assets['question']}\n\
+        \nThe attempt was:\
+        \n{attempt}\n\
+        \nThe correct answer is:\
+        \n{assets['answers']}\n"
+    if "explanation" in assets and assets["explanation"]:
+        display += f"\n\nExplanation: {assets['explanation']}"
+    click.echo(display)
+
+def compose_prompt(assets, show_hint=False):
+    hint = "Hint not provided"
+    if "hint" in assets and assets["hint"] and show_hint is False:
+        hint = "<DELETE FOR HINT>"
+    if "hint" in assets and assets["hint"] and show_hint is True:
+        hint = assets['hint']
+    prompt = f'The prompt is:\n\n{assets["question"]}\n\n{hint}'
+    return prompt
+
+
+def get_attempt(assets):
+    prompt = compose_prompt(assets)
+    attempt = get_multiline_edit(prompt)
+    if attempt == 'show_hint':
+        prompt = compose_prompt(assets, show_hint=True)
+        attempt = get_multiline_edit(prompt, hint_shown=True)
+
+    return attempt
+
+
+def preprocess(quecital_data):
+    topics = list(quecital_data.keys())
+    prompt = "\nFrom which topic will you perform a random recital\n\n"
+    for index, topic in enumerate(topics, start=1):
+        prompt += f"\n{index}: {topic}"
+    prompt += '\n\nEnter one of the following ->'
+    valid_choices = list(map(str, range(1, len(topics) + 1)))
+    selected_index = click.prompt(prompt, type=click.Choice(valid_choices))
+    topic = topics[int(selected_index) - 1]
+    return random.choice(quecital_data[topic]["recitals"])
+
+
+if __name__ == '__main__':
+    quecital_path = Path("quecital.toml")
+    data = tomllib.loads(quecital_path.read_text(encoding="utf-8"))
+    main(data)
