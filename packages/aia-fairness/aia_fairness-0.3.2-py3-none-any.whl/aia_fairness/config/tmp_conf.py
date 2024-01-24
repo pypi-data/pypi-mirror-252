ce015fc59943d20ae3ef2a04b340da38cbfe8a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:07:58 +0000

    Add package aia-fairness/0.3.2/aia_fairness-0.3.2-py3-none-any.whl

---
+++ packages/aia-fairness/aia_fairness-0.3.2-py3-none-any.whl/aia_fairness/config/tmp_conf.py
@@ -0,0 +1,25 @@
+#Set the seed for every experiement
+random_state = 1234
+
+#Numbre of folding used in cross validation 
+#Can be 1,2,3,4 or 5
+NN = 1
+
+#Do you want to use only 100 entries for each dataset ? 
+#(Usefull to test for new features)
+small = True
+no_cuda = False
+
+#Lunch in parallel or not
+para = False
+
+#select dataset 
+dsets = ["CENSUS"]
+
+#Select sensitive attribute (they are not used in training, just for mitigation)
+attribs = ["sex"]
+
+#Select target model
+from ..models import target as targets #Don't touch this line ! 
+#Choose which target model you want to use
+target_models = [targets.NeuralNetwork_AdversarialDebiasing]
