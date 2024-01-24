256ed21bdbff5ebcaac8fab41fcf5db7c348bfcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:26 +0000

    Add package topsis-vineet-102103465/0.1.0/topsis-vineet-102103465-0.1.0.tar.gz

---
+++ packages/topsis-vineet-102103465/topsis-vineet-102103465-0.1.0.tar.gz/topsis-vineet-102103465-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.1.0'
+DESCRIPTION = 'topsis package'
+
+
+# Setting up
+setup(
+    name="topsis-vineet-102103465",
+    version=VERSION,
+    author="vineet",
+    author_email="vineet.vej2003@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    install_requires=['logging', 'numpy', 'pandas'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
