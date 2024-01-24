83bd51a3c2ef72eee6a93fe686d465ab34495f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:23 +0000

    Add package holamundoplayer42/0.0.1/holamundoplayer42-0.0.1-py3-none-any.whl

---
+++ packages/holamundoplayer42/holamundoplayer42-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,21 @@
+"""
+modulo que incluye la clase reproductor de musica
+"""
+
+
+class Player:
+    """
+    esta clase crea un reproductor de musica
+    """
+
+    def play(self, song):
+        """
+        reproduce la cancion que recibio en el constructor
+
+        Parameters:
+        asdasdmaksfmsañf
+
+        Returns:
+        asdasdasdgdsfdg
+        """
+        print("reproduciendo cancion")
