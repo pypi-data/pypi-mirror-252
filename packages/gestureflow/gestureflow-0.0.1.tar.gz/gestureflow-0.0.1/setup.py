251033f1503031b97f16160155fa7ad1dc56e557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 15:04:59 +0000

    Add package gestureflow/0.0.1/gestureflow-0.0.1.tar.gz

---
+++ packages/gestureflow/gestureflow-0.0.1.tar.gz/gestureflow-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Streaming video data via networks'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="gestureflow",
+    version=VERSION,
+    author="Jay Shirgupe",
+    author_email="jns29072003@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'mediapipe'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'gesture detection', 'rapid gesture detection'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
