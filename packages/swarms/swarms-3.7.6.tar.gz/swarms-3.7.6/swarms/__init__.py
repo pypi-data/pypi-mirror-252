a060f722f91e04ace40617f970e1b4021617655a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 05:04:30 +0000

    Add package swarms/3.7.6/swarms-3.7.6.tar.gz

---
+++ packages/swarms/swarms-3.7.6.tar.gz/swarms-3.7.6/swarms/__init__.py
@@ -0,0 +1,10 @@
+from swarms.telemetry.bootup import bootup  # noqa: E402, F403
+
+bootup()
+
+from swarms.agents import *  # noqa: E402, F403
+from swarms.structs import *  # noqa: E402, F403
+from swarms.models import *  # noqa: E402, F403
+from swarms.telemetry import *  # noqa: E402, F403
+from swarms.utils import *  # noqa: E402, F403
+from swarms.prompts import *  # noqa: E402, F403
