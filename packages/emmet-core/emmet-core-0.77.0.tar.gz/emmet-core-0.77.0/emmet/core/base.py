a583166f72e7cf7f249077c79025ecf0fef0ca5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 06:04:44 +0000

    Add package emmet-core/0.77.0/emmet-core-0.77.0.tar.gz

---
+++ packages/emmet-core/emmet-core-0.77.0.tar.gz/emmet-core-0.77.0/emmet/core/base.py
@@ -0,0 +1,58 @@
+# mypy: ignore-errors
+
+"""Base emmet model to add default metadata."""
+
+from datetime import datetime
+from typing import Literal, Optional, TypeVar
+
+from monty.json import MontyDecoder
+from pydantic import BaseModel, Field, field_validator
+from pymatgen.core import __version__ as pmg_version
+
+from emmet.core import __version__
+
+T = TypeVar("T", bound="EmmetBaseModel")
+
+monty_decoder = MontyDecoder()
+
+
+class EmmetMeta(BaseModel):
+    """Default emmet metadata."""
+
+    emmet_version: Optional[str] = Field(
+        __version__, description="The version of emmet this document was built with."
+    )
+    pymatgen_version: Optional[str] = Field(
+        pmg_version, description="The version of pymatgen this document was built with."
+    )
+
+    pull_request: Optional[int] = Field(
+        None, description="The pull request number associated with this data build."
+    )
+
+    database_version: Optional[str] = Field(
+        None, description="The database version for the built data."
+    )
+
+    build_date: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        description="The build date for this document.",
+    )
+
+    license: Optional[Literal["BY-C", "BY-NC"]] = Field(
+        None, description="License for the data entry."
+    )
+
+    # Make sure that the datetime field is properly formatted
+    @field_validator("build_date", mode="before")
+    @classmethod
+    def build_date_dict_ok(cls, v):
+        return monty_decoder.process_decoded(v)
+
+
+class EmmetBaseModel(BaseModel):
+    """Base Model for default emmet data."""
+
+    builder_meta: Optional[EmmetMeta] = Field(
+        default_factory=EmmetMeta, description="Builder metadata."
+    )
