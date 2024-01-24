9752434323610fe5c94cfde2e3c43521f86c1ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:05:28 +0000

    Add package aiosocketproto/0.0.12/aiosocketproto-0.0.12-py3-none-any.whl

---
+++ packages/aiosocketproto/aiosocketproto-0.0.12-py3-none-any.whl/aiosocketproto/__init__.py
@@ -0,0 +1,8 @@
+from .client import AsyncSocketClient
+from .client.serializer import SerializerType
+from .server import AsyncSocketServer
+
+connect = AsyncSocketClient.connect
+start_server = AsyncSocketServer.start
+
+__version__ = '0.0.12'
