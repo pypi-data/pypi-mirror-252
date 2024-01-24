424016b957e6805e29bbce4e3f4f25358686c97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 04:08:30 +0000

    Add package tentacletk/0.9.17/tentacletk-0.9.17.tar.gz

---
+++ packages/tentacletk/tentacletk-0.9.17.tar.gz/tentacletk-0.9.17/tentacle/slots/__init__.py
@@ -0,0 +1,50 @@
+# !/usr/bin/python
+# coding=utf-8
+import inspect
+from PySide2 import QtCore
+import pythontk as ptk
+
+
+module = inspect.getmodule(inspect.currentframe())  # this module.
+path = ptk.get_object_path(module)  # this modules directory.
+
+
+class Slots(QtCore.QObject):
+    """Provides methods that can be triggered by widgets in the ui.
+    Parent to the 'Init' slot class, which is in turn, inherited by every other slot class.
+
+    If you need to create a invokable method that returns some value, declare it as a slot, e.g.:
+    @Slot(result=int, float)
+    ex. def getFloatReturnInt(self, f):
+                    return int(f)
+    """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        """
+        """
+        self.sb = self.switchboard()
+
+    def hide_main(fn):
+        """A decorator that hides the stacked widget main window."""
+        from functools import wraps
+
+        @wraps(fn)  # This ensures the wrapped function retains meta-info
+        def wrapper(self, *args, **kwargs):
+            # If the main window or any widget has grabbed the keyboard, release it.
+            keyboard_grabber = self.sb.parent().keyboardGrabber()
+            if keyboard_grabber:
+                keyboard_grabber.releaseKeyboard()
+
+            result = fn(self, *args, **kwargs)  # execute the method normally
+            self.sb.parent().hide()
+            return result
+
+        return wrapper
+
+
+# --------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
