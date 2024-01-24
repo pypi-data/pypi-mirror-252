ba00ceaf62daf406743c86f59f2600a4ba9eb4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:04:57 +0000

    Add package http-stream-xml/1.3.11/http-stream-xml-1.3.11.tar.gz

---
+++ packages/http-stream-xml/http-stream-xml-1.3.11.tar.gz/http-stream-xml-1.3.11/README.rst
@@ -0,0 +1,65 @@
+http-stream-xml
+===============
+
+|made_with_python| |build_status| |coverage| |pypi_version| |pypi_license| |readthedocs|
+
+Parse XML in HTTP response on the fly, by chunks.
+
+It could be `HTTP protocol chunks <https://en.wikipedia.org/wiki/Chunked_transfer_encoding>`_
+Or just partial download of big HTTP response.
+
+
+I use it to work with `NCBI (PubMed) Entrez API <https://www.ncbi.nlm.nih.gov/>`_.
+
+See usage example in `the blog article <https://sorokin.engineer/posts/en/xml_streaming_chunks_load.html>`_.
+
+Installation
+------------
+
+.. code-block:: bash
+
+    pip install http-stream-xml --upgrade
+
+Scripts
+-------
+
+.. code-block:: bash
+
+    make help
+
+Coverage report
+---------------
+* `Codecov <https://app.codecov.io/gh/andgineer/http-stream-xml/tree/master/src/http_stream_xml>`_
+* `Coveralls <https://coveralls.io/github/andgineer/http-stream-xml>`_
+
+Documentation
+-------------
+`Documentation <https://http-stream-xml.sorokin.engineer/>`_
+
+.. |build_status| image:: https://github.com/andgineer/http-stream-xml//workflows/ci/badge.svg
+    :target: https://github.com/andgineer/http-stream-xml/actions
+    :alt: Latest release
+
+.. |pypi_version| image:: https://img.shields.io/pypi/v/http-stream-xml.svg?style=flat-square
+    :target: https://pypi.org/p/http-stream-xml
+    :alt: Latest release
+
+.. |pypi_license| image:: https://img.shields.io/pypi/l/http-stream-xml.svg?style=flat-square
+    :target: https://pypi.python.org/pypi/http-stream-xml
+    :alt: MIT license
+
+.. |readthedocs| image:: https://readthedocs.org/projects/http-stream-xml/badge/?version=latest
+    :target: https://http-stream-xml.sorokin.engineer/
+    :alt: Documentation Status
+
+.. |made_with_python| image:: https://img.shields.io/badge/Made%20with-Python-1f425f.svg
+    :target: https://www.python.org/
+    :alt: Made with Python
+
+.. |codecov| image:: https://codecov.io/gh/andgineer/http-stream-xml/branch/master/graph/badge.svg
+    :target: https://app.codecov.io/gh/andgineer/http-stream-xml/tree/master/src%2Fhttp_stream_xml
+    :alt: Code coverage
+
+.. |coverage| image:: https://raw.githubusercontent.com/andgineer/http-stream-xml/python-coverage-comment-action-data/badge.svg
+    :target: https://htmlpreview.github.io/?https://github.com/andgineer/http-stream-xml/blob/python-coverage-comment-action-data/htmlcov/index.html
+    :alt: Coverage report
---
+++ packages/http-stream-xml/http-stream-xml-1.3.11.tar.gz/http-stream-xml-1.3.11/tests/test_entrez.py
@@ -0,0 +1,130 @@
+from unittest import mock
+from unittest.mock import Mock, patch
+
+import pytest
+
+import http_stream_xml.entrez
+from http_stream_xml.entrez import GeneFields, Genes
+
+
+@pytest.fixture
+def mock_genes():
+    return Genes()
+
+
+@pytest.fixture
+def mock_session():
+    """Patch requests.Session to return a mock object."""
+    with patch(
+        "http_stream_xml.entrez.requests.Session", return_value=Mock(autospec=True)
+    ) as mock:
+        http_stream_xml.entrez.requests_retry_session.cache_clear()
+        yield mock
+        http_stream_xml.entrez.requests_retry_session.cache_clear()
+
+
+def test_canonical_gene_name(mock_genes):
+    assert mock_genes.canonical_gene_name("Test") == "test"
+
+
+def test_gene_add_locus():
+    genes = Genes(fields=[
+        GeneFields.summary,
+        GeneFields.description,
+        GeneFields.synonyms,
+    ])
+    assert GeneFields.locus in genes.fields
+
+@mock.patch.object(Genes, "get_gene_details", return_value={})
+def test_getitem_from_cache(mock_get_gene_details, mock_genes):
+    mock_genes.db = {
+        "test": {
+            "Entrezgene_summary": "Test Gene",
+            "Gene-ref_desc": "Test description",
+            "Gene-ref_syn": "Test synonym",
+            "Gene-ref_locus": "Test locus",
+        }
+    }
+    result = mock_genes["test"]
+    assert result == {
+        "Entrezgene_summary": "Test Gene",
+        "Gene-ref_desc": "Test description",
+        "Gene-ref_syn": "Test synonym",
+        "Gene-ref_locus": "Test locus",
+    }
+    mock_get_gene_details.assert_not_called()  # Check that the method was not called
+
+
+@mock.patch.object(
+    Genes,
+    "get_gene_details",
+    return_value={
+        "Entrezgene_summary": "Test Gene",
+        "Gene-ref_desc": "Test description",
+        "Gene-ref_syn": "Test synonym",
+        "Gene-ref_locus": "Test locus",
+    },
+)
+def test_getitem_from_cache_incomplete_cache(mock_get_gene_details, mock_genes):
+    """Check that the site request repeated is there are not enough fields from prev request."""
+    mock_genes.db = {
+        "test": {
+            "Entrezgene_summary": "Test Gene",
+        }
+    }
+    result = mock_genes["test"]
+    assert result == {
+        "Entrezgene_summary": "Test Gene",
+        "Gene-ref_desc": "Test description",
+        "Gene-ref_syn": "Test synonym",
+        "Gene-ref_locus": "Test locus",
+    }
+    mock_get_gene_details.assert_called_once_with("test")
+
+
+def test_get_gene_id(mock_session, mock_genes):
+    mock_response = Mock()
+    mock_response.json.return_value = {"esearchresult": {"idlist": ["123456"]}}
+    mock_session.return_value.get.return_value = mock_response
+
+    gene_id = mock_genes.get_gene_id("test")
+    assert gene_id == "123456"
+
+
+def test_get_gene_details(mock_session, mock_genes):
+    mock_response = Mock()
+    mock_response.json.return_value = {"esearchresult": {"idlist": ["123456"]}}
+    mock_session.return_value.get.return_value = mock_response
+
+    with patch.object(mock_genes, "get_gene_details_by_id") as mock_details:
+        mock_details.return_value = {"Entrezgene_summary": "Test Gene"}
+        gene = mock_genes.get_gene_details("test")
+        assert gene == {"Entrezgene_summary": "Test Gene"}
+
+
+def test_get_gene_details_by_id(mock_session, mock_genes):
+    mock_response = Mock()
+    mock_response.iter_lines.return_value = [b"<Entrezgene_summary>Test Gene</Entrezgene_summary>"]
+    mock_session.return_value.get.return_value = mock_response
+
+    gene = mock_genes.get_gene_details_by_id("123456")
+    assert gene == {GeneFields.summary: "Test Gene"}
+
+
+def test_api_key_query_param(mock_genes):
+    mock_genes.api_key = "test_key"
+    result = mock_genes.api_key_query_param()
+    assert result == "&api_key=test_key"
+
+
+def test_search_id_url(mock_genes):
+    url = mock_genes.search_id_url("test")
+    assert (
+        url
+        == "/entrez/eutils/esearch.fcgi?db=gene&term=test[Gene+Name]+AND+homo+sapience[Organism]&retmode=json"
+    )
+
+
+def test_get_details_url(mock_genes):
+    url = mock_genes.get_details_url("123456")
+    assert url == "/entrez/eutils/efetch.fcgi?db=gene&id=123456&retmode=xml"
