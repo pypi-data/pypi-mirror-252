c8c74218434816df4c82aaa775998736065c8b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:05:51 +0000

    Add package trailblazer/20.1.3/trailblazer-20.1.3.tar.gz

---
+++ packages/trailblazer/trailblazer-20.1.3.tar.gz/trailblazer-20.1.3/trailblazer/store/crud/create.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+
+from trailblazer.constants import TrailblazerStatus
+from trailblazer.store.base import BaseHandler
+from trailblazer.store.database import get_session
+from trailblazer.store.models import Analysis, User
+
+
+class CreateHandler(BaseHandler):
+    """Class for creating items in the database."""
+
+    def add_pending_analysis(
+        self,
+        case_id: str,
+        config_path: str,
+        out_dir: str,
+        priority: str,
+        type: str,
+        email: str = None,
+        data_analysis: str = None,
+        ticket_id: str = None,
+        workflow_manager: str = None,
+    ) -> Analysis:
+        """Add pending analysis with user if supplied with email."""
+        session: Session = get_session()
+        new_analysis = Analysis(
+            config_path=config_path,
+            data_analysis=data_analysis,
+            case_id=case_id,
+            out_dir=out_dir,
+            priority=priority,
+            started_at=datetime.now(),
+            status=TrailblazerStatus.PENDING,
+            ticket_id=ticket_id,
+            type=type,
+            workflow_manager=workflow_manager,
+        )
+        new_analysis.user = self.get_user(email=email) if email else None
+        session.add(new_analysis)
+        session.commit()
+        return new_analysis
+
+    def add_user(self, name: str, email: str) -> User:
+        """Add a new user to the database."""
+        session: Session = get_session()
+        new_user = User(email=email, name=name)
+        session.add(new_user)
+        session.commit()
+        return new_user
