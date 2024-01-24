b92ab5627dc1445df112c73c5876dd4f0cab8fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:33 +0000

    Add package damo-embedding/1.1.11/damo_embedding-1.1.11-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/damo-embedding/damo_embedding-1.1.11-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/damo_embedding/config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+# `Damo-Embedding` - 'c++ tool for sparse parameter server'
+# Copyright (C) 2019 - present timepi <timepi123@gmail.com>
+# `Damo-Embedding` is provided under: GNU Affero General Public License
+# (AGPL3.0) https:#www.gnu.org/licenses/agpl-3.0.html unless stated otherwise.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+import os
+
+
+# damo instance
+DAMO_INSTANCE = None
+
+# damo embedding http address
+DAMO_SERVICE_ADDRESS = "http://localhost:9275"
+
+# damo server binaray file path
+DAMO_SERVICE_BINARY = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), "damo-server"
+)
