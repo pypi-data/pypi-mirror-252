90f02b88ca76838a5be024fd815f9b658e497985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 16:04:52 +0000

    Add package toot-tooi/0.6.0/toot_tooi-0.6.0-py3-none-any.whl

---
+++ packages/toot-tooi/toot_tooi-0.6.0-py3-none-any.whl/tooi/widgets/account.py
@@ -0,0 +1,33 @@
+from textual.widget import Widget
+from textual.widgets import Static
+
+from tooi.context import get_context
+from tooi.entities import Account
+
+
+class AccountHeader(Widget):
+    DEFAULT_CSS = """
+    AccountHeader {
+        height: auto;
+    }
+    .account_acct {
+        color: green;
+    }
+    .account_name {
+        color: yellow;
+    }
+    """
+
+    def __init__(self, account: Account, *, classes: str | None = None):
+        super().__init__(classes=classes)
+        self.account = account
+
+    def compose(self):
+        ctx = get_context()
+
+        acct = self.account.acct
+        acct = acct if "@" in acct else f"{acct}@{ctx.auth.domain}"
+
+        yield Static(f"@{acct}", markup=False, classes="account_acct")
+        if self.account.display_name:
+            yield Static(self.account.display_name, markup=False, classes="account_name")
---
+++ packages/toot-tooi/toot_tooi-0.6.0-py3-none-any.whl/tooi/widgets/image.py
@@ -0,0 +1,30 @@
+from rich import markup
+from textual import work
+from textual.widgets import Static
+from textual.worker import Worker, WorkerState
+
+from tooi.entities import MediaAttachment
+from tooi.utils.images import render_half_block_remote_image
+
+
+class HalfblockImage(Static):
+    def __init__(self, attachment: MediaAttachment):
+        self.attachment = attachment
+        # TODO: Display an image-sized placeholder instead
+        super().__init__("Loading...")
+        self.worker = self.load()
+
+    @work(exit_on_error=False, thread=True)
+    def load(self):
+        url = self.attachment.preview_url
+        # TODO: dynamic size based on viewport
+        return render_half_block_remote_image(url, 50, 40)
+
+    def on_worker_state_changed(self, event: Worker.StateChanged) -> None:
+        if event.state == WorkerState.SUCCESS:
+            self.update(event.worker.result)
+        if event.state == WorkerState.ERROR:
+            self.update(f"[red]Failed loading image:\n{markup.escape(str(event.worker.error))}[/]")
+
+    def on_unmount(self) -> None:
+        self.worker.cancel()
