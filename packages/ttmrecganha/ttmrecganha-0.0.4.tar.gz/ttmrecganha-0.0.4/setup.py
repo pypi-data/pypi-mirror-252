8a5c92361de4f0d8c462bc18710d086e1cc95f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 14:05:22 +0000

    Add package ttmrecganha/0.0.4/ttmrecganha-0.0.4.tar.gz

---
+++ packages/ttmrecganha/ttmrecganha-0.0.4.tar.gz/ttmrecganha-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Two Tower Model'
+LONG_DESCRIPTION = 'A package that contains a Two Tower Model'
+
+setup(
+    name="ttmrecganha",
+    version=VERSION,
+    author="acbessa",
+    author_email="<acbessa@nos.pt>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Programming Language :: Python",
+    ]
+)
