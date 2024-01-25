88fed2f45b4d2e4133ee2e128eeccab5f98bce52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:58 +0000

    Add package scippneutron/24.1.0/scippneutron-24.1.0-py3-none-any.whl

---
+++ packages/scippneutron/scippneutron-24.1.0-py3-none-any.whl/scippneutron/tof/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+
+# flake8: noqa
+r"""
+Specifics for time-of-flight neutron-scattering data reduction, including coordinate transformations.
+"""
+
+from . import chopper_cascade
+from .diagram import TimeDistanceDiagram
+from .frames import unwrap_frames
