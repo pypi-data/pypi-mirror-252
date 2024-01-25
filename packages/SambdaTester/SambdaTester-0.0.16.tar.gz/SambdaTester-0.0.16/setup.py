aaec3bfd82371f06a6659ee971b3f6fd05cdb932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:25 +0000

    Add package SambdaTester/0.0.16/SambdaTester-0.0.16.tar.gz

---
+++ packages/SambdaTester/SambdaTester-0.0.16.tar.gz/SambdaTester-0.0.16/setup.py
@@ -0,0 +1,28 @@
+# from setuptools import setup
+from distutils.core import setup
+
+setup(
+    packages=[
+        'SambdaTester',
+        'SambdaTester.modulator',
+        'SambdaTester.yams',
+        'SambdaTester.yams.models',
+        'SambdaTester.config',
+        'SambdaTester.Server'
+        ],
+    entry_points = {
+        'console_scripts': [
+            'SambdaTester=SambdaTester:main',
+            'bin/SambdaTester=SambdaTester.command:main'
+        ]
+    },
+    require=[
+        "pyyaml",
+        "pydantic",
+        "python-dotenv"
+        ],
+    version="0.0.16",
+    author_email = "tarnold0788@gmail.com"
+)
+
+# setup()
