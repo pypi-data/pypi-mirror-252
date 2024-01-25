dd16aa9dddd5f77200e79b6c3f39bb672c295790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:05:04 +0000

    Add package scikit-digital-health/0.13.2/scikit_digital_health-0.13.2-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/scikit-digital-health/scikit_digital_health-0.13.2-cp310-cp310-macosx_10_9_x86_64.whl/skdh/preprocessing/__init__.py
@@ -0,0 +1,44 @@
+"""
+Inertial Data Preprocessing (:mod:`skdh.preprocessing`)
+=======================================================
+
+.. currentmodule:: skdh.preprocessing
+
+Sensor Calibration
+------------------
+
+.. autosummary::
+    :toctree: generated/
+
+    CalibrateAccelerometer
+
+Wear Detection
+--------------
+
+.. autosummary::
+    :toctree: generated/
+
+    DETACH
+    CountWearDetection
+    CtaWearDetection
+    AccelThresholdWearDetection
+"""
+from skdh.preprocessing.calibrate import CalibrateAccelerometer
+from skdh.preprocessing import calibrate
+from skdh.preprocessing.wear_detection import (
+    AccelThresholdWearDetection,
+    CtaWearDetection,
+    DETACH,
+    CountWearDetection,
+)
+from skdh.preprocessing import wear_detection
+
+__all__ = (
+    "CalibrateAccelerometer",
+    "calibrate",
+    "AccelThresholdWearDetection",
+    "CtaWearDetection",
+    "DETACH",
+    "CountWearDetection",
+    "wear_detection",
+)
