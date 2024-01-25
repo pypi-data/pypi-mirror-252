061c8c426a86f1331db1baa7996615728a39ef02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:05:06 +0000

    Add package pydark/0.0.4/pydark-0.0.4.tar.gz

---
+++ packages/pydark/pydark-0.0.4.tar.gz/pydark-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os
+
+# Carregar o conteúdo do arquivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pydark',
+    version='0.0.4',
+    description='DarkPy is a Python library which brings some useful calculations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['pydark', 'pydark.date', 'pydark.math', 'pydark.text', 'pydark.chart'],
+    install_requires=[],
+)
