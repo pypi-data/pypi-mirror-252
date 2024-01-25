239a035bceba9fc405e30bc2c2ec581181a5c277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:05:01 +0000

    Add package pd-db-wrangler/0.28.0/pd_db_wrangler-0.28.0-py2.py3-none-any.whl

---
+++ packages/pd-db-wrangler/pd_db_wrangler-0.28.0-py2.py3-none-any.whl/pd_db_wrangler/tester.py
@@ -0,0 +1,27 @@
+from .pd_db_wrangler import Pandas_DB_Wrangler
+
+
+"""
+pdw = Pandas_DB_Wrangler(
+    "postgresql+psycopg2://taiga:Squeezing-Subprime9-Partake@taiga.murraycountymed.org:5432/taiga"
+)
+sql = pdw.read_sql_file("build/history.sql")
+df = pdw.df_fetch(sql)
+# df = pdw.df_fetch("select * from accounts", index_col="guid")
+print(df.index.name)
+print(df.index.dtype)
+print(df.dtypes)
+print(df)
+"""
+
+
+pdw = Pandas_DB_Wrangler("/home/justin/.config/joplin-desktop/database.sqlite")
+# sql = pdw.read_sql_file("build/transactions_master.sql")
+
+sql = pdw.read_sql_file("build/joplin.sql")
+# notes = pdw.df_fetch(pdw.sql)
+notes = pdw.df_fetch(sql + " WHERE title like 'Push-ups%'")
+sql2 = pdw.read_sql_file("build/joplin2.sql")
+notes2 = pdw.df_fetch(sql2)
+print(pdw.options)
+print(notes2.dtypes)
