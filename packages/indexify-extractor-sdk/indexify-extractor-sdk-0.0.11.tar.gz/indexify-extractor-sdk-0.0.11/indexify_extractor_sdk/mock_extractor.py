339e8b9f6419a616603e244cce8b54830f970fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:05:03 +0000

    Add package indexify-extractor-sdk/0.0.11/indexify-extractor-sdk-0.0.11.tar.gz

---
+++ packages/indexify-extractor-sdk/indexify-extractor-sdk-0.0.11.tar.gz/indexify-extractor-sdk-0.0.11/indexify_extractor_sdk/mock_extractor.py
@@ -0,0 +1,54 @@
+from indexify_extractor_sdk.base_extractor import Content
+from .base_extractor import Extractor, Content, Feature
+
+from typing import List
+
+from pydantic import BaseModel
+
+import json
+
+class InputParams(BaseModel):
+    a: int = 0
+    b: str = ""
+
+class MockExtractor(Extractor):
+    input_mime_types = ["text/plain", "application/pdf", "image/jpeg"]
+    def __init__(self):
+        super().__init__()
+
+    def extract(
+        self, content: Content, params: InputParams
+    ) -> List[Content]:
+        return [
+                Content.from_text(
+                    text="Hello World", feature=Feature.embedding(values=[1, 2, 3])
+                ),
+                Content.from_text(
+                    text="Pipe Baz", feature=Feature.embedding(values=[1, 2, 3])
+                ),
+                Content.from_text(
+                    text="Hello World",
+                    feature=Feature.metadata(json.loads('{"a": 1, "b": "foo"}')),
+                    labels={"label1": "val1", "label2": "val2"}
+                ),
+            ]
+    
+    def sample_input(self) -> Content:
+        return Content.from_text("hello world")
+    
+class MockExtractorNoInputParams(Extractor):
+    def __init__(self):
+        super().__init__()
+
+    def extract(self, content: Content, params=None) -> List[Content]:
+        return [
+                Content.from_text(
+                    text="Hello World", feature=Feature.embedding(values=[1, 2, 3])
+                ),
+                Content.from_text(
+                    text="Pipe Baz", feature=Feature.embedding(values=[1, 2, 3])
+                ),
+            ]
+    
+    def sample_input(self) -> Content:
+        return Content.from_text("hello world")
