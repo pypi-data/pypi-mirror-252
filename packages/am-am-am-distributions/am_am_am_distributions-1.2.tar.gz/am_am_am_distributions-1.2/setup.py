990ce85f158dd881c405f9f955035daf06b6f229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:05:16 +0000

    Add package am-am-am-distributions/1.2/am_am_am_distributions-1.2.tar.gz

---
+++ packages/am-am-am-distributions/am_am_am_distributions-1.2.tar.gz/am_am_am_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='am_am_am_distributions',
+      version='1.02',
+      description='Gaussian distributions',
+      packages=['am_am_am_distributions'],
+      auther = "Abdurrahman",
+      auther_emai = "abdurrahmanmorsi@gmail.com",
+      zip_safe=False)
