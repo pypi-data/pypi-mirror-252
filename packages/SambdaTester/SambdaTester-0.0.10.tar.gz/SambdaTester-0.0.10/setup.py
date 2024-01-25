55c4b00f0c17a691bc5bf1a21221705a558db6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:50 +0000

    Add package SambdaTester/0.0.10/SambdaTester-0.0.10.tar.gz

---
+++ packages/SambdaTester/SambdaTester-0.0.10.tar.gz/SambdaTester-0.0.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# setup(
+#     packages=find_packages(),
+#     entry_points = {
+#         'console_scripts': [
+#             'SambdaTester=SambdaTester:main',
+#             'bin/SambdaTester=SambdaTester.command:main'
+#         ]
+#     },
+#     version="0.0.8",
+#     author_email = "tarnold0788@gmail.com"
+# )
+
+setup()
