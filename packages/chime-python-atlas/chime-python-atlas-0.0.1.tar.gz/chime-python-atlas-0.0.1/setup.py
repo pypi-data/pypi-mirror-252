77d882fefa625c6ac74e711060eb9194dbaf4903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:04:53 +0000

    Add package chime-python-atlas/0.0.1/chime-python-atlas-0.0.1.tar.gz

---
+++ packages/chime-python-atlas/chime-python-atlas-0.0.1.tar.gz/chime-python-atlas-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-python-atlas',
+    packages=find_packages(include=['chime-python-atlas']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
