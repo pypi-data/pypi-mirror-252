03aa7d70ca298a32529e1be80dea14304dd38247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:06:08 +0000

    Add package ussl/1.0.8/ussl-1.0.8.tar.gz

---
+++ packages/ussl/ussl-1.0.8.tar.gz/ussl-1.0.8/ussl/protocol/__init__.py
@@ -0,0 +1,11 @@
+from .winrm import WinRMProtocol
+from .ssh import SSHProtocol
+
+
+INTERFACES = {
+    'ssh': SSHProtocol,
+    # 'winexe': WinexeProtocol,
+    # 'wmi': WMIProtocol,
+    'winrm': WinRMProtocol,
+    # 'ldap': LDAPProtocol,
+}
