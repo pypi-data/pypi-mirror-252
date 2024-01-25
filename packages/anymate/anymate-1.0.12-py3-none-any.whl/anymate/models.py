187ce86613fb86dd0a4e6aa0f472c9587c739a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:04:53 +0000

    Add package anymate/1.0.12/anymate-1.0.12-py3-none-any.whl

---
+++ packages/anymate/anymate-1.0.12-py3-none-any.whl/anymate/models.py
@@ -0,0 +1,74 @@
+from typing import Literal
+from pydantic import BaseModel, Any
+
+
+class AuthResponse:
+    def __init__(self, access_token, expires_in, token_type, scope):
+        self.access_token = access_token
+        self.expires_in = expires_in
+        self.token_type = token_type
+        self.scope = scope
+
+class AnymateResponse(BaseModel):
+    succeeded: bool
+    message: str
+
+class AnymateCreateTaskResponse(BaseModel):
+    succeeded: bool
+    message: str
+    taskId: int
+
+class AnymateCreateTasksResponse(BaseModel):
+    succeeded: bool
+    message: str
+    taskIds: list
+
+
+class AnymateProcessFailure(BaseModel):
+    processKey: str
+    message: str
+
+
+class AnymateFinishRun(BaseModel):
+    runId: int
+    overwriteSecondsSaved: int = None
+    overwriteEntries: int = None
+
+
+class AnymateOkToRun(BaseModel):
+    gateOpen: bool
+    tasksAvailable: bool
+    notBlockedDate: bool
+    okToRun: bool
+
+
+class AnymateRunResponse(BaseModel):
+    processKey: str
+    runId: int
+
+
+class AnymateTaskAction(BaseModel):
+    taskId: int
+    reason: str
+    comment: str = ''
+    overwriteSecondsSaved: int = None
+    overwriteEntries: int = None
+
+
+class AnymateRetryTaskAction(BaseModel):
+    taskId: int
+    reason: str
+    comment: str = ''
+    activationDate: str = None
+    overwriteSecondsSaved: int = None
+    overwriteEntries: int = None
+
+class AnymateReturnTaskAction(BaseModel):
+    taskId: int
+    action: Literal['Solved', 'Manual', 'Error', 'Retry']
+    reason: str
+    comment: str = ''
+    activationDate: str = None
+    overwriteSecondsSaved: int = None
+    overwriteEntries: int = None
+
