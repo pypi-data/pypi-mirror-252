a9cc3639b6aefa2c9f48bcc66010dd576cabe9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:05:21 +0000

    Add package mslm-test-haris-m/1.0.0/mslm-test-haris-m-1.0.0.tar.gz

---
+++ packages/mslm-test-haris-m/mslm-test-haris-m-1.0.0.tar.gz/mslm-test-haris-m-1.0.0/mslm_lib/__init__.py
@@ -0,0 +1,13 @@
+# import sys
+# import types
+#
+# from .mslm_lib import Lib
+# from .req_opts import ReqOpts
+# from .mslm_errors import MslmError, RequestQuotaExceededError
+#
+# # def reject_numpy(fullname, path, target=None):
+# #     print(fullname)
+# #
+# #
+# # # Adapt it to the loader protocol and put it before the defaults.
+# # sys.meta_path.insert(0, types.SimpleNamespace(find_spec=reject_numpy))
