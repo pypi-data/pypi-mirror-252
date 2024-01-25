9f115a59bbba0ebb37fb044a13b02dd41b08c8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:05:23 +0000

    Add package draws/1.0.0a3/draws-1.0.0a3.tar.gz

---
+++ packages/draws/draws-1.0.0a3.tar.gz/draws-1.0.0a3/src/draws/colors.py
@@ -0,0 +1,14 @@
+"""Maps AWS services to colors."""
+
+# TODO: generate map for all services
+color_mapping = {
+    "AWS-Lambda": "#ED7100",
+    "AWS-ApiGateway": "#E7157B",
+    "AWS-ApiGatewayV2": "#E7157B",
+    "AWS-Cognito": "#DD344C",
+    "AWS-AppSync": "#E7157B",
+    "AWS-S3": "#7AA116",
+    "Custom-S3BucketNotifications": "#7AA116",
+    "AWS-DynamoDB": "#C925D1",
+    "AWS-IAM": "#DD344C",
+}
