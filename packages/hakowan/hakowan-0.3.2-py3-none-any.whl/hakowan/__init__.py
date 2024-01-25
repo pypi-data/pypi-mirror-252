b1e1dbd1e213891ea4cc1759cdd471e01170a076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 15:05:13 +0000

    Add package hakowan/0.3.2/hakowan-0.3.2-py3-none-any.whl

---
+++ packages/hakowan/hakowan-0.3.2-py3-none-any.whl/hakowan/__init__.py
@@ -0,0 +1,12 @@
+""" Hakowan: A 3D data visualization grammer """
+
+__version__ = "0.3.2"
+
+from .common import logger
+from .setup import Config as config
+from .grammar import dataframe, mark, channel, scale, texture, transform
+from .grammar.layer import Layer as layer
+from .grammar.scale import Attribute as attribute
+from .grammar.channel import material
+from .compiler import compile
+from .render import render
