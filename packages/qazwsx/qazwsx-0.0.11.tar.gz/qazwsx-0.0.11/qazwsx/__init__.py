07a2af4a238862345229c10d85100e274732e8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:21 +0000

    Add package qazwsx/0.0.11/qazwsx-0.0.11.tar.gz

---
+++ packages/qazwsx/qazwsx-0.0.11.tar.gz/qazwsx-0.0.11/qazwsx/__init__.py
@@ -0,0 +1,11 @@
+from .mover import (
+    Mover
+)
+from qazwsx import alibabaScraper
+
+# __version__ = "0.0.8"
+
+# __all__ = [
+#     "__version__",
+#     "Mover",
+# ]
---
+++ packages/qazwsx/qazwsx-0.0.11.tar.gz/qazwsx-0.0.11/qazwsx/mover.py
@@ -0,0 +1,23 @@
+from qazwsx import alibabaScraper
+
+
+class Mover:
+    def __init__(self, colorList = [], sizeList = []):
+        self.colorList = colorList
+        self.sizeList = sizeList
+        
+    def toAmazon(self, columns, df_preAmz):
+        return 0
+        # amazonListing.po(self)
+        
+    @staticmethod
+    def from1688(url):
+        # 从1688平台创建Mover对象的代码逻辑
+        # ...
+        # sku采集
+        colorList, sizeList = alibabaScraper.getSKUInfo(url)
+
+        # 创建Mover对象并返回
+        mover = Mover(colorList, sizeList)
+
+        return mover
