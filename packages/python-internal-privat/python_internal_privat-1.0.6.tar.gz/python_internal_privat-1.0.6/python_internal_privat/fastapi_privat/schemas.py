6119043529bd52653d0a5538021577c63bb203de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:05:07 +0000

    Add package python-internal-privat/1.0.6/python_internal_privat-1.0.6.tar.gz

---
+++ packages/python-internal-privat/python_internal_privat-1.0.6.tar.gz/python_internal_privat-1.0.6/python_internal_privat/fastapi_privat/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+
+
+class PrivatSchema(BaseModel):
+    privat_token: str = Field(..., max_length=292)
+    privat_iban: str = Field(..., max_length=29)
+    user_id: str
+
+    class Config:
+        from_attributes = True
+
+
+class PrivatSchemaUpdate(BaseModel):
+    privat_token: str = Field(..., max_length=292)
+    privat_iban: str = Field(..., max_length=29)
+
+    class Config:
+        from_attributes = True
+
+
+class PrivatSchemaStatement(BaseModel):
+    period: int
+    limit: int
+    user_id: str
+
+    class Config:
+        from_attributes = True
+
+
+class PrivatSchemaPayment(BaseModel):
+    amount: float = Field(..., ge=0.01)
+    recipient: str = Field(..., max_length=16)
+    user_id: str
+
+    class Config:
+        from_attributes = True
