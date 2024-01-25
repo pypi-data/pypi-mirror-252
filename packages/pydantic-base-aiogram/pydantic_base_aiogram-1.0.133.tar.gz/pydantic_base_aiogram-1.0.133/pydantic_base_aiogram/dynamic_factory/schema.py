87b8e0c0be3954e10a5f70d9fbc01182cc5416d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:06:20 +0000

    Add package pydantic-base-aiogram/1.0.133/pydantic_base_aiogram-1.0.133.tar.gz

---
+++ packages/pydantic-base-aiogram/pydantic_base_aiogram-1.0.133.tar.gz/pydantic_base_aiogram-1.0.133/pydantic_base_aiogram/dynamic_factory/schema.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from .enums import InputFormTypesEnum 
+
+
+class InputSchema(BaseModel):
+    field_name: str = Field(view_text='Введите название поля')
+    view_text: str = Field(view_text='Введите отображаемый текст')
+    type: InputFormTypesEnum = Field(view_text='Выберите тип полья')
+    is_list: bool = Field(view_text='Является ли данный тип списком')
+    extra_keys: Optional[str] = Field(view_text='Введите дополнительные кнопки разделенные новой строкой')
+
+
+class SchemaBuilderSchema(BaseModel):
+    name: str = Field(view_text='Введите название формы')
+    fields: list[InputSchema] 
+
