c71807c973e32b8e8c34e9d450b08865fd1c8ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:04:53 +0000

    Add package chime-ml-feature-library-internal/0.0.1/chime-ml-feature-library-internal-0.0.1.tar.gz

---
+++ packages/chime-ml-feature-library-internal/chime-ml-feature-library-internal-0.0.1.tar.gz/chime-ml-feature-library-internal-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-ml-feature-library-internal',
+    packages=find_packages(include=['chime-ml-feature-library-internal']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
