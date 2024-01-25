779e012792a95af02f9ac5f4a0e1c874c0291f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:50 +0000

    Add package Topsis-Aaditya-102117021/0.0.0.1/Topsis-Aaditya-102117021-0.0.0.1.tar.gz

---
+++ packages/Topsis-Aaditya-102117021/Topsis-Aaditya-102117021-0.0.0.1.tar.gz/Topsis-Aaditya-102117021-0.0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0.1'
+DESCRIPTION = 'Implementing TOPSIS'
+LONG_DESCRIPTION = 'Technique for Order of Preference by Similarity to Ideal Solution is a multi-criteria decision analysis method'
+
+# Setting up
+setup(
+    name="Topsis-Aaditya-102117021",
+    version=VERSION,
+    author="intrinsicvardhan (Aaditya Vardhan)",
+    author_email="intrinsicvardhan@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description = LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+    keywords=['topsis', 'decision-analysis', 'similarity'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
