29c37b3d29c8371d68598cc9897dd7fae559b7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:49 +0000

    Add package procrastinate/2.0.0b1/procrastinate-2.0.0b1-py3-none-any.whl

---
+++ packages/procrastinate/procrastinate-2.0.0b1-py3-none-any.whl/procrastinate/contrib/django/utils.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Any
+
+from django.db import connections
+
+
+def connector_params(alias: str = "default") -> dict[str, Any]:
+    """
+    Returns parameters for in a format that is suitable to be passed to a
+    connector constructor (see `howto/django`).
+
+    Parameters
+    ----------
+    alias :
+        Alias of the database, to read in the keys of settings.DATABASES,
+        by default ``default``.
+
+    Returns
+    -------
+    ``Dict[str, Any]``
+        Provide these keyword arguments when instantiating your connector
+    """
+    wrapper = connections[alias]
+    params = wrapper.get_connection_params()
+    params.pop("cursor_factory", None)
+    params.pop("context", None)
+    return params
