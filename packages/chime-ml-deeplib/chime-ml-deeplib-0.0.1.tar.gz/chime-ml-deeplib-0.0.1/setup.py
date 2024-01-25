556d914994c30ad0ed68e8025166f2ce7b8acd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:04:53 +0000

    Add package chime-ml-deeplib/0.0.1/chime-ml-deeplib-0.0.1.tar.gz

---
+++ packages/chime-ml-deeplib/chime-ml-deeplib-0.0.1.tar.gz/chime-ml-deeplib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-ml-deeplib',
+    packages=find_packages(include=['chime-ml-deeplib']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
