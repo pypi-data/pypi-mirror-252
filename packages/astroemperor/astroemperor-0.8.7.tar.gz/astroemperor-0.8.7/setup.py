1cecbf676ec8f5d41d3641d250510d15e4643b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:44 +0000

    Add package astroemperor/0.8.7/astroemperor-0.8.7.tar.gz

---
+++ packages/astroemperor/astroemperor-0.8.7.tar.gz/astroemperor-0.8.7/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="astroemperor",
+    version="0.8.7",
+    author="ReddTea",
+    author_email="redd@tea.com",
+    description="PTMCMC sampler for exoplanet search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    #data_files=[(path1, [path1+'/exo_list.csv', path1+'/ss_list.csv'])],
+    include_package_data=True,
+    install_requires=['numpy', 'matplotlib>=3.5.1', 'PyAstronomy', 'gatspy',
+                      'ipywidgets', 'tabulate', 'termcolor', 'reddutils', 'kepler.py',
+                      'emcee', 'arviz', 'corner', 'reddcolors',
+                      'reddemcee'],
+    python_requires=">=3.6",
+)
