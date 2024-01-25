cd53ed54e8130ec7f3c40ecb7300a84e06be2fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:57 +0000

    Add package ah-probability/0.1/ah_probability-0.1.tar.gz

---
+++ packages/ah-probability/ah_probability-0.1.tar.gz/ah_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ah_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ah_probability'],
+      author = 'Aliaksandr Hryshchuk',
+      author_email = 'alex_grishchuk@tut.by',
+      zip_safe=False)
