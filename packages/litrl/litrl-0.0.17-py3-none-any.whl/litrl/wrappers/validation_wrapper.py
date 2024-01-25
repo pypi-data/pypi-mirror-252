ca5b57a5e2e62d62a71e37c482f32837ece7284b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:05 +0000

    Add package litrl/0.0.17/litrl-0.0.17-py3-none-any.whl

---
+++ packages/litrl/litrl-0.0.17-py3-none-any.whl/litrl/wrappers/validation_wrapper.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import TYPE_CHECKING, Generic
+
+from dateutil import tz
+from gymnasium.core import Wrapper, WrapperActType, WrapperObsType
+from gymnasium.wrappers.record_video import RecordVideo
+
+from litrl.env.typing import ActType, ObsType
+from litrl.wrappers.static_opponent_wrapper import AgentType, StaticOpponentWrapper
+
+if TYPE_CHECKING:
+    import gymnasium as gym
+
+
+class ValidationWrapper(
+    Wrapper[WrapperObsType, WrapperActType, ObsType, ActType],
+    Generic[AgentType, WrapperObsType, WrapperActType, ObsType, ActType],
+):
+    def __init__(
+        self,
+        env: gym.Env[ObsType, ActType] | StaticOpponentWrapper[ObsType, ActType, AgentType],
+        render_each_n_episodes: int,
+        video_folder: str | None = None,
+    ) -> None:
+        self.render_each_n_episodes = render_each_n_episodes
+        self.video_folder = (
+            video_folder if video_folder is not None else f"temp/videos/{env.unwrapped}/{datetime.now(tz.UTC)}"
+        )
+
+        def episode_trigger(episode: int) -> bool:
+            return episode % self.render_each_n_episodes == 0
+
+        super().__init__(
+            RecordVideo(
+                env=env,
+                video_folder=self.video_folder,
+                episode_trigger=episode_trigger,
+                disable_logger=True,
+            ),
+        )
