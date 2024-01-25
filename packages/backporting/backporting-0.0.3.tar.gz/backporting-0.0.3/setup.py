0d73b3320e9dfe3172a5a27a839c575365463e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:05:13 +0000

    Add package backporting/0.0.3/backporting-0.0.3.tar.gz

---
+++ packages/backporting/backporting-0.0.3.tar.gz/backporting-0.0.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="backporting",
+    version="0.0.3",
+    description="A Python client for backporting.",
+    license="MIT",
+    author="LY",
+    author_email="",
+    url="https://github.com/levon2111/backporting.git",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+    ],
+    keywords="Backporting",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3.6",
+    ],
+    zip_safe=True,
+)
