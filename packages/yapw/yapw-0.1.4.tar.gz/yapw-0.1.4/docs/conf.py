62f1868d30ca3b13550eb959d7fe3b31a53a92f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:51 +0000

    Add package yapw/0.1.4/yapw-0.1.4.tar.gz

---
+++ packages/yapw/yapw-0.1.4.tar.gz/yapw-0.1.4/docs/conf.py
@@ -0,0 +1,82 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import sys
+from pathlib import Path
+
+sys.path.insert(0, Path("..").resolve())
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Yet Another Pika Wrapper"
+copyright = "2021, Open Contracting Partnership"
+author = "Open Contracting Partnership"
+
+# The short X.Y version
+version = "0.1.4"
+# The full version, including alpha/beta/rc tags
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_design",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
+
+
+# -- Extension configuration -------------------------------------------------
+
+autodoc_default_options = {
+    "members": None,
+    "member-order": "bysource",
+}
+autodoc_typehints = "description"
+autodoc_type_aliases = {
+    "ConsumerCallback": "yapw.types.ConsumerCallback",
+    "Decode": "yapw.types.Decode",
+    "Decorator": "yapw.types.Decorator",
+    "Encode": "yapw.types.Encode",
+}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "pika": ("https://pika.readthedocs.io/en/stable/", None),
+}
