05a988ad94407d944c45db2da00c9a600b2d09d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:35 +0000

    Add package yutto/2.0.0b34/yutto-2.0.0b34-py3-none-any.whl

---
+++ packages/yutto/yutto-2.0.0b34-py3-none-any.whl/yutto/utils/funcutils/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from .aobject import aobject
+from .as_sync import as_sync
+from .filter_none_value import filter_none_value
+from .singleton import Singleton
+from .xmerge import xmerge
+
+__all__ = [
+    "aobject",
+    "Singleton",
+    "as_sync",
+    "filter_none_value",
+    "xmerge",
+]
