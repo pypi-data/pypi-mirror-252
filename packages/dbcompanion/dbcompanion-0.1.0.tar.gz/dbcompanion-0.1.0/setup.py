cf3953185ec169ee1bf0a8c20e75d69258da2520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:05:07 +0000

    Add package dbcompanion/0.1.0/dbcompanion-0.1.0.tar.gz

---
+++ packages/dbcompanion/dbcompanion-0.1.0.tar.gz/dbcompanion-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbcompanion',
+    version='0.1.0',
+    author='Ransaka Ravihara',
+    author_email='ransakaravihara@gmail.com',
+    description='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
