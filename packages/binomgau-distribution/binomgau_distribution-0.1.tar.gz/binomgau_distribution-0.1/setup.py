3aa0f5b75bc3400e633ad747c3dc0a447bc35ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:05:17 +0000

    Add package binomgau-distribution/0.1/binomgau_distribution-0.1.tar.gz

---
+++ packages/binomgau-distribution/binomgau_distribution-0.1.tar.gz/binomgau_distribution-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binomgau_distribution',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['binomgau_distribution'],
+      author = 'Hillary Okello',
+      zip_safe=False)
