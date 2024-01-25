bec06901f79268f5027e780418b548e8cba82df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:05:14 +0000

    Add package MyCreds/0.1.2/MyCreds-0.1.2.tar.gz

---
+++ packages/MyCreds/MyCreds-0.1.2.tar.gz/MyCreds-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyCreds',
+    version='0.1.2',
+    long_description="The MyCreds project is a python program developed for WatSPEED internal use. Given a course and section number, the program scrapes the student portal (DestinyOne) for a list of students who passed and their data. The program also produces files needed to be uploaded to MyCreds: MyCredsCertificate.csv, and PDF certificates for each passing student.",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mycreds = MyCreds:__main__',
+        ],
+    },
+    install_requires=[
+        'selenium',
+        'ReportLab',
+    ],
+)
