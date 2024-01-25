adca0d93b7184ef26194e2146be1567da22079da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:05:11 +0000

    Add package rococo/0.1.49/rococo-0.1.49-py3-none-any.whl

---
+++ packages/rococo/rococo-0.1.49-py3-none-any.whl/rococo/models/otp_method.py
@@ -0,0 +1,22 @@
+"""
+OtpMethod model
+"""
+
+from dataclasses import dataclass, field
+from typing import List
+
+from . import VersionedModel
+
+
+@dataclass
+class OtpMethod(VersionedModel):
+    """An OTP method model."""
+
+    person: str = field(default=None, metadata={
+        'relationship': {'model': 'Person', 'type': 'direct'},
+        'field_type': 'record_id'
+    })
+    secret: str = None
+    name: str = None
+    enabled: bool = False
+    recovery_codes: List[str] = None
