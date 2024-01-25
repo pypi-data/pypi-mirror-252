9f623ec01af23bac676bdf422474f870818534f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 06:04:53 +0000

    Add package chime-ml-mrs-client/0.0.1/chime-ml-mrs-client-0.0.1.tar.gz

---
+++ packages/chime-ml-mrs-client/chime-ml-mrs-client-0.0.1.tar.gz/chime-ml-mrs-client-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-ml-mrs-client',
+    packages=find_packages(include=['chime-ml-mrs-client']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
