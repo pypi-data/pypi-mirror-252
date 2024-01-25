ae98351c302c1affb1fead1e0763f93b87e799ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:42 +0000

    Add package AssayingAnomalies/1.2/AssayingAnomalies-1.2.tar.gz

---
+++ packages/AssayingAnomalies/AssayingAnomalies-1.2.tar.gz/AssayingAnomalies-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AssayingAnomalies',
+    version='1.2',
+    author='Joshua Lawson',
+    author_email='jlaws13@simon.rochester.edu',
+    description='A brief description of your package',
+    packages=find_packages(),
+    install_requires=[],
+    long_description='Package installed successfully. Please run \'setup_library\' to configure your settings and begin ' \
+                                                                               'using the toolkit'
+
+)
