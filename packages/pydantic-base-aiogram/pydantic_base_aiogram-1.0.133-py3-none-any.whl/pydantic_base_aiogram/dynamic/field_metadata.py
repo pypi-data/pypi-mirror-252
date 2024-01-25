99593218c33a1bd6cb181be3302235879a28b3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:06:20 +0000

    Add package pydantic-base-aiogram/1.0.133/pydantic_base_aiogram-1.0.133-py3-none-any.whl

---
+++ packages/pydantic-base-aiogram/pydantic_base_aiogram-1.0.133-py3-none-any.whl/pydantic_base_aiogram/dynamic/field_metadata.py
@@ -0,0 +1,20 @@
+from uuid import uuid4
+from typing import Optional
+
+
+class FieldMetadata:
+    def __init__(
+        self, 
+        type: str,
+        view_text: Optional[str] = None,
+        field_name: Optional[str] = None,
+        getter_name: Optional[str] = None,
+        extra_keys: Optional[list[str]] = None,
+    ) -> None:
+        self._type = type
+        self._view_text = view_text
+        self._field_name = field_name or str(uuid4())
+        self._getter_name = getter_name
+        self._extra_keys = dict(zip(extra_keys, extra_keys)) if extra_keys else None
+
+
