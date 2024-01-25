2f070603414061ec75664a0b49156302c036ecf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:04 +0000

    Add package datasets-sql/0.4.0/datasets_sql-0.4.0-py3-none-any.whl

---
+++ packages/datasets-sql/datasets_sql-0.4.0-py3-none-any.whl/datasets_sql/__init__.py
@@ -0,0 +1,16 @@
+import pyarrow
+from packaging import version
+
+
+__version___ = "0.4.0"
+
+
+if version.parse(pyarrow.__version__).major < 5:
+    raise ImportWarning(
+        "To use `datasets_sql`, the module `pyarrow>=5.0.0` is required, and the current version of `pyarrow` doesn't match this condition."
+    )
+
+del pyarrow
+del version
+
+from .query import query
