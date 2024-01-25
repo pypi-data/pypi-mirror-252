32ae40d11ddf78c9eb1b1df86a56b61b070efffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:57 +0000

    Add package revel2d2l/0.3.0/revel2d2l-0.3.0.tar.gz

---
+++ packages/revel2d2l/revel2d2l-0.3.0.tar.gz/revel2d2l-0.3.0/tests/test_revel2d2l.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `revel2d2l` package."""
+
+
+import unittest
+
+# from revel2d2l import revel2d2l
+
+
+class TestRevel2d2l(unittest.TestCase):
+    """Tests for `revel2d2l` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
