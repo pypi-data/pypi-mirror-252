3f3d6b3bf9cca1afef23a51f4b6b35bae3c37ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:05:13 +0000

    Add package snaptrade-python-sdk/11.0.0/snaptrade_python_sdk-11.0.0-py3-none-any.whl

---
+++ packages/snaptrade-python-sdk/snaptrade_python_sdk-11.0.0-py3-none-any.whl/snaptrade_client/type/manual_trade.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+"""
+    SnapTrade
+
+    Connect brokerage accounts to your app for live positions and trading
+
+    The version of the OpenAPI document: 1.0.0
+    Contact: api@snaptrade.com
+    Created by: https://snaptrade.com/
+"""
+
+from datetime import datetime, date
+import typing
+from enum import Enum
+from typing_extensions import TypedDict, Literal, TYPE_CHECKING
+
+from snaptrade_client.type.action import Action
+from snaptrade_client.type.manual_trade_symbol import ManualTradeSymbol
+from snaptrade_client.type.order_type import OrderType
+from snaptrade_client.type.price import Price
+from snaptrade_client.type.time_in_force import TimeInForce
+
+class RequiredManualTrade(TypedDict):
+    pass
+
+class OptionalManualTrade(TypedDict, total=False):
+    id: str
+
+    account: str
+
+    order_type: OrderType
+
+    time_in_force: TimeInForce
+
+    symbol: ManualTradeSymbol
+
+    action: Action
+
+    # Trade Units. Cannot work with notional value.
+    units: typing.Union[int, float]
+
+    price: Price
+
+class ManualTrade(RequiredManualTrade, OptionalManualTrade):
+    pass
