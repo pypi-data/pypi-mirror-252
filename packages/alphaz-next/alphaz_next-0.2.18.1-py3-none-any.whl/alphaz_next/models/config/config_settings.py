3cf40ee479a7966de3da8e403a7ef1eeb2c0183a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:05:56 +0000

    Add package alphaz-next/0.2.18.1/alphaz_next-0.2.18.1-py3-none-any.whl

---
+++ packages/alphaz-next/alphaz_next-0.2.18.1-py3-none-any.whl/alphaz_next/models/config/config_settings.py
@@ -0,0 +1,46 @@
+# MODULES
+from pathlib import Path
+from typing import Type, TypeVar
+
+# PYDANTIC
+from pydantic import Field, computed_field
+from pydantic_settings import BaseSettings
+
+# LIBS
+from alphaz_next.libs.file_lib import open_json_file
+from alphaz_next.models.config.alpha_config import AlphaConfigSchema
+
+_T = TypeVar("_T", bound=AlphaConfigSchema)
+
+
+def create_config_settings(
+    model: Type[_T],
+    environment_alias: str = "ALPHA_ENV",
+    root_alias: str = "ALPHA_ROOT",
+    config_file_path_alias: str = "ALPHA_CONFIG_FILE_PATH",
+    port_alias: str = "ALPHA_PORT",
+    workers_alias: str = "ALPHA_WORKERS",
+):
+    class AlphaConfigSettingsSchema(BaseSettings):
+        environment: str = Field(validation_alias=environment_alias)
+        root: str = Field(validation_alias=root_alias)
+        config_file_path: str = Field(validation_alias=config_file_path_alias)
+        port: int = Field(validation_alias=port_alias)
+        workers: int = Field(validation_alias=workers_alias)
+
+        @computed_field
+        @property
+        def main_config(self) -> _T:
+            data = open_json_file(path=Path(self.config_file_path))
+
+            data_ext = {
+                "environment": self.environment,
+                "root": self.root,
+                "config_file_path": self.config_file_path,
+            }
+
+            data.update(data_ext)
+
+            return model.model_validate(data)
+
+    return AlphaConfigSettingsSchema().main_config
