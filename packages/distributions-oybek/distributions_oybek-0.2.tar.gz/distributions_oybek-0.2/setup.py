057fafa346466a044a7e648efb9d7d0283f7df02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:05:51 +0000

    Add package distributions-oybek/0.2/distributions_oybek-0.2.tar.gz

---
+++ packages/distributions-oybek/distributions_oybek-0.2.tar.gz/distributions_oybek-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_oybek',
+      version='0.2',
+      description='Gaussian_distributions',
+      packages=['distributions_oybek'],
+      zip_safe=False)
