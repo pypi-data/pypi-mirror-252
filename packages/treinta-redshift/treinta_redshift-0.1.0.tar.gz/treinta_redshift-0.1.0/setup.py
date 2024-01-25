c4d53e0e1eb68237eacd10169d909ae423155b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:42 +0000

    Add package treinta-redshift/0.1.0/treinta_redshift-0.1.0.tar.gz

---
+++ packages/treinta-redshift/treinta_redshift-0.1.0.tar.gz/treinta_redshift-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='treinta_redshift',
+    version='0.1.0',
+    author='cristian.pinela',
+    author_email='cristian.pinela@treinta.co',
+    packages=find_packages(),
+    license='LICENSE',
+    description='Libreria para data engineering',
+    long_description='Librería que permite programar sobre redshift utilizando python para tareas de data engineering',
+    long_description_content_type='text/markdown',  # Specify Markdown format
+    install_requires=[
+        'boto3==1.34.23',
+        'botocore==1.34.23',
+        'numpy==1.25.0',
+        'pandas==2.0.2',
+        'pyarrow==13.0.0',
+    ]
+)
