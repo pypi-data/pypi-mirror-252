0255e7f1699c06f58fd841bc836deae162725e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:22 +0000

    Add package my-math-lib-my/0.1.1/my_math_lib_my-0.1.1.tar.gz

---
+++ packages/my-math-lib-my/my_math_lib_my-0.1.1.tar.gz/my_math_lib_my-0.1.1/tests/test.py
@@ -0,0 +1,9 @@
+from my_math_lib_my    import operations
+
+def main():
+    print("Addition:", operations.add(2, 3))
+    print("Subtraction:", operations.subtract(5, 3))
+    # Add more tests as needed
+
+if __name__ == "__main__":
+    main()
