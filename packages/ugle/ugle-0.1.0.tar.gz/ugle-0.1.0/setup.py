c8005fa5e23b512cf557be627100bcc0799b3dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:45 +0000

    Add package ugle/0.1.0/ugle-0.1.0.tar.gz

---
+++ packages/ugle/ugle-0.1.0.tar.gz/ugle-0.1.0/setup.py
@@ -0,0 +1,76 @@
+from setuptools import setup, find_packages
+
+PREREQS = [ "torch==1.12.0" ]
+
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+def requires( packages ): 
+    from os import system
+    from sys import executable as PYTHON_PATH
+    from pkg_resources import require
+    require( "pip" )
+    CMD_TMPLT = '"' + PYTHON_PATH + '" -m pip install %s'
+    for pkg in packages: system( CMD_TMPLT % (pkg,) )       
+
+class OrderedInstall( install ):
+    def run( self ):
+        requires( PREREQS )
+        install.run( self )        
+
+class OrderedDevelop( develop ):
+    def run( self ):
+        requires( PREREQS )
+        develop.run( self )        
+
+class OrderedEggInfo( egg_info ):
+    def run( self ):
+        requires( PREREQS )
+        egg_info.run( self )        
+
+CMD_CLASSES = { 
+     "install" : OrderedInstall
+   , "develop" : OrderedDevelop
+   , "egg_info": OrderedEggInfo 
+}        
+
+
+setup(name='ugle', 
+      description='This is a repository for investigating implementations of GNNs for \
+                   unsupervised clustering.',
+      url='https://github.com/willleeney/ugle',
+      author='William Leeney',
+      author_email='will.leeney@outlook.com',
+      license_files = ('LICENSE',),
+      version='0.1.0', 
+      packages=find_packages(),
+      install_requires=['pytest',
+                        'omegaconf',
+                        'torch==1.12.0',
+                        'torch_geometric',
+                        'torch_scatter',
+                        'torch_sparse',
+                        'torch_cluster',
+                        'torch_spline_conv',
+                        'numpy<=1.23.0',
+                        'networkx',
+                        'gdown',
+                        'optuna',
+                        'memory_profiler',
+                        'line_profiler',
+                        'matplotlib',
+                        'fast-pytorch-kmeans',
+                        'names_generator'],
+      cmdclass = CMD_CLASSES,
+      python_requires=">=3.9.12",
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+       ' Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+)
