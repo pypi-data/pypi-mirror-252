7e0134873716d41125bfe78bc50dc58835b2ec10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:59 +0000

    Add package ecoengine/1.4.4/ecoengine-1.4.4-py3-none-any.whl

---
+++ packages/ecoengine/ecoengine-1.4.4-py3-none-any.whl/ecoengine/objects/systems/MultiPass.py
@@ -0,0 +1,35 @@
+from ecoengine.objects.SystemConfig import SystemConfig
+from ecoengine.objects.SimulationRun import SimulationRun
+from ecoengine.constants.Constants import *
+from ecoengine.objects.systemConfigUtils import convertVolume
+
+class MultiPass(SystemConfig):
+    def __init__(self, storageT_F, defrostFactor, percentUseable, compRuntime_hr, aquaFract, building,
+                 doLoadShift = False, loadShiftPercent = 1, loadShiftSchedule = None, loadUpHours = None, aquaFractLoadUp = None, 
+                 aquaFractShed = None, loadUpT_F = None, systemModel = None, numHeatPumps = None, PVol_G_atStorageT = None, 
+                 PCap_kBTUhr = None, ignoreShortCycleEr = False, useHPWHsimPrefMap = False, inletWaterAdjustment = 0.5):
+        
+        aquaFract = 0.15
+        aquaFractLoadUp = 0.15
+        aquaFractShed = 0.3
+        self.inletWaterAdjustment = inletWaterAdjustment
+
+        super().__init__(storageT_F, defrostFactor, percentUseable, compRuntime_hr, aquaFract, building, doLoadShift, 
+                loadShiftPercent, loadShiftSchedule, loadUpHours, aquaFractLoadUp, aquaFractShed, loadUpT_F, systemModel, 
+                numHeatPumps, PVol_G_atStorageT, PCap_kBTUhr, ignoreShortCycleEr, useHPWHsimPrefMap)
+        
+    def runOneSystemStep(self, simRun : SimulationRun, i, minuteIntervals = 1, oat = None):
+        incomingWater_T = simRun.getIncomingWaterT(i) + ((self.storageT_F - simRun.getIncomingWaterT(i)) * self.inletWaterAdjustment)
+        self.preSystemStepSetUp(simRun, i, incomingWater_T, minuteIntervals, oat)
+
+        # Get exiting and generating water volumes at storage temp
+        mixedDHW = convertVolume(simRun.hwDemand[i], self.storageT_F, simRun.getIncomingWaterT(i), simRun.building.supplyT_F)
+        mixedGHW = convertVolume(simRun.hwGenRate, self.storageT_F, simRun.getIncomingWaterT(i), simRun.building.supplyT_F)
+
+        simRun.pheating, simRun.pV[i], simRun.pGen[i], simRun.pRun[i] = self.runOnePrimaryStep(pheating = simRun.pheating,
+                                                                                            Vcurr = simRun.pV[i-1], 
+                                                                                            hw_out = mixedDHW, 
+                                                                                            hw_in = mixedGHW, 
+                                                                                            mode = simRun.getLoadShiftMode(i),
+                                                                                            modeChanged = (simRun.getLoadShiftMode(i) != simRun.getLoadShiftMode(i-1)),
+                                                                                            minuteIntervals = minuteIntervals) 
