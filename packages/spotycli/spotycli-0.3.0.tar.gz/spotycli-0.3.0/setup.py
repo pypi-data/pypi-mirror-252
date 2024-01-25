75ca0bfec63b78e8d47e29799aae35c7c6ec8c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:18 +0000

    Add package spotycli/0.3.0/spotycli-0.3.0.tar.gz

---
+++ packages/spotycli/spotycli-0.3.0.tar.gz/spotycli-0.3.0/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+from setuptools import setup, find_packages
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+   long_description = open('README.md').read()
+
+setup(
+    name='spotycli',
+    version='0.3.0',
+    author="micheledinelli",
+    author_email="dinellimichele00@gmail.com",
+
+    long_description_content_type="text/markdown",
+
+    long_description=long_description,
+
+    packages=find_packages(),
+    install_requires=[
+        'typer',
+        'spotipy',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spotycli = main:app',
+        ],
+    },
+)
