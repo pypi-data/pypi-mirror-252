43534fcc124edfbae7722bf7d72ee065a462d337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 15:05:07 +0000

    Add package uzbekistan/2.5.9/uzbekistan-2.5.9-py3-none-any.whl

---
+++ packages/uzbekistan/uzbekistan-2.5.9-py3-none-any.whl/uzbekistan/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from uzbekistan.models import Region, District
+
+
+@admin.register(Region)
+class RegionAdmin(ModelAdmin):
+    list_display = ('name_uz', 'name_oz', 'name_ru')
+    search_fields = ('name_uz', 'name_oz', 'name_ru')
+    sortable_by = ('name_uz', 'name_oz', 'name_ru')
+
+
+@admin.register(District)
+class DistrictAdmin(ModelAdmin):
+    list_display = ('name_uz', 'name_oz', 'name_ru', 'get_region_name')
+    search_fields = ('name_uz', 'name_oz', 'name_ru', 'get_region_name')
+    sortable_by = ('name_uz', 'name_oz', 'name_ru', 'region')
+    save_on_top = True
+
+    def get_region_name(self, obj):
+        return obj.region.name_uz
+
+    get_region_name.short_description = 'Region'
