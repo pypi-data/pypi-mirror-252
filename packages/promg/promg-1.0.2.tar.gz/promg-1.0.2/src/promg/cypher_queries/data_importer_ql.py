f646d3bbd1b1e40a5521ada6ee43e45ac8fb7e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:30 +0000

    Add package promg/1.0.2/promg-1.0.2.tar.gz

---
+++ packages/promg/promg-1.0.2.tar.gz/promg-1.0.2/src/promg/cypher_queries/data_importer_ql.py
@@ -0,0 +1,216 @@
+from typing import Dict, Optional, List, Union
+from string import Template
+
+from ..data_managers.datastructures import DataStructure
+from ..data_managers.semantic_header import RecordConstructor
+from ..database_managers.db_connection import Query
+
+
+class DataImporterQueryLibrary:
+    @staticmethod
+    def get_optional_label_str(get_labels_str, optional_labels):
+        record_name = "record"
+        optional_label_str = ""
+        if len(optional_labels) > 0:
+            # language=SQL
+            optional_label_str = '''
+                    $assign_labels_str
+                    CALL apoc.create.addLabels($record_name, $optional_labels_list)
+                    YIELD node as record_with_labels
+                    '''
+        optional_label_str = Template(optional_label_str).safe_substitute({
+            "record_name": record_name,
+            "assign_labels_str": get_labels_str,
+            "optional_labels_list": "+".join(optional_labels)
+        })
+
+        return optional_label_str
+
+    @staticmethod
+    def get_label_constructors(record_constructors: List[Dict[bool, RecordConstructor]]):
+        required_labels = ['Record']
+        optional_labels = []
+        record_name = "record"
+        get_labels_str = ""
+        for index, record_constructor_dict in enumerate(record_constructors):
+            record_constructor = record_constructor_dict["record_constructor"]
+            if record_constructor_dict["required"]:
+                required_labels.extend(record_constructor.record_labels)
+            else:
+                with_str = '''
+                            CALL apoc.when($required_attributes_not_null $attributes_match, 'RETURN $labels as 
+                            labels', 'RETURN [] as labels')
+                            YIELD value as labels$i'''
+                with_str = Template(with_str).safe_substitute({
+                    "attributes_match": record_constructor.get_additional_conditions(),
+                    "required_attributes_not_null": record_constructor.get_required_attributes_is_not_null_pattern(
+                        record_name=record_name),
+                    "labels": record_constructor.get_label_list(as_str=True),
+                    "record_name": record_name,
+                    "i": index
+                })
+                get_labels_str += with_str
+                optional_labels.append(f"labels{index}['labels']")
+
+        optional_labels_str = DataImporterQueryLibrary.get_optional_label_str(optional_labels=optional_labels,
+                                                                              get_labels_str=get_labels_str)
+
+        label_constructor = {
+            "required_labels": required_labels,
+            "optional_labels_str": optional_labels_str
+        }
+
+        return label_constructor
+
+    @staticmethod
+    def get_create_nodes_by_importing_batch_query(batch: List[Dict[str, str]],
+                                                  labels_constructors: Dict[str, Union[List[str], str]]) -> Query:
+        """
+        Create event nodes for each row in the batch with labels
+        The properties of each row are also the property of the node
+        @param batch: List[Dictionary[key: value]], the key and its values form properties of the event nodes
+        @param labels: The labels of the event nodes
+        @return: None,
+        """
+
+        record_name = "record"
+        required_labels = labels_constructors["required_labels"]
+        optional_labels_str = labels_constructors["optional_labels_str"]
+
+        # $batch is a variable we can add in tx.run, this allows us to use string properties
+        # (keys in our dictionary are string)
+        # return is required when using call and yield
+        # language=SQL
+        query_str = '''
+                    UNWIND $batch AS row
+                    CALL apoc.create.node($required_labels, row) YIELD node AS $record_name
+                    $optional_label_str
+                    RETURN count(*)
+                '''
+
+        return Query(query_str=query_str,
+                     template_string_parameters={
+                         "record_name": record_name,
+                         "required_labels": required_labels,
+                         "optional_label_str": optional_labels_str
+                     },
+                     parameters={"batch": batch})
+
+    @staticmethod
+    def get_make_timestamp_date_query(attribute, datetime_object, load_status) -> Query:
+        """
+        Convert the strings of the timestamp to the datetime as used in cypher
+        Remove the str_timestamp property
+        @return: None
+        """
+        offset = datetime_object.timezone_offset
+        offset = f'{attribute}+"{offset}"' if offset != "" else attribute
+
+        # language=SQL
+        query_str = '''
+                CALL apoc.periodic.iterate(
+                'MATCH (e:Record) WHERE e.$attribute IS NOT NULL AND e.loadStatus = $load_status 
+                AND NOT apoc.meta.cypher.isType(e.$attribute, "$date_type")
+                WITH e, e.$offset as timezone_dt
+                WITH e, datetime(apoc.date.convertFormat(timezone_dt, "$datetime_object_format", 
+                    "$datetime_object_convert_to")) as converted
+                RETURN e, converted',
+                'SET e.$attribute = converted',
+                {batchSize:$batch_size, parallel:false})
+            '''
+
+        return Query(query_str=query_str,
+                     template_string_parameters={
+                         "datetime_object_format": datetime_object.format,
+                         "datetime_object_convert_to": datetime_object.convert_to,
+                         "date_type": datetime_object.get_date_type(),
+                         "attribute": attribute,
+                         "offset": offset,
+                         "load_status": load_status
+                     })
+
+    @staticmethod
+    def get_convert_epoch_to_timestamp_query(attribute, datetime_object, load_status) -> Query:
+        # language=SQL
+        query_str = '''
+                CALL apoc.periodic.iterate(
+                'MATCH (e:Record) WHERE e.$attribute IS NOT NULL AND e.loadStatus = $load_status 
+                WITH e, e.$attribute as timezone_dt
+                WITH e, apoc.date.format(timezone_dt, $unit, 
+                    $dt_format) as converted
+                RETURN e, converted',
+                'SET e.$attribute = converted',
+                {batchSize:$batch_size, parallel:false, 
+                params: {unit: $unit,
+                        dt_format: $datetime_object_format}})
+            '''
+
+        return Query(query_str=query_str,
+                     template_string_parameters={"attribute": attribute},
+                     parameters={
+                         "label": "Record",
+                         "unit": datetime_object.unit,
+                         "datetime_object_format": datetime_object.format,
+                         "load_status": load_status
+                     })
+
+    @staticmethod
+    def get_finalize_import_events_query() -> Query:
+        # language=SQL
+        query_str = '''
+            CALL apoc.periodic.iterate(
+                'MATCH (e:Record) 
+                WHERE e.loadStatus IS NOT NULL 
+                RETURN e',
+                'REMOVE e.loadStatus',
+                {batchSize:$batch_size, parallel:false})
+            '''
+
+        return Query(query_str=query_str)
+
+    @staticmethod
+    def get_filter_events_by_property_query(prop: str, load_status: int, values: Optional[List[str]] = None,
+                                            exclude=True) -> Query:
+        if values is None:  # match all events that have a specific property
+            negation = "NOT" if exclude else ""
+            # query to delete all events and its relationship with property
+            # language=SQL
+            query_str = '''
+                    MATCH (e:Event {loadStatus: $load_status})
+                    WHERE e.$prop IS $negation NULL
+                    DETACH DELETE e
+                    '''
+            template_string_parameters = {"prop": prop, "negation": negation}
+        else:  # match all events with specific property and value
+            negation = "" if exclude else "NOT"
+            # match all e and delete them and its relationship
+            # language=SQL
+            query_str = '''
+                    MATCH (e:Event {loadStatus: $load_status})
+                    WHERE $negation e.$prop IN $values
+                    DETACH DELETE e
+                '''
+            template_string_parameters = {
+                "prop": prop, "negation": negation, "values": values, "load_status": load_status
+            }
+
+        # execute query
+        return Query(query_str=query_str,
+                     template_string_parameters=template_string_parameters)
+
+    @staticmethod
+    def get_update_load_status_query(current_load_status: int):
+        query_str = '''
+        CALL apoc.periodic.commit(
+            'MATCH (record:Record) 
+            WHERE record.loadStatus = $old_value
+            WITH record LIMIT $limit
+            SET record.loadStatus = record.loadStatus + 1
+            RETURN COUNT(*)',
+            {old_value: $old_value,
+            limit: $batch_size})
+                            '''
+        return Query(query_str=query_str,
+                     parameters={
+                         "old_value": current_load_status
+                     })
---
+++ packages/promg/promg-1.0.2.tar.gz/promg-1.0.2/src/promg/modules/data_importer.py
@@ -0,0 +1,139 @@
+import math
+from typing import List
+
+import numpy as np
+from tqdm import tqdm
+
+from ..data_managers.semantic_header import RecordConstructor, SemanticHeader
+from ..database_managers.db_connection import DatabaseConnection
+from ..data_managers.datastructures import DatasetDescriptions
+from ..utilities.performance_handling import Performance
+from ..cypher_queries.data_importer_ql import DataImporterQueryLibrary as di_ql
+import pandas as pd
+
+
+class Importer:
+    def __init__(self, data_structures: DatasetDescriptions,
+                 use_sample: bool = False, use_preprocessed_files: bool = False):
+        self.connection = DatabaseConnection()
+        self.structures = data_structures.structures
+        self.records = SemanticHeader().records
+
+        self.load_batch_size = 20000
+        self.use_sample = use_sample
+        self.use_preprocessed_files = use_preprocessed_files
+        self.load_status = 0
+
+    def update_load_status(self):
+        self.connection.exec_query(di_ql.get_update_load_status_query,
+                                   **{
+                                       "current_load_status": self.load_status
+                                   })
+        self.load_status += 1
+
+    def import_data(self) -> None:
+        for structure in self.structures:
+            labels = structure.labels
+            record_constructors = self._get_record_constructors_by_labels(structure=structure, labels=labels)
+            file_directory = structure.file_directory
+            # read in all file names that match this structure
+            for file_name in structure.file_names:
+                # read and import the nodes
+                df_log = structure.read_data_set(file_directory, file_name, use_sample=self.use_sample,
+                                                 use_preprocessed_file=self.use_preprocessed_files)
+                df_log["loadStatus"] = self.load_status
+                self._import_nodes_from_data(df_log=df_log, file_name=file_name,
+                                             record_constructors=record_constructors)
+
+            if structure.has_datetime_attribute():
+                # once all events are imported, we convert the string timestamp to the timestamp as used in Cypher
+                self._reformat_timestamps(structure=structure)
+                self.update_load_status()
+
+            self._filter_nodes(structure=structure)  # filter nodes according to the structure
+
+            self._finalize_import()  # removes temporary properties
+
+    @Performance.track("structure")
+    def _reformat_timestamps(self, structure):
+        datetime_formats = structure.get_datetime_formats()
+        for attribute, datetime_format in datetime_formats.items():
+            if datetime_format.is_epoch:
+                self.connection.exec_query(di_ql.get_convert_epoch_to_timestamp_query,
+                                           **{
+                                               "attribute": attribute,
+                                               "datetime_object": datetime_format
+                                           })
+
+            self.connection.exec_query(di_ql.get_make_timestamp_date_query,
+                                       **{
+                                           "attribute": attribute,
+                                           "datetime_object": datetime_format,
+                                           "load_status": self.load_status
+                                       })
+
+    @Performance.track("structure")
+    def _filter_nodes(self, structure):
+        for boolean in (True, False):
+            attribute_values_pairs_filtered = structure.get_attribute_value_pairs_filtered(exclude=boolean)
+            for name, values in attribute_values_pairs_filtered.items():
+                self.connection.exec_query(di_ql.get_filter_events_by_property_query,
+                                           **{
+                                               "prop": name, "values": values, "exclude": boolean,
+                                               "load_status": self.load_status
+                                           })
+
+    @Performance.track("structure")
+    def _finalize_import(self):
+        # finalize the import
+        self.connection.exec_query(di_ql.get_finalize_import_events_query)
+
+    @Performance.track("file_name")
+    def _import_nodes_from_data(self, df_log, file_name, record_constructors):
+        # start with batch 0 and increment until everything is imported
+        batch = 0
+        print("\n")
+        pbar = tqdm(total=math.ceil(len(df_log) / self.load_batch_size), position=0)
+
+        labels_constructor = di_ql.get_label_constructors(record_constructors)
+
+        while batch * self.load_batch_size < len(df_log):
+            pbar.set_description(f"Loading data from {file_name} from batch {batch}")
+
+            # import the events in batches, use the records of the log
+            batch_without_nans = [{k: int(v) if isinstance(v, np.integer) else v for k, v in m.items()
+                                   if (isinstance(v, list) and len(v) > 0) or (not pd.isna(v) and v is not None)}
+                                  for m in
+                                  df_log[batch * self.load_batch_size:(batch + 1) * self.load_batch_size].to_dict(
+                                      orient='records')]
+
+            self.connection.exec_query(di_ql.get_create_nodes_by_importing_batch_query,
+                                       **{
+                                           "batch": batch_without_nans,
+                                           "labels_constructors": labels_constructor
+                                       })
+
+            pbar.update(1)
+            batch += 1
+        pbar.close()
+
+    def _get_record_constructors_by_labels(self, structure, labels):
+        constructors = []
+        for record_constructor in self.records:
+            if labels is not None:
+                intersection = list(set(labels) & set(record_constructor.record_labels))
+            else:
+                intersection = record_constructor.record_labels
+            if len(intersection) > 0:  # label of record constructor is in intersection or labels is not defined
+                required = True
+                if record_constructor.prevalent_record.where_condition != "":
+                    required = False
+                else:
+                    for required_attribute in record_constructor.required_attributes:
+                        if required_attribute == "index":
+                            continue
+                        if required_attribute not in structure.attributes or structure.attributes[
+                            required_attribute].optional:
+                            required = False
+                constructors.append({"required": required, "record_constructor": record_constructor})
+        return constructors
