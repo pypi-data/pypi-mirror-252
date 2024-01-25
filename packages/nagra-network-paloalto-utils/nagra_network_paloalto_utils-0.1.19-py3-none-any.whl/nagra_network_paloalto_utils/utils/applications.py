556597f70640a658a75b6781b0d20d9148e1e369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:31 +0000

    Add package nagra-network-paloalto-utils/0.1.19/nagra_network_paloalto_utils-0.1.19-py3-none-any.whl

---
+++ packages/nagra-network-paloalto-utils/nagra_network_paloalto_utils-0.1.19-py3-none-any.whl/nagra_network_paloalto_utils/utils/applications.py
@@ -0,0 +1,33 @@
+import logging
+
+from .common.utils import BaseRegistry
+
+log = logging.getLogger(__name__)
+
+
+class Applications(BaseRegistry):
+    def get_data(self):
+        """
+        Find all applications
+        :return: list of all applications
+        """
+        predefined = {
+            "location": "predefined",
+        }
+        return [
+            *self.client.objects.Applications.get(device_group="DG1_GLOBAL"),
+            *self.client.objects.Applications.get(params=predefined, device_group=""),
+        ]
+
+
+def extract_applications(data):
+    applications = []
+    for rules in data:
+        if rules["rules"] is None:
+            continue
+        for rule in rules["rules"]:
+            try:
+                applications.extend(rule["applications"])
+            except KeyError:
+                continue
+    return applications
