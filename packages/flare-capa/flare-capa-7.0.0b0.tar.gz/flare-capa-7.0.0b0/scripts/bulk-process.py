dc334e8175476624801c1978e756377d91accf9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 03:06:30 +0000

    Add package flare-capa/7.0.0b0/flare-capa-7.0.0b0.tar.gz

---
+++ packages/flare-capa/flare-capa-7.0.0b0.tar.gz/flare-capa-7.0.0b0/scripts/bulk-process.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at: [package root]/LICENSE.txt
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+#  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+"""
+bulk-process
+
+Invoke capa recursively against a directory of samples
+and emit a JSON document mapping the file paths to their results.
+
+By default, this will use subprocesses for parallelism.
+Use `-n/--parallelism` to change the subprocess count from
+ the default of current CPU count.
+Use `--no-mp` to use threads instead of processes,
+ which is probably not useful unless you set `--parallelism=1`.
+
+example:
+
+    $ python scripts/bulk-process /tmp/suspicious
+    {
+      "/tmp/suspicious/suspicious.dll_": {
+        "rules": {
+          "encode data using XOR": {
+            "matches": {
+              "268440358": {
+              [...]
+      "/tmp/suspicious/1.dll_": { ... }
+      "/tmp/suspicious/2.dll_": { ... }
+    }
+
+
+usage:
+
+    usage: bulk-process.py [-h] [-r RULES] [-d] [-q] [-n PARALLELISM] [--no-mp]
+                           input
+
+    detect capabilities in programs.
+
+    positional arguments:
+      input                 Path to directory of files to recursively analyze
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -r RULES, --rules RULES
+                            Path to rule file or directory, use embedded rules by
+                            default
+      -d, --debug           Enable debugging output on STDERR
+      -q, --quiet           Disable all output but errors
+      -n PARALLELISM, --parallelism PARALLELISM
+                            parallelism factor
+      --no-mp               disable subprocesses
+
+Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+You may obtain a copy of the License at: [package root]/LICENSE.txt
+Unless required by applicable law or agreed to in writing, software distributed under the License
+ is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+"""
+import os
+import sys
+import json
+import logging
+import argparse
+import multiprocessing
+import multiprocessing.pool
+from pathlib import Path
+
+import capa
+import capa.main
+import capa.rules
+import capa.render.json
+import capa.capabilities.common
+import capa.render.result_document as rd
+from capa.features.common import OS_AUTO
+
+logger = logging.getLogger("capa")
+
+
+def get_capa_results(args):
+    """
+    run capa against the file at the given path, using the given rules.
+
+    args is a tuple, containing:
+      rules (capa.rules.RuleSet): the rules to match
+      signatures (List[str]): list of file system paths to signature files
+      format (str): the name of the sample file format
+      os (str): the name of the operating system
+      path (str): the file system path to the sample to process
+
+    args is a tuple because i'm not quite sure how to unpack multiple arguments using `map`.
+
+    returns an dict with two required keys:
+      path (str): the file system path of the sample to process
+      status (str): either "error" or "ok"
+
+    when status == "error", then a human readable message is found in property "error".
+    when status == "ok", then the capa results are found in the property "ok".
+
+    the capa results are a dictionary with the following keys:
+      meta (dict): the meta analysis results
+      capabilities (dict): the matched capabilities and their result objects
+    """
+    rules, sigpaths, format, os_, path = args
+    should_save_workspace = os.environ.get("CAPA_SAVE_WORKSPACE") not in ("0", "no", "NO", "n", None)
+    logger.info("computing capa results for: %s", path)
+    try:
+        extractor = capa.main.get_extractor(
+            path, format, os_, capa.main.BACKEND_VIV, sigpaths, should_save_workspace, disable_progress=True
+        )
+    except capa.exceptions.UnsupportedFormatError:
+        # i'm 100% sure if multiprocessing will reliably raise exceptions across process boundaries.
+        # so instead, return an object with explicit success/failure status.
+        #
+        # if success, then status=ok, and results found in property "ok"
+        # if error, then status=error, and human readable message in property "error"
+        return {
+            "path": path,
+            "status": "error",
+            "error": f"input file does not appear to be a PE file: {path}",
+        }
+    except capa.exceptions.UnsupportedRuntimeError:
+        return {
+            "path": path,
+            "status": "error",
+            "error": "unsupported runtime or Python interpreter",
+        }
+    except Exception as e:
+        return {
+            "path": path,
+            "status": "error",
+            "error": f"unexpected error: {e}",
+        }
+
+    capabilities, counts = capa.capabilities.common.find_capabilities(rules, extractor, disable_progress=True)
+
+    meta = capa.main.collect_metadata([], path, format, os_, [], extractor, counts)
+    meta.analysis.layout = capa.main.compute_layout(rules, extractor, capabilities)
+
+    doc = rd.ResultDocument.from_capa(meta, rules, capabilities)
+    return {"path": path, "status": "ok", "ok": doc.model_dump()}
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+        parser = argparse.ArgumentParser(description="detect capabilities in programs.")
+        capa.main.install_common_args(parser, wanted={"rules", "signatures", "format", "os"})
+        parser.add_argument("input", type=str, help="Path to directory of files to recursively analyze")
+        parser.add_argument(
+            "-n", "--parallelism", type=int, default=multiprocessing.cpu_count(), help="parallelism factor"
+        )
+        parser.add_argument("--no-mp", action="store_true", help="disable subprocesses")
+        args = parser.parse_args(args=argv)
+        capa.main.handle_common_args(args)
+
+        try:
+            rules = capa.main.get_rules(args.rules)
+            logger.info("successfully loaded %s rules", len(rules))
+        except (IOError, capa.rules.InvalidRule, capa.rules.InvalidRuleSet) as e:
+            logger.error("%s", str(e))
+            return -1
+
+        try:
+            sig_paths = capa.main.get_signatures(args.signatures)
+        except IOError as e:
+            logger.error("%s", str(e))
+            return -1
+
+        samples = []
+        for file in Path(args.input).rglob("*"):
+            samples.append(file)
+
+        cpu_count = multiprocessing.cpu_count()
+
+        def pmap(f, args, parallelism=cpu_count):
+            """apply the given function f to the given args using subprocesses"""
+            return multiprocessing.Pool(parallelism).imap(f, args)
+
+        def tmap(f, args, parallelism=cpu_count):
+            """apply the given function f to the given args using threads"""
+            return multiprocessing.pool.ThreadPool(parallelism).imap(f, args)
+
+        def map(f, args, parallelism=None):
+            """apply the given function f to the given args in the current thread"""
+            for arg in args:
+                yield f(arg)
+
+        if args.no_mp:
+            if args.parallelism == 1:
+                logger.debug("using current thread mapper")
+                mapper = map
+            else:
+                logger.debug("using threading mapper")
+                mapper = tmap
+        else:
+            logger.debug("using process mapper")
+            mapper = pmap
+
+        results = {}
+        for result in mapper(
+            get_capa_results,
+            [(rules, sig_paths, "pe", OS_AUTO, sample) for sample in samples],
+            parallelism=args.parallelism,
+        ):
+            if result["status"] == "error":
+                logger.warning(result["error"])
+            elif result["status"] == "ok":
+                results[result["path"].as_posix()] = rd.ResultDocument.model_validate(result["ok"]).model_dump_json(
+                    exclude_none=True
+                )
+            else:
+                raise ValueError(f"unexpected status: {result['status']}")
+
+        print(json.dumps(results))
+
+        logger.info("done.")
+
+        return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/flare-capa/flare-capa-7.0.0b0.tar.gz/flare-capa-7.0.0b0/scripts/show-capabilities-by-function.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python2
+# Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at: [package root]/LICENSE.txt
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+#  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+"""
+show-capabilities-by-function
+
+Invoke capa to extract the capabilities of the given sample
+and emit the results grouped by function.
+
+This is useful to identify "complex functions" - that is,
+functions that implement a lot of different types of logic.
+
+Example::
+
+    $ python scripts/show-capabilities-by-function.py /tmp/suspicious.dll_
+    function at 0x1000321A with 33 features:
+      - get hostname
+      - initialize Winsock library
+    function at 0x10003286 with 63 features:
+      - create thread
+      - terminate thread
+    function at 0x10003415 with 116 features:
+      - write file
+      - send data
+      - link function at runtime
+      - create HTTP request
+      - get common file path
+      - send HTTP request
+      - connect to HTTP server
+    function at 0x10003797 with 81 features:
+      - get socket status
+      - send data
+      - receive data
+      - create TCP socket
+      - send data on socket
+      - receive data on socket
+      - act as TCP client
+      - resolve DNS
+      - create UDP socket
+      - initialize Winsock library
+      - set socket configuration
+      - connect TCP socket
+    ...
+
+Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+You may obtain a copy of the License at: [package root]/LICENSE.txt
+Unless required by applicable law or agreed to in writing, software distributed under the License
+ is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+"""
+import os
+import sys
+import logging
+import argparse
+import collections
+from typing import Dict
+from pathlib import Path
+
+import colorama
+
+import capa.main
+import capa.rules
+import capa.engine
+import capa.helpers
+import capa.features
+import capa.exceptions
+import capa.render.utils as rutils
+import capa.render.verbose
+import capa.features.freeze
+import capa.capabilities.common
+import capa.render.result_document as rd
+from capa.helpers import get_file_taste
+from capa.features.common import FORMAT_AUTO
+from capa.features.freeze import Address
+from capa.features.extractors.base_extractor import FeatureExtractor, StaticFeatureExtractor
+
+logger = logging.getLogger("capa.show-capabilities-by-function")
+
+
+def render_matches_by_function(doc: rd.ResultDocument):
+    """
+    like:
+
+        function at 0x1000321a with 33 features:
+          - get hostname
+          - initialize Winsock library
+        function at 0x10003286 with 63 features:
+          - create thread
+          - terminate thread
+        function at 0x10003415 with 116 features:
+          - write file
+          - send data
+          - link function at runtime
+          - create HTTP request
+          - get common file path
+          - send HTTP request
+          - connect to HTTP server
+    """
+    assert isinstance(doc.meta.analysis, rd.StaticAnalysis)
+    functions_by_bb: Dict[Address, Address] = {}
+    for finfo in doc.meta.analysis.layout.functions:
+        faddress = finfo.address
+
+        for bb in finfo.matched_basic_blocks:
+            bbaddress = bb.address
+            functions_by_bb[bbaddress] = faddress
+
+    ostream = rutils.StringIO()
+
+    matches_by_function = collections.defaultdict(set)
+    for rule in rutils.capability_rules(doc):
+        if capa.rules.Scope.FUNCTION in rule.meta.scopes:
+            for addr, _ in rule.matches:
+                matches_by_function[addr].add(rule.meta.name)
+        elif capa.rules.Scope.BASIC_BLOCK in rule.meta.scopes:
+            for addr, _ in rule.matches:
+                function = functions_by_bb[addr]
+                matches_by_function[function].add(rule.meta.name)
+        else:
+            # file scope
+            pass
+
+    for f in doc.meta.analysis.feature_counts.functions:
+        if not matches_by_function.get(f.address, {}):
+            continue
+        ostream.writeln(f"function at {capa.render.verbose.format_address(addr)} with {f.count} features: ")
+        for rule_name in sorted(matches_by_function[f.address]):
+            ostream.writeln("  - " + rule_name)
+
+    return ostream.getvalue()
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description="detect capabilities in programs.")
+    capa.main.install_common_args(parser, wanted={"format", "os", "backend", "sample", "signatures", "rules", "tag"})
+    args = parser.parse_args(args=argv)
+    capa.main.handle_common_args(args)
+
+    try:
+        taste = get_file_taste(Path(args.sample))
+    except IOError as e:
+        logger.error("%s", str(e))
+        return -1
+
+    try:
+        rules = capa.main.get_rules(args.rules)
+        logger.info("successfully loaded %s rules", len(rules))
+        if args.tag:
+            rules = rules.filter_rules_by_meta(args.tag)
+            logger.info("selected %s rules", len(rules))
+    except (IOError, capa.rules.InvalidRule, capa.rules.InvalidRuleSet) as e:
+        logger.error("%s", str(e))
+        return -1
+
+    try:
+        sig_paths = capa.main.get_signatures(args.signatures)
+    except IOError as e:
+        logger.error("%s", str(e))
+        return -1
+
+    if (args.format == "freeze") or (args.format == FORMAT_AUTO and capa.features.freeze.is_freeze(taste)):
+        format_ = "freeze"
+        extractor: FeatureExtractor = capa.features.freeze.load(Path(args.sample).read_bytes())
+    else:
+        format_ = args.format
+        should_save_workspace = os.environ.get("CAPA_SAVE_WORKSPACE") not in ("0", "no", "NO", "n", None)
+
+        try:
+            extractor = capa.main.get_extractor(
+                args.sample, args.format, args.os, args.backend, sig_paths, should_save_workspace
+            )
+            assert isinstance(extractor, StaticFeatureExtractor)
+        except capa.exceptions.UnsupportedFormatError:
+            capa.helpers.log_unsupported_format_error()
+            return -1
+        except capa.exceptions.UnsupportedRuntimeError:
+            capa.helpers.log_unsupported_runtime_error()
+            return -1
+
+    capabilities, counts = capa.capabilities.common.find_capabilities(rules, extractor)
+
+    meta = capa.main.collect_metadata(argv, args.sample, format_, args.os, args.rules, extractor, counts)
+    meta.analysis.layout = capa.main.compute_layout(rules, extractor, capabilities)
+
+    if capa.capabilities.common.has_file_limitation(rules, capabilities):
+        # bail if capa encountered file limitation e.g. a packed binary
+        # do show the output in verbose mode, though.
+        if not (args.verbose or args.vverbose or args.json):
+            return -1
+
+    # colorama will detect:
+    #  - when on Windows console, and fixup coloring, and
+    #  - when not an interactive session, and disable coloring
+    # renderers should use coloring and assume it will be stripped out if necessary.
+    colorama.init()
+    doc = rd.ResultDocument.from_capa(meta, rules, capabilities)
+    print(render_matches_by_function(doc))
+    colorama.deinit()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
