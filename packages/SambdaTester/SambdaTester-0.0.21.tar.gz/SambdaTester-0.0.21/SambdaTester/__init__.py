6f13fe71f2f2a92e5409433b72e8a43020a45057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 05:04:36 +0000

    Add package SambdaTester/0.0.21/SambdaTester-0.0.21.tar.gz

---
+++ packages/SambdaTester/SambdaTester-0.0.21.tar.gz/SambdaTester-0.0.21/SambdaTester/__init__.py
@@ -0,0 +1,30 @@
+from .app import main
+from .yams import Yams
+from .config import Config
+import getopt
+import sys
+
+
+
+def execute():
+    config_path: str = ""
+
+    try:
+        opts, args = getopt.getopt(sys.argv, ":c", ["config="])
+        
+        print(len(opts))
+        for arg in args:
+            print(f"args: {args}")
+        
+        for opt, arg in opts:
+            
+            print(f"{opt}")
+            if opt == "-c":
+                config_path = arg
+        
+        assert config_path != "", "Must include option -c to specify config file path"    
+        main(config_path)
+                
+    except ValueError as ve:
+        print(ve)
+    main(config_path)
