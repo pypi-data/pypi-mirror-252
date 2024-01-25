7a66f8cff818f3e88096286c5c93961e1a2e38f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:05:05 +0000

    Add package mitos/2.1.7/mitos-2.1.7.tar.gz

---
+++ packages/mitos/mitos-2.1.7.tar.gz/mitos-2.1.7/mitos/sequin/__init__.py
@@ -0,0 +1,20 @@
+'''
+@author: M. Bernt
+'''
+
+from sys import stdout
+
+
+def sequinwriter(featurelist, acc, outfile=None, mode="w"):
+    out = ">Feature %s\n" % acc
+    featurelist.sort(key=lambda x: x.start)
+    for feature in featurelist:
+        out += feature.sequinstr()
+
+    if isinstance(outfile, str):
+        with open(outfile, mode) as f:
+            f.write(out)
+    elif outfile is None:
+        stdout.write(out)
+    else:
+        outfile.write(out)
