d7b3aac7cc804f8bdd28f21510c9d4008d0cfd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 04:04:22 +0000

    Add package SambdaTester/0.0.4/SambdaTester-0.0.4.tar.gz

---
+++ packages/SambdaTester/SambdaTester-0.0.4.tar.gz/SambdaTester-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    packages=['SambdaTester'],
+    entry_points = {
+        'console_scripts': [
+            'SambdaTester=SambdaTester:main',
+            'bin/SambdaTester=SambdaTester.command:main'
+        ]
+    },
+    version="0.0.4",
+    author_email = "tarnold0788@gmail.com"
+)
