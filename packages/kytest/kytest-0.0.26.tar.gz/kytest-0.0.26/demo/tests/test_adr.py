de278420d8715e4f0bbc2189fee26289c517adc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 15:05:02 +0000

    Add package kytest/0.0.26/kytest-0.0.26.tar.gz

---
+++ packages/kytest/kytest-0.0.26.tar.gz/kytest-0.0.26/demo/tests/test_adr.py
@@ -0,0 +1,20 @@
+import kytest
+from kytest import *
+from pages.adr_page import DemoPage
+
+
+@story('测试demo')
+class TestAdrDemo(kytest.AdrCase):
+    def start(self):
+        self.page = DemoPage(self.driver)
+        self.set_act = '.me.MeSettingActivity'
+
+    @title('进入设置页')
+    def test_go_setting(self):
+        self.page.adBtn.click_exists()
+        self.page.myTab.click()
+        self.page.setBtn.click()
+        self.assert_act(self.set_act)
+        self.screenshot("设置页")
+
+
